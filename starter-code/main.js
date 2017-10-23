0a58802a2aeb75d1af3e51963cd54a996052a32d
Dario Vigil <dariovigil@gmail.com>
2017-10-23 19:09:40 +0200

    Set interval

--- starter-code/main.js
+++ starter-code/main.js
@@ -2,17 +2,16 @@ var myChrono = new Chronometer;
 
 // Start/Stop Button
 document.getElementById("btnLeft").addEventListener("click", function() {
-    console.log("Left button clicked");
+    // console.log("Left button clicked");
     if (this.classList.contains("stop")) {
-      myChrono.startClick();
-      // myChrono.startCounting();
+      myChrono.stopClick();
     } else {
-      myChrono.resetClick();
+      myChrono.startClick();
     }
   });
-//
-// // Reset/Split Button
-// document.getElementById("btnRight").addEventListener("click", function () {
-//   console.log("Right button clicked");
-//   myChrono.resetClick();
-// });
+
+// Reset/Split Button
+document.getElementById("btnRight").addEventListener("click", function () {
+  // console.log("Right button clicked");
+  myChrono.resetClick();
+});
