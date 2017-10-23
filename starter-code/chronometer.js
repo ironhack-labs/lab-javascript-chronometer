81ca1d7f3484ef27c00153a65b2560bc45d5ac9a
Sara Morales <saringa@gmail.com>
2017-10-23 19:19:48 +0200

    progress

---
+++ starter-code/chronometer.js
@@ -0,0 +1,16 @@
+"use strict";
+
+var chronometer = {
+  btnLeft: "START",
+  btnRight: "RESET",
+
+
+  startClick: function() {
+    this.btnLeft = "STOP";
+    this.btnRight = "SPLIT";
+  },
+  stopClick: function() {
+
+  }
+
+};
