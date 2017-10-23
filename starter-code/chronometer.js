0e690505ef023d1ff22d13d30a415605004eb272
Camila Maya <camilamaya21@gmail.com>
2017-10-23 19:37:09 +0200

    Iteration 2: Time start/stop and Iteration 4: Reset Chronometer

--- starter-code/chronometer.js
+++ starter-code/chronometer.js
@@ -1,7 +1,16 @@
+var secCen = 0;
+var secDec = 0;
+var minCen = 0;
+var minDec = 0;
+
+var idSecCen = document.getElementById('secCen');
+var idSecDec = document.getElementById('secDec');
+var idMinCen = document.getElementById('minCen');
+var idMinDec = document.getElementById('minDec');
+
 function Chronometer(status) {
   this.status = status;
 }
-
 
 Chronometer.prototype.startClick = function() {
   var btnLeft = document.getElementById('btnLeft');
@@ -34,9 +43,52 @@ Chronometer.prototype.splitStyle = function() {
 };
 
 Chronometer.prototype.resetClick = function() {
-//define reset 00:00:00 Chronometer function
+  var secCen = 0;
+  var secDec = 0;
+  var minCen = 0;
+  var minDec = 0;
+  idSecCen.innerText = secCen;
+  idSecDec.innerText = secDec;
+  idMinCen.innerText = minCen;
+  idMinDec.innerText = minDec;
+  //define reset 00:00 Chronometer function
 };
 
 Chronometer.prototype.splitClick = function() {
-//define create laps in the Chronometer
+  //define create laps in the Chronometer
+};
+
+Chronometer.prototype.startTime = function() {
+  var intervalId = setInterval(function() {
+  if (secCen >= 9) {
+  secDec++;
+  secCen = 0;
+  if (secDec > 5) {
+    minCen++;
+    secDec = 0;
+    if (minCen >= 9) {
+      minDec++;
+      minCen = 0;
+      if (minDec > 5) {
+        secCen = 0;
+        secDec = 0;
+        minCen = 0;
+        minDec = 0;
+      };
+    };
+  };
+} else {
+  secCen++;
+  idSecCen.innerText = secCen;
+  idSecDec.innerText = secDec;
+  idMinCen.innerText = minCen;
+  idMinDec.innerText = minDec;
+
+};
+    console.log(minDec + " " + minCen  + " " + secDec + " " + secCen);
+
+    if (chrono.status == "stopped") {
+      clearInterval(intervalId);
+    }
+  }, 1000);
 };
--- starter-code/main.js
+++ starter-code/main.js
@@ -4,20 +4,19 @@ var chrono = new Chronometer("stopped");
 
 // Start/Stop Button
 document.getElementById("btnLeft").addEventListener("click", function() {
-  console.log("Left button clicked");
   if (chrono.status == "stopped") {
     chrono.startClick();
     chrono.resetStyle();
+    chrono.startTime();
   } else if (chrono.status == "running") {
     chrono.stopClick();
     chrono.splitStyle();
   }
+    console.log("Left button clicked");
 });
 
 // Reset/Split Button
 document.getElementById("btnRight").addEventListener("click", function() {
   console.log("Right button clicked");
-
-
-
+  chrono.resetClick();
 });
