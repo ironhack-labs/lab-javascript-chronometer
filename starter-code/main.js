523d6ae2922a8e6676a795401881f46cc1ecfa69
Pablo Gutierrez Barcelo <pablo@gutierrezbarcelo.com>
2017-10-23 14:55:43 +0200

    Starting Exercise

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,9 +1,21 @@
 // Start/Stop Button
 document.getElementById("btnLeft").addEventListener("click", function () {
-  console.log("Left button clicked");
-});
+  if(document.querySelector('#btnLeft').innerHTML == 'START'){
+    // Hago lo que tenga que hacer START
+    console.log("Left button clicked");
+  }else{
+    // Hago lo que tenga que hacer STOP
+  }
+  });
 
 // Reset/Split Button
 document.getElementById("btnRight").addEventListener("click", function () {
   console.log("Right button clicked");
+  if(document.querySelector('#btnLeft').innerHTML == 'RESET'){
+    // Hago lo que tenga que hacer RESET
+    console.log("Left button clicked");
+  }else{
+    // Hago lo que tenga que hacer SPLIT
+  }
+  });
 });
