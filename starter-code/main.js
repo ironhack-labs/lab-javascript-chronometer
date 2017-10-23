b84072a443f5b4ccc158edafd757fb939a2dcd24
Anyelina Moreno Robledo <anyelinamoreno@gmail.com>
2017-10-23 17:49:16 +0200

    MAD - Manuel Serrano - Angelina Moreno

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,9 +1,16 @@
+var chrone = new Chronometer()
+
 // Start/Stop Button
 document.getElementById("btnLeft").addEventListener("click", function () {
-  console.log("Left button clicked");
+  var start = document.getElementById('btnLeft').textContent;
+  if (start == 'START'){
+    chrone.startClick();
+  }else
+  chrone.stopClick();
 });
 
 // Reset/Split Button
 document.getElementById("btnRight").addEventListener("click", function () {
+  // chrone.stopClick();
   console.log("Right button clicked");
 });
