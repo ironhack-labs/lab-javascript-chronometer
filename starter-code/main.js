d31f690e8f74b47148d1a515fa2be99b4d6b622c
Jean Nothomb <jeannothomb@gmail.com>
2017-10-24 09:26:48 +0200

    not completed

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,9 +1,24 @@
+"use strict";
+
+var chronometer = new Chronometer();
+var chronometerRunning = false;
+
 // Start/Stop Button
-document.getElementById("btnLeft").addEventListener("click", function () {
+document.getElementById("btnLeft").addEventListener("click", function() {
   console.log("Left button clicked");
+  if (chronometerRunning === false) {
+    chronometerRunning = true;
+    chronometer.start();
+  } else {
+    chronometerRunning = false;
+    chronometer.stop();
+  }
 });
 
 // Reset/Split Button
-document.getElementById("btnRight").addEventListener("click", function () {
+document.getElementById("btnRight").addEventListener("click", function() {
   console.log("Right button clicked");
+  // var split = chronometer.getSplit();
+  // var min = split / 60;
+  // var min = split % 60;
 });
