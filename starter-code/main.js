edb069edbcd9215ef1e48277eed26775c83d1e5a
Eloi Gil <eloigil6@gmail.com>
2017-10-24 09:38:00 +0200

    hi TAs!

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,9 +1,32 @@
 // Start/Stop Button
-document.getElementById("btnLeft").addEventListener("click", function () {
+var theChrono = new Chronometer();
+
+document.getElementById("btnLeft").addEventListener("click", function() {
   console.log("Left button clicked");
+
+
+  if (document.getElementById("btnLeft").innerHTML === "START") {
+    console.log("START");
+    theChrono.startClick();
+  } else if (document.getElementById("btnLeft").innerHTML === "STOP") {
+    console.log("STOP");
+    theChrono.stopClick();
+  }
+
+
 });
 
 // Reset/Split Button
-document.getElementById("btnRight").addEventListener("click", function () {
+document.getElementById("btnRight").addEventListener("click", function() {
   console.log("Right button clicked");
+
+  if (document.getElementById("btnLeft").innerHTML === "START") {
+    console.log("RESET");
+    theChrono.reset();
+
+  } else if (document.getElementById("btnLeft").innerHTML === "STOP") {
+    console.log("SPLIT");
+    theChrono.split();
+  }
+
 });
