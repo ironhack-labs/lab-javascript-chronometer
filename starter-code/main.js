09d336a5aec34a7e8128c38fdc385fb312873c3f
Alejandro Roman <aromanarguello@gmail.com>
2017-10-25 15:03:57 -0400

    finished timer

--- starter-code/main.js
+++ starter-code/main.js
@@ -15,19 +15,40 @@ startBtn.addEventListener("click", function () {
     var increaseTimer = toNum += 1;
     seconds.innerHTML = increaseTimer;
 
+
+    var secondsDec = document.querySelector('#secDec');
     if (seconds.innerHTML > 9 ){
       seconds.innerHTML = 0;
 
-      var secondsDec = document.querySelector('#secDec');
+
       var toNumDec = parseInt(secondsDec.innerHTML);
       var increaseTimerDec = toNumDec += 1;
       secondsDec.innerHTML = increaseTimerDec;
+
     }
+
+
+    var minCen = document.querySelector('#minCen');
+    if (secondsDec.innerHTML > 5) {
+      secondsDec.innerHTML = 0;
+      var toNumMin = parseInt(minCen.innerHTML);
+      var increaseTimerMin = toNumMin += 1;
+      minCen.innerHTML = increaseTimerMin;
+    }
+
+    var minDec = document.querySelector('#minDec');
+    if ( minCen.innerHTML > 9 ) {
+      minCen.innerHTML = 0;
+      var toNumMinDec = parseInt(minDec.innerHTML);
+      var increaseTimerMinDec = toNumMinDec += 1;
+      minDec.innerHTML = increaseTimerMinDec;
+    }
+
 
 
     console.log(increaseTimer);
   },
-  1000
+  100
 );
 
 });
