9ad79cf8ed8def97368445692c86ee90b78923ff
Alejandro Roman <aromanarguello@gmail.com>
2017-10-25 13:58:39 -0400

    initial commit

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,9 +1,23 @@
 // Start/Stop Button
-document.getElementById("btnLeft").addEventListener("click", function () {
-  console.log("Left button clicked");
-});
+var startBtn = document.getElementById("btnLeft");
 
 // Reset/Split Button
-document.getElementById("btnRight").addEventListener("click", function () {
+var stopBtn = document.getElementById("btnRight").addEventListener("click", function () {
   console.log("Right button clicked");
+});
+
+startBtn.addEventListener("click", function () {
+  setInterval(function() {
+    var seconds = document.querySelector('#secCen');
+    var toNum = parseInt(seconds.innerHTML);
+    var increaseTimer = toNum += 1;
+    seconds.innerHTML = increaseTimer;
+
+
+
+    console.log(increaseTimer);
+  },
+  1000
+);
+
 });
