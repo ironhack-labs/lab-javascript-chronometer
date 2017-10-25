db38e8964cc57801c8c760c6ec79ca1d799d8f14
Alejandro Roman <aromanarguello@gmail.com>
2017-10-25 14:26:13 -0400

    started on seconds

--- starter-code/main.js
+++ starter-code/main.js
@@ -6,12 +6,28 @@ var stopBtn = document.getElementById("btnRight").addEventListener("click", func
   console.log("Right button clicked");
 });
 
+var intervalId;
+
 startBtn.addEventListener("click", function () {
   setInterval(function() {
     var seconds = document.querySelector('#secCen');
     var toNum = parseInt(seconds.innerHTML);
     var increaseTimer = toNum += 1;
     seconds.innerHTML = increaseTimer;
+
+    if (seconds.innerHTML > 9 ){
+      seconds.innerHTML = 0;
+    }
+
+      setTimeout(function() {
+        secondsDec = document.querySelector('#secDec');
+        toNumDec = parseInt(secondsDec.innerHTML);
+        increaseTimerDec = toNumDec += 1;
+        secondsDec.innerHTML = increaseTimerDec;
+    },
+    10000
+  );
+
 
 
 
