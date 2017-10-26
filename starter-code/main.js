9771a16c4c410177494975737ab45a9f97982ea8
Alejandro Roman <aromanarguello@gmail.com>
2017-10-25 23:11:31 -0400

    add restart function after reset click ;

--- starter-code/main.js
+++ starter-code/main.js
@@ -43,6 +43,10 @@ startBtn.addEventListener("click", function () {
       if (startBtn.innerHTML === 'STOP') {
         startBtn.classList.remove('start');
         startBtn.classList.add('stop');
+
+        startBtn.addEventListener("click", function () {
+          clearInterval(intervalId);
+        });
       }
 
     console.log(increaseTimer);
@@ -70,4 +74,64 @@ resetBtn.addEventListener("click", function () {
     minutes2.innerHTML = 0;
   }
   startBtn.innerHTML = 'START';
+  startBtn.classList.remove('stop');
+  startBtn.classList.add('start');
+
+  // reused to add function to start after reset.
+  startBtn.addEventListener("click", function () {
+    intervalId =
+    setInterval(function() {
+      var seconds = document.querySelector('#secCen');
+      var toNum = parseInt(seconds.innerHTML);
+      var increaseTimer = toNum += 1;
+      seconds.innerHTML = increaseTimer;
+
+
+      var secondsDec = document.querySelector('#secDec');
+      if (seconds.innerHTML > 9 ){
+        seconds.innerHTML = 0;
+
+
+        var toNumDec = parseInt(secondsDec.innerHTML);
+        var increaseTimerDec = toNumDec += 1;
+        secondsDec.innerHTML = increaseTimerDec;
+
+      }
+
+
+      var minCen = document.querySelector('#minCen');
+      if (secondsDec.innerHTML > 5) {
+        secondsDec.innerHTML = 0;
+        var toNumMin = parseInt(minCen.innerHTML);
+        var increaseTimerMin = toNumMin += 1;
+        minCen.innerHTML = increaseTimerMin;
+      }
+
+      var minDec = document.querySelector('#minDec');
+      if ( minCen.innerHTML > 9 ) {
+        minCen.innerHTML = 0;
+        var toNumMinDec = parseInt(minDec.innerHTML);
+        var increaseTimerMinDec = toNumMinDec += 1;
+        minDec.innerHTML = increaseTimerMinDec;
+      }
+
+        startBtn.innerHTML = 'STOP';
+
+        if (startBtn.innerHTML === 'STOP') {
+          startBtn.classList.remove('start');
+          startBtn.classList.add('stop');
+
+          startBtn.addEventListener("click", function () {
+            clearInterval(intervalId);
+          });
+        }
+
+      console.log(increaseTimer);
+    },
+    1000
+  );
+  });
+
+
+
 });
