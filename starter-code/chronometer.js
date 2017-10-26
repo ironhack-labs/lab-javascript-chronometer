ad81c260054ed9906ee72d26d2beaa463dd2340c
Ariel Fonticiella <arielfonticiella@Ariels-MacBook-Pro.local>
2017-10-26 15:34:36 -0400

    Second Commit- got Start/Stop button working. Still need to get timer counting.

--- starter-code/chronometer.js
+++ starter-code/chronometer.js
@@ -1,35 +1,32 @@
 var startClick = document.getElementById('btnLeft');
 var resetClick = document.getElementById('btnRight');
-var firstNum = document.getElementById('minDec');
-var currentTimer;
+var counter = document.getElementsByClassName('number');
+var minNum1 = document.getElementById('minDec');
+var minNum2 = document.getElementById('minCen');
+var secNum1 = document.getElementById('secDec');
+var secNum2 = document.getElementById('secCen');
 
-//START CLICK
+// START CLICK (VISUAL)
 startClick.onclick = function () {
+  var stop = document.getElementById('btnLeft');
+  var split = document.getElementById('btnRight');
 
-var stop = document.querySelector('button');
-var split = document.getElementById('btnRight');
+  switch( stop.innerHTML ) {
 
-  if (//need if statement) {
+     case "STOP":
+        stop.innerHTML = 'START';
+        stop.setAttribute('class','btn start');
 
+        split.innerHTML = 'RESET';
+        split.setAttribute('class','btn reset');
+          break;
+
+     case "START":
         stop.innerHTML = 'STOP';
         stop.setAttribute('class','btn stop');
 
         split.innerHTML = 'SPLIT';
         split.setAttribute('class','btn split');
-
-  } else if (//need else statment) {
-      stop.innerHTML = 'START';
-      stop.setAttribute('class','btn start');
-
-      split.innerHTML = 'RESET';
-      split.setAttribute('class','btn reset');
-    }
-  };
-
-
-/*
-stopClick, that will be triggered when the left button is clicked while
-the chronometer is running. In this iteration, this method has to:
-  Set the btnLeft button with the text START, and the class btn start.
-  Set the btnRight button with the text RESET, and the class btn reset.
-*/
+          break;
+      }
+    };
