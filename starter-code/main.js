ba02931c0976a44c23df76398122734309fac824
Antoine Marc <antoinefl_marc@yahoo.fr>
2017-11-04 14:46:06 +0100

    done

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,9 +1,50 @@
+function Chronometer() {
+}
+
+Chronometer.prototype.startClick = function() {
+    $("#btnLeft").addClass('stop').removeClass('start');
+    $("#btnLeft").text("STOP");
+    $("#btnLeft").next().addClass('split').removeClass('reset');
+    $("#btnLeft").next().text("SPLIT");
+}
+
+Chronometer.prototype.stopClick = function () {
+    $('#btnLeft').addClass('start').removeClass('stop');
+    $('#btnLeft').text("START");
+    $('#btnLeft').next().addClass('reset').removeClass('split');
+    $('#btnLeft').next().text("RESET");
+}
+
+
+var chrono = new Chronometer();
+
 // Start/Stop Button
-document.getElementById("btnLeft").addEventListener("click", function () {
-  console.log("Left button clicked");
+$("#btnLeft").click(function() {
+    console.log("Left button clicked");
+    if ($(this).hasClass("start")) {
+        chrono.startClick();
+        var secDecCounter = 0;
+        var secCenCounter = 0;
+        var minDecCounter = 0;
+        var minCenCounter = 0;
+        var time = setInterval(function () {
+          secCenCounter++;
+          $("#secCen").text(secCenCounter);
+
+          if (secCenCounter === 9) {
+            secDecCounter++;
+            secCenCounter = 0;
+          }
+        }, 1000);
+
+        }
+    else if($(this).hasClass("stop")){
+        chrono.stopClick();
+    }
 });
 
 // Reset/Split Button
-document.getElementById("btnRight").addEventListener("click", function () {
+$("#btnRight").click(function () {
   console.log("Right button clicked");
+
 });
