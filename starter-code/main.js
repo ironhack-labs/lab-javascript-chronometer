bfc33ca3220584fcb797de044ed2d593d642af9f
juliabellone <juliabellone@gmail.com>
2017-11-11 17:34:10 +0100

    counter a medias

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,9 +1,21 @@
+
+
+var chronometer = new Chronometer();
 // Start/Stop Button
 document.getElementById("btnLeft").addEventListener("click", function () {
-  console.log("Left button clicked");
+  if (document.getElementById("btnLeft").className == "btn start"){
+    chronometer.startClick();
+  }else {
+    chronometer.stopClick();
+  }
 });
 
 // Reset/Split Button
 document.getElementById("btnRight").addEventListener("click", function () {
-  console.log("Right button clicked");
+  if (document.getElementById("btnRight").className == "btn reset"){
+    chronometer.resetClock();
+  }else {
+    chronometer.split();
+  }
+
 });
