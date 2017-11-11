7f616939eadecc0c7883efffb3e1b52feb989718
Lilia Borrazas <liliaborrazas@MacBook-Pro-de-Lilia.local>
2017-11-11 17:11:09 +0100

    objeto

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,4 +1,13 @@
-// Start/Stop Button
+function Chronometer() {
+}
+Chronometer.prototype.startClick = function(){
+  var btnleft = document.getElementbyId("btnLeft");
+
+}
+button.onclick = function(){
+  var btnLeft = document.getElementsbyClass("btn start")
+  btnLeft =
+}// Start/Stop Button
 document.getElementById("btnLeft").addEventListener("click", function () {
   console.log("Left button clicked");
 });
