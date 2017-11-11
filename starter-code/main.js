5fbff42fd75c11060f116cd36bb5f3ba33eb0fb0
Lilia Borrazas <liliaborrazas@MacBook-Pro-de-Lilia.local>
2017-11-12 00:36:28 +0100

    crea carpeta JS

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,12 +1,4 @@
-function Chronometer() {
-}
-Chronometer.prototype.startClick = function(){
-  var btnleft = document.getElementbyId("btnLeft");
 
-}
-button.onclick = function(){
-  var btnLeft = document.getElementsbyClass("btn start")
-  btnLeft =
 }// Start/Stop Button
 document.getElementById("btnLeft").addEventListener("click", function () {
   console.log("Left button clicked");
