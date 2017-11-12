215740740b09f6c80254b60f019badfdb28ddcf6
Marcos Monzon <marco238cuba@gmail.com>
2017-11-12 17:46:24 +0100

    feat: start/stop button is working

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,11 +1,23 @@
+var loopId;
 // Start/Stop Button
 document.getElementById("btnLeft").addEventListener("click", function() {
-  console.log("Left button clicked");
-  var secCen = document.querySelector("#secCen");
-  var loopId = setInterval(function() {
-      loop();
-    }, 1000);
-  });
+  var btnL = document.getElementById("btnLeft");
+  var btnR = document.getElementById("btnRight");
+  if(btnL.className === "btn start"){
+    loopId = setInterval(loop, 1000);
+    btnL.className = "btn stop";
+    btnL.innerHTML = "STOP";
+    btnR.className = "btn split";
+    btnR.innerHTML = "SPLIT";
+  } else {
+    clearInterval(loopId);
+    btnL.className = "btn start";
+    btnL.innerHTML = "START";
+    btnR.className = "btn reset";
+    btnR.innerHTML = "RESET";
+  }
+});
+
 function loop() {
   if (!this.count) {
     this.count = 1;
@@ -16,44 +28,48 @@ function loop() {
   secCen.innerHTML = this.count;
   this.count++;
 }
-var secDec = document.querySelector("#secDec");
+
+var secDec = document.getElementById("secDec");
 function incrementSecDec() {
   if (!this.count2) {
     this.count2 = 1;
   }
   else if (this.count2 === 6) {
     this.count2 = 0;
-    this.innerHTML = secDec;
+    secDec.innerHTML = this.count2;
     incrementMinCen();
   }
   secDec.innerHTML = this.count2;
   this.count2++;
 }
-var minCen = document.querySelector("#minCen");
+
+var minCen = document.getElementById("minCen");
 function incrementMinCen() {
   if (!this.count3) {
     this.count3 = 1;
   }
   else if (this.count3 === 10) {
     this.count3 = 0;
-    this.innerHTML = minCen;
+    minCen.innerHTML = this.count3;
     incrementMinDec();
   }
   minCen.innerHTML = this.count3;
   this.count3++;
 }
-var minDec = document.querySelector("#minDec");
+
+var minDec = document.getElementById("minDec");
 function incrementMinDec() {
   if (!this.count4) {
     this.count4 = 1;
   }
   else if (this.count4 === 6) {
     this.count4 = 0;
-    this.innerHTML = minDec;
+    minDec.innerHTML = this.count4;
   }
   minDec.innerHTML = this.count4;
   this.count4++;
 }
+
 // Reset/Split Button
 document.getElementById("btnRight").addEventListener("click", function () {
   console.log("Right button clicked");
