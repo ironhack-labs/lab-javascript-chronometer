2f83329f1c391d49de5af5daaaa890743308a37d
mouyaq <mouyaq@gmail.com>
2017-11-12 21:34:33 +0100

    feat: startClick & stopClick

--- starter-code/chronometer.js
+++ starter-code/chronometer.js
@@ -1,13 +1,21 @@
-function Chronometer ( btnLeft, btnRight, sphere ) {
-    this.btnLeft = document.getElementById("btnLeft");
-    this.btnRight = document.getElementById("btnRight");
-    this.sphere = document.getElementById("sphere");
+function Chronometer(btnLeft, btnRight, sphere) {
+    this.btnLeft = document.getElementById(btnLeft);
+    this.btnRight = document.getElementById(btnRight);
+    this.sphere = document.getElementById(sphere);
 }
 
-Chronometer.prototype.startClick(){
-    
+Chronometer.prototype.startClick = function() {
+    console.log("START CLICK");
+    this.btnLeft.innerHTML = "STOP";
+    this.btnLeft.className = "btn stop";
+    this.btnRight.innerHTML = "SPLIT";
+    this.btnRight.className = "btn split";
 }
 
-Chronometer.prototype.stopClick(){
-    
+Chronometer.prototype.stopClick = function() {
+    console.log("STOP CLICK");
+    this.btnLeft.innerHTML = "START";
+    this.btnLeft.className = "btn start";
+    this.btnRight.innerHTML ="RESET";
+    this.btnRight.className = "btn reset";
 }
--- starter-code/main.js
+++ starter-code/main.js
@@ -1,9 +1,17 @@
+var chrono = new Chronometer("btnLeft", "btnRight", "sphere");
+console.log(chrono);
+
 // Start/Stop Button
 document.getElementById("btnLeft").addEventListener("click", function () {
   console.log("Left button clicked");
+  if(this.className === "btn start") {
+    chrono.startClick();  
+  }
+  console.log(chrono);
 });
 
 // Reset/Split Button
 document.getElementById("btnRight").addEventListener("click", function () {
   console.log("Right button clicked");
+  console.log(this);
 });
