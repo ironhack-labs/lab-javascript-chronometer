3734c480c14c0ff86463f74bdb0b129a7a8e30cb
Juancho Arregui <juan.arregui.lopez@gmail.com>
2017-11-16 17:23:28 +0100

    feat: finished. with seconds and tenths of second

--- starter-code/main.js
+++ starter-code/main.js
@@ -5,10 +5,32 @@ var myChrono = new Chronometer("btnLeft", "btnRight", "sphere");
 
 // Start/Stop Button
 document.getElementById("btnLeft").addEventListener("click", function () {
-  console.log("Left button clicked");
+  switch(myChrono.status) {
+    case "Stopped":
+      console.log("Left button clicked option START");
+      myChrono.startClock();
+      break; 
+    case "Running":
+      console.log("Left button clicked option STOP");
+      myChrono.stopClock();
+      break;
+    default:
+    console.log("Left button clicked SOMETHING WENT WRONG");
+  }
 });
 
 // Reset/Split Button
 document.getElementById("btnRight").addEventListener("click", function () {
-  console.log("Right button clicked");
+  switch(document.getElementById("btnRight").className) {
+    case "btn split":
+      console.log("Right button clicked option: SPLIT");
+      myChrono.getSplitTime();
+      break;
+    case "btn reset":
+      console.log("Right button clicked option: RESET");
+      myChrono.doReset();
+      break;
+    default:
+    console.log("Right button clicked SOMETHING WENT WRONG");
+  }
 });
