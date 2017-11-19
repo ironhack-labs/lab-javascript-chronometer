f2a82213bb26203936aed40afba3a32940a9dbce
Mario Velasco Alonso <mario1velasco@gmail.com>
2017-11-19 23:00:15 +0100

    Feat: All Done!

--- starter-code/chronometer.js
+++ starter-code/chronometer.js
@@ -1,11 +1,11 @@
 function Chronometer() {
 
 }
-var createPartialTimeLi = function(secCen, secDec, minCen, minDec) {
+var createPartialTimeLi = function(secCen, secDec, minCen, minDec, milCen, milDec) {
   olTag = document.getElementById('olTimeList');
   liTag = document.createElement('li');
   liTag.setAttribute('class', 'li-time-list');
-  text = document.createTextNode(minDec + minCen + ':' + secDec + secCen);
+  text = document.createTextNode(minDec + minCen + ':' + secDec + secCen + ':' + milDec + milCen+"\"");
   liTag.appendChild(text);
   olTag.appendChild(liTag);
 };
@@ -14,21 +14,31 @@ var eliminateTimeList = function() {
   document.getElementById('secDec').innerHTML = "0";
   document.getElementById('minCen').innerHTML = "0";
   document.getElementById('minDec').innerHTML = "0";
-  document.getElementById('olTimeList').innerHTML='Split';
+  document.getElementById('milCen').innerHTML = "0";
+  document.getElementById('milDec').innerHTML = "0";
+  document.getElementById('olTimeList').innerHTML = 'Split';
 
 };
-var countSecondsMinutes = function(secCen, secDec, minCen, minDec) {
-  secCen += 1;
+var countSecondsMinutes = function(secCen, secDec, minCen, minDec, milCen, milDec) {
+  milCen += 1;
   startZeroSeconds = setInterval(function() {
-    if (secCen === 10) {
-      secCen = 0;
-      secDec += 1;
-      if (secDec === 6) {
-        secDec = 0;
-        minCen += 1;
-        if (minCen === 10) {
-          minCen = 0;
-          minDec += 1;
+    if (milCen === 10) {
+      milCen = 0;
+      milDec += 1;
+      if (milDec === 10) {
+        milDec = 0;
+        secCen += 1;
+        if (secCen === 10) {
+          secCen = 0;
+          secDec += 1;
+          if (secDec === 6) {
+            secDec = 0;
+            minCen += 1;
+            if (minCen === 10) {
+              minCen = 0;
+              minDec += 1;
+            }
+          }
         }
       }
     }
@@ -36,8 +46,10 @@ var countSecondsMinutes = function(secCen, secDec, minCen, minDec) {
     document.getElementById('secDec').innerHTML = secDec;
     document.getElementById('minCen').innerHTML = minCen;
     document.getElementById('minDec').innerHTML = minDec;
-    secCen++;
-  }, 1000);
+    document.getElementById('milCen').innerHTML = milCen;
+    document.getElementById('milDec').innerHTML = milDec;
+    milCen++;
+  }, 10);
 };
 
 Chronometer.prototype.startClick = function(whichBtn) {
@@ -45,6 +57,8 @@ Chronometer.prototype.startClick = function(whichBtn) {
   secDec = document.getElementById('secDec').innerHTML;
   minCen = document.getElementById('minCen').innerHTML;
   minDec = document.getElementById('minDec').innerHTML;
+  milCen = document.getElementById('milCen').innerHTML;
+  milDec = document.getElementById('milDec').innerHTML;
   if (whichBtn === "btnLeft") {
     btnLeft = document.getElementById('btnLeft');
     btnLeft.setAttribute('class', 'btn stop');
@@ -52,12 +66,12 @@ Chronometer.prototype.startClick = function(whichBtn) {
     btnRight = document.getElementById('btnRight');
     btnRight.setAttribute('class', 'btn split');
     document.getElementById('btnRight').innerHTML = 'SPLIT';
-    countSecondsMinutes(parseInt(secCen), parseInt(secDec), parseInt(minCen), parseInt(minDec));
+    countSecondsMinutes(parseInt(secCen), parseInt(secDec), parseInt(minCen), parseInt(minDec), parseInt(milCen), parseInt(milDec));
     return "startClick";
   } else if (whichBtn === "btnRight") {
     //HACER QUE SE GUARDE EL TIEMPO EN LA LISTA
     if (document.getElementById('btnLeft').innerHTML === 'STOP') {
-      createPartialTimeLi(secCen, secDec, minCen, minDec);
+      createPartialTimeLi(secCen, secDec, minCen, minDec, milCen, milDec);
     }
     return "startClick";
   }
@@ -76,7 +90,6 @@ Chronometer.prototype.stopClick = function(whichBtn) {
     //HACER QUE EL TIEMPO Y LA LISTA SE BORRE
     if (document.getElementById('btnLeft').innerHTML === 'START') {
       eliminateTimeList();
-
     }
     return "stopClick";
   }
