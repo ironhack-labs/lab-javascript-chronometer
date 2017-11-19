d1f885994e1737ccd98013adc9d711e34ddd3c89
Mario Velasco Alonso <mario1velasco@gmail.com>
2017-11-19 19:05:19 +0100

    Fix:Split time working, and set to 0 working as well

--- starter-code/chronometer.js
+++ starter-code/chronometer.js
@@ -1,6 +1,22 @@
 function Chronometer() {
 
 }
+var createPartialTimeLi = function(secCen, secDec, minCen, minDec) {
+  olTag = document.getElementById('olTimeList');
+  liTag = document.createElement('li');
+  liTag.setAttribute('class', 'li-time-list');
+  text = document.createTextNode(minDec + minCen + ':' + secDec + secCen);
+  liTag.appendChild(text);
+  olTag.appendChild(liTag);
+};
+var eliminateTimeList = function() {
+  document.getElementById('secCen').innerHTML = "0";
+  document.getElementById('secDec').innerHTML = "0";
+  document.getElementById('minCen').innerHTML = "0";
+  document.getElementById('minDec').innerHTML = "0";
+  document.getElementById('olTimeList').innerHTML='Times Stop';
+
+};
 var countSecondsMinutes = function(secCen, secDec, minCen, minDec) {
   secCen += 1;
   startZeroSeconds = setInterval(function() {
@@ -23,22 +39,26 @@ var countSecondsMinutes = function(secCen, secDec, minCen, minDec) {
     secCen++;
   }, 1000);
 };
+
 Chronometer.prototype.startClick = function(whichBtn) {
+  secCen = document.getElementById('secCen').innerHTML;
+  secDec = document.getElementById('secDec').innerHTML;
+  minCen = document.getElementById('minCen').innerHTML;
+  minDec = document.getElementById('minDec').innerHTML;
   if (whichBtn === "btnLeft") {
-    secCen = document.getElementById('secCen').innerHTML;
-    secDec = document.getElementById('secDec').innerHTML;
-    minCen = document.getElementById('minCen').innerHTML;
-    minDec = document.getElementById('minDec').innerHTML;
     btnLeft = document.getElementById('btnLeft');
     btnLeft.setAttribute('class', 'btn stop');
     document.getElementById('btnLeft').innerHTML = 'STOP';
+    btnRight = document.getElementById('btnRight');
+    btnRight.setAttribute('class', 'btn split');
+    document.getElementById('btnRight').innerHTML = 'SPLIT';
     countSecondsMinutes(parseInt(secCen), parseInt(secDec), parseInt(minCen), parseInt(minDec));
-
     return "startClick";
   } else if (whichBtn === "btnRight") {
-    btnRight = document.getElementById('btnRight');
-    btnRight.setAttribute('class', 'btn reset');
-    document.getElementById('btnRight').innerHTML = 'RESET';
+    //HACER QUE SE GUARDE EL TIEMPO EN LA LISTA
+    if (document.getElementById('btnLeft').innerHTML === 'STOP') {
+      createPartialTimeLi(secCen, secDec, minCen, minDec);
+    }
     return "startClick";
   }
 };
@@ -48,11 +68,16 @@ Chronometer.prototype.stopClick = function(whichBtn) {
     btnLeft = document.getElementById('btnLeft');
     btnLeft.setAttribute('class', 'btn start');
     document.getElementById('btnLeft').innerHTML = 'START';
+    btnRight = document.getElementById('btnRight');
+    btnRight.setAttribute('class', 'btn reset');
+    document.getElementById('btnRight').innerHTML = 'RESET';
     return "stopClick";
   } else if (whichBtn === "btnRight") {
-    btnRight = document.getElementById('btnRight');
-    btnRight.setAttribute('class', 'btn split');
-    document.getElementById('btnRight').innerHTML = 'SPLIT';
+    //HACER QUE EL TIEMPO Y LA LISTA SE BORRE
+    if (document.getElementById('btnLeft').innerHTML === 'START') {
+      eliminateTimeList();
+
+    }
     return "stopClick";
   }
 };
