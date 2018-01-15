b983c99d21da81d95eaf8179a6bff582d08a955d
Carlos San Martin <carlosanmartin@gmail.com>
2018-01-15 13:45:24 +0100

    trabajando en ello

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,23 +1,39 @@
 // Constructor
-// function Chronometer() {
+function Chronometer() {
+  this.currentTime = 0;
+  this.intervalId = 1000;
+}
 
-// }
+Chronometer.prototype.startClick = function () {
+  var that = this;
+  setInterval(function(){
+    console.log(that.currentTime);
+    parseInt(that.currentTime++);
+  },that.intervalId);
+}
 
-// Chronometer.prototype.startClick = function () {
+var minutes;
+var seconds;
+Chronometer.prototype.setMinutes = function () {
+  minutes = parseInt(this.currentTime/60);
+  return minutes;
+};
 
-// };
+Chronometer.prototype.setSeconds = function () {
+  var secondsFloat = this.currentTime/60; 
+  seconds = parseInt((secondsFloat % 1)*60);
+  return seconds;
+};
 
-// Chronometer.prototype.setMinutes = function () {
-  
-// };
-
-// Chronometer.prototype.setSeconds = function () {
-  
-// };
-
-// Chronometer.prototype.twoDigitsNumber = function () {
-  
-// };
+Chronometer.prototype.twoDigitsNumber = function (number) {
+  var twoDigits = '00';
+  if (number < 10){
+    twoDigits = '0'+number;
+  } else if (number === 15) {
+    twoDigits = '15';
+  }
+  return twoDigits;
+};
 
 // Chronometer.prototype.setTime = function () {
 
