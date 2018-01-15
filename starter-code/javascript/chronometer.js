b60eb53ae410c56f396afbef2623c6cb958cbd7d
Alvaro <amartinezfernandez.61@gmail.com>
2018-01-15 18:59:37 +0100

    Iteración 1

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,40 +1,47 @@
-// Constructor
-// function Chronometer() {
+function Chronometer() {
+  this.timeOut = 0;
+  this.currentTime = 0;
+  this.intervalId = 0;
+}
 
-// }
+Chronometer.prototype.startClick = function(startClick) {
+  this.currentTime = 0;
+  var that = this;
+  var currentTime = setInterval(function() {
+    that.currentTime ++;
+    //     if (i > 10) {
+    //     clearInterval(intervalId);
+    //   }
+  }, 1000);
+};
 
-// Chronometer.prototype.startClick = function () {
+Chronometer.prototype.setMinutes = function() {
+    var minutes = this.currentTime / 60;
+    return parseInt(minutes);
+};
 
-// };
+Chronometer.prototype.setSeconds = function() {
+        var seconds = this.currentTime % 60;
+        return parseInt(seconds);
+    };
 
-// Chronometer.prototype.setMinutes = function () {
-  
-// };
+Chronometer.prototype.twoDigitsNumber = function(number) {
+    var twoDigits = number + '';
+    if (twoDigits.length == 1){
+        return '0' + twoDigits;
+    }
+    return twoDigits;
+};
 
-// Chronometer.prototype.setSeconds = function () {
-  
-// };
+Chronometer.prototype.setTime = function() {
+    var min = this.twoDigitsNumber(this.setMinutes());
+    var seg = this.twoDigitsNumber(this.setSeconds());
+};
 
-// Chronometer.prototype.twoDigitsNumber = function () {
-  
-// };
+Chronometer.prototype.setMilliseconds = function() {};
 
-// Chronometer.prototype.setTime = function () {
+Chronometer.prototype.stopClick = function() {};
 
-// };
+Chronometer.prototype.resetClick = function() {};
 
-// Chronometer.prototype.setMilliseconds = function () {
-
-// };
-
-// Chronometer.prototype.stopClick = function () {
-  
-// };
-
-// Chronometer.prototype.resetClick = function () {
-
-// };
-
-// Chronometer.prototype.splitClick = function () {
-
-// };
+Chronometer.prototype.splitClick = function() {};
