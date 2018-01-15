dcfb46dea6f1a829c57b4c88f13a2ea0e2f86c4e
AlbertoSP <ALBERTOSANCHEZHTML@GMAIL.COM>
2018-01-15 19:39:43 +0100

    iteration1

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,39 +1,51 @@
-// Constructor
-// function Chronometer() {
+function Chronometer() {
+    this.currentTime = 0;
+    this.intervalId = 0;
+};
 
-// }
+Chronometer.prototype.startClick = function() {
+ var newClock = this;
+   this.intervalId = setInterval(function() {
+      newClock.currentTime++;
+   }, 1000);
+};
 
-// Chronometer.prototype.startClick = function () {
+Chronometer.prototype.setMinutes = function() {
+    return parseInt(this.currentTime / 60);
+   };
 
-// };
+   Chronometer.prototype.setSeconds = function () {
+    return parseInt(this.currentTime % 60);
+   };
 
-// Chronometer.prototype.setMinutes = function () {
-  
-// };
+   Chronometer.prototype.twoDigitsNumber = function(number) {
+     if (number < 10) {
+       return (number = "0" + number);
+      } else {
+        return (number = "" + number);
+      }
+    };
 
-// Chronometer.prototype.setSeconds = function () {
-  
-// };
+    Chronometer.prototype.setTime = function() {
+      return (
+        this.twoDigitsNumber(this.setMinutes()) +
+        ":" +
+        this.twoDigitsNumber(this.setSeconds())
+      );
+     };
 
-// Chronometer.prototype.twoDigitsNumber = function () {
-  
-// };
-
-// Chronometer.prototype.setTime = function () {
-
-// };
+     Chronometer.prototype.stopClick = function () {
+      clearInterval(this.setTime);
+      };
 
 // Chronometer.prototype.setMilliseconds = function () {
 
 // };
+Chronometer.prototype.resetClick = function () {
+  this.setTime = 0;
+  };
 
-// Chronometer.prototype.stopClick = function () {
-  
-// };
 
-// Chronometer.prototype.resetClick = function () {
-
-// };
 
 // Chronometer.prototype.splitClick = function () {
 
