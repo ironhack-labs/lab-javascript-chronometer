d45c545604871e0c2a388e2be46b824fffea1648
danielthurdekoos <danielkoos@gmail.com>
2018-01-16 17:07:35 +0100

    done

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,40 +1,61 @@
 // Constructor
-// function Chronometer() {
+function Chronometer() {
+  this.currentTime = 0;
+  this.intervalId = 0;
 
-// }
+ }
 
-// Chronometer.prototype.startClick = function () {
+ Chronometer.prototype.startClick = function () {
+    var self = this;
 
-// };
+    self.intervalId = window.setInterval(function() {
+        self.currentTime += 1;
+        self.setTime(); 
+    }, 1000)
+ };
 
-// Chronometer.prototype.setMinutes = function () {
-  
-// };
+ Chronometer.prototype.setMinutes = function () {
+    var self = this;
+    var minutes = Math.floor(self.currentTime/60);
+    return minutes;
+ };
 
-// Chronometer.prototype.setSeconds = function () {
-  
-// };
+ Chronometer.prototype.setSeconds = function () {
+    var self = this;
+    var seconds = self.currentTime % 60;
+    return seconds;
+ };
 
-// Chronometer.prototype.twoDigitsNumber = function () {
-  
-// };
+ /*function twoDigitsNumber(value){
+      return value.padStart(2, "0");
+ }*/
 
-// Chronometer.prototype.setTime = function () {
+ Chronometer.prototype.twoDigitsNumber = function (value) {
+    return ("0" + value).slice(-2);
+ };
 
-// };
+ Chronometer.prototype.setTime = function () {
+    var self = this;
+     var minutes = self.twoDigitsNumber(self.setMinutes());
+     var seconds = self.twoDigitsNumber(self.setSeconds());
+
+ };
 
 // Chronometer.prototype.setMilliseconds = function () {
-
+var self = this;
 // };
 
-// Chronometer.prototype.stopClick = function () {
-  
-// };
+ Chronometer.prototype.stopClick = function () {
+    var self = this;
+    clearInterval(self.intervalId);
+};
 
-// Chronometer.prototype.resetClick = function () {
+ Chronometer.prototype.resetClick = function () {
+var self = this;
 
-// };
+     self.currentTime = 0;
+ };
 
 // Chronometer.prototype.splitClick = function () {
-
+var self = this;
 // };
