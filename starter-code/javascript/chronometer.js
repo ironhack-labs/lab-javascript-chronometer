f90f2f5dc09894d250b5a83ae8d5968abec4b5fc
VladC <ciobanu.vlad@yahoo.com>
2018-01-16 16:42:58 +0100

    chronom

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,27 +1,40 @@
-// Constructor
-// function Chronometer() {
+'use strict'
 
-// }
+function Chronometer(){
+    this.currentTime = 0;
+    this.intervalId = 0;
+}
 
-// Chronometer.prototype.startClick = function () {
+Chronometer.prototype.startClick = function () {
+    var self = this;
 
-// };
+    self.intervalId = window.setInterval(function() {
+        self.currentTime = self.currentTime + 1;
+    },1000);
+};
 
-// Chronometer.prototype.setMinutes = function () {
-  
-// };
+Chronometer.prototype.setMinutes = function () {
+    var self = this;
+    self.currentTime = Math.floor(self.currentTime/60);
+    return self.currentTime;
+};
 
-// Chronometer.prototype.setSeconds = function () {
-  
-// };
+Chronometer.prototype.setSeconds = function () {
+    var self = this;
+    return self.currentTime % 60;
+};
 
-// Chronometer.prototype.twoDigitsNumber = function () {
-  
-// };
+Chronometer.prototype.twoDigitsNumber = function (number) {
+    var self = this;
+    if (number < 10) {
+        return '0' + number;
+    } 
+    return String(number);
+};
 
-// Chronometer.prototype.setTime = function () {
+Chronometer.prototype.setTime = function () {
 
-// };
+};
 
 // Chronometer.prototype.setMilliseconds = function () {
 
