1561d9dbfa450f9a41a05e87af575d190ce2ddd8
MacBook <macbook@Macbook.local>
2018-01-29 14:00:40 -0600

    almost done

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,23 +1,42 @@
 // Constructor
-// function Chronometer() {
+function Chronometer() {
+    this.currentTime= 0;
+    this.intervalId =0;
+     }
+    
+    
+    Chronometer.prototype.startClick = function (){
+            var that = this;
+            this.intervalId = setInterval(function () {
+            
+              that.currentTime ++;
+                   console.log(that.currentTime);
+             }, 1000);
+    };
+    var chrono = new Chronometer();
+            chrono.startClick();
 
-// }
+Chronometer.prototype.setMinutes = function () {
+var minutos = this.currentTime/60
+return Math.floor(minutos)
+};
 
-// Chronometer.prototype.startClick = function () {
+Chronometer.prototype.setSeconds = function () {
+  var segundos = this.currentTime
+    return segundos % 60
+};
 
-// };
+Chronometer.prototype.twoDigitsNumber = function (num) {
+    var dec = this.currentTime
+    if (num>=9){
+    return dec
+    }
+    else if (num<9){
+        return "0 "+ dec
+    }
 
-// Chronometer.prototype.setMinutes = function () {
-  
-// };
-
-// Chronometer.prototype.setSeconds = function () {
-  
-// };
-
-// Chronometer.prototype.twoDigitsNumber = function () {
-  
-// };
+   // return ("  ")
+};
 
 // Chronometer.prototype.setTime = function () {
 
