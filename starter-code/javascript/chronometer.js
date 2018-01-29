95bd2a145108dcf5cdaeadc5044e8ad5d30ac6db
AliciaJz <zilaphono@gmail.com>
2018-01-29 14:01:38 -0600

    semi Done

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,34 +1,43 @@
 // Constructor
-// function Chronometer() {
+function Chronometer() {
+  this.currentTime = 0;
+  this.intervalId = 0;
+}
 
-// }
+Chronometer.prototype.startClick = function() {
+  var chronoClick = this;
+  this.intervalId = setInterval(function() {
+    chronoClick.currentTime++;
+  }, 1000);
+};
+Chronometer.prototype.setMinutes = function() {
+  return Math.floor(this.currentTime / 60);
+};
 
-// Chronometer.prototype.startClick = function () {
+Chronometer.prototype.setSeconds = function() {
+  return this.currentTime % 60;
+};
 
-// };
+Chronometer.prototype.twoDigitsNumber = function(value) {
+  if (value < 9) {
+    var singleDig = value;
+    return "0" + singleDig;
+  } else {
+    return toString(value);
+  }
+  return toString(value);
+};
 
-// Chronometer.prototype.setMinutes = function () {
-  
-// };
+Chronometer.prototype.setTime = function () {
 
-// Chronometer.prototype.setSeconds = function () {
-  
-// };
-
-// Chronometer.prototype.twoDigitsNumber = function () {
-  
-// };
-
-// Chronometer.prototype.setTime = function () {
-
-// };
+};
 
 // Chronometer.prototype.setMilliseconds = function () {
 
 // };
 
 // Chronometer.prototype.stopClick = function () {
-  
+
 // };
 
 // Chronometer.prototype.resetClick = function () {
