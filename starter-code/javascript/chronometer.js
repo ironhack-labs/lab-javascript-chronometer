2ec5547d4b2e84a1fed5d76a5bb2b73a13b0880b
Gabriel Cebrián <kunry3@hotmail.com>
2018-03-26 13:40:45 +0200

    Estamos en setTime

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,27 +1,38 @@
 // Constructor
-// function Chronometer() {
+function Chronometer() {
+    this.currentTime = 0;
+    this.intervalId = 0;
+}
 
-// }
+Chronometer.prototype.startClick = function () {
+    var that = this
+    setInterval (function (){
+        that.intervalId;
+        that.currentTime++;
+        Chronometer.setTime();
+    },1000);
+};
 
-// Chronometer.prototype.startClick = function () {
+Chronometer.prototype.setMinutes = function () {
+  var minutos = parseInt(this.currentTime / 60);
+  return minutos;
+};
 
-// };
+Chronometer.prototype.setSeconds = function () {
+  var segundos = this.currentTime % 60;
+  return segundos;
+};
 
-// Chronometer.prototype.setMinutes = function () {
-  
-// };
+Chronometer.prototype.twoDigitsNumber = function (digitos2) {
+  if (digitos2 < 10){
+      digitos2 = "0" + digitos2;
+  }
+  return toString(digitos2);
+};
 
-// Chronometer.prototype.setSeconds = function () {
-  
-// };
+Chronometer.prototype.setTime = function () {
 
-// Chronometer.prototype.twoDigitsNumber = function () {
-  
-// };
-
-// Chronometer.prototype.setTime = function () {
-
-// };
+};
 
 // Chronometer.prototype.setMilliseconds = function () {
 
@@ -38,3 +49,5 @@
 // Chronometer.prototype.splitClick = function () {
 
 // };
+ 
+console.log(toString(152));
