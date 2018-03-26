720496bb76e2a4626de8919e595bff10351cbf98
Pedro Piury Blanquez <pedropiuryblanquez@MacBook-Air-de-Pedro.local>
2018-03-26 13:34:36 +0200

    Mitad de iteracion 1, a 15 fallos del final

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,23 +1,39 @@
-// Constructor
-// function Chronometer() {
+//Constructor
+function Chronometer() {
+  this.currentTime = 0;
+  this.intervalId = 0;
+}
 
-// }
+Chronometer.prototype.startClick = function () {
+    var that = this;
 
-// Chronometer.prototype.startClick = function () {
+    setInterval(function(){ 
+      that.currentTime +=1;
+       }, 1000);
 
-// };
+};
 
-// Chronometer.prototype.setMinutes = function () {
-  
-// };
 
-// Chronometer.prototype.setSeconds = function () {
-  
-// };
+Chronometer.prototype.setMinutes = function () {
+  var minutos = Math.floor(this.currentTime/60);
+  return minutos; 
+ };
 
-// Chronometer.prototype.twoDigitsNumber = function () {
-  
-// };
+Chronometer.prototype.setSeconds = function () {
+  if(this.currentTime == 0){
+    return 0;
+  } else { 
+    return (this.currentTime - (chronometer.setMinutes() * 60 ));
+  }  
+};
+
+Chronometer.prototype.twoDigitsNumber = function (n) {
+  if(n < 9) {
+    return "0" + 9;
+  } else {
+    return n;
+  }
+};
 
 // Chronometer.prototype.setTime = function () {
 
