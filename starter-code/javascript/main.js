c55d7c6d5faad18ae0882586590bdf6c84b33fd7
lenniez <lenniezhu@gmail.com>
2018-03-26 23:27:51 +0200

    almost done with iteration 2

--- starter-code/javascript/main.js
+++ starter-code/javascript/main.js
@@ -7,11 +7,18 @@ var secDec      = document.getElementById('secDec');
 var secUni      = document.getElementById('secUni');
 var milDec      = document.getElementById('milDec');
 var milUni      = document.getElementById('milUni');
+var timeToPrint     = chronometer.setTime();
 
+/* NOT WORKING ----- GRRRR --------
 
 function printTime() {
-
+  minDec = this.timeToPrint[0];
+  minUni = this.timeToPrint[1];
+  secDec = this.timeToPrint[5];
+  secUni = this.timeToPrint[6];
 }
+
+*/
 
 function printMinutes() {
 
@@ -51,10 +58,36 @@ function setResetBtn() {
 
 // Start/Stop Button
 btnLeft.addEventListener('click', function () {
+  
+  if (document.getElementById("btnLeft").classList.contains("start")) {
+    document.getElementById("btnLeft").classList.replace("start", "stop");
+    chronometer.startClick();
+  } else {
+    document.getElementById("btnLeft").classList = "btn start";
+    chronometer.stopClick();
+  }
+
+  if (document.getElementById("btnLeft").innerText === "START") {
+    document.getElementById("btnLeft").innerText = "STOP";
+  } else {
+    document.getElementById("btnLeft").innerText = "START";
+  }
 
 });
 
 // Reset/Split Button
 btnRight.addEventListener('click', function () {
+  
+  if (document.getElementById("btnRight").classList.contains("reset")) {
+    document.getElementById("btnRight").classList.replace("reset", "split");
+  } else {
+    document.getElementById("btnRight").classList = "btn reset";
+  }
+
+  if (document.getElementById("btnRight").innerText === "RESET") {
+    document.getElementById("btnRight").innerText = "SPLIT";
+  } else {
+    document.getElementById("btnRight").innerText = "RESET";
+  }
 
 });
