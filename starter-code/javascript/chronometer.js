ecbdab1db525ea612465f66709a79e9231fb626a
Raul.dll <rdl.llave@gmail.com>
2018-03-26 13:39:35 +0200

    hasta setminutes

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,22 +1,32 @@
 // Constructor
-// function Chronometer() {
+function Chronometer() {
+  this.currentTime = 0;
+  this.intervalId = 0;
+}
 
-// }
+Chronometer.prototype.startClick = function() {
+  var that = this;
 
-// Chronometer.prototype.startClick = function () {
+  var intervalId = setInterval(function() {
+    that.currentTime += 1;
+  }, 1000);
+};
 
-// };
+ Chronometer.prototype.setMinutes = function () {
+ var that = this;
 
-// Chronometer.prototype.setMinutes = function () {
-  
-// };
+  var  minuto = setMinutes(function() {
+    that.currentTime;
+  },1000/60)
+  return
+ };
 
 // Chronometer.prototype.setSeconds = function () {
-  
+
 // };
 
 // Chronometer.prototype.twoDigitsNumber = function () {
-  
+
 // };
 
 // Chronometer.prototype.setTime = function () {
@@ -28,7 +38,7 @@
 // };
 
 // Chronometer.prototype.stopClick = function () {
-  
+
 // };
 
 // Chronometer.prototype.resetClick = function () {
