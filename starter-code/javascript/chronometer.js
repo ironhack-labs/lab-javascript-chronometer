d73fd543865d9c4d830e206d79e937d839e05079
lloren27 <llorenteyuste@hotmail.com>
2018-03-26 13:39:54 +0200

    Realizados truquis

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,15 +1,26 @@
 // Constructor
-// function Chronometer() {
+function Chronometer() {
 
-// }
+    this.currentTime = 0; // hay que añadir como parametro un valor 
+    this.intervalId = " "; // hay que iniciarlo como String vacio 
 
-// Chronometer.prototype.startClick = function () {
+ };
 
-// };
+ Chronometer.prototype.startClick = function () {
+    
+    var that = this;
 
-// Chronometer.prototype.setMinutes = function () {
+    this.intervalId = setInterval (function (){
+        that.currentTime ++; // utilizamos el that porque dentro de la funcion de setInterval no llega el this ( "el truquito")
+    },1000);
+    
+ };
+
+ Chronometer.prototype.setMinutes = function () {
+
+    this.currentTime = (this.currentTime)/(60);
   
-// };
+};
 
 // Chronometer.prototype.setSeconds = function () {
   
