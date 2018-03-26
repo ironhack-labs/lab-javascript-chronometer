6739c4ec75443097188063e8a77ea6c64cff98da
Mapi Carrillo <mp.carrillo@yahoo.es>
2018-03-26 17:30:28 +0200

    Atascados en que el reloj muestre los botones de forma correcta (colores, clases de DOM)

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -6,11 +6,12 @@ function Chronometer() {
     this.intervalId = 0
 }
 
-Chronometer.prototype.startClick = function () {
+Chronometer.prototype.startClick = function (setTime) {
     
         var that = this
         setInterval (function(){
-            that.currentTime ++;
+           var time = that.currentTime ++;
+            that.setTime();
         },1000)
 
     };
@@ -23,26 +24,36 @@ Chronometer.prototype.setSeconds = function () {
   return Math.round(this.currentTime - 60 * this.setMinutes())
  };
 
-Chronometer.prototype.twoDigitsNumber = function () {
-  
+Chronometer.prototype.twoDigitsNumber = function (zegar) {
+  if (zegar < 10){
+      return 0 + zegar.toString()
+  }else{
+      return zegar.toString()
+  }
 };
 
-// Chronometer.prototype.setTime = function () {
+ Chronometer.prototype.setTime = function () {
+     var minuta = this.twoDigitsNumber(this.setMinutes); 
+     var secunda = this.twoDigitsNumber(this.setSeconds);
 
-// };
+
+ };
 
 // Chronometer.prototype.setMilliseconds = function () {
 
 // };
 
-// Chronometer.prototype.stopClick = function () {
-  
-// };
+ Chronometer.prototype.stopClick = function () {
+  clearInterval(this.startClick);
+ };
 
-// Chronometer.prototype.resetClick = function () {
-
-// };
+ Chronometer.prototype.resetClick = function () {
+    this.currentTime = parseInt(this.currentTime);
+    this.currentTime = 0;
+ };
 
 // Chronometer.prototype.splitClick = function () {
 
 // };
+
+
--- starter-code/javascript/main.js
+++ starter-code/javascript/main.js
@@ -1,4 +1,6 @@
 var chronometer = new Chronometer();
+
+
 var btnLeft     = document.getElementById('btnLeft');
 var btnRight    = document.getElementById('btnRight');
 var minDec      = document.getElementById('minDec');
@@ -7,7 +9,10 @@ var secDec      = document.getElementById('secDec');
 var secUni      = document.getElementById('secUni');
 var milDec      = document.getElementById('milDec');
 var milUni      = document.getElementById('milUni');
-
+var btnStart    = document.getElementsByClassName('start')[0];
+var btnReset    = document.getElementsByClassName('reset')[0];
+var btnStop     = document.getElementsByClassName('stop')[0];
+var btnSplit    = document.getElementsByClassName('split')[0];
 
 function printTime() {
 
@@ -51,10 +56,23 @@ function setResetBtn() {
 
 // Start/Stop Button
 btnLeft.addEventListener('click', function () {
-
+    if ( chronometer.currentTime === 0 ){
+        //chronometer.startClick()
+        btnLeft.setAttribute('class', 'btn stop')
+        btnLeft.innerHTML ='STOP'
+        btnRight.setAttribute('class', 'btn split')
+        btnRight.innerHTML = 'SPLIT'
+    } else {
+        chronometer.startClick()
+        btnLeft.setAttribute('class', 'btn start')
+        btnLeft.innerHTML = 'START'
+        btnRight.setAttribute('class', 'btn reset')
+        btnRight.innerHTML = 'RESET'
+    }
 });
-
+btnStart.onclick = chronometer.startClick
+btnReset.onclick = chronometer.resetClick
 // Reset/Split Button
 btnRight.addEventListener('click', function () {
-
+    chronometer.resetClick()
 });
