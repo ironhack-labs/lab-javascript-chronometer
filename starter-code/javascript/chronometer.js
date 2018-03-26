2e353cee8c2619107e64157a917d66ad7fa5ef1b
Gabriel Cebrián <kunry3@hotmail.com>
2018-03-26 15:44:24 +0200

    Error setTime

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -6,10 +6,10 @@ function Chronometer() {
 
 Chronometer.prototype.startClick = function () {
     var that = this
-    setInterval (function (){
+    var a = setInterval (function (){
         that.intervalId;
         that.currentTime++;
-        Chronometer.setTime();
+        that.setTime;
     },1000);
 };
 
@@ -27,27 +27,29 @@ Chronometer.prototype.twoDigitsNumber = function (digitos2) {
   if (digitos2 < 10){
       digitos2 = "0" + digitos2;
   }
-  return toString(digitos2);
+  return digitos2.toString();
 };
 
 Chronometer.prototype.setTime = function () {
-
+    console.log("pepe");
+    chronometer.twoDigitsNumber(chronometer.setSeconds);
+    chronometer.twoDigitsNumber(chronometer.setMinutes);
 };
 
 // Chronometer.prototype.setMilliseconds = function () {
 
 // };
 
-// Chronometer.prototype.stopClick = function () {
-  
-// };
+Chronometer.prototype.stopClick = function () {
+    clearInterval(this.startClick.a);
+};
 
-// Chronometer.prototype.resetClick = function () {
-
-// };
+Chronometer.prototype.resetClick = function () {
+    that = this
+    that.currentTime = 0;
+};
 
 // Chronometer.prototype.splitClick = function () {
 
 // };
- 
-console.log(toString(152));
+
