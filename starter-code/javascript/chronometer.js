d52f9021c19801be0901503b7005999e240b50de
Luna Fidalgo <lunafidalgo@gmail.com>
2018-03-26 13:40:34 +0200

    todo hasta setTime

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,23 +1,41 @@
-// Constructor
-// function Chronometer() {
+//Constructor
+function Chronometer() {
+  this.currentTime = 0;
+  this.intervalId = 0;
+}
 
-// }
+Chronometer.prototype.startClick = function() {
+  var that = this;
+  this.intervalId = setInterval(function() {
+    that.currentTime++;
+    console.log(that.currentTime);
+  }, 1000);
+};
 
-// Chronometer.prototype.startClick = function () {
+Chronometer.prototype.setMinutes = function() {
+  //hay que pasar de segundos a minutos
 
-// };
+  return Math.trunc(this.currentTime / 60);
+};
 
-// Chronometer.prototype.setMinutes = function () {
-  
-// };
+Chronometer.prototype.setSeconds = function() {
+  if (this.currentTime == 0) {
+    return 0;
+  }
 
-// Chronometer.prototype.setSeconds = function () {
-  
-// };
+  this.currentTime = this.currentTime - 60 * this.setMinutes();
 
-// Chronometer.prototype.twoDigitsNumber = function () {
-  
-// };
+  return this.currentTime;
+};
+
+Chronometer.prototype.twoDigitsNumber = function (numero) {
+  //recibe un numero, ejemplo: 
+  if(numero < 10 ){
+    return 0+""+numero;
+  }else{
+    return ""+numero;
+  }
+};
 
 // Chronometer.prototype.setTime = function () {
 
@@ -28,7 +46,7 @@
 // };
 
 // Chronometer.prototype.stopClick = function () {
-  
+
 // };
 
 // Chronometer.prototype.resetClick = function () {
