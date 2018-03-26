5ddd15814c09fd6d69e3b46880d1e7ce80ca439d
lloren27 <llorenteyuste@hotmail.com>
2018-03-26 17:00:23 +0200

    Test realizado interaccion 1

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -10,41 +10,54 @@ function Chronometer() {
     
     var that = this;
 
-    this.intervalId = setInterval (function (){
-        that.currentTime ++; // utilizamos el that porque dentro de la funcion de setInterval no llega el this ( "el truquito")
+    this.intervalId = setInterval (function () {
+    
+        that.currentTime ++; // utilizamos el that porque dentro de la funcion de setInterval no llega el this ( "el truquito"
+        that.setTime();
     },1000);
     
  };
 
  Chronometer.prototype.setMinutes = function () {
-
-    this.currentTime = (this.currentTime)/(60);
-  
+    return this.currentTime = Math.trunc((this.currentTime)/ 60);    
+    console.log(this.currentTime)
 };
 
-// Chronometer.prototype.setSeconds = function () {
-  
-// };
+Chronometer.prototype.setSeconds = function () {
+//    
+    return this.currentTime = this.currentTime % 60;
+};
 
-// Chronometer.prototype.twoDigitsNumber = function () {
-  
-// };
+Chronometer.prototype.twoDigitsNumber = function (value) {
+    // this.setMinutes;
+    // this.setSeconds;
+    if (value < 10) {
+        return ("0" + value)
+    } else {
+        return ("" + value)
+    }  
+};
 
-// Chronometer.prototype.setTime = function () {
+Chronometer.prototype.setTime = function () {
+    
+    // this.startClick ();
+    // this.setSeconds ();
+    // this.setMinutes ();
+    // this.twoDigitsNumber ();
 
-// };
+};
 
 // Chronometer.prototype.setMilliseconds = function () {
 
 // };
 
-// Chronometer.prototype.stopClick = function () {
-  
-// };
+Chronometer.prototype.stopClick = function () {
+    clearInterval(this.intervalId)
+};
 
-// Chronometer.prototype.resetClick = function () {
-
-// };
+Chronometer.prototype.resetClick = function () {
+    this.currentTime = 0;
+};
 
 // Chronometer.prototype.splitClick = function () {
 
