6a91a1ee12cfd09d493b8ede24504809e9dadb48
Antea Martín <anteamartinacinas@MacBook-Air-de-Antea.local>
2018-03-26 13:41:00 +0200

    setMinutes finalizado

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,15 +1,27 @@
 // Constructor
 // function Chronometer() {
-
+function Chronometer(){
+    this.currentTime = 0;
+    this.intervalId = 0;
+    
+}
 // }
 
-// Chronometer.prototype.startClick = function () {
+Chronometer.prototype.startClick = function () {
+    var that = this;
+    this.intervalId = setInterval(function (){
+        that.currentTime++
+    }, 1000);
+};
 
-// };
-
-// Chronometer.prototype.setMinutes = function () {
-  
-// };
+Chronometer.prototype.setMinutes = function () {
+  if(this.currentTime < 60){
+      return 0;
+  }
+  if((this.currentTime / 60) > 1){
+      return Math.floor(this.currentTime/60);
+  }
+};
 
 // Chronometer.prototype.setSeconds = function () {
   
