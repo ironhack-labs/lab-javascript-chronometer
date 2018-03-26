b9da1fbc47ec63dbc5a3264f5f37a96c5d1b3d73
Pablo García <heypablete@gmail.com>
2018-03-26 18:17:47 +0200

    tenemos que sacar los segundos y minutos en el reloj

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,40 +1,53 @@
 // Constructor
-// function Chronometer() {
+function Chronometer() {
+  this.currentTime = 0;
+  this.intervalId = 0;
+}
 
-// }
+Chronometer.prototype.startClick = function() {
+  var that = this;
+  console.log(this.currentTime);
+  setInterval(function(){
+  that.currentTime++;
+  that.setTime();
+ }, 1000)
+};
 
-// Chronometer.prototype.startClick = function () {
+Chronometer.prototype.setMinutes = function () {
+    return Math.trunc(this.currentTime / 60);
+};
 
-// };
+Chronometer.prototype.setSeconds = function () {
+  return this.currentTime % 60;
+};
 
-// Chronometer.prototype.setMinutes = function () {
-  
-// };
+Chronometer.prototype.twoDigitsNumber = function (number) {
+  console.log(number);
+  if(true) {
+    return number;
+  }
+  return number.toString();
+};
 
-// Chronometer.prototype.setSeconds = function () {
-  
-// };
+Chronometer.prototype.setTime = function () {
+  var minutes = this.twoDigitsNumber(this.setMinutes(this.currentTime));
+  var seconds = this.twoDigitsNumber(this.setSeconds(this.currentTime));
+  var hora = minutes + " " + seconds;
+  return hora;
+};
 
-// Chronometer.prototype.twoDigitsNumber = function () {
-  
-// };
+Chronometer.prototype.setMilliseconds = function () {
 
-// Chronometer.prototype.setTime = function () {
+};
 
-// };
+Chronometer.prototype.stopClick = function () {
+  clearInterval(this.currentTime);
+};
 
-// Chronometer.prototype.setMilliseconds = function () {
+Chronometer.prototype.resetClick = function () {
 
-// };
+};
 
-// Chronometer.prototype.stopClick = function () {
-  
-// };
+Chronometer.prototype.splitClick = function () {
 
-// Chronometer.prototype.resetClick = function () {
-
-// };
-
-// Chronometer.prototype.splitClick = function () {
-
-// };
+};
--- starter-code/javascript/main.js
+++ starter-code/javascript/main.js
@@ -10,15 +10,18 @@ var milUni      = document.getElementById('milUni');
 
 
 function printTime() {
-
+  var inter = setInterval(function() {
+    printMinutes();
+    printSeconds();
+  }, 1000);
 }
 
 function printMinutes() {
-
+  console.log(chronometer.setTime());
 }
 
 function printSeconds() {
-
+  console.log(chronometer.setTime());
 }
 
 function printMilliseconds() {
@@ -34,27 +37,46 @@ function clearSplits() {
 }
 
 function setStopBtn() {
-
+  btnLeft.innerHTML = "STOP";
+  btnLeft.classList.remove("start");
+  btnLeft.classList.add("stop");
 }
 
 function setSplitBtn() {
-
+  btnRight.innerHTML = "SPLIT";
+  btnRight.classList.remove("reset");
+  btnRight.classList.add("split");
 }
 
 function setStartBtn() {
-
+  btnLeft.innerHTML = "START";
+  btnLeft.classList.remove("stop");
+  btnLeft.classList.add("start");
 }
 
 function setResetBtn() {
-
+  btnRight.innerHTML = "RESET";
+  btnRight.classList.remove("split");
+  btnRight.classList.add("reset");
 }
 
 // Start/Stop Button
 btnLeft.addEventListener('click', function () {
-
+  if (btnLeft.classList.contains("start")) {
+    setStopBtn();
+    chronometer.startClick();
+    printTime();
+  } else {
+    setStartBtn();
+    chronometer.stopClick();
+  }
 });
 
 // Reset/Split Button
 btnRight.addEventListener('click', function () {
-
+  if (btnRight.classList.contains("reset") ) {
+    setSplitBtn();
+  } else {
+    setResetBtn();
+  }
 });
