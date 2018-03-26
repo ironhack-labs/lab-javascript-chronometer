6f3f61fe3140f3eaf6fb0b16cf594079b0e9d983
Miriam Aparicio <miriam.aparicio@gmail.com>
2018-03-26 23:51:16 +0200

    BCN-Miriam.Aparicio

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,40 +1,46 @@
-// Constructor
-// function Chronometer() {
+function Chronometer() {
+  this.currentTime = 0;
+  this.intervalId = 0;
+}
 
-// }
+Chronometer.prototype.startClick = function() {
+  var self = this;
+  this.intervalId = setInterval(function () {
+    self.currentTime++;
+    self.setTime();   
+  }, 1000);
+};
 
-// Chronometer.prototype.startClick = function () {
+Chronometer.prototype.setMinutes = function() {
+  var minutes = Math.floor(this.currentTime/60);
+  return minutes;
+};
 
-// };
+Chronometer.prototype.setSeconds = function() {
+  var seconds = this.currentTime - this.setMinutes() * 60;
+  return seconds;
+};
 
-// Chronometer.prototype.setMinutes = function () {
-  
-// };
+Chronometer.prototype.twoDigitsNumber = function(number) {
+  var twoDigitNumber = (number < 10 ? '0' : '') + number;
+  return twoDigitNumber;
+};
 
-// Chronometer.prototype.setSeconds = function () {
-  
-// };
+Chronometer.prototype.setTime = function() {  
+  var minutes = this.twoDigitsNumber(this.setMinutes());
+  var seconds = this.twoDigitsNumber(this.setSeconds());
+  //console.log(seconds);
+  return [minutes, seconds];
+};
 
-// Chronometer.prototype.twoDigitsNumber = function () {
-  
-// };
+//Chronometer.prototype.setMilliseconds = function() {};
 
-// Chronometer.prototype.setTime = function () {
+Chronometer.prototype.stopClick = function() {
+  clearInterval(this.intervalId);
+};
 
-// };
+Chronometer.prototype.resetClick = function() {
+  this.currentTime = 0;
+};
 
-// Chronometer.prototype.setMilliseconds = function () {
-
-// };
-
-// Chronometer.prototype.stopClick = function () {
-  
-// };
-
-// Chronometer.prototype.resetClick = function () {
-
-// };
-
-// Chronometer.prototype.splitClick = function () {
-
-// };
+Chronometer.prototype.splitClick = function() {};
