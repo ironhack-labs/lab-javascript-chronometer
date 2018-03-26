be90fff87d84369c099ebcbbe3c93eb09068cdab
FernandoGalende <fernandogalende@gmail.com>
2018-03-26 13:37:19 +0200

    set minutes and second

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,25 +1,27 @@
 // Constructor
- function Chronometer() {
- this.currentTime=0;
- this.intervalId=0;
- }
+function Chronometer() {
+	this.currentTime = 0;
+	this.intervalId = 0;
+}
 
- Chronometer.prototype.startClick = function () {
-intervalId = setInterval(function(){
- that.currentTime +=1; 
-},1000)
+Chronometer.prototype.startClick = function() {
+	var that = this;
+	this.intervalId = setInterval(function() {
+		that.currentTime += 1;
+	}, 1000);
 };
 
-// Chronometer.prototype.setMinutes = function () {
-  
-// };
+Chronometer.prototype.setMinutes = function () {
+return Math.floor(this.currentTime / 60);
+};
 
-// Chronometer.prototype.setSeconds = function () {
-  
-// };
+Chronometer.prototype.setSeconds = function () {
+  return  this.currentTime - (this.setMinutes()*60);
+
+ };
 
 // Chronometer.prototype.twoDigitsNumber = function () {
-  
+
 // };
 
 // Chronometer.prototype.setTime = function () {
@@ -31,7 +33,7 @@ intervalId = setInterval(function(){
 // };
 
 // Chronometer.prototype.stopClick = function () {
-  
+
 // };
 
 // Chronometer.prototype.resetClick = function () {
