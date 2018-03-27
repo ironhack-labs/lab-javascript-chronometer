2b5c6fa3ac89629bb1c6dff737f2d44788995450
Sebastian Nugnez <sebastian.nugnez@gmail.com>
2018-03-27 09:08:54 +0200

    need to finish, i could not touch any code yesterday night

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,40 +1,58 @@
 // Constructor
-// function Chronometer() {
+function Chronometer() {
+  this.currentTime = 0;
+  this.intervalId = 0;
 
-// }
+}
 
-// Chronometer.prototype.startClick = function () {
+Chronometer.prototype.startClick = function () {
+  var self = this 
+  setInterval(function() {
+    self.currentTime += 1
+  }, 1000);
+};
 
-// };
+Chronometer.prototype.setMinutes = function () {
+  if (this.currentTime < 60) {
+    return 0;
+  } else {
+    return Math.floor(this.currentTime / 60);
+  };
+};
 
-// Chronometer.prototype.setMinutes = function () {
+Chronometer.prototype.setSeconds = function () {
+  var minutes = Math.floor(this.currentTime / 60);
+  var seconds = this.currentTime - minutes * 60;
+  return seconds;
+};
+
+Chronometer.prototype.twoDigitsNumber = function (number) {
+  var string = number + ""
+  if (string.length === 1) {
+    return "0" + string;
+  } else {
+    return string;
+  };
+};
+
+Chronometer.prototype.setTime = function () {
+  var seconds = twoDigitsNumber(setSeconds());
+  var minutes = twoDigitsNumber(setMinutes());
+};
+
+Chronometer.prototype.setMilliseconds = function () {
+
+};
+
+Chronometer.prototype.stopClick = function () {
   
-// };
+};
 
-// Chronometer.prototype.setSeconds = function () {
-  
-// };
+Chronometer.prototype.resetClick = function () {
 
-// Chronometer.prototype.twoDigitsNumber = function () {
-  
-// };
+};
 
-// Chronometer.prototype.setTime = function () {
+Chronometer.prototype.splitClick = function () {
 
-// };
+};
 
-// Chronometer.prototype.setMilliseconds = function () {
-
-// };
-
-// Chronometer.prototype.stopClick = function () {
-  
-// };
-
-// Chronometer.prototype.resetClick = function () {
-
-// };
-
-// Chronometer.prototype.splitClick = function () {
-
-// };
