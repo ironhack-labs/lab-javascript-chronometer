846617e8c39bc33b70408b6786133958d5608531
Sari <freja.sender@gmail.com>
2018-05-28 15:30:01 +0200

    Iteration 1 to 4

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,39 +1,46 @@
 // Constructor
-// function Chronometer() {
 
-// }
+function Chronometer() {
+  this.currentTime = 0; // represents the number of seconds
+  this.intervalId = 0;
+}
 
-// Chronometer.prototype.startClick = function () {
+Chronometer.prototype.startClick = function() {
+  var chronometer = this;
+  // we need to store our current object into sth else, so we can use it later!
+  // if a pass a lambda into the interval function this is not going to change
+  this.intervalId = setInterval(function() {
+    chronometer.setTime();
+    chronometer.currentTime++;
+  }, 1000);
+};
 
-// };
+Chronometer.prototype.setMinutes = function() {
+  return Math.floor(this.currentTime / 60);
+};
 
-// Chronometer.prototype.setMinutes = function () {
-  
-// };
+Chronometer.prototype.setSeconds = function() {
+  return Math.floor(this.currentTime % 60);
+};
 
-// Chronometer.prototype.setSeconds = function () {
-  
-// };
+Chronometer.prototype.twoDigitsNumber = function(number) {
+  if (number < 10) return "0" + number;
+  return number.toString();
+};
 
-// Chronometer.prototype.twoDigitsNumber = function () {
-  
-// };
-
-// Chronometer.prototype.setTime = function () {
-
-// };
+Chronometer.prototype.setTime = function() {};
 
 // Chronometer.prototype.setMilliseconds = function () {
 
 // };
 
-// Chronometer.prototype.stopClick = function () {
-  
-// };
+Chronometer.prototype.stopClick = function() {
+  clearInterval(this.intervalId);
+};
 
-// Chronometer.prototype.resetClick = function () {
-
-// };
+Chronometer.prototype.resetClick = function() {
+  this.currentTime = 0;
+};
 
 // Chronometer.prototype.splitClick = function () {
 
