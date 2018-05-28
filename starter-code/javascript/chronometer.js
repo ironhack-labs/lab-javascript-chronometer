9d902601c35cb8852bedce4541a26444c49658a5
Hannes Reinsch <hannes.reinsch99@gmail.com>
2018-05-28 15:34:29 +0200

    BER-HAn

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,23 +1,42 @@
 // Constructor
-// function Chronometer() {
+function Chronometer() {
+  this.currentTime = 0,
+  this.intervalId = ""
+}
 
-// }
+Chronometer.prototype.startClick = function() {
+  var that = this;
+  that.intervalId = setInterval(function() {
+    that.currentTime++;
+  }, 1000);
+};
 
-// Chronometer.prototype.startClick = function () {
-
+// document.getElementById("btnLeft").onclick = function() {
+//   chronometer.startClick();
 // };
 
-// Chronometer.prototype.setMinutes = function () {
-  
-// };
 
-// Chronometer.prototype.setSeconds = function () {
-  
-// };
+Chronometer.prototype.setMinutes = function() {
+    return Math.floor(this.currentTime / 60);
+  };
 
-// Chronometer.prototype.twoDigitsNumber = function () {
-  
-// };
+
+
+ Chronometer.prototype.setSeconds = function () {
+  return this.currentTime - (Math.floor(this.currentTime / 60)*60);
+ };
+
+Chronometer.prototype.twoDigitsNumber = function (val) {
+  if(typeof val == 'undefined'){
+    return "00";
+  }
+  val = toString(val);
+  if(val.length == 1){
+    return "0" + val;
+  }else{
+    return val;
+  }
+ };
 
 // Chronometer.prototype.setTime = function () {
 
@@ -28,7 +47,7 @@
 // };
 
 // Chronometer.prototype.stopClick = function () {
-  
+
 // };
 
 // Chronometer.prototype.resetClick = function () {
