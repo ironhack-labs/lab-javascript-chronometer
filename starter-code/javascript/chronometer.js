9cf852cb4af1eeb513fe7c78444a92bcbf5dba3b
LJ Skadi <LJ.Skadi@gmail.com>
2018-05-29 08:53:30 +0200

    BER Elliot & Ayumi

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,39 +1,54 @@
 // Constructor
-// function Chronometer() {
+function Chronometer() {
+this.currentTime = 0;
+this.intervalId = null;
 
-// }
+}
 
-// Chronometer.prototype.startClick = function () {
+Chronometer.prototype.startClick = function () {
+    var that = this; // Here that refers to this and this can be seen as a pointer/reference
+        //that.intervalId = (this.intervalId===null) ? null : this.resetClick();
+        that.intervalId = setInterval(function(){
+        that.currentTime++ ;
+        that.setTime();
+        }, 1000)
+        this.resetClick();
+};
 
-// };
+ Chronometer.prototype.setMinutes = function () {
+    var that = this;
+        return Math.floor((that.currentTime/60));
+ };
 
-// Chronometer.prototype.setMinutes = function () {
-  
-// };
+ Chronometer.prototype.setSeconds = function () {
+    var that = this;
+    return that.currentTime % 60;
+ };
 
-// Chronometer.prototype.setSeconds = function () {
-  
-// };
+ Chronometer.prototype.twoDigitsNumber = function (value) {
+    return (value < 10) ? "0" + value : value.toString()
+ };
 
-// Chronometer.prototype.twoDigitsNumber = function () {
-  
-// };
+ Chronometer.prototype.setTime = function () {
+        var minutes = "00";
+        var seconds = "00";
+        minutes = this.setMinutes();
+        seconds = this.setSeconds();
 
-// Chronometer.prototype.setTime = function () {
-
-// };
+ };
 
 // Chronometer.prototype.setMilliseconds = function () {
 
 // };
 
-// Chronometer.prototype.stopClick = function () {
-  
-// };
+ Chronometer.prototype.stopClick = function () {
+     var that = this;
+    clearInterval(that.intervalId);
+ };
 
-// Chronometer.prototype.resetClick = function () {
-
-// };
+ Chronometer.prototype.resetClick = function () {
+    return this.intervalId = 0;
+ };
 
 // Chronometer.prototype.splitClick = function () {
 
--- starter-code/javascript/main.js
+++ starter-code/javascript/main.js
@@ -7,18 +7,22 @@ var secDec      = document.getElementById('secDec');
 var secUni      = document.getElementById('secUni');
 var milDec      = document.getElementById('milDec');
 var milUni      = document.getElementById('milUni');
-
-
+      
 function printTime() {
+    
 
 }
 
 function printMinutes() {
-
+    minDec.innerText = this.chronometer.minutes[0];
+    minUni.innerText = minutes[1];
 }
 
 function printSeconds() {
-
+    var seconds = this.chronometer.setSeconds();
+    secondes = this.twoDigitsNumber;
+    secDec.innerText = seconds[0];
+    secUni.innerText = seconds[1];
 }
 
 function printMilliseconds() {
