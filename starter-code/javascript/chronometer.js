d788528fc7a39e0af10df35ba5d2e6ad21750695
aparna pattathil <aparnapattathil@hotmail.com>
2018-06-01 18:01:17 +0200

    Iteration 1 committed

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,40 +1,55 @@
-// Constructor
-// function Chronometer() {
+//Constructor
+function Chronometer() {
+this.currentTime = 0;
+this.intervalId = 0;
+}
 
-// }
+Chronometer.prototype.startClick = function () {
+    var that = this;
+   
+   that.intervalId = setInterval(function(){
+    that.currentTime++;
+    that.setTime();
+    }, 1000);
 
-// Chronometer.prototype.startClick = function () {
+};
 
-// };
+Chronometer.prototype.setMinutes = function () {
+  var currentMinutes = Math.floor(this.currentTime / 60);
+  return currentMinutes;
+};
 
-// Chronometer.prototype.setMinutes = function () {
-  
-// };
+Chronometer.prototype.setSeconds = function () {
+  var remainingSeconds = this.currentTime % 60;
+  return remainingSeconds;
+};
 
-// Chronometer.prototype.setSeconds = function () {
-  
-// };
+Chronometer.prototype.twoDigitsNumber = function (value) {
+  if(value < 10){
+      return "0" + value;
+  }
+  else if(value >=10) {
+      return "" + value;
+  }
+};
 
-// Chronometer.prototype.twoDigitsNumber = function () {
-  
-// };
+Chronometer.prototype.setTime = function () {
+    var minutes = this.twoDigitsNumber(that.setMinutes());
+    var seconds = this.twoDigitsNumber(that.setSeconds());
+};
 
-// Chronometer.prototype.setTime = function () {
+Chronometer.prototype.setMilliseconds = function () {
 
-// };
+};
 
-// Chronometer.prototype.setMilliseconds = function () {
+Chronometer.prototype.stopClick = function () {
+  clearInterval(this.intervalId);
+};
 
-// };
+Chronometer.prototype.resetClick = function () {
+    this.currentTime = 0;
+};
 
-// Chronometer.prototype.stopClick = function () {
-  
-// };
+Chronometer.prototype.splitClick = function () {
 
-// Chronometer.prototype.resetClick = function () {
-
-// };
-
-// Chronometer.prototype.splitClick = function () {
-
-// };
+};
