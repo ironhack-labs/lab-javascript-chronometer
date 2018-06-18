c5f6ccc83d48a115c3eb40c846c11fded2cca9de
Lili Cadena <anlica_99@hotmail.com>
2018-06-18 14:19:30 -0500

    Intentamos hacerlo con atributos diferentes y no salió al 100. Intentaré hacerlo con los originales

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,21 +1,61 @@
-// Constructor
-// function Chronometer() {
+ //Constructor
 
-// }
+function cronometro () {
+	var min = 0;
+	var sec = 0;
 
-// Chronometer.prototype.startClick = function () {
+var interval = setInterval(function(){
+  sec++;
+  if(sec > 59){
+    sec = 0;
+    min++;
+  }
+  var time = document.getElementById("minDec");
+  if (time !== null) {
+  	time.innerHTML = ((min < 10) ? ("0" + min) : min) + ":" + ( (sec < 10) ? ("0" + sec) : sec ) ;
+  	split = ((min < 10) ? ("0" + min) : min) + ":" + ( (sec < 10) ? ("0" + sec) : sec ) ;
+  } else clearInterval ();
+}, 100);
+}
 
-// };
+function clear (){
+	document.getElementById("minDec").removeAttribute("id");
+	splitF();
+}
 
-// Chronometer.prototype.setMinutes = function () {
-  
-// };
 
-// Chronometer.prototype.setSeconds = function () {
-  
-// };
+
+
+
+/*
+ function Chronometer() {
+ 	this.currentTime = 0;
+ 	this.intervalId = 0;
+
+ 	this.startClick = function (){
+ 		this.intervalId = setInterval (function (){
+ 		this.currentTime++;
+ 		console.log(this.intervalId);
+ 		},1000);
+ 	}
+ }
+
+ console.log (Chronometer());
+
+ Chronometer.prototype.setMinutes = function () {
+  	return this.currentTime *60;
+ };
+
+ Chronometer.prototype.setSeconds = function () {
+  	if (currentTime > 59){
+  		currentTime = 0;
+
+  	}
+ };
 
 // Chronometer.prototype.twoDigitsNumber = function () {
+
+	*/
   
 // };
 
