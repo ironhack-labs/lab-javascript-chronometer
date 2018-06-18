ebdd019950b1e8241440da6a6fa9dd8d80a8bbb8
Verónica Reina <veroreinah@gmail.com>
2018-06-18 17:56:55 +0200

    Iteration 1: The Logic

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,40 +1,48 @@
 // Constructor
-// function Chronometer() {
+function Chronometer() {
+  this.currentTime = 0;
+  this.intervalId = "";
+}
 
-// }
+Chronometer.prototype.startClick = function () {
+  var that = this;
 
-// Chronometer.prototype.startClick = function () {
+  this.intervalId = setInterval(function() {
+    that.currentTime++;
+    that.setTime();
+  }, 1000);
+};
 
-// };
+Chronometer.prototype.setMinutes = function () {
+  return Math.floor(this.currentTime / 60);
+};
 
-// Chronometer.prototype.setMinutes = function () {
-  
-// };
+Chronometer.prototype.setSeconds = function () {
+  return this.currentTime % 60;
+};
 
-// Chronometer.prototype.setSeconds = function () {
-  
-// };
+Chronometer.prototype.twoDigitsNumber = function (number) {
+  return ('0' + number).slice(-2);
+};
 
-// Chronometer.prototype.twoDigitsNumber = function () {
-  
-// };
+Chronometer.prototype.setTime = function () {
+  this.minutes = this.twoDigitsNumber(this.setMinutes());
+  this.seconds = this.twoDigitsNumber(this.setSeconds());
+};
 
-// Chronometer.prototype.setTime = function () {
+Chronometer.prototype.setMilliseconds = function () {
 
-// };
+};
 
-// Chronometer.prototype.setMilliseconds = function () {
+Chronometer.prototype.stopClick = function () {
+  clearInterval(this.intervalId);
+};
 
-// };
+Chronometer.prototype.resetClick = function () {
+  this.currentTime = 0;
+  this.setTime();
+};
 
-// Chronometer.prototype.stopClick = function () {
-  
-// };
+Chronometer.prototype.splitClick = function () {
 
-// Chronometer.prototype.resetClick = function () {
-
-// };
-
-// Chronometer.prototype.splitClick = function () {
-
-// };
+};
