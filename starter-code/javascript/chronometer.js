a1cec1540e7ec253bef863dee9075a8facaa6225
Ricardo Campos <ricamposm@gmail.com>
2018-06-18 17:09:41 -0500

    done

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,40 +1,62 @@
 // Constructor
-// function Chronometer() {
+var sec =0;
+var min = 0;
+var time = "";
 
-// }
+function Chronometer() {
+  this.currentTime = 0;
+  this.intervalId = "";
 
-// Chronometer.prototype.startClick = function () {
+     
+  this.setMinutes = function () {
+    min++;
+    this.twoDigitsNumber();
+    
+  }
 
-// };
-
-// Chronometer.prototype.setMinutes = function () {
+  this.setSeconds = function () {
+  sec++;
+  if (sec > 59) {
+    sec = 0;
+    this.setMinutes();
+    this.twoDigitsNumber();
+    }
+  }
   
+  this.twoDigitsNumber = function () {
+    this.currentTime = ((min < 10) ? ("0" + min) : min) + ":" + ( (sec < 10) ? ("0" + sec) : sec ) ;
+    printSeconds();
+  };
+
+  this.setTime = function () {
+    console.log(time);
+  };
+
+  this.resetClick = function () {
+    this.currentTime = 0;
+    sec = 0;
+    min = 0;
+    this.twoDigitsNumber();
+  };
+
+  this.splitClick = function () {
+    this.twoDigitsNumber();
+  };
+
+  this.startClick = function () {
+    chronometer.intervalId = setInterval(function(){
+    chronometer.setSeconds();
+    chronometer.twoDigitsNumber();
+    }, 100);
+  };
+
+  this.stopClick = function () {
+      clearInterval(chronometer.intervalId);
+    };
+}
+
+
+  // Chronometer.prototype.setMilliseconds = function () {
+
 // };
 
-// Chronometer.prototype.setSeconds = function () {
-  
-// };
-
-// Chronometer.prototype.twoDigitsNumber = function () {
-  
-// };
-
-// Chronometer.prototype.setTime = function () {
-
-// };
-
-// Chronometer.prototype.setMilliseconds = function () {
-
-// };
-
-// Chronometer.prototype.stopClick = function () {
-  
-// };
-
-// Chronometer.prototype.resetClick = function () {
-
-// };
-
-// Chronometer.prototype.splitClick = function () {
-
-// };
