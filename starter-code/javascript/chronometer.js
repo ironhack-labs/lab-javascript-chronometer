0850e3f56d6796700a271b14802fb14ad5a6a66f
Enrique Pernia <Epernia96@gmail.com>
2018-06-18 22:09:16 +0200

    Solucionado el problema con el reset

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,58 +1,55 @@
 // Constructor
 function Chronometer() {
 	this.currentTime = 0;
-     this.intervalId = 0;
+	this.intervalId = 0;
 }
 
 Chronometer.prototype.startClick = function() {
-     var that=this;
+	var that = this;
 	this.intervalId = setInterval(function() {
-          that.currentTime++;
-          that.setTime();
+		that.currentTime++;
+		that.setTime();
 		if (that.intervalId > 60) {
-               that.currentTime = 1;
+			that.currentTime = 1;
 		}
 	}, 1000);
 };
 
-
- Chronometer.prototype.setMinutes = function () {
-     return Math.floor(this.currentTime/60);
- };
-
-Chronometer.prototype.setSeconds = function () {
-     if(this.currentTime<=0){
-          return 0;
-     }
-    return this.currentTime- (Math.floor(this.currentTime/60)*60) ;
+Chronometer.prototype.setMinutes = function() {
+	return Math.floor(this.currentTime / 60);
 };
 
-Chronometer.prototype.twoDigitsNumber = function (e) {
-if(e<10){
-     return "0"+e;
-}
-else {
-     return e.toString()
-}
+Chronometer.prototype.setSeconds = function() {
+	if (this.currentTime <= 0) {
+		return 0;
+	}
+	return this.currentTime - Math.floor(this.currentTime / 60) * 60;
 };
 
-Chronometer.prototype.setTime = function () {
-     this.minutes=this.twoDigitsNumber(this.setMinutes());
-     this.seconds=this.twoDigitsNumber(this.setSeconds());;
+Chronometer.prototype.twoDigitsNumber = function(e) {
+	if (e < 10) {
+		return "0" + e;
+	} else {
+		return e.toString();
+	}
 };
 
-Chronometer.prototype.setMilliseconds = function () {
+Chronometer.prototype.setTime = function() {
+	this.minutes = this.twoDigitsNumber(this.setMinutes());
+	this.seconds = this.twoDigitsNumber(this.setSeconds());
 };
 
-Chronometer.prototype.stopClick = function () {
-     clearInterval(this.intervalId);
+Chronometer.prototype.setMilliseconds = function() {};
+
+Chronometer.prototype.stopClick = function() {
+	clearInterval(this.intervalId);
 };
 
-Chronometer.prototype.resetClick = function () {
-    this.minutes="00"
-    this.seconds="00"
+Chronometer.prototype.resetClick = function() {
+	this.minutes = "00";
+	this.seconds = "00";
 };
 
- Chronometer.prototype.splitClick = function () {
-     this.currentTime=0;
+Chronometer.prototype.splitClick = function() {
+	this.currentTime = 0;
 };
