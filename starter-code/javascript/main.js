426201906e9369da40ac26fcaf05bdcd4288f1f7
Raúl Valenzuela <raulvalen7@hotmail.com>
2018-06-18 20:50:03 +0200

    Iteration1, falta split y bonus

--- starter-code/javascript/main.js
+++ starter-code/javascript/main.js
@@ -10,15 +10,22 @@ var milUni      = document.getElementById('milUni');
 
 
 function printTime() {
-
+    this.intervalId = setInterval(function(){
+        printMinutes();
+        printSeconds();
+    },1000);
 }
 
 function printMinutes() {
-
+    var min = chronometer.twoDigitsNumber(chronometer.setMinutes());
+    minDec.innerHTML = min[0];
+    minUni.innerHTML = min[1];    
 }
 
 function printSeconds() {
-
+    var sec = chronometer.twoDigitsNumber(chronometer.setSeconds());
+    secDec.innerHTML = sec[0];
+    secUni.innerHTML = sec[1]; 
 }
 
 function printMilliseconds() {
@@ -34,27 +41,45 @@ function clearSplits() {
 }
 
 function setStopBtn() {
-
+    btnLeft.innerText="STOP";
+    btnLeft.className = "btn stop";
 }
 
 function setSplitBtn() {
-
+    btnRight.innerText="SPLIT";
+    btnRight.className="btn split";
 }
 
 function setStartBtn() {
-
+    btnLeft.innerText="START";
+    btnLeft.className = "btn start"; 
 }
 
 function setResetBtn() {
-
+    btnRight.innerText="RESET";
+    btnRight.className="btn reset";
 }
 
 // Start/Stop Button
 btnLeft.addEventListener('click', function () {
-
+    if (btnLeft.innerText == "START"){
+        setStopBtn();
+        setSplitBtn();
+        chronometer.startClick();
+        printTime();
+    }else{
+        setStartBtn();
+        setResetBtn();
+        chronometer.stopClick();
+    }
 });
 
 // Reset/Split Button
 btnRight.addEventListener('click', function () {
-
+    if(btnLeft.innerText == "START"){
+        setResetBtn();
+        chronometer.resetClick();
+    }else{
+        setSplitBtn();
+    }
 });
