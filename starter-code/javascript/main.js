035b4bc8d9d9e0c1df2ca955b16e73ff15f21055
Andrés Martin <monkeyandres@protonmail.com>
2018-06-18 23:14:51 +0200

    Finished

--- starter-code/javascript/main.js
+++ starter-code/javascript/main.js
@@ -8,53 +8,101 @@ var secUni      = document.getElementById('secUni');
 var milDec      = document.getElementById('milDec');
 var milUni      = document.getElementById('milUni');
 
+var splitContainer = document.getElementById("splits");
+
 
 function printTime() {
-
+    var time = chronometer.setTime();
+    printSeconds(time);
+    printMinutes(time);
+    printMilliseconds(time);
 }
 
-function printMinutes() {
-
+function printMinutes(time) {
+    var minutes = time.minutes;
+    minUni.innerHTML = minutes[1];
+    minDec.innerHTML = minutes[0];
 }
 
-function printSeconds() {
-
+function printSeconds(time) {
+    var seconds = time.seconds;
+    secUni.innerHTML = seconds[1];
+    secDec.innerHTML = seconds[0];
 }
 
-function printMilliseconds() {
-
+function printMilliseconds(time) {
+    var milSecs = time.milSecs;
+    console.log(milSecs);
+    milUni.innerHTML = milSecs[1];
+    milDec.innerHTML = milSecs[0];
 }
 
 function printSplit() {
+    var time = chronometer.setTime();
+    var split = document.createElement("li");
+    split.innerHTML = time.minutes + ":" + time.seconds + ":" + time.milSecs;
 
+    splitContainer.appendChild(split);
 }
 
 function clearSplits() {
-
+    splitContainer.innerHTML = "";
 }
 
 function setStopBtn() {
-
+    btnLeft.className = "btn stop"
+    btnLeft.innerHTML = "STOP"
 }
 
 function setSplitBtn() {
-
+    btnRight.className = "btn split"
+    btnRight.innerHTML = "SPLIT"
 }
 
 function setStartBtn() {
-
+    btnLeft.className = "btn start"
+    btnLeft.innerHTML = "START"
 }
 
 function setResetBtn() {
+    btnRight.className = "btn reset"
+    btnRight.innerHTML = "RESET"
+}
 
+function changeBtnStyle(){
+    if(btnLeft.innerHTML == "START"){
+        setStopBtn();
+        setSplitBtn();
+    } else {
+        setStartBtn();
+        setResetBtn();
+    }
+}
+
+function isRunning(){
+    return btnLeft.innerHTML == "STOP";
 }
 
 // Start/Stop Button
+var intervalId;
 btnLeft.addEventListener('click', function () {
-
+    if(isRunning()){
+        clearInterval(intervalId);
+        chronometer.stopClick();
+    } else {
+        chronometer.startClick();
+        intervalId = setInterval(printTime, 10);
+    }
+    changeBtnStyle();
 });
 
 // Reset/Split Button
 btnRight.addEventListener('click', function () {
-
+    if(isRunning()){
+        printSplit();    
+    } else {
+        clearSplits();
+        chronometer.resetClick();
+        printTime();
+    }
 });
