5ca7d098a3001658ac586795f6940754545c5253
Enrique Pernia <Epernia96@gmail.com>
2018-06-18 21:16:36 +0200

    Finalizado sin el bonus

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,40 +1,58 @@
 // Constructor
-// function Chronometer() {
+function Chronometer() {
+	this.currentTime = 0;
+     this.intervalId = 0;
+}
 
-// }
+Chronometer.prototype.startClick = function() {
+     var that=this;
+	this.intervalId = setInterval(function() {
+          that.currentTime++;
+          that.setTime();
+		if (that.intervalId > 60) {
+               that.currentTime = 1;
+		}
+	}, 1000);
+};
 
-// Chronometer.prototype.startClick = function () {
 
-// };
+ Chronometer.prototype.setMinutes = function () {
+     return Math.floor(this.currentTime/60);
+ };
 
-// Chronometer.prototype.setMinutes = function () {
-  
-// };
+Chronometer.prototype.setSeconds = function () {
+     if(this.currentTime<=0){
+          return 0;
+     }
+    return this.currentTime- (Math.floor(this.currentTime/60)*60) ;
+};
 
-// Chronometer.prototype.setSeconds = function () {
-  
-// };
+Chronometer.prototype.twoDigitsNumber = function (e) {
+if(e<10){
+     return "0"+e;
+}
+else {
+     return e.toString()
+}
+};
 
-// Chronometer.prototype.twoDigitsNumber = function () {
-  
-// };
+Chronometer.prototype.setTime = function () {
+     this.minutes=this.twoDigitsNumber(this.setMinutes());
+     this.seconds=this.twoDigitsNumber(this.setSeconds());;
+};
 
-// Chronometer.prototype.setTime = function () {
+Chronometer.prototype.setMilliseconds = function () {
+};
 
-// };
+Chronometer.prototype.stopClick = function () {
+     clearInterval(this.intervalId);
+};
 
-// Chronometer.prototype.setMilliseconds = function () {
+Chronometer.prototype.resetClick = function () {
+    this.minutes="00"
+    this.seconds="00"
+};
 
-// };
-
-// Chronometer.prototype.stopClick = function () {
-  
-// };
-
-// Chronometer.prototype.resetClick = function () {
-
-// };
-
-// Chronometer.prototype.splitClick = function () {
-
-// };
+ Chronometer.prototype.splitClick = function () {
+     this.currentTime=0;
+};
--- starter-code/javascript/main.js
+++ starter-code/javascript/main.js
@@ -7,22 +7,23 @@ var secDec      = document.getElementById('secDec');
 var secUni      = document.getElementById('secUni');
 var milDec      = document.getElementById('milDec');
 var milUni      = document.getElementById('milUni');
-
+var i=0;
 
 function printTime() {
 
 }
 
 function printMinutes() {
-
+     minDec=chronometer.twoDigitsNumber(chronometer.setSeconds)[1];
+     minUni=chronometer.twoDigitsNumber(chronometer.setSeconds)[0];
 }
 
 function printSeconds() {
-
+     secDec=chronometer.twoDigitsNumber(chronometer.setSeconds)[0];
 }
 
 function printMilliseconds() {
-
+     secUni=chronometer.twoDigitsNumber(chronometer.setSeconds)[1];
 }
 
 function printSplit() {
@@ -51,10 +52,66 @@ function setResetBtn() {
 
 // Start/Stop Button
 btnLeft.addEventListener('click', function () {
+    if(btnLeft.innerText=="START"){
+         btnLeft.innerText="STOP"
+          btnLeft.classList.remove("start");
+          btnLeft.classList.add("stop");
+          chronometer.startClick();
+          setInterval(function() {
+               minDec.innerText=chronometer.minutes[0];
+               minUni.innerText=chronometer.minutes[1];
+               secUni.innerText=chronometer.seconds[1];
+               secDec.innerText=chronometer.seconds[0];
+          },1000)
+     }
 
+
+     else{
+          btnLeft.innerText="START"
+          btnLeft.classList.remove("stop");
+          btnLeft.classList.add("start");
+          chronometer.stopClick();
+     }
 });
 
 // Reset/Split Button
 btnRight.addEventListener('click', function () {
 
+     if(btnRight.innerText=="SPLIT" && btnLeft.innerText=="STOP") {
+          btnRight.innerText="RESET"
+          btnRight.classList.remove("split");
+          btnRight.classList.add("reset");
+          var lista=document.createElement("li");
+          lista.classList.add("lista");
+          var padre=document.getElementById("splits");
+          var texto=document.createTextNode(" "+chronometer.minutes[0]+ chronometer.minutes[1]+":"+chronometer.seconds[0]+ chronometer.seconds[1]);
+          padre.appendChild(lista);
+          lista.appendChild(texto);
+     }
+      else if(btnRight.innerText=="RESET" && btnLeft.innerText=="START"){
+          btnRight.innerText="SPLIT"
+          btnRight.classList.remove("reset");
+          btnRight.classList.add("split");
+          var padre=document.getElementById("splits");
+          var hijo =document.getElementsByClassName("lista")
+          while(padre.hasChildNodes()){
+	          padre.removeChild(padre.firstChild);	
+          }
+          chronometer.resetClick();
+          minDec.innerHTML="0";
+          minUni.innerHTML="0";
+          secDec.innerHTML="0";
+          secUni.innerHTML="0";
+          //chronometer.resetClick();
+      }
+      else if(btnRight.innerText=="SPLIT" && btnLeft.innerText=="START"){
+          btnRight.innerText="RESET"
+          btnRight.classList.remove("split");
+          btnRight.classList.add("reset");
+      }
+      else{
+          btnRight.innerText="SPLIT"
+           btnRight.classList.remove("reset");
+           btnRight.classList.add("split");
+      }
 });
