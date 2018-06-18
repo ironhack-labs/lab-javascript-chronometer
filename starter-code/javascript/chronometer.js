fd58277b6353e677b7ef9ccef6c7f3295f2ef04f
JuanCasnova <casanovajm199621@gmail.com>
2018-06-18 17:31:38 -0500

    De mex.Juany Alberto

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,23 +1,34 @@
-// Constructor
-// function Chronometer() {
 
-// }
+function Chronometer() 
+{
+    this.currentTime=0;
+    this.intervalId=0;
+};
 
-// Chronometer.prototype.startClick = function () {
+ Chronometer.prototype.startClick = function () {
+   this.intervalId=setInterval(function(){
+        this.currentTime++;
+   },1000);
+ };
 
-// };
+ Chronometer.prototype.setMinutes = function () 
+ {
+    var minutes = Math.floor(this.currentTime/60);
+    return minutes;
+ };
 
-// Chronometer.prototype.setMinutes = function () {
-  
-// };
+ Chronometer.prototype.setSeconds = function () {
+    var seconds = this.currentTime - (this.setMinutes()*60);
+    return seconds;
+ };
 
-// Chronometer.prototype.setSeconds = function () {
-  
-// };
-
-// Chronometer.prototype.twoDigitsNumber = function () {
-  
-// };
+ Chronometer.prototype.twoDigitsNumber = function () {
+    var twoDigits = "" + n;
+    if (twoDigits.length <= 1){
+       return twoDigits = "0" + n;
+    };
+    return twoDigits;
+};
 
 // Chronometer.prototype.setTime = function () {
 
@@ -27,13 +38,14 @@
 
 // };
 
-// Chronometer.prototype.stopClick = function () {
-  
-// };
+Chronometer.prototype.stopClick = function () {
+    return clearInterval(this.intervalId);  
+};
 
-// Chronometer.prototype.resetClick = function () {
+ Chronometer.prototype.resetClick = function () {
+    return this.currentTime = 0;
 
-// };
+ };
 
 // Chronometer.prototype.splitClick = function () {
 
