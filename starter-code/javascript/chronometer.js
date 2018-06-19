a462c0c101ceec767061dc76e7af1d04a5fdd25e
Javier Romero <javier.romero@Henkel-Backup-2.local>
2018-06-19 16:39:17 +0200

    done

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,39 +1,51 @@
-// Constructor
-// function Chronometer() {
+/// Constructor
+function Chronometer() {
+    this.currentTime = 0;
+    this.intervalId = 0;
+ }
 
-// }
+Chronometer.prototype.startClick = function () {
+    var self = this;
+    this.intervalId = setInterval(function() {
+        self.currentTime ++;
+        self.setTime();
+    }, 1000);
 
-// Chronometer.prototype.startClick = function () {
+};
 
-// };
+Chronometer.prototype.setMinutes = function () {   
+    return Math.floor(this.currentTime / 60);
+    // var minutosEnteros = Math.floor(this.currentTime / 60);
+    // var minutosRestantes = Math
+};
 
-// Chronometer.prototype.setMinutes = function () {
-  
-// };
+Chronometer.prototype.setSeconds = function () {
+    if (this.currentTime === 0) {
+        return 0;
+    }
+    return this.currentTime - this.setMinutes() * 60;
+};
 
-// Chronometer.prototype.setSeconds = function () {
-  
-// };
+Chronometer.prototype.twoDigitsNumber = function (number) {
+    var formatNumber = ("0" + number).slice(-2);
+    return formatNumber;
+};
 
-// Chronometer.prototype.twoDigitsNumber = function () {
-  
-// };
+Chronometer.prototype.setTime = function () {
 
-// Chronometer.prototype.setTime = function () {
-
-// };
+};
 
 // Chronometer.prototype.setMilliseconds = function () {
 
 // };
 
-// Chronometer.prototype.stopClick = function () {
-  
-// };
+Chronometer.prototype.stopClick = function () {
+    clearInterval(this.intervalId);
+};
 
-// Chronometer.prototype.resetClick = function () {
+Chronometer.prototype.resetClick = function () {
 
-// };
+};
 
 // Chronometer.prototype.splitClick = function () {
 
