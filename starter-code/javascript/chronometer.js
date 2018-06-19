c887215b8700f257bc2972911c01c2203925969a
Andrea Moya <andrea@MacBook-Pro-de-Andrea.local>
2018-06-19 16:28:25 +0200

    idk

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,40 +1,52 @@
 // Constructor
-// function Chronometer() {
+function Chronometer() {
+ this.currentTime = 0;
+ this.intervalId = null;
+}
 
-// }
 
-// Chronometer.prototype.startClick = function () {
+Chronometer.prototype.startClick = function () {
+  var self = this;
 
-// };
+  this.intervalId = setInterval(function(){
+    self.currentTime += 1;
+  }, 1000);
+};
 
-// Chronometer.prototype.setMinutes = function () {
-  
-// };
+Chronometer.prototype.setMinutes = function () {
+  return Math.floor(this.currentTime/60);
+};
 
-// Chronometer.prototype.setSeconds = function () {
-  
-// };
+Chronometer.prototype.setSeconds = function () {
+  return this.currentTime%60;
+};
 
-// Chronometer.prototype.twoDigitsNumber = function () {
-  
-// };
+Chronometer.prototype.twoDigitsNumber = function (valor) {
+  if(valor<10){
+    var numero = '' + valor;
+    return '0'+ numero;
+  }
+  else { 
+    return '' + valor;
+  }
+};
 
-// Chronometer.prototype.setTime = function () {
-
-// };
+Chronometer.prototype.setTime = function () {
+  setInterval(setTime,)
+};
 
 // Chronometer.prototype.setMilliseconds = function () {
 
 // };
 
-// Chronometer.prototype.stopClick = function () {
-  
-// };
+Chronometer.prototype.stopClick = function () {
+  this.startClick.stop(this.intervalId);
+};
 
-// Chronometer.prototype.resetClick = function () {
+Chronometer.prototype.resetClick = function () {
 
-// };
+};
 
-// Chronometer.prototype.splitClick = function () {
+Chronometer.prototype.splitClick = function () {
 
-// };
+};
