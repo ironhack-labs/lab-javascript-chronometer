2a22220c8a0bc06d74dffce4864d5fffc10b6773
Samuraty <alexandra.garciagasco@gmail.com>
2018-06-19 09:15:58 +0200

    No bonus, no split

--- starter-code/javascript/main.js
+++ starter-code/javascript/main.js
@@ -10,51 +10,58 @@ var milUni      = document.getElementById('milUni');
 
 
 function printTime() {
-
+  var intervalIdMain = setInterval(function(){
+    printMinutes();
+    printSeconds();
+  },1000);
 }
 
 function printMinutes() {
-
+  minDec.innerHTML = chronometer.minutes[0];
+  minUni.innerHTML = chronometer.minutes[1];
 }
 
 function printSeconds() {
-
+  secDec.innerHTML = chronometer.seconds[0];
+  secUni.innerHTML = chronometer.seconds[1];
 }
 
-function printMilliseconds() {
-
-}
-
-function printSplit() {
-
-}
-
-function clearSplits() {
-
-}
-
-function setStopBtn() {
-
-}
-
-function setSplitBtn() {
-
-}
-
-function setStartBtn() {
-
-}
-
-function setResetBtn() {
-
-}
 
 // Start/Stop Button
 btnLeft.addEventListener('click', function () {
-
+  if (btnLeft.className === "btn start") { // START
+    // change classes
+    btnLeft.innerHTML = "STOP";
+    btnLeft.setAttribute("class", "btn stop");
+    btnRight.innerHTML = "SPLIT";
+    btnRight.setAttribute("class", "btn split");
+    // start and print stuff
+    chronometer.startClick(); 
+    printTime();
+  }
+    else { // STOP
+    // change classes
+    btnLeft.innerHTML = "START";
+    btnLeft.setAttribute("class", "btn start");
+    btnRight.innerHTML = "RESET";
+    btnRight.setAttribute("class", "btn reset");
+    // stop
+    chronometer.stopClick(); 
+  }
 });
 
 // Reset/Split Button
 btnRight.addEventListener('click', function () {
+    if (btnRight.className === "btn reset") { // RESET
+        // clear numbers
+        chronometer.resetClick();
+        printTime();
+        document.getElementById("splits").remove();
+    }
+    else { //SPLIT
+      var node = document.createElement("li");
+      getElementById("splits").appendchild(node);
+      return 
 
+    }
 });
