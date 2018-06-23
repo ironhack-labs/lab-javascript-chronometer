f4fb9a16592f1e0f8c370ddd377888250115f9f4
casher0009 <the.casher.uber@gmail.com>
2018-06-23 18:35:57 -0500

    tarde pero seguro

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,40 +1,55 @@
-// Constructor
-// function Chronometer() {
+//Constructor
+function Chronometer() {
+  this.currentTime = 0
+  this.intervalId = 0;
+  this.minutes = "00";
+  this.seconds = "00";
+  this.milliseconds = "00"
+}
 
-// }
+Chronometer.prototype.startClick = function () {
+  this.intervalId = setInterval(() => {
+      this.currentTime += 1;
+      this.setTime();
+  }, 1000);
+};
 
-// Chronometer.prototype.startClick = function () {
+Chronometer.prototype.setMinutes = function () {
+  return Math.floor(this.currentTime / 60);
+};
 
-// };
+Chronometer.prototype.setSeconds = function () {
+  return (this.currentTime < 60) ? this.currentTime : (this.currentTime % 60);
+};
 
-// Chronometer.prototype.setMinutes = function () {
-  
-// };
+Chronometer.prototype.twoDigitsNumber = function (value) {
+  if (value < 10) {
+      return "0" + value;
+  } else {
+      return value.toString()
+  }
+};
 
-// Chronometer.prototype.setSeconds = function () {
-  
-// };
+Chronometer.prototype.setTime = function () {
+  this.minutes = this.twoDigitsNumber(this.setMinutes())
+  this.seconds = this.twoDigitsNumber(this.setSeconds())
+};
 
-// Chronometer.prototype.twoDigitsNumber = function () {
-  
-// };
+Chronometer.prototype.setMilliseconds = function () {
 
-// Chronometer.prototype.setTime = function () {
 
-// };
+};
 
-// Chronometer.prototype.setMilliseconds = function () {
+Chronometer.prototype.stopClick = function () {
+  clearInterval(this.intervalId)
+};
 
-// };
+Chronometer.prototype.resetClick = function () {
+  setTimeout(() => {
+      this.currentTime = 0;
+      this.setTime()
+  }, 0)
+};
+Chronometer.prototype.splitClick = function () {
 
-// Chronometer.prototype.stopClick = function () {
-  
-// };
-
-// Chronometer.prototype.resetClick = function () {
-
-// };
-
-// Chronometer.prototype.splitClick = function () {
-
-// };
+};
