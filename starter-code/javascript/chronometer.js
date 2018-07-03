2d0f413b51bec949e4dacbfd0d0078d014665886
Anna Fredriksson <leliloro@gmail.com>
2018-07-03 16:24:15 +0200

    done

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,40 +1,64 @@
 // Constructor
-// function Chronometer() {
+function Chronometer() {
+  this.currentTime = 0;
+  this.intervalId = 0;
+}
 
-// }
+Chronometer.prototype.startClick = function () {
+  var self = this;
+  this.intervalId = setInterval(function(){
+    self.currentTime++;
+    self.setTime();
+  },1000);
+};
 
-// Chronometer.prototype.startClick = function () {
+Chronometer.prototype.setMinutes = function () {
+  var seconds = this.currentTime,
+      minutes = parseInt(seconds/60);
 
-// };
+  return minutes;
+};
 
-// Chronometer.prototype.setMinutes = function () {
+Chronometer.prototype.setSeconds = function () {
+  var minutes = this.setMinutes();
+  var seconds = this.currentTime-(minutes*60);
+  return seconds;
+};
+
+Chronometer.prototype.twoDigitsNumber = function (digits) {
+  var dstring = "0";
+  var result = digits.toString();
+
+  if(result.length < 2){
+     result = dstring+result;
+  }
+  return result;
+};
+
+Chronometer.prototype.setTime = function () {
+  var minutes = this.setMinutes();
+  var seconds = this.setSeconds();
   
-// };
+  minutes = this.twoDigitsNumber(minutes);
+  seconds = this.twoDigitsNumber(seconds);
 
-// Chronometer.prototype.setSeconds = function () {
-  
-// };
+  var time = [minutes,seconds];
+  return time;
+};
 
-// Chronometer.prototype.twoDigitsNumber = function () {
-  
-// };
+Chronometer.prototype.setMilliseconds = function () {
 
-// Chronometer.prototype.setTime = function () {
+};
 
-// };
+Chronometer.prototype.stopClick = function () {
+  clearInterval(this.intervalId);
+};
 
-// Chronometer.prototype.setMilliseconds = function () {
+Chronometer.prototype.resetClick = function () {
+  this.currentTime = 0;
+  this.stopClick();
+};
 
-// };
+Chronometer.prototype.splitClick = function () {
 
-// Chronometer.prototype.stopClick = function () {
-  
-// };
-
-// Chronometer.prototype.resetClick = function () {
-
-// };
-
-// Chronometer.prototype.splitClick = function () {
-
-// };
+};
