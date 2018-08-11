057da090531155b8315286145271ed31d24efef7
Adolfo Vela <vela@tekplanner.com>
2018-08-11 17:42:30 -0500

    Terminado...faltan los extras

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -8,40 +8,58 @@ function Chronometer() {
 Chronometer.prototype.startClick = function () {
   this.intervalId = setInterval(() => {
     this.currentTime++;
-    console.log(this.currentTime);
+    this.setTime();
+    // console.log(this.currentTime);
+    return this.currentTime;
   }, 1000);
 };
 
-var curent = new Chronometer();
-console.log(curent.startClick())
+
 
 Chronometer.prototype.setMinutes = function () {
-
+  this.startClick();
+  var minutes = Math.floor(this.currentTime / 60);
+  // console.log(this.currentTime);
+  return minutes;
 };
 
-// Chronometer.prototype.setSeconds = function () {
+var curent = new Chronometer();
+console.log(curent.setMinutes());
 
-// };
+Chronometer.prototype.setSeconds = function () {
+  console.log(this.currentTime);
+  return this.currentTime % 60;
+};
+// console.log(curent.setSeconds());
 
-// Chronometer.prototype.twoDigitsNumber = function () {
+Chronometer.prototype.twoDigitsNumber = function (value) {
+  if(value < 10 || value == 0){
+    return "0"+value;
+  }
+  return value.toString();
+  
+};
+console.log(curent.twoDigitsNumber(15));
 
-// };
 
-// Chronometer.prototype.setTime = function () {
+Chronometer.prototype.setTime = function () {
 
-// };
+  var minutes = this.currentTime/60;
+  var seconds = this.currentTime;
+
+};
 
 // Chronometer.prototype.setMilliseconds = function () {
 
 // };
 
-// Chronometer.prototype.stopClick = function () {
+Chronometer.prototype.stopClick = function () {
+clearInterval(this.intervalId);
+};
 
-// };
-
-// Chronometer.prototype.resetClick = function () {
-
-// };
+Chronometer.prototype.resetClick = function () {
+this.currentTime = 0;
+};
 
 // Chronometer.prototype.splitClick = function () {
 
