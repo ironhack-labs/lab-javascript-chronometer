81db5e33206dc863706e78de1bf54c74b8e33495
Adolfo Vela <vela@tekplanner.com>
2018-08-11 15:39:24 -0500

    Listo startClick, usando arrow functions

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,22 +1,30 @@
 // Constructor
-// function Chronometer() {
+function Chronometer() {
+  this.currentTime = 0;
+  this.intervalId = 0;
+}
 
-// }
 
-// Chronometer.prototype.startClick = function () {
+Chronometer.prototype.startClick = function () {
+  this.intervalId = setInterval(() => {
+    this.currentTime++;
+    console.log(this.currentTime);
+  }, 1000);
+};
 
-// };
+var curent = new Chronometer();
+console.log(curent.startClick())
 
-// Chronometer.prototype.setMinutes = function () {
-  
-// };
+Chronometer.prototype.setMinutes = function () {
+
+};
 
 // Chronometer.prototype.setSeconds = function () {
-  
+
 // };
 
 // Chronometer.prototype.twoDigitsNumber = function () {
-  
+
 // };
 
 // Chronometer.prototype.setTime = function () {
@@ -28,7 +36,7 @@
 // };
 
 // Chronometer.prototype.stopClick = function () {
-  
+
 // };
 
 // Chronometer.prototype.resetClick = function () {
