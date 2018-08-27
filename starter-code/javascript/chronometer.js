ee7ad066f49a675f3a0d21ef9d2f113a8505770c
elillo <elilloperez35@gmail.com>
2018-08-27 23:14:18 +0100

    Ejercicio chronometer

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,39 +1,58 @@
-// Constructor
-// function Chronometer() {
 
-// }
+ function Chronometer() {
+    this.currentTime=0;
+    this.intervalId=0;
+    this.minutes=0;
+    this.seconds=0;
+  }
 
-// Chronometer.prototype.startClick = function () {
+Chronometer.prototype.startClick = function () {
+    var self = this;
+    this.intervalId = setInterval(
+        function(){
+           self.currentTime++;
+           self.setTime();
 
-// };
+        }, 1000);
+ };
 
-// Chronometer.prototype.setMinutes = function () {
+ Chronometer.prototype.setMinutes = function () {
   
-// };
+   return  Math.floor(this.currentTime / 60);
+};
 
-// Chronometer.prototype.setSeconds = function () {
+Chronometer.prototype.setSeconds = function () {
   
-// };
+  return Math.ceil(this.currentTime % 60);
+};
 
-// Chronometer.prototype.twoDigitsNumber = function () {
-  
-// };
+Chronometer.prototype.twoDigitsNumber = function (a) {
+   if(a==0){
+       return "00";
+   }
+    else if(a <10){
+       return "0" + a;
+   }else{
+       return a.toString();
+   }
+ };
 
-// Chronometer.prototype.setTime = function () {
-
-// };
+Chronometer.prototype.setTime = function () {
+   this.minutes=this.twoDigitsNumber(this.setMinutes());
+   this.seconds=this.twoDigitsNumber(this.setSeconds());
+};
 
 // Chronometer.prototype.setMilliseconds = function () {
 
 // };
 
-// Chronometer.prototype.stopClick = function () {
-  
-// };
+Chronometer.prototype.stopClick = function () {
+  clearInterval(this.intervalId);
+};
 
-// Chronometer.prototype.resetClick = function () {
-
-// };
+Chronometer.prototype.resetClick = function () {
+    return this.intervalId=0;
+};
 
 // Chronometer.prototype.splitClick = function () {
 
--- starter-code/javascript/main.js
+++ starter-code/javascript/main.js
@@ -1,4 +1,4 @@
-var chronometer = new Chronometer();
+var chronometer;
 var btnLeft     = document.getElementById('btnLeft');
 var btnRight    = document.getElementById('btnRight');
 var minDec      = document.getElementById('minDec');
@@ -9,12 +9,19 @@ var milDec      = document.getElementById('milDec');
 var milUni      = document.getElementById('milUni');
 
 
-function printTime() {
+
+function printTime(chronometer) {
+  var minSplit=chronometer.minutes.split("");
+  var secSplit=chronometer.seconds.split("");
+  minDec.innerHTML=minSplit[0];
+  minUni.innerHTML=minSplit[1];
+  secDec.innerHTML=secSplit[0];
+  secUni.innerHTML=secSplit[1];
 
 }
-
+/*
 function printMinutes() {
-
+ 
 }
 
 function printSeconds() {
@@ -48,13 +55,40 @@ function setStartBtn() {
 function setResetBtn() {
 
 }
+*/
+
+function reset (){
+  return chronometer = new Chronometer();
+}
 
 // Start/Stop Button
-btnLeft.addEventListener('click', function () {
+btnLeft.addEventListener('click', function (e) {
+  var elemento = e.currentTarget;
+  if (elemento.innerHTML == 'START') {
+    elemento.innerHTML = 'STOP'
+    elemento.style.backgroundColor = "red";
 
+   reset();
+    chronometer.startClick();
+    chronometer.intervalId = setInterval(
+// accedemos a propiedad intervalid del cronometro y se lo igualamos a setinterval para que tenga
+// el id del intervalo y así podemos pararlo ya que resetearlo lo hace la funcion reset
+
+        function(){
+           printTime(chronometer);
+
+        }, 1000);
+  } else {
+    elemento.innerHTML = 'START'
+    elemento.style.backgroundColor = "green";
+    chronometer.stopClick();
+
+  }
 });
 
 // Reset/Split Button
 btnRight.addEventListener('click', function () {
+
+  reset();
 
 });
