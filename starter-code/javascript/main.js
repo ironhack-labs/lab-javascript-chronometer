279ad2b5e001f6b68ca35ca82ebfab7e6f39fbb8
Chris Lopez <pagarizabal2@gmail.com>
2018-08-27 13:09:25 +0200

    Almost done , reset/stop to go

--- starter-code/javascript/main.js
+++ starter-code/javascript/main.js
@@ -10,15 +10,18 @@ var milUni      = document.getElementById('milUni');
 
 
 function printTime() {
-
+ printMinutes();
+ printSeconds();
 }
 
 function printMinutes() {
-
+ minDec.innerText = chronometer.minutes[0];
+ minUni.innerText = chronometer.minutes[1];
 }
 
 function printSeconds() {
-
+  secDec.innerText = chronometer.seconds[0];
+  secUni.innerText = chronometer.seconds[1];
 }
 
 function printMilliseconds() {
@@ -34,7 +37,7 @@ function clearSplits() {
 }
 
 function setStopBtn() {
-
+  chronometer.stopClick();
 }
 
 function setSplitBtn() {
@@ -42,16 +45,34 @@ function setSplitBtn() {
 }
 
 function setStartBtn() {
-
+  chronometer.startClick();
+  var printInterval = setInterval(function() {
+    printTime();
+  }, 1000);
 }
 
 function setResetBtn() {
+  chronometer.resetClick();
+  printTime();
+
 
 }
 
-// Start/Stop Button
-btnLeft.addEventListener('click', function () {
 
+btnLeft.addEventListener('click', function () {
+  if (btnLeft.getAttribute("class") == "btn start") {
+    setStartBtn();
+    btnLeft.setAttribute("class","btn stop");
+    btnLeft.innerText = "STOP";
+    btnRight.setAttribute("class", "btn split");
+    btnRight.innerText = "SPLIT";
+  } else if (btnLeft.getAttribute("class") == "btn stop") {
+    setStopBtn();
+    btnLeft.setAttribute("class","btn start");
+    btnLeft.innerText = "START";
+    btnRight.setAttribute("class", "btn reset");
+    btnRight.innerText = "RESET"; 
+  }
 });
 
 // Reset/Split Button
