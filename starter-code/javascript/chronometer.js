8d75cb84b32e3e768c455812fbaabee3b61f4070
robotic-amoeba <raul.martin.majadas@gmail.com>
2018-08-27 12:21:27 +0200

    Iteración 1

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,19 +1,37 @@
 // Constructor
-// function Chronometer() {
 
-// }
+function Chronometer() {
+  this.currentTime = 0;
+  this.intervalId = "";
+  this.miliSeconds = 0;
+}
 
-// Chronometer.prototype.startClick = function () {
-
-// };
-
-// Chronometer.prototype.setMinutes = function () {
+Chronometer.prototype.startClick = function () {
+  var self = this;
+  var ourIntervalId = setInterval(function(){
+    self.currentTime += 1;
+    console.log(self.currentTime);
+  },1000);
   
-// };
-
-// Chronometer.prototype.setSeconds = function () {
+  this.intervalId = ourIntervalId;
   
-// };
+};
+
+
+Chronometer.prototype.setMinutes = function () {
+  var minutes = 0;
+  if (this.currentTime !== 0) {
+    var minutes = Math.floor(this.currentTime / 60);
+    return minutes;
+  } else {
+    return minutes;
+  }
+};
+
+Chronometer.prototype.setSeconds = function () {
+  var seconds = this.currentTime - this.setMinutes()*60;
+  return seconds;
+};
 
 // Chronometer.prototype.twoDigitsNumber = function () {
   
