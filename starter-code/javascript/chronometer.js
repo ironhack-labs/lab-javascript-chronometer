146ce075cbe5578bcd62c05f6c51b77e988e111f
Eliot Cano <eliot7cano@gmail.com>
2018-08-28 09:22:15 -0500

    PP-Mex- Eliot Cano Juan Carlos Amel cd ..

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,40 +1,47 @@
 // Constructor
-// function Chronometer() {
+ function Chronometer() {
+    this.currentTime = 0;
+    this. intervalId = 0;
+}
+Chronometer.prototype.startClick = function(){
+  //var thos = this;
+  this.intervalId = setInterval(() => {
+    return this.currentTime++;
+  },1000);
+}
 
-// }
+Chronometer.prototype.setMinutes = function () {
+  return Math.floor(this.currentTime/60)  
+};
 
-// Chronometer.prototype.startClick = function () {
+Chronometer.prototype.setSeconds = function () {
+  return this.currentTime%60
+};
 
-// };
+Chronometer.prototype.twoDigitsNumber = function (num) {
+  if(num<10) {
+    num = "0" + num;
+  return num
+  } 
+};
 
-// Chronometer.prototype.setMinutes = function () {
-  
-// };
+Chronometer.prototype.setTime = function () {
+  this.minutes = this.twoDigitsNumber(this.setMinutes());
+  this.seconds = this.twoDigitsNumber(this.setSeconds());
+};
 
-// Chronometer.prototype.setSeconds = function () {
-  
-// };
+Chronometer.prototype.setMilliseconds = function () {
 
-// Chronometer.prototype.twoDigitsNumber = function () {
-  
-// };
+};
 
-// Chronometer.prototype.setTime = function () {
+Chronometer.prototype.stopClick = function () {
+  clearInterval(this.intervalId)
+};
 
-// };
+Chronometer.prototype.resetClick = function () {
+  this.currentTime = 0
+};
 
-// Chronometer.prototype.setMilliseconds = function () {
+Chronometer.prototype.splitClick = function () {
 
-// };
-
-// Chronometer.prototype.stopClick = function () {
-  
-// };
-
-// Chronometer.prototype.resetClick = function () {
-
-// };
-
-// Chronometer.prototype.splitClick = function () {
-
-// };
+};
