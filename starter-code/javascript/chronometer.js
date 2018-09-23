45dc29989c067efc22116d626ee190f79e580ce4
Teresa Romero <tromerolozano@gmail.com>
2018-09-23 10:15:01 +0200

    Iteration 01 - almost done 3 Jasmine errors

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,39 +1,52 @@
 // Constructor
-// function Chronometer() {
-
-// }
-
-// Chronometer.prototype.startClick = function () {
-
-// };
-
-// Chronometer.prototype.setMinutes = function () {
+function Chronometer() {
+  this.currentTime=0;
+  this.intervalId=0;
   
-// };
+}
 
-// Chronometer.prototype.setSeconds = function () {
-  
-// };
 
-// Chronometer.prototype.twoDigitsNumber = function () {
-  
-// };
+//cada 1000ms se ejecuta una suma de 1 a currentTime
+Chronometer.prototype.startClick = function () {
+    this.intervalId = setInterval(function() {
+      this.currentTime++;
+      this.setTime();
+    }, 1000);
 
-// Chronometer.prototype.setTime = function () {
+};
 
-// };
+Chronometer.prototype.setMinutes = function () {
+  return Math.floor(this.currentTime/60);
+};
+
+Chronometer.prototype.setSeconds = function () {
+  return this.currentTime%60;
+};
+
+Chronometer.prototype.twoDigitsNumber = function (number) {
+  if(number<10) {
+    return "0" + number;
+  } else {
+    return number.toString();
+  }
+};
+
+Chronometer.prototype.setTime = function () {
+  this.minutes = this.twoDigitsNumber(this.setMinutes());
+  this.seconds = this.twoDigitsNumber(this.setSeconds());
+};
 
 // Chronometer.prototype.setMilliseconds = function () {
 
 // };
 
-// Chronometer.prototype.stopClick = function () {
-  
-// };
+Chronometer.prototype.stopClick = function () {
+  clearInterval(this.intervalId);
+};
 
-// Chronometer.prototype.resetClick = function () {
-
-// };
+Chronometer.prototype.resetClick = function () {
+  this.currentTime=0;
+};
 
 // Chronometer.prototype.splitClick = function () {
 
