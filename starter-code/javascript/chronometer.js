4914dce5f917e391b66066915a509d0dde2f961e
Merwan Guerrib <merwanguerrib@gmail.com>
2018-10-06 15:07:42 +0200

    function startClick ok

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,40 +1,48 @@
 // Constructor
-// function Chronometer() {
+ function Chronometer() {
+   this.currentTime = 0;
+   this.intervalId = 0;
+   this.min = 0;
+ }
 
-// }
-
-// Chronometer.prototype.startClick = function () {
-
-// };
-
-// Chronometer.prototype.setMinutes = function () {
+ /*
+ function TimeHandler () {
+   currentTime += 1;
+ }
+ */
+ Chronometer.prototype.timerHandler = function () {
+   this.currentTime += 1; //seconds
+   console.log("Incrementing current time... new val :" + this.currentTime);
+ }
+ Chronometer.prototype.startClick = function () {
+   this.intervalId = setInterval (this.timerHandler.bind(this), 1000);
+ };
+ Chronometer.prototype.setMinutes = function (currentTime) {
+  var currentMin = Math.floor(this.currentTime % 60000); 
+ };
+ 
+ Chronometer.prototype.setSeconds = function () { 
   
-// };
+  }
 
-// Chronometer.prototype.setSeconds = function () {
-  
-// };
+   };
 
-// Chronometer.prototype.twoDigitsNumber = function () {
-  
-// };
+ Chronometer.prototype.twoDigitsNumber = function () {
 
-// Chronometer.prototype.setTime = function () {
+ };
+ Chronometer.prototype.setTime = function () {
+ };
+ Chronometer.prototype.setMilliseconds = function () {
+   //this.currentTime = setInterval (this.timerHandler.bind(this),1000);
 
-// };
-
-// Chronometer.prototype.setMilliseconds = function () {
-
-// };
-
-// Chronometer.prototype.stopClick = function () {
-  
-// };
-
-// Chronometer.prototype.resetClick = function () {
-
-// };
-
-// Chronometer.prototype.splitClick = function () {
-
-// };
+ };
+ Chronometer.prototype.stopClick = function () {
+  if (-1 != this.intervalId) {
+    clearInterval(this.intervalId);
+    this.intervalId = -1;
+}
+};
+ Chronometer.prototype.resetClick = function () {
+ };
+ Chronometer.prototype.splitClick = function () {
+ };
