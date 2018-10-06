46a4c3e029d7d6c3bf6e2fc7619738cfb2c4b40a
JZ <justinezahiri@gmail.com>
2018-10-06 15:06:38 +0200

    lab currentTime OK

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,40 +1,49 @@
-// Constructor
-// function Chronometer() {
+//constructor 
+function Chronometer() {
+    this.currentTime = 0;
+    this.intervalId = 0;
+ }
 
-// }
+ //Callback function, will be called every 1000 sec until call the StopClick
+ Chronometer.prototype.timerHandler = function () {
+     this.currentTime += 1; //seconds
+     console.log("Incrementing currentTime... new val: " + this.currentTime);
+ };
 
-// Chronometer.prototype.startClick = function () {
+ Chronometer.prototype.startClick = function () {
+     this.intervalId = setInterval(this.timerHandler.bind(this),1000);
+ };
 
-// };
+ Chronometer.prototype.setMinutes = function () {
+     return 0;
+};
 
-// Chronometer.prototype.setMinutes = function () {
+Chronometer.prototype.setSeconds = function () {
+    this.sec = sec;
+    return this.sec;
   
-// };
+};
 
-// Chronometer.prototype.setSeconds = function () {
-  
-// };
+Chronometer.prototype.twoDigitsNumber = function () {
 
-// Chronometer.prototype.twoDigitsNumber = function () {
-  
-// };
+};
 
-// Chronometer.prototype.setTime = function () {
+Chronometer.prototype.setTime = function () {
+};
 
-// };
+Chronometer.prototype.setMilliseconds = function () {
+ };
 
-// Chronometer.prototype.setMilliseconds = function () {
+//this function stops the timerHandler
+ Chronometer.prototype.stopClick = function () {
+     if (-1 != this.intervalId) {
+         clearInterval (this.intervalId);
+         this.intervalId = -1;
+     }
+ };
 
-// };
+ Chronometer.prototype.resetClick = function () {
+ };
 
-// Chronometer.prototype.stopClick = function () {
-  
-// };
-
-// Chronometer.prototype.resetClick = function () {
-
-// };
-
-// Chronometer.prototype.splitClick = function () {
-
-// };
+ Chronometer.prototype.splitClick = function () {
+ };
