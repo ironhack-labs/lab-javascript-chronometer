3e4f5da395f40a6fae456bfb8f67b44b72283073
sylvia_mavuba <smavuba@voyages-sncf.com>
2018-10-06 15:43:44 +0200

    done first iteration

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,39 +1,64 @@
-// Constructor
-// function Chronometer() {
+//Constructor
+function Chronometer() {
+    this.currentTime = 0;
+    this.intervalId = 0;
+}
 
-// }
+var Chrono = new Chronometer();
 
-// Chronometer.prototype.startClick = function () {
+Chronometer.prototype.startClick = function() {
+    this.intervalId = setInterval(function() {
+            this.currentTime += 1
+            this.setTime();
+        }.bind(this),
+        1000);
+};
 
-// };
+Chronometer.prototype.setMinutes = function() {
+    var minute = parseInt(this.currentTime / 60);
+    return minute;
+};
 
-// Chronometer.prototype.setMinutes = function () {
-  
-// };
 
-// Chronometer.prototype.setSeconds = function () {
-  
-// };
+Chronometer.prototype.setSeconds = function() {
+    var second = this.currentTime % 60;
+    return second;
+};
 
-// Chronometer.prototype.twoDigitsNumber = function () {
-  
-// };
 
-// Chronometer.prototype.setTime = function () {
+Chronometer.prototype.twoDigitsNumber = function(number) {
+    if (number < 10) {
+        return number = '0' + number;
+    } else {
+        return number += '';
+    }
+};
 
-// };
+Chronometer.prototype.setTime = function() {
+    var seconds = this.twoDigitsNumber(this.setSeconds(this.currentTime));
+    var minutes = this.twoDigitsNumber(this.setMinutes(this.currentTime));
+};
 
 // Chronometer.prototype.setMilliseconds = function () {
 
 // };
 
-// Chronometer.prototype.stopClick = function () {
-  
-// };
+Chronometer.prototype.stopClick = function() {
+    clearInterval(this.intervalId);
+};
 
-// Chronometer.prototype.resetClick = function () {
 
-// };
+Chronometer.prototype.resetClick = function() {
+    this.currentTime = 0;
+};
+
+Chrono.startClick();
+Chrono.setMinutes();
+Chrono.setSeconds();
+Chrono.twoDigitsNumber();
+Chrono.setTime();
+Chronometer.stopClick();
+Chrono.resetClick();
 
 // Chronometer.prototype.splitClick = function () {
 
