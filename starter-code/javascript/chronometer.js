10596d6ba9f2dac27d78ce4a4da6cc9583d9537d
Sandra Dupont <ssduptcom@gmail.com>
2018-10-09 19:47:12 +0200

    debut main.js

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -2,17 +2,21 @@
 function Chronometer() {
   this.currentTime = 0;
   this.intervalId = 0;
+//initialiser toutes les autres variables : this.min, this.sec...
 }
 
 Chronometer.prototype.timeHandler = function() {
   this.currentTime += 1; //sec
   //setTime method should be called on every interval of the setInterval
   this.setTime();
+  console.log(this.currentTime);
 };
 
 //bind !
+//setInterval appelle une fonction, ici timeHandler !
 Chronometer.prototype.startClick = function() {
   this.intervalId = setInterval(this.timeHandler.bind(this), 1000);
+  
 };
 
 Chronometer.prototype.setMinutes = function(currentTime) {
@@ -31,24 +35,22 @@ Chronometer.prototype.twoDigitsNumber = function(number) {
   } else return "" + number;
 };
 
-Chronometer.prototype.setTime = function (currentTime) {
-
-    var digitMin = twoDigitsNumber ( setMInutes(this.currentTime) );
-    var digitSec = twoDigitsNumber ( setSeconds(this.currentTime) );
-    return digitMin + digitSec;
-
+Chronometer.prototype.setTime = function(currentTime) {
+  var digitMin = twoDigitsNumber(setMInutes(this.currentTime));
+  var digitSec = twoDigitsNumber(setSeconds(this.currentTime));
+  return digitMin + digitSec;
 };
 
 // Chronometer.prototype.setMilliseconds = function () {
 
 // };
 
-Chronometer.prototype.stopClick = function () {
-    clearInterval(this.intervalId);
+Chronometer.prototype.stopClick = function() {
+  clearInterval(this.intervalId);
 };
 
-Chronometer.prototype.resetClick = function () {
-    this.currentTime = 0;
+Chronometer.prototype.resetClick = function() {
+  this.currentTime = 0;
 };
 
 // Chronometer.prototype.splitClick = function () {
--- starter-code/javascript/main.js
+++ starter-code/javascript/main.js
@@ -1,60 +1,87 @@
 var chronometer = new Chronometer();
-var btnLeft     = document.getElementById('btnLeft');
-var btnRight    = document.getElementById('btnRight');
-var minDec      = document.getElementById('minDec');
-var minUni      = document.getElementById('minUni');
-var secDec      = document.getElementById('secDec');
-var secUni      = document.getElementById('secUni');
-var milDec      = document.getElementById('milDec');
-var milUni      = document.getElementById('milUni');
+var btnLeft = document.getElementById("btnLeft");
+var btnRight = document.getElementById("btnRight");
+var minDec = document.getElementById("minDec");
+var minUni = document.getElementById("minUni");
+var secDec = document.getElementById("secDec");
+var secUni = document.getElementById("secUni");
+var milDec = document.getElementById("milDec");
+var milUni = document.getElementById("milUni");
 
+function printTime() {}
 
-function printTime() {
+function printMinutes() {}
 
-}
+function printSeconds() {}
 
-function printMinutes() {
+function printMilliseconds() {}
 
-}
+function printSplit() {}
 
-function printSeconds() {
+function clearSplits() {}
 
-}
+function setStopBtn() {}
 
-function printMilliseconds() {
+function setSplitBtn() {}
 
-}
+function setStartBtn() {}
 
-function printSplit() {
+function setResetBtn() {}
 
-}
-
-function clearSplits() {
-
-}
-
-function setStopBtn() {
-
-}
-
-function setSplitBtn() {
-
-}
-
-function setStartBtn() {
-
-}
-
-function setResetBtn() {
-
-}
+//var  winTimerId
 
 // Start/Stop Button
-btnLeft.addEventListener('click', function () {
+btnLeft.addEventListener("click", function() {
+  if (this.className == "btn start") {
+    btnLeft.className = "btn stop";
+    btnLeft.innerHTML = "STOP";
+    btnRight.className = "btn split";
+    btnRight.innerHTML = "SPLIT";
+    chronometer.startClick();
+// if (-1 != winTimerId) {
+//    window.clearInterval(winTimerId);
+//    winTimerId = -1;
+// }
 
+}
+  } 
+  else {
+    btnLeft.className = "btn start";
+    btnLeft.innerHTML = "START";
+    btnRight.className = "btn reset";
+    btnRight.innerHTML = "RESET";
+    chronometer.stopClick();
+//  
+  }
 });
 
 // Reset/Split Button
-btnRight.addEventListener('click', function () {
+btnRight.addEventListener("click", function() {
+
+
+
 
 });
+
+
+/* update the 4 digits (10-minute, minute, 10-second and 1-second) of the Chronometer watchface
+function updateClockDisplay() {
+
+    var minD = chronometer.minutes.substr(0,1);
+
+    var minU = chronometer.minutes.substr(1,1);
+
+    var secD = chronometer.seconds.substr(0,1);
+
+    var secU = chronometer.seconds.substr(1,1);
+
+    minDec.innerHTML = minD;
+
+    minUni.innerHTML = minU;
+
+    secDec.innerHTML = secD;
+
+    secUni.innerHTML = secU;
+
+}
+*/
