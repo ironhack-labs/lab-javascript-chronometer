67ad210e1a92f96817614aaf0b46d3a133fb29f1
Pernille Kloster <pernillekloster@Pernilles-MacBook-Pro.local>
2018-10-15 16:29:54 +0200

    done

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,4 +1,59 @@
 // Constructor
+
+class Chronometer{
+    constructor() {
+        this.currentTime = 0;
+        this.intervalId = 0;
+    }
+
+    startClick() {
+        this.intervalId = setInterval(() => {
+            console.log(this.intervalId);
+            this.currentTime++;
+            this.setTime();
+        }, 1000); 
+    }
+
+    setMinutes() {
+        return Math.floor(this.currentTime / 60)
+    }
+
+    setSeconds() {
+        return Math.floor(this.currentTime % 60)
+    }
+
+    twoDigitsNumber(value) { 
+    return (this.setMinutes() < 10 ? "0" : "") +  parseInt(value) ;
+    }
+
+
+    setTime() {
+        var seconds =  this.setSeconds()
+        var minutes =  this.setMinutes();
+        if(minutes === 0 || seconds === 0) {
+            this.twoDigitsNumber();
+        }
+    }
+
+    setMilliseconds() {
+    }
+
+    stopClick() {
+        //setTime().clear(); 
+        clearInterval;
+    }
+
+    resetClick() {
+        this.currentTime = 0;
+    }
+
+    splitClick() {
+
+    }
+
+}
+
+
 // function Chronometer() {
 
 // }
