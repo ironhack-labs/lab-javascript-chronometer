52651a5fa4de0047ebcea1f7a7caaba12c58987f
nirtzur <nirtzur89@gmail.com>
2018-10-27 16:10:09 +0200

    class

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,3 +1,52 @@
+class Chronometer {
+    constructor(){
+        this.intervalId = 0;
+        this.currentTime = 0;
+    }
+
+    startClick() {
+        this.intervalId = setInterval(function() {
+            this.currentTime += 1;
+            this.setTime();
+        }.bind(this), 1000);
+    }   
+
+    setMinutes() {
+
+        return Math.floor(this.currentTime/60);
+    }
+
+    setSeconds() {
+        return this.currentTime%60;
+    }
+
+    twoDigitsNumber(numbers) {
+        if (numbers<10){
+            return "0"+ numbers;
+        }
+        else {
+            return numbers+ "";
+        }
+    }
+
+    setTime (){
+
+        var minutes = this.twoDigitsNumber(this.setMinutes);
+        var seconds = this.twoDigitsNumber(this.setSeconds);
+    }
+
+    stopClick() {
+        return clearInterval("intervalId");
+    }
+
+    resetClick() {
+
+    }
+
+    splitClick() {
+
+    }
+} 
 // Constructor
 // function Chronometer() {
 
--- starter-code/javascript/main.js
+++ starter-code/javascript/main.js
@@ -10,7 +10,8 @@ var milUni      = document.getElementById('milUni');
 
 
 function printTime() {
-
+ document.getElementById("btnLeft").onclick = startClick();
+ 
 }
 
 function printMinutes() {
@@ -51,10 +52,27 @@ function setResetBtn() {
 
 // Start/Stop Button
 btnLeft.addEventListener('click', function () {
+if (document.getElementById('btnLeft').innerText === "START"){
+    document.getElementById('btnLeft').innerText = "STOP"
+    document.getElementById('btnLeft').className = "btn stop"
+}
+else {
+    document.getElementById('btnLeft').innerText = "START"
+    document.getElementById('btnLeft').className = "btn start"
+}
+    
 
+    //document.getElementById("btnLeft").onclick =
 });
 
 // Reset/Split Button
 btnRight.addEventListener('click', function () {
-
+    if (document.getElementById('btnRight').innerText === "RESET"){
+        document.getElementById('btnRight').innerText = "SPLIT"
+        document.getElementById('btnRight').className = "btn split"
+    }
+    else {
+        document.getElementById('btnRight').innerText = "RESET"
+        document.getElementById('btnRight').className = "btn reset"
+    }
 });
