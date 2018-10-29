8d9fb5d21e99de29314f2dab60afed9ca6b29751
David Bros <david_corp896@outlook.es>
2018-10-29 22:21:58 +0100

    Add files via upload

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,40 +1,61 @@
-// Constructor
-// function Chronometer() {
+var chronometer = {
+    hours: 0,
+    minutes: 0,
+    seconds: 0,
+    divHours: document.getElementById("hours").innerHTML,
+    divMinutes: document.getElementById("minutes").innerHTML,
+    divSeconds: document.getElementById("seconds").innerHTML,
+    //algo esta mal en la declaracion
+    start: function(){
 
-// }
+        this.seconds++;
+        this.divHours = this.hours.toString();
+        this.divMinutes = this.minutes.toString();
+        this.divSeconds = this.seconds.toString();
 
-// Chronometer.prototype.startClick = function () {
+        document.getElementById("hours").innerHTML = divHours;
+        document.getElementById("minutes").innerHTML = divMinutes;
+        document.getElementById("seconds").innerHTML = divSeconds;
 
-// };
+        if(this.seconds == 60){
+            this.seconds = 0;
+            this.minutes++;
+        }
+        else if(minutes == 60 && seconds == 60){
+            this.hours++;
+            this.seconds = 0;
+            this.minutes = 0;
+        }
+        
+    },
+     //aqui pa parar el intervalo.
+    reference: {},
 
-// Chronometer.prototype.setMinutes = function () {
-  
-// };
+    stop: function(){
+        clearInterval(chronometer.reference);
+        this.divHours.innerHTML = this.hours.toString();
+        this.divMinutes.innerHTML = this.minutes.toString();
+        this.divSeconds.innerHTML = this.seconds.toString();
+    },
 
-// Chronometer.prototype.setSeconds = function () {
-  
-// };
+    whereYouWant: function(){
 
-// Chronometer.prototype.twoDigitsNumber = function () {
-  
-// };
+        var inputHours = document.createElement("input");
+        var inputMinutes = document.createElement("input");
+        var inputSeconds = document.createElement("input");
+        
+        
+        document.body.appendChild(inputHours);
+        document.body.appendChild(inputMinutes);
+        document.body.appendChild(inputSeconds);
+        
+        this.inputHours.onChange = function(){this.divHours = this.inputHours.value;};
+        this.inputMinutes.onChange = function(){this.divMinutes = this.inputMinutes.value;};
+        this.inputSeconds.onChange = function(){this.divSeconds = this.inputSeconds.value;};
+    },
+    startC: function(){
+        chronometer.reference = setInterval(chronometer.start(), 1000);
+    }
 
-// Chronometer.prototype.setTime = function () {
+}
 
-// };
-
-// Chronometer.prototype.setMilliseconds = function () {
-
-// };
-
-// Chronometer.prototype.stopClick = function () {
-  
-// };
-
-// Chronometer.prototype.resetClick = function () {
-
-// };
-
-// Chronometer.prototype.splitClick = function () {
-
-// };
