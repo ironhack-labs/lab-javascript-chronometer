e752b6041b79f7fd0bf2f972ec76ae1902f72749
sitobusnan <sito@citytonight.es>
2018-10-29 20:24:50 +0100

    js ok

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,39 +1,56 @@
 // Constructor
-// function Chronometer() {
+function Chronometer() {
+  this.currentTime = 0;
+  this.intervalId = 0;
 
-// }
+ }
 
-// Chronometer.prototype.startClick = function () {
+Chronometer.prototype.startClick = function () {
+  var that=this;  
+  this.intervalId = setInterval(function(){
+    that.currentTime++;
+    that.setTime();
+  }, 1000);
+};
 
-// };
+Chronometer.prototype.setMinutes = function () {
+  return Math.floor(this.currentTime/60);
+};
 
-// Chronometer.prototype.setMinutes = function () {
-  
-// };
+Chronometer.prototype.setSeconds = function () {
+  if(this.currentTime>=60){
+    return parseInt(this.currentTime%60);
+  }else{
+    return this.currentTime;
+  }
+};
 
-// Chronometer.prototype.setSeconds = function () {
-  
-// };
+Chronometer.prototype.twoDigitsNumber = function (valor) {
+  if (valor < 10){
+    return ("0"+parseInt(valor)).toString();
+  }else{
+    return (parseInt(valor)).toString();
+  }
 
-// Chronometer.prototype.twoDigitsNumber = function () {
-  
-// };
 
-// Chronometer.prototype.setTime = function () {
+};
 
-// };
+Chronometer.prototype.setTime = function () {
+  var minutes = this.twoDigitsNumber (this.setMinutes());
+  var seconds = this.twoDigitsNumber(this.setSeconds());
+};
 
 // Chronometer.prototype.setMilliseconds = function () {
 
 // };
 
-// Chronometer.prototype.stopClick = function () {
-  
-// };
+Chronometer.prototype.stopClick = function () {
+  clearInterval(this.currentTime);
+};
 
-// Chronometer.prototype.resetClick = function () {
-
-// };
+Chronometer.prototype.resetClick = function () {
+  this.currentTime = 0;
+};
 
 // Chronometer.prototype.splitClick = function () {
 
