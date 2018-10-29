265e9bc256052862bf3d036225144311f37e0076
Deakristina <deakristina@gmail.com>
2018-10-29 21:03:03 +0100

    done

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,40 +1,115 @@
-// Constructor
-// function Chronometer() {
 
-// }
+let chronometer = {
+  milliSecond: 0,
+  second: 0,
+  minute: 0,
 
-// Chronometer.prototype.startClick = function () {
+  milUni: document.getElementById('milUni'),
+  milDec: document.getElementById('milDec'),
+  secUni: document.getElementById('secUni'),
+  secDec: document.getElementById('secDec'),
+  minUni: document.getElementById('minUni'),
+  minDec: document.getElementById('minDec'),
 
-// };
+  run: function() {
+    
+    if (chronometer.milliSecond === 59) {
+      chronometer.milliSecond = 0;
+      chronometer.second++;
+    } else {
+      chronometer.milliSecond++;
+    };
 
-// Chronometer.prototype.setMinutes = function () {
+    if (chronometer.second === 59) {
+      chronometer.second = 0;
+      chronometer.minute++;
+    } 
+
+    let milliSplitted = chronometer.milliSecond.toString().split('');
+    if(milliSplitted.length === 1){
+      chronometer.milUni.innerHTML = milliSplitted[0];
+      chronometer.milDec.innerHTML = 0;
+    } else if(milliSplitted.length === 2){
+      chronometer.milUni.innerHTML = milliSplitted[1];
+      chronometer.milDec.innerHTML = milliSplitted[0];
+    } 
+
+    let secondSplitted = chronometer.second.toString().split('');
+    if(secondSplitted.length === 1){
+      chronometer.secUni.innerHTML = secondSplitted[0];
+      chronometer.secDec.innerHTML = 0;
+    } else if(secondSplitted.length === 2){
+      chronometer.secUni.innerHTML = secondSplitted[1];
+      chronometer.secDec.innerHTML = secondSplitted[0];
+    } 
   
-// };
+    let minuteSplitted = chronometer.minute.toString().split('');
+    if(minuteSplitted.length === 1){
+      chronometer.minUni.innerHTML = minuteSplitted[0];
+      chronometer.minDec.innerHTML = 0;
+    } else if(minuteSplitted.length === 2){
+      chronometer.minUni.innerHTML = minuteSplitted[1];
+      chronometer.minDec.innerHTML = minuteSplitted[0];
+    }
+  },
 
-// Chronometer.prototype.setSeconds = function () {
+  refInterval: {},
   
-// };
+  start: function() {
+    let btnLeft = document.getElementById('btnLeft');
+    btnLeft.className = 'btn stop';
+    btnLeft.innerHTML = "STOP";
+    btnLeft.onclick = chronometer.stop;
 
-// Chronometer.prototype.twoDigitsNumber = function () {
-  
-// };
+    let btnRight = document.getElementById('btnRight');
+    btnRight.className = 'btn split';
+    btnRight.innerHTML = "SPLIT";
+    btnRight.onclick = chronometer.split;
 
-// Chronometer.prototype.setTime = function () {
+    chronometer.refInterval = setInterval(chronometer.run, 10);
+  },
 
-// };
+  stop: function() {
+    let btnLeft = document.getElementById('btnLeft');
+    btnLeft.className = 'btn start';
+    btnLeft.innerHTML = "START";
+    btnLeft.onclick = chronometer.start;
 
-// Chronometer.prototype.setMilliseconds = function () {
+    let btnRight = document.getElementById('btnRight');
+    btnRight.className = 'btn reset';
+    btnRight.innerHTML = "RESET";
+    btnRight.onclick = chronometer.reset;
+    clearInterval(chronometer.refInterval)
+  },
 
-// };
+  reset: function() {
+    chronometer.milliSecond = 0;
+    chronometer.second = 0;
+    chronometer.minute = 0;
+    chronometer.milUni.innerHTML = 0;
+    chronometer.milDec.innerHTML = 0;
+    chronometer.secUni.innerHTML = 0;
+    chronometer.secDec.innerHTML = 0;
+    chronometer.minUni.innerHTML = 0;
+    chronometer.minDec.innerHTML = 0;
 
-// Chronometer.prototype.stopClick = function () {
-  
-// };
+    let parentSection = document.getElementById('splits-container');
+    let childOl = document.getElementById('splits');
+    parentSection.removeChild(childOl);
+  },
 
-// Chronometer.prototype.resetClick = function () {
+  split: function() {
+    let parentOl = document.getElementById('splits');
+    let childrenLi = document.createElement('li');
+    parentOl.appendChild(childrenLi);
+    childrenLi.innerHTML = `${chronometer.minDec.innerHTML}${chronometer.minUni.innerHTML}:`;
+    childrenLi.innerHTML += `${chronometer.secDec.innerHTML}${chronometer.secUni.innerHTML}:`;
+    childrenLi.innerHTML += `${chronometer.milDec.innerHTML}${chronometer.milUni.innerHTML}`;
+  }
+}
 
-// };
 
-// Chronometer.prototype.splitClick = function () {
 
-// };
+
+
+
--- starter-code/javascript/main.js
+++ starter-code/javascript/main.js
@@ -1,4 +1,4 @@
-var chronometer = new Chronometer();
+// var chronometer = new Chronometer();
 var btnLeft     = document.getElementById('btnLeft');
 var btnRight    = document.getElementById('btnRight');
 var minDec      = document.getElementById('minDec');
