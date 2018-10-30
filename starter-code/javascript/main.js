ad2bfe6b87f01557a4b061e210edf33c2c666b61
kovitx <sllonk@gmail.com>
2018-10-30 13:17:12 +0100

    it 2 minus print out chronometer

--- starter-code/javascript/main.js
+++ starter-code/javascript/main.js
@@ -51,10 +51,30 @@ function setResetBtn() {
 
 // Start/Stop Button
 btnLeft.addEventListener('click', function () {
-  
+  if(btnLeft.classList.contains('start')) {
+    chronometer.startClick();
+    btnLeft.innerText = "STOP";
+    btnRight.innerText = "SPLIT";
+    btnLeft.classList.remove('start');
+    btnLeft.classList.add('btn');
+    btnLeft.classList.add('stop');
+    btnRight.classList.remove('reset');
+    btnRight.classList.add('btn');
+    btnRight.classList.add('split');
+  } else if (btnLeft.classList.contains('stop')) {
+    chronometer.stopClick();
+    btnLeft.innerText = "START";
+    btnRight.innerText = "RESET";
+    btnLeft.classList.remove('stop');
+    btnLeft.classList.add('btn');
+    btnLeft.classList.add('start');
+    btnRight.classList.remove('split')
+    btnRight.classList.add('btn');
+    btnRight.classList.add('reset');
+  }
 });
 
 // Reset/Split Button
-btnRight.addEventListener('click', function () {
+btnRight.addEventListener('click', function ()  {
 
 });
