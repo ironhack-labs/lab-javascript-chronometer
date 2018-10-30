f9244ef2f0523b583fb637b70dac6489ece65541
Fer Perez Ayala <ferp@protonmail.ch>
2018-10-30 07:38:58 +0100

    Iterarion 1 complete. Iteration 2 underway.

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,40 +1,93 @@
-// Constructor
-// function Chronometer() {
+ //Constructor
+ function Chronometer() {
+  this.currentTime = 0
+  this.intervalId = 0
+ }
 
-// }
+ Chronometer.prototype.startClick = function () {
+ this.intervalId = setInterval(function(){
+   this.currentTime++ 
+   console.log(this.currentTime);
+   this.setTime();
+    
+ }.bind(this) ,1000) 
 
-// Chronometer.prototype.startClick = function () {
+ };
 
-// };
 
-// Chronometer.prototype.setMinutes = function () {
-  
-// };
 
-// Chronometer.prototype.setSeconds = function () {
-  
-// };
+ Chronometer.prototype.setMinutes = function () {
+  //var count = 0;
+    // setInterval(function(){
+    //   count++;
+    //   this.currentTime = count;
+    //   console.log(parseInt(this.currentTime / 60));
+    // }.bind(this) ,1000) 
+    return parseInt(this.currentTime / 60);
+  };
 
-// Chronometer.prototype.twoDigitsNumber = function () {
-  
-// };
+ 
 
-// Chronometer.prototype.setTime = function () {
 
-// };
+ Chronometer.prototype.setSeconds = function () {
+  // var count = 0;
+  //   setInterval(function(){
+  //     count++;
+  //     this.currentTime = count;
+  //     console.log(newChrono.twoDigitsNumber(this.currentTime % 60) );
+  //   }.bind(this) ,100) 
+  return (this.currentTime % 60);
+ };
+
+ Chronometer.prototype.twoDigitsNumber = function (num) {
+
+ if (num <= 9) {
+   return "0" + num.toString();
+ } else if (num < 60) {
+   return num.toString();
+ } else {
+   return "xx";
+ }
+ };
+
+ Chronometer.prototype.setTime = function () {
+  var minutes = this.twoDigitsNumber(this.setMinutes());
+  var second = this.twoDigitsNumber(this.setSeconds());
+
+
+ };
 
 // Chronometer.prototype.setMilliseconds = function () {
 
 // };
 
-// Chronometer.prototype.stopClick = function () {
-  
-// };
+ Chronometer.prototype.stopClick = function () {
+  clearInterval(this.intervalId);
+ };
 
-// Chronometer.prototype.resetClick = function () {
-
-// };
+ Chronometer.prototype.resetClick = function () {
+  this.currentTime = 0;
+ };
 
 // Chronometer.prototype.splitClick = function () {
 
 // };
+
+
+
+// var newChrono = new Chronometer;
+
+// function btnLeftClick() {
+//   if (newChrono.intervalId === 0) {
+//      newChrono.startClick();
+//   } else {
+//     newChrono.stopClick();
+//     newChrono.intervalId = 0;
+//   }
+// }
+
+// function btnRightClick() {
+//   newChrono.resetClick();
+// }
+
+
--- starter-code/javascript/main.js
+++ starter-code/javascript/main.js
@@ -10,15 +10,18 @@ var milUni      = document.getElementById('milUni');
 
 
 function printTime() {
-
+printSeconds();
+printMinutes();
 }
 
 function printMinutes() {
-
+  minDec.innerHTML = newChrono.twoDigitsNumber(newChrono.setMinutes())[0];
+  minUni.innerHTML = newChrono.twoDigitsNumber(newChrono.setMinutes())[1];
 }
 
 function printSeconds() {
-
+secDec.innerHTML = newChrono.twoDigitsNumber(newChrono.setSeconds())[0];
+secUni.innerHTML = newChrono.twoDigitsNumber(newChrono.setSeconds())[1];
 }
 
 function printMilliseconds() {
@@ -34,27 +37,61 @@ function clearSplits() {
 }
 
 function setStopBtn() {
-
+  btnLeft.setAttribute("class", "btn stop");
 }
 
 function setSplitBtn() {
-
+  btnRight.setAttribute("class", "btn split");
 }
 
 function setStartBtn() {
+  btnLeft.setAttribute("class", "btn start");
 
 }
 
 function setResetBtn() {
+  btnRight.setAttribute("class", "btn reset");
 
 }
 
 // Start/Stop Button
 btnLeft.addEventListener('click', function () {
+  
+  if (btnLeft.getAttribute("class") === 'btn start') {
+    setStopBtn();
+    setSplitBtn();
+    newChrono.startClick();
+    printTime();
+
+ } else {
+   setStartBtn();
+   setResetBtn();
+    newChrono.stopClick();
+
+ }
 
 });
 
 // Reset/Split Button
 btnRight.addEventListener('click', function () {
-
 });
+
+var newChrono = new Chronometer;
+
+// function btnLeftClick() {
+//   if (newChrono.intervalId === 0) {
+//      newChrono.startClick();
+//   } else {
+//     newChrono.stopClick();
+//     newChrono.intervalId = 0;
+//   }
+// }
+
+// function btnRightClick() {
+//   newChrono.resetClick();
+// }
+
+
+function display() {
+
+}
