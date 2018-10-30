13dda19a07844faa2ca4553ba1f38288c3607932
danielsuidan <danielsuidan@gmail.com>
2018-10-30 09:16:11 +0100

    Done

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,40 +1,23 @@
-// Constructor
-// function Chronometer() {
-
-// }
-
-// Chronometer.prototype.startClick = function () {
-
-// };
-
-// Chronometer.prototype.setMinutes = function () {
-  
-// };
-
-// Chronometer.prototype.setSeconds = function () {
-  
-// };
-
-// Chronometer.prototype.twoDigitsNumber = function () {
-  
-// };
-
-// Chronometer.prototype.setTime = function () {
-
-// };
-
-// Chronometer.prototype.setMilliseconds = function () {
-
-// };
-
-// Chronometer.prototype.stopClick = function () {
-  
-// };
-
-// Chronometer.prototype.resetClick = function () {
-
-// };
-
-// Chronometer.prototype.splitClick = function () {
-
-// };
+function Chronometer() {
+    this.currentTime= 0 ,
+    this.intervalId = 0,Chronometer.prototype.methodStartClick = function () {
+    this.intervalId = setInterval(function (){
+     this.currentTime+=1;
+   }
+   ,1000)
+    };Chronometer.prototype.setMinutes = function()  {
+    if (Chronometer.currentTime >= 60){
+      return Chronometer.currentTime / 60;
+    } else {
+      return  0;
+    }
+   };Chronometer.prototype.setSeconds = function() {
+    if (Chronometer.currentTime >= 60) {
+      return Chronometer.currentTime = 0;
+    } else {
+      return Chronometer.currentTime;
+    }
+   };Chronometer.prototype.twoDigitsNumber = function(time) {
+   if (time < 10) {
+     return (“0” + time)
+   } else {
