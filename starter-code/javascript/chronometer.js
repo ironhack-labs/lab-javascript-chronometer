c8624980d3351605adf55f2b3502a5dac6d84452
Omar Avelar <omar.avelar.f@gmail.com>
2018-11-05 21:23:13 -0600

    Done

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,40 +1,75 @@
-// Constructor
-// function Chronometer() {
 
-// }
+  var counterUni =0
+  var counterDec = 0
+  var counterMinUni = 0
+  var counterMinDec = 0
+  var stateButton = ""
 
-// Chronometer.prototype.startClick = function () {
 
-// };
+  function myFunction() {
+//  stateButton = document.getElementById("btnLeft").innerHTML
+   // console.log(stateButton)
 
-// Chronometer.prototype.setMinutes = function () {
-  
-// };
+    
+    
+  counterUni = counterUni
+  counterDec = counterDec
+var interval = setInterval(numerito, 10)
 
-// Chronometer.prototype.setSeconds = function () {
-  
-// };
+   /* if(stateButton === "STOP"){
+      clearInterval(interval) ;
+                              }
+    else{stateButton = "STOP";}
+    console.log(stateButton)
+  document.getElementById('btnLeft').classList.remove('start')
+      document.getElementById('btnLeft').classList.add('stop')*/
 
-// Chronometer.prototype.twoDigitsNumber = function () {
-  
-// };
 
-// Chronometer.prototype.setTime = function () {
+function numerito(){
+  counterUni++
+    document.getElementById("secUni").innerHTML = counterUni
 
-// };
-
-// Chronometer.prototype.setMilliseconds = function () {
-
-// };
-
-// Chronometer.prototype.stopClick = function () {
-  
-// };
-
-// Chronometer.prototype.resetClick = function () {
-
-// };
-
-// Chronometer.prototype.splitClick = function () {
-
-// };
+    if(counterUni > 9) {
+                     counterDec++;
+        document.getElementById("secDec").innerHTML = counterDec;
+      counterUni = 0;
+      document.getElementById("secUni").innerHTML = counterUni;
+      //clearInterval(interval);
+                      
+        if(counterDec > 5) {
+                               counterMinUni++;
+        document.getElementById("minUni").innerHTML = counterMinUni;
+          
+          counterDec = 0;
+        document.getElementById("secDec").innerHTML = counterDec;
+      counterUni = 0;
+      document.getElementById("secUni").innerHTML = counterUni;
+          
+                //clearInterval(interval);
+                    }
+      //###################
+      
+      if(counterMinUni > 9) {
+                               counterMinDec++;
+        document.getElementById("minDec").innerHTML = counterMinDec;
+          
+          counterDec = 0;
+        document.getElementById("secDec").innerHTML = counterDec;
+      counterUni = 0;
+      document.getElementById("secUni").innerHTML = counterUni;
+          counterMinUni = 0;
+      document.getElementById("minUni").innerHTML = counterMinUni;
+                //clearInterval(interval);
+      if(counterMinDec > 8) {
+        clearInterval(interval);
+      }             
+      
+      }
+      
+      
+      
+      
+      
+      
+    }
+}}
