32dc9140f5eedbaaa5c934cbdde773b876ba2f71
Arturo <arturo.magana@shq.com.mx>
2018-11-10 13:45:00 -0600

    ya mero

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,39 +1,45 @@
 // Constructor
-// function Chronometer() {
+function Chronometer() {
+  this.currentTime = 0; //Cambiar
+  this.intervalId = null;
+}
 
-// }
+Chronometer.prototype.startClick = function () {
+  this.intervalId = setInterval(() => { //Lexically bind
+    this.currentTime++;
+    this.setTime();
+  }, 1000);
+};
 
-// Chronometer.prototype.startClick = function () {
+Chronometer.prototype.setMinutes = function () {
+  return Math.floor(this.currentTime / 60);
+};
 
-// };
+Chronometer.prototype.setSeconds = function () {
+  return this.currentTime % 60;
+};
 
-// Chronometer.prototype.setMinutes = function () {
-  
-// };
+Chronometer.prototype.twoDigitsNumber = function (num) {
+  return num.toString().padStart(2, '0');
+};
 
-// Chronometer.prototype.setSeconds = function () {
-  
-// };
-
-// Chronometer.prototype.twoDigitsNumber = function () {
-  
-// };
-
-// Chronometer.prototype.setTime = function () {
-
-// };
+Chronometer.prototype.setTime = function () {
+  var minutes = this.twoDigitsNumber(this.setMinutes());
+  var seconds = this.twoDigitsNumber(this.setSeconds());
+  return minutes + ":" + seconds; //Retomar
+};
 
 // Chronometer.prototype.setMilliseconds = function () {
 
 // };
 
-// Chronometer.prototype.stopClick = function () {
-  
-// };
+Chronometer.prototype.stopClick = function () {
+  clearInterval(this.intervalId);
+};
 
-// Chronometer.prototype.resetClick = function () {
-
-// };
+Chronometer.prototype.resetClick = function () {
+  this.currentTime = 0;
+};
 
 // Chronometer.prototype.splitClick = function () {
 
--- starter-code/javascript/main.js
+++ starter-code/javascript/main.js
@@ -10,14 +10,24 @@ var milUni      = document.getElementById('milUni');
 
 
 function printTime() {
-
+  //var time = chronometer.setTime(); //"00:05"
+  printMinutes();
+  printSeconds();
 }
 
 function printMinutes() {
+  var minutes = chronometer.twoDigitsNumber(chronometer.setMinutes()); //"09"
+  minUni.innerHTML = minutes[1];
+  minDec.innerHTML = minutes[0];
 
+  //Display en html
 }
 
 function printSeconds() {
+  var seconds = chronometer.twoDigitsNumber(chronometer.setSeconds()); //"25"
+  //Display en html
+  secUni.innerHTML = seconds[1];
+  secDec.innerHTML = seconds[0];
 
 }
 
@@ -51,10 +61,40 @@ function setResetBtn() {
 
 // Start/Stop Button
 btnLeft.addEventListener('click', function () {
+  //insertar que cuando le des click aparezca start en verde, y viceversa stop rojo
+  var status = btnLeft.getAttribute("class");
+  if (status === 'btn start') {
+    chronometer.startClick(); 
+    btnLeft.setAttribute("class", 'btn stop');
+    btnLeft.innerHTML = "STOP";
+    btnRight.setAttribute("class", 'btn split');
+    btnRight.innerHTML = "SPLIT";
+    interval = setInterval(() => {
+      printTime();
+    },1000);
+    // cambir  STOP, cambiar la clase y color
+  } else {
+    chronometer.stopClick();
+    btnLeft.setAttribute("class", 'btn start');
+    btnLeft.innerHTML = "START";
+    btnRight.setAttribute("class", 'btn reset');
+    btnRight.innerHTML = "RESET";
+    clearInterval(interval);
+   // cambiar a START, etc
+  }
 
 });
 
 // Reset/Split Button
-btnRight.addEventListener('click', function () {
+/* btnRight.addEventListener('click', function () {
+  var status = btnLeft.getAttribute("class");
+  if (status === 'btn stop') {
+    btnRight.setAttribute("class", 'btn split');
+    btnRight.innerHTML = "SPLIT";
+  } else {
+    btnRight.setAttribute("class", 'btn reset');
+    btnRight.innerHTML = "RESET";    
+  }
 
-});
+});*/
+
