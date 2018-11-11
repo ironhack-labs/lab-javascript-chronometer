a8c8490190d5df035be7969d2d4c0e0202836183
Moises Garcia <moisesgarcia83@gmail.com>
2018-11-11 16:51:37 +0100

    finished work

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,40 +1,97 @@
 // Constructor
-// function Chronometer() {
+function Chronometer() {
+ this.secUni = document.querySelector('#secUni');
+ this.secDec = document.querySelector('#secDec');
+ this.minUni = document.querySelector('#minUni');
+ this.minDec = document.querySelector('#minDec');
+ this.milUni = document.querySelector('#milUni');
+ this.milDec = document.querySelector('#milDec');
 
-// }
+ this.onBtn = document.querySelector(' .start');
+ this.onBtn.addEventListener('click', this.startClick.bind(this));
 
-// Chronometer.prototype.startClick = function () {
+ this.resetBtn = document.querySelector(' .reset');
+ this.resetBtn.addEventListener('click', this.resetClick.bind(this));
 
-// };
+ this.setIntervalId = undefined; 
 
-// Chronometer.prototype.setMinutes = function () {
-  
-// };
+ this.time = new Date();
+ this.time.setSeconds(0);
+ this.time.setMinutes(0);
+ this.time.setMilliseconds(0);
+}
 
-// Chronometer.prototype.setSeconds = function () {
-  
-// };
+// con startClick activamos el cronometro, si pulsamos de nuevo lo paramos
+ Chronometer.prototype.startClick = function () {
+    if (!this.setIntervalId) {
+        this.setIntervalId = setInterval(function() {
+            this.countUp();
+            this.updateDisplays();
+        }.bind(this), 10);   
+    } else{
+        this.stopClick();
+    }
+ };
 
-// Chronometer.prototype.twoDigitsNumber = function () {
-  
-// };
+ // stopClick para parar el cronómetro
+ Chronometer.prototype.stopClick = function () {
+    clearInterval(this.setIntervalId)
+    this.setIntervalId = undefined;
+ }
 
-// Chronometer.prototype.setTime = function () {
+ // con resetClick si esta en running el cronometro si pulsamos añadiremos al contador split, 
+ // si no ponemos todo a cero.
+ Chronometer.prototype.resetClick = function () {
+     if (btnRight.innerHTML === "SPLIT"){
+         this.splitClick();
+     } else {
+    this.time.setSeconds(0);
+    this.time.setMinutes(0);
+    this.time.setMilliseconds(0);
+    this.updateDisplays();
+    this.removeSplit();
+     }
+ };
 
-// };
+ //  el contador
+ Chronometer.prototype.countUp = function () {
+    this.time.setMilliseconds(this.time.getMilliseconds() + 10);
+ };
 
-// Chronometer.prototype.setMilliseconds = function () {
+ // updateDisplays actualiza el display 
+ Chronometer.prototype.updateDisplays = function () {
+    var seconds = this.time.getSeconds();
+    seconds = (seconds > 9) ? seconds + '' : '0' + seconds;
+    this.secUni.innerText = seconds[1];
+    this.secDec.innerText = seconds[0];
+    var minutes = this.time.getMinutes();
+    minutes = (minutes > 9) ? minutes + '' : '0' + minutes;
+    this.minUni.innerText = minutes[1];
+    this.minDec.innerText = minutes[0];
+    var milliseconds = this.time.getMilliseconds();
+    milliseconds = (milliseconds > 9) ? milliseconds + '' : '0' + milliseconds;
+    this.milUni.innerText = milliseconds[1];
+    this.milDec.innerText = milliseconds[0];
+ };
 
-// };
+ // splitClick añade al ol de html li's que cada vez que pulsamos split
+ Chronometer.prototype.splitClick = function () {
+    var splitTime = this.minDec.innerText + this.minUni.innerText + ":" + this.secDec.innerText + this.secUni.innerText + ":" + this.milDec.innerText + this.milUni.innerText;
+    var ol = document.getElementById("splits");
+    var li = document.createElement('li');
+    li.appendChild(document.createTextNode(splitTime));
+    ol.appendChild(li); 
+};
 
-// Chronometer.prototype.stopClick = function () {
-  
-// };
+// con removeSplit limpiamos el ol de html cuando pulsamos reset
+ Chronometer.prototype.removeSplit = function () {
+    var ol = document.getElementById("splits");
+while(ol.firstChild) ol.removeChild(ol.firstChild);
 
-// Chronometer.prototype.resetClick = function () {
+ }
+ 
+ 
 
-// };
+ 
 
-// Chronometer.prototype.splitClick = function () {
 
-// };
--- starter-code/javascript/main.js
+++ starter-code/javascript/main.js
@@ -1,60 +1,30 @@
 var chronometer = new Chronometer();
-var btnLeft     = document.getElementById('btnLeft');
-var btnRight    = document.getElementById('btnRight');
-var minDec      = document.getElementById('minDec');
-var minUni      = document.getElementById('minUni');
-var secDec      = document.getElementById('secDec');
-var secUni      = document.getElementById('secUni');
-var milDec      = document.getElementById('milDec');
-var milUni      = document.getElementById('milUni');
+var btnLeft = document.getElementById('btnLeft');
+var btnRight = document.getElementById('btnRight');
+var minDec = document.getElementById('minDec');
+var minUni = document.getElementById('minUni');
+var secDec = document.getElementById('secDec');
+var secUni = document.getElementById('secUni');
+var milDec = document.getElementById('milDec');
+var milUni = document.getElementById('milUni');
 
+//Start/Stop Button
 
-function printTime() {
-
-}
-
-function printMinutes() {
-
-}
-
-function printSeconds() {
-
-}
-
-function printMilliseconds() {
-
-}
-
-function printSplit() {
-
-}
-
-function clearSplits() {
-
-}
-
-function setStopBtn() {
-
-}
-
-function setSplitBtn() {
-
-}
-
-function setStartBtn() {
-
-}
-
-function setResetBtn() {
-
-}
-
-// Start/Stop Button
+// cuando pulsamos start se cambian los botones del cronometro a stop y split, y viceversa cuando pulsamos stop
 btnLeft.addEventListener('click', function () {
-
+    this.classList.add("stop");
+    btnRight.classList.add("split");
+     if(btnLeft.innerHTML === "START"){
+         btnLeft.innerHTML = "STOP";
+         btnRight.innerHTML = "SPLIT"; 
+     } else {
+       btnRight.classList.remove("split");
+       this.classList.remove("stop");
+       btnLeft.innerHTML = "START";
+       btnRight.innerHTML = "RESET"; 
+    }
 });
 
-// Reset/Split Button
-btnRight.addEventListener('click', function () {
 
-});
+
+
