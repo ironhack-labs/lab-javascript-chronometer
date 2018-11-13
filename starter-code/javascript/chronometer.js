6499a5edbb4d5a06310010807996822906c87276
AntonioSalazar <antonio.salazar.emrsn@gmail.com>
2018-11-13 12:42:17 -0600

    work in progress

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,27 +1,36 @@
 // Constructor
-// function Chronometer() {
+function Chronometer() {
+  this.currentTime = 0;
+  this.intervalId = 0;
+}
 
-// }
+Chronometer.prototype.startClick = function () {
+  this.intervalId = setInterval(() =>{
+    this.currentTime ++
+  }, 1000);
+  spy
 
-// Chronometer.prototype.startClick = function () {
+};
 
-// };
-
-// Chronometer.prototype.setMinutes = function () {
+Chronometer.prototype.setMinutes = function () {
   
-// };
+  this.intervalId = Math.floor(this.currentTime / 60) ;
 
-// Chronometer.prototype.setSeconds = function () {
-  
-// };
+  return this.intervalId
+};
 
-// Chronometer.prototype.twoDigitsNumber = function () {
-  
-// };
+Chronometer.prototype.setSeconds = function () {
+  this.intervalId = this.currentTime % 60;
+  return this.intervalId;
+};
 
-// Chronometer.prototype.setTime = function () {
+Chronometer.prototype.twoDigitsNumber = function (number) {
+  return ("0" + number).slice(-2)
+};
 
-// };
+ Chronometer.prototype.setTime = function () {
+
+};
 
 // Chronometer.prototype.setMilliseconds = function () {
 
