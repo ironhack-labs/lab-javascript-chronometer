1811d5d41d4db71ccfbd625fb9c16f9c6e597284
“momo@mosshi.be” <momo@mosshi.be>
2018-11-13 19:18:05 -0600

    done

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,35 +1,63 @@
-// Constructor
-// function Chronometer() {
+function Chronometer() {
+    this.currentTime = 0;
+    this.intervalId = "intervalId";
 
-// }
+ }
 
-// Chronometer.prototype.startClick = function () {
+Chronometer.prototype.startClick = function () {
+this.intervalId = setInterval(function(){
+    this.currentTime++;
+    console.log(this.currentTime);
+    this.setTime();
+}.bind(this), 1000);
 
-// };
+ };
 
-// Chronometer.prototype.setMinutes = function () {
-  
-// };
+//Otra forma de hacerlo con arrow function
+//Chronometer.prototype.startClick = function () {
+  //  this.intervalId = setInterval(()=>{
+    //    this.currentTime++;
+    //console.log(this.currentTime)
+//}, 1000);
 
-// Chronometer.prototype.setSeconds = function () {
-  
-// };
+//};
 
-// Chronometer.prototype.twoDigitsNumber = function () {
-  
-// };
 
-// Chronometer.prototype.setTime = function () {
+Chronometer.prototype.setMinutes = function () {
+  var minutes = Math.floor(this.currentTime/60);
+  return minutes;
+    console.log(this.currentTime);
 
-// };
+ };
+
+ Chronometer.prototype.setSeconds = function () {
+  var seconds = Math.floor(this.currentTime%60);
+  return seconds;
+ };
+
+ Chronometer.prototype.twoDigitsNumber = function (numbers) {
+if(numbers<10){
+    return "0" + numbers.toString();
+
+
+} else{
+    return numbers.toString();
+
+}
+
+ };
+
+Chronometer.prototype.setTime = function () {
+
+ };
 
 // Chronometer.prototype.setMilliseconds = function () {
 
 // };
 
-// Chronometer.prototype.stopClick = function () {
-  
-// };
+Chronometer.prototype.stopClick = function (clearInterval) {
+    this.intervalId=clearInterval;
+ };
 
 // Chronometer.prototype.resetClick = function () {
 
