1a380b97eb8b86b80679c5c5cf53f87ee335b4b9
Svenja Weiler <Svenja.Weiler@gmx.de>
2019-01-14 19:21:38 +0100

    not done at all

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,40 +1,62 @@
-// Constructor
-// function Chronometer() {
+class Chronometer {
+constructor(){
+  this.currentTime = 0 
+  this.intervalId = 0
+}
+startClick(){
 
-// }
-
-// Chronometer.prototype.startClick = function () {
-
-// };
-
-// Chronometer.prototype.setMinutes = function () {
+  var intervalId = setInterval(()=>{
+    this.currentTime += 1
+    this.setTime()
+  },1000)
   
-// };
+}
 
-// Chronometer.prototype.setSeconds = function () {
+setMinutes(){
+  var timeToMin = this.currentTime / 60
+  timeToMin = parseInt(timeToMin)
+  return timeToMin
+ }
+
+ setSeconds(){
+  var secLeft = this.currentTime
+  secLeft = this.currentTime % 60
+
+  return secLeft
+ }
+
+
+
+ /*
+We are almost done! We need a setTime method that will be called on every interval of the setInterval in our startClick method. 
+This function will create a minutes and seconds variable whit their values according the currentTime. 
+We should use the setMinutes, setMinutes and twoDigitsNumber methods to do this 😉
+ */
+
+twoDigitsNumber(value){
+//return value = '00'
+if(value < 10){
+  return '0' + value.toString()
+}
+else {
+return value.toString()
+}
+}
+
+setTime(){
+  //
+  var sec = this.setSeconds()
+  var min = this.setMinutes()
   
-// };
+return this.twoDigitsNumber(sec) + ' ' + this.twoDigitsNumber(min)
+}
 
-// Chronometer.prototype.twoDigitsNumber = function () {
-  
-// };
+stopClick(){
+  clearInterval(this.intervalId)
+}
 
-// Chronometer.prototype.setTime = function () {
+resetClick(){
+  this.currentTime = 0;
+}
 
-// };
-
-// Chronometer.prototype.setMilliseconds = function () {
-
-// };
-
-// Chronometer.prototype.stopClick = function () {
-  
-// };
-
-// Chronometer.prototype.resetClick = function () {
-
-// };
-
-// Chronometer.prototype.splitClick = function () {
-
-// };
+}
