1174baeb1c2504c082351a99fd9242e895970330
Zenchy <alvaro.corral.soilan@gmail.com>
2019-01-14 18:13:01 +0100

    Iteracion 2 finalizada

--- starter-code/javascript/main.js
+++ starter-code/javascript/main.js
@@ -9,16 +9,28 @@ var milDec      = document.getElementById('milDec');
 var milUni      = document.getElementById('milUni');
 
 
+var intervalId2 = 0;
 function printTime() {
+    intervalId2=setInterval(function()
+    {
+        var arr = chronometer.setTime()
 
+        printSeconds(arr[0]);
+        printMinutes(arr[1]);
+
+    },1000);
 }
 
-function printMinutes() {
+function printMinutes(tiempoM) {
+    var min = tiempoM.split("");
+    minDec.innerHTML = min[0];
+    minUni.innerHTML = min[1];
+}   
 
-}
-
-function printSeconds() {
-
+function printSeconds(tiempoS) {
+    var sec = tiempoS.split("");
+    secDec.innerHTML = sec[0];
+    secUni.innerHTML = sec[1];
 }
 
 function printMilliseconds() {
@@ -50,9 +62,37 @@ function setResetBtn() {
 }
 
 // Start/Stop Button
-btnLeft.addEventListener('click', function () {
+btnLeft.addEventListener('click', function () 
 
+{
+    
+    if (btnLeft.className==="btn start")
+    {
+        btnLeft.className="btn stop"
+        btnLeft.innerText="STOP"
+
+        btnRight.className="btn split"
+        btnRight.innerText="SPLIT"
+
+        chronometer.startClick()
+        printTime();
+    } else
+    {
+
+        btnLeft.className="btn start"
+        btnLeft.innerText="START"
+
+        
+        btnRight.className="btn reset"
+        btnRight.innerText="RESET"
+
+        chronometer.stopClick()
+        intervalId2.clearInterval();
+    }
+        
 });
+
+
 
 // Reset/Split Button
 btnRight.addEventListener('click', function () {
