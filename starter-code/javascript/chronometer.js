1587353ecce13965155766298da6be0f16402cde
Cristiana Yasuda <cristiana.yasuda@gmail.com>
2019-01-14 18:02:17 -0200

    chronometer

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,39 +1,54 @@
 // Constructor
-// function Chronometer() {
 
-// }
+ function Chronometer() {
+    this.currentTime = 0;
+    this.intervalId = 0;
+     
+ }
 
-// Chronometer.prototype.startClick = function () {
+ Chronometer.prototype.startClick = function () {
+     let self = this;
+     this.intervalId = setInterval(function() {
+        self.currentTime += 1; 
+        self.setTime();
+    }, 1000)
 
-// };
+ };
 
-// Chronometer.prototype.setMinutes = function () {
-  
-// };
+Chronometer.prototype.setMinutes = function () {
+    return Math.floor(this.currentTime/60);
+};
 
-// Chronometer.prototype.setSeconds = function () {
-  
-// };
+ Chronometer.prototype.setSeconds = function () {
+    return this.currentTime % 60;
+ };
 
-// Chronometer.prototype.twoDigitsNumber = function () {
-  
-// };
+ Chronometer.prototype.twoDigitsNumber = function (number) {
+    if (number < 10) {
+        return '0' + number;
+    } else {
+        return number.toString();
+    }
+ };
 
-// Chronometer.prototype.setTime = function () {
-
-// };
+ Chronometer.prototype.setTime = function () {
+     let minutes = this.twoDigitsNumber(this.setMinutes());
+     let seconds = this.twoDigitsNumber(this.setSeconds());
+     return [minutes, seconds];
+ };
 
 // Chronometer.prototype.setMilliseconds = function () {
 
 // };
 
-// Chronometer.prototype.stopClick = function () {
-  
-// };
+ Chronometer.prototype.stopClick = function () {
+    clearInterval(this.intervalId);
+ };
 
-// Chronometer.prototype.resetClick = function () {
-
-// };
+ Chronometer.prototype.resetClick = function () {
+    clearInterval(this.intervalId);
+   this.currentTime = 0;
+ } ;
 
 // Chronometer.prototype.splitClick = function () {
 
--- starter-code/javascript/main.js
+++ starter-code/javascript/main.js
@@ -7,6 +7,7 @@ var secDec      = document.getElementById('secDec');
 var secUni      = document.getElementById('secUni');
 var milDec      = document.getElementById('milDec');
 var milUni      = document.getElementById('milUni');
+let stopInterval;
 
 
 function printTime() {
@@ -15,10 +16,13 @@ function printTime() {
 
 function printMinutes() {
 
-}
+minDec.innerHTML = chronometer.setTime()[0][0]; 
+minUni.innerHTML = chronometer.setTime()[0][1]; 
+};
 
 function printSeconds() {
-
+    secDec.innerHTML = chronometer.setTime()[1][0]; 
+    secUni.innerHTML = chronometer.setTime()[1][1]; 
 }
 
 function printMilliseconds() {
@@ -46,15 +50,42 @@ function setStartBtn() {
 }
 
 function setResetBtn() {
-
+    btnRight.innerHTML = chronometer.setTime(); 
+    btnRight.innerHTML = chronometer.setTime(); 
 }
 
 // Start/Stop Button
 btnLeft.addEventListener('click', function () {
 
+if(this.className === 'btn start'){
+    this.className = 'btn stop';
+    this.innerHTML = 'STOP';
+    chronometer.startClick();
+    stopInterval = setInterval(function(){
+        printMinutes();
+        printSeconds();
+    }, 1000)
+} else{
+    this.className = 'btn start';
+    this.innerHTML = 'START';
+    chronometer.stopClick();
+    clearInterval(stopInterval);
+};
+
 });
 
 // Reset/Split Button
 btnRight.addEventListener('click', function () {
-
+    
+    if(this.className === 'btn reset'){
+        this.className = 'btn split';
+        this.innerHTML = 'SPLIT';
+        chronometer.startClick();
+        clearInterval()
+    } else {
+        this.className = 'btn reset';
+        this.innerHTML = 'RESET';
+        chronometer.resetClick();
+        clearInterval(stopInterval);
+    };
 });
