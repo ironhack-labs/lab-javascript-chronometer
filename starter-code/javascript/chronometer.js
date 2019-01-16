d1c2472bb8327fa3d4f3f12e0c5dbcbca61671bf
E-Drop <erosendo.developer@gmail.com>
2019-01-16 09:18:36 +0100

    functions

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,40 +1,54 @@
 // Constructor
-// function Chronometer() {
+function Chronometer() {
+     this.currentTime = 0;
+     this.intervalId = 0;
+}
 
-// }
+var c = new Chronometer();
 
-// Chronometer.prototype.startClick = function () {
+// https://coderwall.com/p/65073w/using-this-in-scope-based-settimeout-setinterval
+Chronometer.prototype.startClick = function () {
+    var _this = this;
+    this.intervalID = setInterval(function() {
+            _this.setTime();
+            return _this.currentTime++;
+    }, 1000);
+};
 
-// };
+Chronometer.prototype.setMinutes = function () {
+    console.log(this.currentTime);
+    return parseInt(this.currentTime / 60);
+};
 
-// Chronometer.prototype.setMinutes = function () {
-  
-// };
+Chronometer.prototype.setSeconds = function () {
+  return this.currentTime % 60;
+};
 
-// Chronometer.prototype.setSeconds = function () {
-  
-// };
+Chronometer.prototype.twoDigitsNumber = function (value) {
+    var my_string = '' + value;
+    if(value < 9){
+        return my_string = '0' + my_string;
+    }else{
+        return value.toString();
+    }
+};
 
-// Chronometer.prototype.twoDigitsNumber = function () {
-  
-// };
+Chronometer.prototype.setTime = function () {
 
-// Chronometer.prototype.setTime = function () {
+};
 
-// };
+Chronometer.prototype.setMilliseconds = function () {
 
-// Chronometer.prototype.setMilliseconds = function () {
+};
 
-// };
+Chronometer.prototype.stopClick = function () {
+  clearInterval(this.intervalId);
+};
 
-// Chronometer.prototype.stopClick = function () {
-  
-// };
+Chronometer.prototype.resetClick = function () {
 
-// Chronometer.prototype.resetClick = function () {
+};
 
-// };
+Chronometer.prototype.splitClick = function () {
 
-// Chronometer.prototype.splitClick = function () {
-
-// };
+};
