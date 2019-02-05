82fc240739037d1b3394835b853f48d07e221efd
Jelte Homminga <jeltehomminga@gmail.com>
2019-02-05 16:59:11 +0100

    splitprint milliseconds

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -2,6 +2,7 @@
 class Chronometer {
     constructor() {
         this.currentTime = '0';
+        this.currentMilliSeconds = '0';
         this.intervalId = "";
         this.seconds = '0';
         this.minutes = '0';
@@ -25,7 +26,7 @@ class Chronometer {
     }
     startMilliSeconds(){
         this.milliIntervalRef = setInterval(()=>{
-        this.milliSeconds ++;   
+        this.currentMilliSeconds ++;   
         this.setMilliSeconds();
         },10)    
     }
@@ -37,8 +38,8 @@ class Chronometer {
         clearInterval(this.milliIntervalRef);
     }
     resetClick(){
-        this.currentTime = '0';
-        this.milliSeconds = '0';
+        this.currentTime = 0;
+        this.milliSeconds = 0;
         this.setMilliSeconds();
         this.setSeconds();
         this.setMinutes();
@@ -51,6 +52,7 @@ class Chronometer {
         minDec.innerHTML = Math.floor(this.minutes / 10) % 10;
     }
     setMilliSeconds(){
+        this.milliSeconds = this.currentMilliSeconds % 100;
         milUni.innerHTML = this.milliSeconds % 10;
         milDec.innerHTML = Math.floor(this.milliSeconds / 10) % 10;
     } 
@@ -61,7 +63,7 @@ class Chronometer {
         this.currentTime !==0 && this.currentTime % 60 === 0 ? this.minutes++ : "";
     }
     printSplit(){
-        this.splitTime = `${this.minutes > 9 ? this.minutes : '0' + this.minutes }:${this.seconds > 9 ? this.seconds : '0' + this.seconds}`
+        this.splitTime = `${this.minutes > 9 ? this.minutes : '0' + this.minutes }:${this.seconds > 9 ? this.seconds : '0' + this.seconds}:${this.milliSeconds > 9 ? this.milliSeconds : '0' + this.milliSeconds}`
         splits.innerHTML += `<li>${this.splitTime}</li>`      
     }
 }
