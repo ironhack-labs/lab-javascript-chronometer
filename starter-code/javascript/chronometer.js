a57e4fdcf4127446fdb40e6d04abe24c7bcd85ea
rpernicone <roberto.pernicone@gmail.com>
2019-02-04 21:03:07 -0600

    faltó split y miliseconds

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,40 +1,55 @@
-// Constructor
-// function Chronometer() {
+ //Constructor
+ function Chronometer() {
+    this.currentTime = 0;
+    this.intervalId = 0;
+    this.ms = 0;
+ }
+ Chronometer.prototype.startClick = function () {
+    this.intervalId = setInterval( () => {  
+        this.currentTime ++;
+        this.setTime();
+        printTime();
+        this.ms ++;
+        }, 1000);
+        // con un intervalo de 10 y otra arrow function dentro de startClick funciona bien pero no logré hacer los cálculos 
+ };
 
-// }
+ Chronometer.prototype.setMinutes = function () {
+    var m  = Math.floor(this.currentTime/60);
+    return m;
+ };
 
-// Chronometer.prototype.startClick = function () {
+ Chronometer.prototype.setSeconds = function () {
+    let s = this.currentTime % 60;
+    return s;
+ };
 
-// };
+ Chronometer.prototype.twoDigitsNumber = function (value) {
+    if(value < 10){
+        return "0" + value 
+    } 
+    return value.toString()
+ };
 
-// Chronometer.prototype.setMinutes = function () {
-  
-// };
+ Chronometer.prototype.setTime = function () {
+    this.minutes = this.twoDigitsNumber(this.setMinutes());
+    this.seconds = this.twoDigitsNumber(this.setSeconds());
+    this.milliseconds = this.twoDigitsNumber(this.setMilliseconds());
+ };
 
-// Chronometer.prototype.setSeconds = function () {
-  
-// };
+ Chronometer.prototype.setMilliseconds = function () {
+    let ms = (this.currentTime/60)/60;
+    return ms;
+ };
 
-// Chronometer.prototype.twoDigitsNumber = function () {
-  
-// };
+ Chronometer.prototype.stopClick = function () {
+    clearInterval(this.intervalId);
+ };
 
-// Chronometer.prototype.setTime = function () {
+ Chronometer.prototype.resetClick = function () {
+    this.currentTime = 0;
+ };
 
-// };
+ Chronometer.prototype.splitClick = function () {
 
-// Chronometer.prototype.setMilliseconds = function () {
-
-// };
-
-// Chronometer.prototype.stopClick = function () {
-  
-// };
-
-// Chronometer.prototype.resetClick = function () {
-
-// };
-
-// Chronometer.prototype.splitClick = function () {
-
-// };
+ };
--- starter-code/javascript/main.js
+++ starter-code/javascript/main.js
@@ -10,19 +10,24 @@ var milUni      = document.getElementById('milUni');
 
 
 function printTime() {
-
+    printMinutes();
+    printSeconds();
+    printMilliseconds();
 }
 
 function printMinutes() {
-
+    minDec.innerHTML = chronometer.minutes[0];
+    minUni.innerHTML = chronometer.minutes[1];
 }
 
 function printSeconds() {
-
+    secDec.innerHTML = chronometer.seconds[0];
+    secUni.innerHTML = chronometer.seconds[1];
 }
 
 function printMilliseconds() {
-
+    milDec.innerHTML = chronometer.milliseconds[0];
+    milUni.innerHTML = chronometer.milliseconds[1];
 }
 
 function printSplit() {
@@ -34,27 +39,49 @@ function clearSplits() {
 }
 
 function setStopBtn() {
-
+    btnLeft.className = "btn stop";
+    btnLeft.innerHTML = "Stop";
 }
 
 function setSplitBtn() {
-
+    btnRight.className = "btn split";
+    btnRight.innerHTML = "Split"
 }
 
 function setStartBtn() {
-
+ btnLeft.className = "btn start";
+ btnLeft.innerHTML = "Start"
 }
 
 function setResetBtn() {
-
+btnRight.className = "btn reset";
+btnRight.innerHTML = "Reset"
 }
 
 // Start/Stop Button
 btnLeft.addEventListener('click', function () {
-
+    if(btnLeft.className === "btn start"){
+        chronometer.startClick()
+        setStopBtn();
+        setSplitBtn();
+    }else {
+        chronometer.stopClick();
+        setStartBtn();
+        setResetBtn();
+    }
 });
 
 // Reset/Split Button
 btnRight.addEventListener('click', function () {
-
+    if(btnRight.className === "btn reset"){
+        chronometer.resetClick();
+        minDec.innerHTML = "0";
+        minUni.innerHTML = "0";
+        secDec.innerHTML = "0";
+        secUni.innerHTML = "0";
+        milDec.innerHTML = "0";
+        milUni.innerHTML = "0";
+    }else{
+        console.log("aqui va el split")
+    }
 });
