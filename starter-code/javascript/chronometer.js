9176ef4679d6cf948493a871f9fd55c46eac627d
Ivonne Yanez Mendoza <ivonne.wftm@gmail.com>
2019-02-10 22:44:28 +0100

    chronometer

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,40 +1,60 @@
 // Constructor
-// function Chronometer() {
-
-// }
-
-// Chronometer.prototype.startClick = function () {
-
-// };
-
-// Chronometer.prototype.setMinutes = function () {
+function Chronometer() {
+    this.currentTime = 0;
+    this.intervalId = 0;
+  }
   
-// };
+  Chronometer.prototype.startClick = function () {
+      this.intervalId = setInterval(() => {
+      this.currentTime+=1;
+        this.setTime();
+    }, 1000);
 
-// Chronometer.prototype.setSeconds = function () {
+  };
   
-// };
-
-// Chronometer.prototype.twoDigitsNumber = function () {
+  Chronometer.prototype.setMinutes = function () {
+    var minutes = this.currentTime/60;
+    return minutes;
+    
+  };
   
-// };
-
-// Chronometer.prototype.setTime = function () {
-
-// };
-
-// Chronometer.prototype.setMilliseconds = function () {
-
-// };
-
-// Chronometer.prototype.stopClick = function () {
+  Chronometer.prototype.setSeconds = function () {
+    var seconds = this.currentTime % 60;
+    return seconds
+  };
   
-// };
+  Chronometer.prototype.twoDigitsNumber = function (num) {
+    if (num < 10){
+     num = ("0" + num);
+    }
+    return num.toString();
+  
+  };
+  
+  Chronometer.prototype.setTime = function () {
+    this.minutes = this.twoDigitsNumber(this.setMinutes());
+    this.seconds = this.twoDigitsNumber(this.setSeconds());
+    
+  };
+  
+//   Chronometer.prototype.setMilliseconds = function () {
+    
+//   };
+  
+  Chronometer.prototype.stopClick = function () {
+    clearInterval(this.intervalId);
+    clearInterval(this.currentTime);
+    
+  };
+  
+  Chronometer.prototype.resetClick = function () {
+    this.intervalId = 0;
+    this.currentTime = 0;
+    this.setTime();
+    
+  };
+  
 
-// Chronometer.prototype.resetClick = function () {
 
-// };
 
-// Chronometer.prototype.splitClick = function () {
 
-// };
--- starter-code/javascript/main.js
+++ starter-code/javascript/main.js
@@ -10,51 +10,85 @@ var milUni      = document.getElementById('milUni');
 
 
 function printTime() {
-
+  setInterval(function () {
+    printSeconds();
+    printMinutes();
+}, 1000);
 }
 
 function printMinutes() {
-
+  minDec.innerText = chronometer.minutes[0]; 
+  minUni.innerText = chronometer.minutes[1]; 
 }
 
 function printSeconds() {
-
+  secDec.innerText = chronometer.seconds[0]; 
+  secUni.innerText = chronometer.seconds[1];
 }
 
-function printMilliseconds() {
-
-}
 
 function printSplit() {
-
+  var slist = document.createElement("li");
+  slist.innerText = (chronometer.minutes + ':' + chronometer.seconds);
+  var parameter = document.getElementById("splits");
+  parameter.appendChild(slist);
 }
 
 function clearSplits() {
-
+  var par = document.getElementById('splits');
+  par.innerText = "";
 }
 
 function setStopBtn() {
-
+  chronometer.stopClick();
 }
 
 function setSplitBtn() {
-
+  printSplit();
 }
 
 function setStartBtn() {
-
+  chronometer.startClick();
+  printTime();
 }
 
 function setResetBtn() {
-
+  chronometer.resetClick();
+  clearSplits();
 }
 
 // Start/Stop Button
-btnLeft.addEventListener('click', function () {
+btnLeft.addEventListener("click", function () {
+
+
+  if(this.className === "btn start") {
+    this.className = "btn stop";
+    this.innerHTML = "STOP";
+    btnRight.className = "btn split";
+    btnRight.innerHTML = "SPLIT";
+    setStartBtn();
+} else if(this.className === "btn stop") {
+    this.className = "btn start";
+    this.innerHTML = "START";
+    btnRight.className = "btn reset";
+    btnRight.innerHTML = "RESET";
+    setStopBtn();
+
+ }
+
+
+
+
 
 });
 
 // Reset/Split Button
-btnRight.addEventListener('click', function () {
-
+btnRight.addEventListener("click", function() {
+    if(this.className === "btn reset") {
+        setResetBtn();
+        printTime();
+        clearSplits();
+    } else if(this.className === "btn split") {
+        setSplitBtn();
+    }
 });
