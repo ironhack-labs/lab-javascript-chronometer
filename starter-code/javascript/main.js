c8c4d8ae1b3f265f13d8e7a03f6e11a8013fb789
Antonio Méndez <antonio.mendez.martin89@gmail.com>
2019-02-10 16:40:51 +0100

    changing buttons name

--- starter-code/javascript/main.js
+++ starter-code/javascript/main.js
@@ -9,7 +9,34 @@ var milDec      = document.getElementById('milDec');
 var milUni      = document.getElementById('milUni');
 
 
+//change start name to stop and reset to split
+//document.getElementById("btnLeft").innerHTML = "STOP"
+
+//document.getElementById("btnRight").innerHTML = "SPLIT"
+
+
+/* Trying to change the button's name when it is clicked
+function change() 
+{
+    var elem = document.getElementById("btnLeft)");
+    if (elem.value=="START") elem.value = "STOP";
+    else elem.value = "START";
+}
+
+function toggleText(btnLeft) 
+{
+   if (document.getElementById('btnLeft').textContent == 'START') 
+   {
+       document.getElementById('btnLeft').textContent = 'STOP';
+   }
+   else 
+   {
+     document.getElementById('btnLeft').textContent = 'START';
+   }
+}*/
+
 function printTime() {
+   
 
 }
 
