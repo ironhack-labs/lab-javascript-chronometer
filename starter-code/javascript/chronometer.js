cc605a2ff34b4adc64d2ed970ed7cdec984aa9ae
Jelena Jukic <jelena.dj.pg@gmail.com>
2019-02-11 13:55:12 +0100

    version with ms

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,72 +1,75 @@
 // Constructor
 function Chronometer() {
   this.currentTime = 0;
-  this.intervalId
+  this.intervalId;  // we will use it to stop chronometer. 
 }
 
 
 
 Chronometer.prototype.startClick = function () {
   _this = this;
-  this.intervalId = setInterval(function () {
+  this.intervalId = setInterval(function () {  // set interval will add 1 to currentTime every 10 ms. After 100 times of setInterval execution we will have 
+    //current time equal to 100 = 1s; 
     _this.currentTime++;
     _this.setTime();
-
-  }, 1000);
-
+  }, 10);
 };
 
-
+//how we count minutes. We divide current time with 100 to get seconds and then with 60 to get minutes. we need to round the result. 
 Chronometer.prototype.setMinutes = function () {
-  var myMinutes = this.currentTime / 60;
+  var myMinutes = (this.currentTime / 100) / 60;
   return Math.floor(myMinutes);
-
 };
 
 
-
+// currentTime/100 = number of seconds = > numberOfSeconds/60 => number of minutes => rest after division with 60 are seconds.
 Chronometer.prototype.setSeconds = function () {
-  var mySeconds = this.currentTime % 60;
-
-  return mySeconds;
-
+  var mySeconds = (this.currentTime / 100) % 60;
+  return Math.floor(mySeconds);
 };
 
+
+// currentTime/100 is equal to seconds. Rest after division with 100 is = to milliseconds.
+Chronometer.prototype.setMilliseconds = function () {
+  return ((this.currentTime) % 100);
+};
+
+//this returns two digit representation of minutes and seconds.
 Chronometer.prototype.twoDigitsNumber = function (timeToTwoDigit) {
   var timeToTwoDigitString = timeToTwoDigit.toString();
   if (timeToTwoDigitString.length < 2) {
     timeToTwoDigitString = 0 + timeToTwoDigitString;
-
   }
-
   return timeToTwoDigitString;
 };
 
-Chronometer.prototype.setTime = function () {
 
+//it will check time after every 10 ms and print it.
+Chronometer.prototype.setTime = function () {
   seconds = this.twoDigitsNumber(this.setSeconds());
   minutes = this.twoDigitsNumber(this.setMinutes());
-  // milliSeconds = this.twoDigitsNumber(this.setMilliseconds());
-  console.log(minutes, seconds);
-  // console.log(minutes, seconds, milliSeconds);
-  document.getElementById("minUni").innerHTML = minutes.charAt(1);
+  milliSeconds = this.setMilliseconds();
+  console.log(minutes, seconds, milliSeconds); //this line is for my checkup.
+
+  // change HTML on proper place with proper value. minutes and seconds are strings, because of that I use charAt function.
+  document.getElementById("minUni").innerHTML = minutes.charAt(1); 
   document.getElementById("minDec").innerHTML = minutes.charAt(0);
   document.getElementById("secDec").innerHTML = seconds.charAt(0);
   document.getElementById("secUni").innerHTML = seconds.charAt(1);
-  // document.getElementById("milDec").innerHTML = milliSeconds.charAt(0);
-  // document.getElementById("milUni").innerHTML = milliSeconds.charAt(1);
+  document.getElementById("milDec").innerHTML = milliSeconds; //milliSeconds are number.
 
 };
 
-Chronometer.prototype.setMilliseconds = function () {
-  var myMilliseconds = this.currentTime/1000;
-  return (myMilliseconds);
-};
+//----------------------
+// it will stop current setInterval iteration. We use clearInterval function which uses setInterval Id as parameter. setInterval function returns its Id.
+//because f that we used intervalId property in constructor.
 
 Chronometer.prototype.stopClick = function () {
   clearInterval(this.intervalId);
 };
 
+
+// it will set current time on 0 and print 00:00:00 on screen;
 Chronometer.prototype.resetClick = function () {
   this.currentTime = 0;
   seconds = this.twoDigitsNumber(this.setSeconds());
@@ -75,26 +78,19 @@ Chronometer.prototype.resetClick = function () {
   document.getElementById("minDec").innerHTML = minutes.charAt(0);
   document.getElementById("secDec").innerHTML = seconds.charAt(0);
   document.getElementById("secUni").innerHTML = seconds.charAt(1);
-
   document.getElementById("splits").innerHTML = "";
-
-
 };
 
-Chronometer.prototype.splitClick = function () {
 
+//this function just prints current time on left side screen and doesn't stop time flow.
+
+Chronometer.prototype.splitClick = function () {
   seconds = this.twoDigitsNumber(this.setSeconds());
   minutes = this.twoDigitsNumber(this.setMinutes());
-
+  milliSeconds = this.setMilliseconds();
   var liSplitsElement = document.createElement("li"); //create li in orderd list
-
   document.getElementById("splits").appendChild(liSplitsElement); //add created li element to ordered list with id = "splits"
-
-  var liText = document.createTextNode(`${minutes}:${seconds}`); //create text element. Text content is equal to current time.
+  var liText = document.createTextNode(`${minutes}:${seconds}:${milliSeconds}`); //create text element. Text content is equal to current time.
   liSplitsElement.appendChild(liText); //add text to li element
-
-
-
-
 
 };
--- starter-code/javascript/main.js
+++ starter-code/javascript/main.js
@@ -14,12 +14,11 @@ function printTime() {
 }
 
 function printMinutes() {
-  var minutes = chronometer.twoDigitsNumber(chronometer.setMinutes());
-  document.getElementById("minDec").innerHTML = minutes;
+
 }
 
 function printSeconds() {
-  var time = chronometer.setTime();
+
 
 }
 
@@ -50,7 +49,6 @@ function setStartBtn() {
 
   chronometer.startClick();
 
-
 }
 
 function setResetBtn() {
@@ -58,14 +56,21 @@ function setResetBtn() {
 }
 
 
+/*When we start application we should see on screen STOP and SPLIT button. While
+chronometer is running we can split time in every moment. When we stop chronometer we should see START button on the left and RESET button on the right.
+If we press again START, reset should become SPLIT. If we pres reset => call resetClick() => set everything on 00:00:00 and clean split result on
+the left side of screen.
 
-var turnStopStart = 0;
+*/
+
+var turnStopStart = 0;  //with this variable check appearance of buttons on screen.
 var turnResetSplit = 0;
 
+
+//listen for click event on btnLeft. If turnStopStart = 0 => we should start chronometer and change btnLeft to STOP, and btnRight to SPLIT.
+//turnStopStart increase to 1. => next time when we click the same button code in else statement will be executed.
+
 btnLeft.addEventListener('click', function () {
-
-
-
 
   if (turnStopStart == 0) {
     chronometer.startClick();
@@ -74,7 +79,7 @@ btnLeft.addEventListener('click', function () {
     document.getElementById("btnRight").innerHTML = "SPLIT";
     document.getElementById("btnRight").setAttribute("class", "btn split");
     turnStopStart++;
-    
+
   } else {
     chronometer.stopClick();
     document.getElementById("btnLeft").innerHTML = "START";
@@ -83,7 +88,7 @@ btnLeft.addEventListener('click', function () {
     document.getElementById("btnRight").setAttribute("class", "btn reset");
 
     turnStopStart--;
-    // turnResetSplit = 0;
+    
   }
 
 });
@@ -91,19 +96,20 @@ btnLeft.addEventListener('click', function () {
 
 btnRight.addEventListener('click', function () {
 
+
+  //as long as turnStopStart = 1 => chronometer is running. and we can split our time. If that is not a case => our apllication is stopped
+  // and we should see START button on teh left and RESET button on the right side of screen. As long as we don't press RESET button, we can 
+  //start chronometer again. In the case that we press RESET =>  time is on 00:00:00, turnStopStart = 0 and we can start chronometer again.
+  
   if (turnStopStart == 1) {
 
     chronometer.splitClick();
-    // document.getElementById("btnRight").innerHTML = "SPLIT";
-    // document.getElementById("btnRight").setAttribute("class", "btn split");
-    // turnResetSplit--;
     
+
   } else {
     chronometer.resetClick();
-    // document.getElementById("btnRight").innerHTML = "RESET";
-    // document.getElementById("btnRight").setAttribute("class", "btn reset");
-    // turnResetSplit++;
-    
+  
+
   }
 
 });
