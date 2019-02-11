2b8ffdb1fa6f74ecbc433a2adae4c76b299a9ccf
RedBalloon24 <worldtraveler757@gmail.com>
2019-02-11 18:54:45 +0100

    iteration 1

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,27 +1,39 @@
 // Constructor
-// function Chronometer() {
+ function Chronometer() {
+  this.currentTime = 0
+  this.intervalId = null
+ }
 
-// }
+ Chronometer.prototype.startClick = function () {
+    this.intervalId = setInterval(function () {
+  console.log(currentTime);
 
-// Chronometer.prototype.startClick = function () {
+  this.currentTime += 1;
+  this.setTime();
+  }, 1000)
+};
 
-// };
+ Chronometer.prototype.setMinutes = function () {
+  return Math.floor(this.currentTime / 60);
+ };
 
-// Chronometer.prototype.setMinutes = function () {
-  
-// };
+ Chronometer.prototype.setSeconds = function () {
+  return Math.floor(this.currentTime % 60)
+ };
 
-// Chronometer.prototype.setSeconds = function () {
-  
-// };
+ Chronometer.prototype.twoDigitsNumber = function () {
+  if (number < 10) {
+    return "0" + number.toString();
+  } else {
+      return number.toString();
+  }
 
-// Chronometer.prototype.twoDigitsNumber = function () {
-  
-// };
+ };
 
-// Chronometer.prototype.setTime = function () {
-
-// };
+Chronometer.prototype.setTime = function () {
+  var minutes = this.twoDigitsNumber(this.setMinutes());
+  var seconds = this.twoDigitsNumber(this.setSeconds());
+ };
 
 // Chronometer.prototype.setMilliseconds = function () {
 
