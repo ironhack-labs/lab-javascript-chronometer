f3153de93fa732587e824c08c23ffeb4dd07d272
Cristina Blanco <cristinablanco@MacBook-Pro-de-Cristina.local>
2019-02-12 09:40:05 +0100

    done

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,40 +1,54 @@
 // Constructor
-// function Chronometer() {
+ function Chronometer() {
+  this.currentTime = 0
+  this.intervalId = 0 //propiedad que se sobreescribe al darle a startclick 
+ }
 
-// }
+Chronometer.prototype.startClick = function () { //that solo se usa aqui para que no se pierda el scope
+  var that = this
+  this.intervalId = setInterval(function(){
+    that.currentTime++
+    
+    printTime(that.setTime()) //that.setTime devuelve un paquete que es un array con el calculo hecho de mins y segs
 
-// Chronometer.prototype.startClick = function () {
+  }, 1000)
 
-// };
+};
 
-// Chronometer.prototype.setMinutes = function () {
-  
-// };
+Chronometer.prototype.setMinutes = function () {
+  return Math.floor(this.currentTime/60) //hasta q no llegue a 60 no me va a devolver otra cosa que no sea 0
+};
 
-// Chronometer.prototype.setSeconds = function () {
-  
-// };
+Chronometer.prototype.setSeconds = function () {
+  return this.currentTime%60 //quiero que me devuelva la parte que sobra de los segundos acumulados (cada 60)
+};
 
-// Chronometer.prototype.twoDigitsNumber = function () {
-  
-// };
+Chronometer.prototype.twoDigitsNumber = function (num) { //esta funcion no forma parte de lo anterior, solo le pedimos que nos devuelva dos numeros cuando solo hay 1
+  if (num < 10) {
+    return "0" + num
+  } else {
+    return "" + num //string vacio para que siempre lo devuelva de la misma manera
+  }
+};
 
-// Chronometer.prototype.setTime = function () {
-
-// };
+Chronometer.prototype.setTime = function () {
+  var minutes = this.twoDigitsNumber(this.setMinutes()) //llamo a la funcion setminutes y setseconds que me va a ir acumulando en las var los numeros de 2 digitos
+  var seconds = this.twoDigitsNumber(this.setSeconds()) //ahora mismo solo esta acumulando aqui. //le pongo los parentesis xq lo que pido es el return // this. SON MÉTODOS DEL CRONÓMETRO
+      return [minutes,seconds] //para que me devuelva un array  con mins y segs                          
+};                                                      
 
 // Chronometer.prototype.setMilliseconds = function () {
 
 // };
 
-// Chronometer.prototype.stopClick = function () {
-  
-// };
+Chronometer.prototype.stopClick = function () {
+  clearInterval(this.intervalId)
+};
 
-// Chronometer.prototype.resetClick = function () {
+Chronometer.prototype.resetClick = function () { //este reseteo no pasa hasta que no lo llamo desde otro lado
+  this.currentTime = 0                           //pongo el cronometro a 0 en la lógica interna del cronómetro ( no visualmente)
+};
 
-// };
-
-// Chronometer.prototype.splitClick = function () {
-
-// };
+Chronometer.prototype.splitClick = function () {
+  return this.setTime() // esto ya me estaba devolviendo un array como lo queremos [mins, segs], establecido en la funcion setTime
+};
--- starter-code/javascript/main.js
+++ starter-code/javascript/main.js
@@ -1,5 +1,5 @@
 var chronometer = new Chronometer();
-var btnLeft     = document.getElementById('btnLeft');
+var btnLeft     = document.getElementById('btnLeft'); //los botones ya estan cacheados en estas var
 var btnRight    = document.getElementById('btnRight');
 var minDec      = document.getElementById('minDec');
 var minUni      = document.getElementById('minUni');
@@ -8,9 +8,15 @@ var secUni      = document.getElementById('secUni');
 var milDec      = document.getElementById('milDec');
 var milUni      = document.getElementById('milUni');
 
+var splitList = document.getElementById("splits") //en esta var voy a guardar los splits impresos
 
-function printTime() {
-
+function printTime(arrayFromSetTime) { //queremos llamar a setTime
+  var min = arrayFromSetTime[0] //el indice 0 del array que trae setTime 
+  var sec = arrayFromSetTime[1] //el indice 1 del array que trae setTime 
+  minDec.innerHTML = min[0] // este string se queda con el primer elemento en las decenas de los minutos
+  minUni.innerHTML = min[1] // este string se queda con el segundo elemento en las unidades de los minutos
+  secDec.innerHTML = sec[0] // este string se queda con el primer elemento en las decenas de los segundos
+  secUni.innerHTML = sec[1] // este string se queda con el segundo elemento en las unidades de los segundos 
 }
 
 function printMinutes() {
@@ -25,12 +31,16 @@ function printMilliseconds() {
 
 }
 
-function printSplit() {
+function printSplit(text) {     //vamos a ir creando <li> y meterlos en el HTML
+  var li = document.createElement("li")
+  li.innerHTML = text          // a la lista li le imprimo el texto pasado como parámetro
+  splitList.appendChild(li)     // meto el texto en los <li> de la <ol>
+
 
 }
 
-function clearSplits() {
-
+function clearSplits() { //BORRAR TODOS LOS SPLITS, TODO SE PONE A 0 Y LOS SPLITS SE BORRAN
+  splitList.innerHTML = "" // vacío los splits
 }
 
 function setStopBtn() {
@@ -45,16 +55,47 @@ function setStartBtn() {
 
 }
 
-function setResetBtn() {
+function setResetBtn() { //reset va a estar activo cuando el cronometro esté en STOP
+
 
 }
 
 // Start/Stop Button
 btnLeft.addEventListener('click', function () {
+  if(btnLeft.className.includes("start")){
+    btnLeft.className = "btn stop"      //no necesito getElementById/className porque ya estan cacheados arriba
+    btnLeft.innerText = "STOP"
+    btnRight.className = "btn split"
+    btnRight.innerText = "SPLIT"
+   
+
+    chronometer.startClick()            // aparte de cambiar todo, quiero que empiece a funcionar
+  } else {
+    btnLeft.className = "btn start"     //cuando el caso sea al revés, que lo invierta 
+    btnLeft.innerText = "START"
+    btnRight.className = "btn reset"
+    btnRight.innerText = "RESET"
+
+    chronometer.stopClick() // y aqui que pare
+  }
 
 });
 
 // Reset/Split Button
-btnRight.addEventListener('click', function () {
+btnRight.addEventListener('click', function () { //FUNCIONES DEL SPLIT/RESET <-- SPLIT
+  if(btnLeft.className.includes("stop")){       // quiero hacer split cuando esté el cronometro corriendo, osea en cuando en el boton aparece STOP
+    var arrayMinSec = chronometer.splitClick()  // recojo el array que me hace splitClick en chronometer.js
+    var timeFormated = arrayMinSec[0] + ":" + arrayMinSec[1] //le asigno a esta var el array indice 0 + indice 1 
+    printSplit(timeFormated)                    // llamamos a printSplit cada vez que clickamos en SPLIT, y está metiendo timeFormated en los <li>
+
+} else {                            //FUNCIONES DEL SPLIT/RESET <-- RESET
+
+  clearSplits()                     // al hacer click en RESET, llamo a clearSplits y vacío todos los splits
+  chronometer.resetClick()          // llamo a la función del cronómetro que pone a 0 la lógica interna, 
+  printTime(["00", "00"])           // hasta que no le digo que visualmente me imprima 00:00, se resetea internamente pero no imprime por pantalla el 00:00. 
+
+}
 
 });
+
+
