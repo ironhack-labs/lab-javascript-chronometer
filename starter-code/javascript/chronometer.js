a2b392a3e110146c93de1bfde9b7f1335cadde5b
Fran Enriquez <fjenriglez@gmail.com>
2019-02-15 18:30:34 +0100

    Inicio del chronometer pasando - Definiendo setTime()

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,27 +1,37 @@
 // Constructor
 function Chronometer() {
-
+  this.currentTime = 0;
+  this.intervalId = 0;
+  this.minutes = 0;
 }
 
-// Chronometer.prototype.startClick = function () {
+Chronometer.prototype.setTime = function () {
+  var minutes = this.twoDigitsNumber(this.setMinutes)
+};
 
-// };
+Chronometer.prototype.startClick = function () {
+  this.currentTime += 1;
+  setInterval(setTime, 1000);
+};
 
-// Chronometer.prototype.setMinutes = function () {
+Chronometer.prototype.setMinutes = function () {    
   
-// };
+  return parseInt(this.currentTime / 60);
 
-// Chronometer.prototype.setSeconds = function () {
-  
-// };
+ };
 
-// Chronometer.prototype.twoDigitsNumber = function () {
-  
-// };
+Chronometer.prototype.setSeconds = function () {
+  return this.currentTime % 60;
+};
 
-// Chronometer.prototype.setTime = function () {
+Chronometer.prototype.twoDigitsNumber = function (num) {
 
-// };
+  var myNum = ''+ num;
+  if ( num < 10 ){
+    myNum = '0' + myNum;
+  }
+  return myNum;
+};
 
 // Chronometer.prototype.setMilliseconds = function () {
 
