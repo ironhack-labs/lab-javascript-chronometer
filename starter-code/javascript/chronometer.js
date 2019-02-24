b0988885b3ca18e77d8a977be3edb7a7ed50505d
wilkor <wilkorguitarrista@msn.com>
2019-02-24 00:14:58 -0300

    exercicio está feito mas, não entendi muito bem em como implementar utilizar as regras que estão no jasmine

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,40 +1,163 @@
-// Constructor
-// function Chronometer() {
+//Constructor
+function Chronometer() {
 
-// }
+}
 
-// Chronometer.prototype.startClick = function () {
 
-// };
 
-// Chronometer.prototype.setMinutes = function () {
+var minDec = document.getElementById("minDec");
+var minUni = document.getElementById("minUni");
+var secDec = document.getElementById("secDec");
+var secUni = document.getElementById("secUni");
+var milDec = document.getElementById("milDec");
+var milUni = document.getElementById("milUni");
+
+
+var centesimas = 0;
+var segundos = 0;
+var minutos = 0;
+
+
+Chronometer.prototype.startClick = function () {
+
+    if (centesimas < 99) {
+		centesimas++;
+		if (centesimas >= 10) {
+             
+            milDec.style.display="none"
+            
+             }
+		milUni.innerHTML = centesimas;
+    }
+    
+	if (centesimas == 99) {
+		centesimas = -1;
+    }
+    
+
+	if (centesimas == 0) {
+        segundos ++;
+        
+
+		if (segundos >= 10) { 
+            
+          var splitS = '' + segundos
+            secDec.innerHTML=splitS[0]
+        	secUni.innerHTML =splitS[1]
+        }else{
+            secDec.innerHTML="0"
+           	secUni.innerHTML =segundos; 
+        }
+	
+    }
+    
+
+    if (segundos == 59) {
+      
+		segundos = -1;
+    }
+
+    
+
+	if ( (centesimas == 0)&&(segundos == 0) ) {
+		minutos++;
+        if (minutos >= 10) { 
+            
+            var splitM = ''+minutos
+            minDec.innerHTML = splitM[0]
+        
+            minUni.innerHTML = splitM[1]
+        }else{
+
+            minUni.innerHTML =minutos;
+        }
+	     
+	}
+	if (minutos == 59) {
+		minutos = -1;
+	}
+
+};
+
+Chronometer.prototype.setMinutes = function () {
   
-// };
+};
 
-// Chronometer.prototype.setSeconds = function () {
+Chronometer.prototype.setSeconds = function () {
   
-// };
+};
 
-// Chronometer.prototype.twoDigitsNumber = function () {
+Chronometer.prototype.twoDigitsNumber = function () {
   
-// };
+};
 
-// Chronometer.prototype.setTime = function () {
+Chronometer.prototype.setTime = function () {
 
-// };
+};
 
-// Chronometer.prototype.setMilliseconds = function () {
+Chronometer.prototype.setMilliseconds = function () {
 
-// };
+};
 
-// Chronometer.prototype.stopClick = function () {
+Chronometer.prototype.stopClick = function (stopTime) {
   
-// };
+  clearInterval(stopTime)
+};
 
-// Chronometer.prototype.resetClick = function () {
+Chronometer.prototype.resetClick = function (stopTime) {
+    milDec.removeAttribute("style");
 
-// };
+    clearInterval(stopTime);
+	centesimas = 0;
+	segundos = 0;
+    minutos = 0;
+    secDec.innerHTML="0"
+    secUni.innerHTML="0"
+    milDec.innerHTML="0"
+    milUni.innerHTML="0"
+    minDec.innerHTML="0"
+    minUni.innerHTML="0"
 
-// Chronometer.prototype.splitClick = function () {
 
-// };
+   
+  var  list = document.getElementById("splits");
+
+  var tam = document.getElementById("splits").getElementsByTagName("li").length;
+
+
+
+  const ul_list = document.getElementById("splits");
+  const childs = Array.from(ul_list.childNodes).reverse();
+
+  childs.forEach(item => {
+      ul_list.removeChild(item);
+  });
+    
+};
+
+Chronometer.prototype.splitClick = function (a,b,c,d,e,f) {
+
+    var splitOne  = `${e}${f}:${c}${d}:${a}${b}`.split(":")
+
+    var resultIndexTree = splitOne[2].substring(1, 3).length
+
+    if(resultIndexTree==1){
+        
+        resultIndexTree = '0' + splitOne[2].substring(1, 3)
+
+    }else{
+
+        resultIndexTree = splitOne[2].substring(1, 3)
+    }
+
+    var newString = splitOne[0] +":"+ splitOne[1] +":"+ resultIndexTree ;
+
+    var node = document.createElement("li");
+    var mySplit = document.createTextNode(newString);
+    node.appendChild(mySplit);
+    document.getElementById("splits").appendChild(node);
+
+
+
+
+};
--- starter-code/javascript/main.js
+++ starter-code/javascript/main.js
@@ -1,4 +1,5 @@
 var chronometer = new Chronometer();
+
 var btnLeft     = document.getElementById('btnLeft');
 var btnRight    = document.getElementById('btnRight');
 var minDec      = document.getElementById('minDec');
@@ -9,6 +10,9 @@ var milDec      = document.getElementById('milDec');
 var milUni      = document.getElementById('milUni');
 
 
+var getIdSetTimeout = 0;
+
+
 function printTime() {
 
 }
@@ -17,16 +21,19 @@ function printMinutes() {
 
 }
 
-function printSeconds() {
+function printSeconds() {   
 
 }
 
 function printMilliseconds() {
 
+    
+
 }
 
 function printSplit() {
 
+ chronometer.splitClick(milDec.textContent,milUni.textContent,secDec.textContent,secUni.textContent,minDec.textContent,minUni.textContent)
 }
 
 function clearSplits() {
@@ -34,6 +41,10 @@ function clearSplits() {
 }
 
 function setStopBtn() {
+
+ 
+
+  chronometer.stopClick(getIdSetTimeout)
 
 }
 
@@ -43,18 +54,68 @@ function setSplitBtn() {
 
 function setStartBtn() {
 
+
+    getIdSetTimeout =  setInterval(function(){
+
+       chronometer.startClick()
+
+    },10)
+ 
+   
+
+
 }
 
 function setResetBtn() {
 
+    chronometer.resetClick(getIdSetTimeout)
+
 }
 
 // Start/Stop Button
-btnLeft.addEventListener('click', function () {
+btnLeft.addEventListener('click', function (e) {
+    
+
+
+    if(btnLeft.textContent=="START"){
+        setStartBtn()
+        btnLeft.textContent ="STOP"
+        btnLeft.setAttribute("class", "btn stop");
+
+        btnRight.textContent="SPLIT"
+        btnRight.setAttribute("class", "btn split");
+    }else{
+        setStopBtn()
+        btnLeft.textContent="START"
+        btnLeft.setAttribute("class", "btn start");
+        btnRight.textContent="RESET"             
+        btnRight.setAttribute("class", "btn reset"); 
+    }
 
 });
 
 // Reset/Split Button
 btnRight.addEventListener('click', function () {
+
+
+    if(btnRight.textContent=="RESET"){
+        
+        btnLeft.textContent ="START"
+
+        setResetBtn()
+
+    
+    }else if(btnRight.textContent=="SPLIT"){ 
+
+
+        printSplit();
+         
+        
+    }else if(btnLeft.textContent=="START"){
+
+        btnRight.textContent=="RESET"
+
+        
+    }
 
 });
