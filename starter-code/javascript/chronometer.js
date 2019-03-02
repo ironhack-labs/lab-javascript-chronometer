df8474620869c99c4cc576b15508e4f112507f7c
fredconti <fred@conticonti.com>
2019-03-02 17:08:23 -0300

    (SAO-PT2-PP) Paul Divet e Fred Conti

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,40 +1,52 @@
 // Constructor
-// function Chronometer() {
+function Chronometer() {
+    this.currentTime = 0;
+    this.intervalId = 0;
+    this.seconds = 0;
+    this.minutes = 0;
+}
 
-// }
+Chronometer.prototype.startClick = function () {
+    this.intervalId = setInterval(function() {
+        this.setTime();
+    }.bind(this), 1000);
+};
 
-// Chronometer.prototype.startClick = function () {
+Chronometer.prototype.setMinutes = function () {
+    this.minutes = Math.floor(this.currentTime/60);
+    return this.minutes;
+};
 
-// };
+Chronometer.prototype.setSeconds = function () {
+    this.seconds = this.currentTime%60;
+    return this.seconds;
+};
 
-// Chronometer.prototype.setMinutes = function () {
-  
-// };
+Chronometer.prototype.twoDigitsNumber = function (value) {
+    if (value < 10) {
+        return "0" + value;
+    } 
+    return value.toString();
+};  
 
-// Chronometer.prototype.setSeconds = function () {
-  
-// };
+Chronometer.prototype.setTime = function () {
+    this.currentTime += 1;
+    this.minutes = this.twoDigitsNumber(this.setMinutes());
+    this.seconds = this.twoDigitsNumber(this.setSeconds());
+};
 
-// Chronometer.prototype.twoDigitsNumber = function () {
-  
-// };
+Chronometer.prototype.stopClick = function () {
+  clearInterval(this.intervalId);
+};
 
-// Chronometer.prototype.setTime = function () {
+Chronometer.prototype.resetClick = function () {
+    this.currentTime = 0;
+};
+
+// Chronometer.prototype.splitClick = function () {
 
 // };
 
 // Chronometer.prototype.setMilliseconds = function () {
-
-// };
-
-// Chronometer.prototype.stopClick = function () {
-  
-// };
-
-// Chronometer.prototype.resetClick = function () {
-
-// };
-
-// Chronometer.prototype.splitClick = function () {
 
 // };
