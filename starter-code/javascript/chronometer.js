ea13657bff8aa873faea6603ff1941449ab1d9ab
MartaVaqgut <martavaquerogutierrez@gmail.com>
2019-03-02 12:14:27 +0100

    test jasmine compeltos

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,31 +1,40 @@
  //Constructor
  function Chronometer() {
     this.currentTime = 0;
-    this.intervalId = "";
+    this.intervalId = 0;
  }
 
  Chronometer.prototype.startClick = function () {
+    var that = this;
     intervalId = setInterval (function (){
-    Chronometer.currentTime ++;
+    that.currentTime ++;
     },1000);
+    return intervalId
  };
 
  Chronometer.prototype.setMinutes = function () {
-    var minutes = Math.floor (chronometer.currentTime/60)
+    var minutes = Math.floor (this.currentTime/60)
     return minutes;
  };
 
  Chronometer.prototype.setSeconds = function () {
-    var seconds = chronometer.currentTime - (minutes*60)
+    var seconds = this.currentTime%60; 
+    //var seconds = this.currentTime - (minutes*60)
     return seconds;
  };
 
- Chronometer.prototype.twoDigitsNumber = function () {
-  
+ Chronometer.prototype.twoDigitsNumber = function (time) {
+   if (time <= 9){
+      return "0" + time
+   } else {
+      return time.string
+   }
  };
 
  Chronometer.prototype.setTime = function () {
-
+   var minutes = twoDigitsNumber(setMinutes());
+   var seconds = twoDigitsNumber (seconds());
+   return this.second; 
  };
 
  Chronometer.prototype.setMilliseconds = function () {
@@ -37,7 +46,7 @@
  };
 
  Chronometer.prototype.resetClick = function () {
-
+  var currentTime = 0;
  };
 
  Chronometer.prototype.splitClick = function () {
