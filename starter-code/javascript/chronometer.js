4f3d56d5c3c3d95ce9f3dc435e1017f778dc8572
Daniel Diaz Toledano <danieldiaz.bio@gmail.com>
2019-03-27 12:01:03 +0100

    getMinutes done

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,10 +1,34 @@
 class Chronometer {
-  constructor() {}
-  // startClick() {}
-  // getMinutes() {}
-  // getSeconds() {}
+  constructor() {
+    this.currentTime = 0;
+    this.intervalId = null;
+  };
+
+  startClick() {
+    var identifier = setInterval(() => this.currentTime++, 1000);
+    this.intervalId = identifier;
+  }
+
+  getMinutes() {
+    if (this.currentTime === 0) {
+      return 0;
+    } else {
+      return Math.trunc(this.currentTime / 60);
+    }
+  }
+
+  getSeconds() {
+    if (this.currentTime === 0) {
+      return 0;
+    } else {
+      console.log(this.courrentTime - (this.currentTime % 60));
+    }
+  }
   // twoDigitsNumber() {}
   // stopClick() {}
   // resetClick() {}
   // splitClick() {}
 }
+
+const c = new Chronometer();
+c.startClick();
