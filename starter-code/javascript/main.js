6d028e3a5477838f22d4392452109e1b7a752b11
Daniel Diaz Toledano <danieldiaz.bio@gmail.com>
2019-03-27 15:51:55 +0100

    classes of buttons change when click

--- starter-code/javascript/main.js
+++ starter-code/javascript/main.js
@@ -1,12 +1,12 @@
 var chronometer = new Chronometer();
-var btnLeft     = document.getElementById('btnLeft');
-var btnRight    = document.getElementById('btnRight');
-var minDec      = document.getElementById('minDec');
-var minUni      = document.getElementById('minUni');
-var secDec      = document.getElementById('secDec');
-var secUni      = document.getElementById('secUni');
-var milDec      = document.getElementById('milDec');
-var milUni      = document.getElementById('milUni');
+var btnLeft = document.getElementById('btnLeft');
+var btnRight = document.getElementById('btnRight');
+var minDec = document.getElementById('minDec');
+var minUni = document.getElementById('minUni');
+var secDec = document.getElementById('secDec');
+var secUni = document.getElementById('secUni');
+var milDec = document.getElementById('milDec');
+var milUni = document.getElementById('milUni');
 
 
 function printTime() {
@@ -34,27 +34,36 @@ function clearSplits() {
 }
 
 function setStopBtn() {
+  btnLeft.classList.replace('start', 'stop');
 
 }
 
 function setSplitBtn() {
-
+  btnRight.classList.replace('reset', 'split');
 }
 
 function setStartBtn() {
-
+  btnLeft.classList.replace('stop', 'start');
 }
 
 function setResetBtn() {
-
+  btnRight.classList.replace('split', 'reset');
 }
 
 // Start/Stop Button
 btnLeft.addEventListener('click', function () {
-
+  if (btnLeft.className === 'btn start') {
+    setStopBtn();
+  } else {
+    setStartBtn();
+  };
 });
 
 // Reset/Split Button
 btnRight.addEventListener('click', function () {
-
+  if (btnRight.className === 'btn reset') {
+    setSplitBtn();
+  } else {
+    setResetBtn();
+  };
 });
