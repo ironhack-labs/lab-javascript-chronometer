9e3585a8ef80f7d095275325176b89235a66be44
Frederic Chhean <chheanfrederic@gmail.com>
2019-03-27 18:42:15 +0100

    first commmt lab chronometer

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,10 +1,53 @@
 class Chronometer {
-  constructor() {}
-  // startClick() {}
-  // getMinutes() {}
-  // getSeconds() {}
-  // twoDigitsNumber() {}
-  // stopClick() {}
-  // resetClick() {}
-  // splitClick() {}
+  constructor() {
+    this.currentTime = 0;
+    this.intervalId = null;
+  };
+  startClick() {
+    var identifier = setInterval(() => {
+      this.currentTime++;
+      printSeconds();
+      printMinutes()}, 1000);
+    this.intervalId = identifier;
+  }
+
+  getMinutes() {
+    // if(!this.currentTime) 
+    if (this.currentTime === 0) {
+      return 0;
+    } else {
+      let minutes = Math.trunc(this.currentTime / 60)
+      return minutes;
+    }
+  }
+  getSeconds() {
+    if (this.currentTime === 0) {
+      return 0;
+    } else {
+      let seconds = this.currentTime % 60;
+      return seconds;
+    }
+  }
+  twoDigitsNumber(time) {
+    if (time === 0) {
+      return '00';
+    } else if (time < 10) {
+      return '0' + time;
+    } else {
+      return String(time);
+    }
+    //return ('0' + i).slice(-2); Another way to do in one line
+  }
+  stopClick() {
+    clearInterval(this.intervalId);
+  }
+  resetClick() {
+    this.currentTime = 0;
+  }
+  splitClick() {
+    var splitC = 0;
+    return this.getMinutes() + ":" + this.getSeconds();
+  }
 }
+const c = new Chronometer();
+c.startClick();
