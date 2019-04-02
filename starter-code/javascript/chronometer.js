906ff5997912e1a308f761509804d631b3afa232
saulscs <saulcoronasantos@ymail.com>
2019-04-02 17:37:37 -0600

    v2

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,9 +1,27 @@
 class Chronometer {
-  constructor() {}
-  // startClick() {}
-  // getMinutes() {}
-  // getSeconds() {}
-  // twoDigitsNumber() {}
+  constructor( ) {
+    this.currenTime = 0
+    this.intervalId = 0
+  };
+   startClick() {
+    this.intervalId = setInterval(() => {this.currenTime += 1},1000)
+   }
+   getMinutes() {
+     let minutes = currenTime/60;
+     return Math.floor(minutes);
+
+   }
+  getSeconds() {
+    return currenTime%60;
+  }
+  twoDigitsNumber(arg) {
+   if ( arg < 10){
+    return "0"+arg;
+  } else {
+    return arg.toString();
+  }
+   }
+  }
   // stopClick() {}
   // resetClick() {}
   // splitClick() {}
