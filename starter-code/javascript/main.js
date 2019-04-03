9499498ead06bdf2da915206898ef0c96ce913cf
Ricardo-MaGuz <ricardo@wides.mx>
2019-04-03 09:44:25 -0600

    Por si no acabo

--- starter-code/javascript/main.js
+++ starter-code/javascript/main.js
@@ -14,7 +14,9 @@ function printTime() {
 }
 
 function printMinutes() {
-
+  let minutes =chronometer.getMinutes
+  minutes =minDec.firstChild.data = (chronometer.getMinutes).substring(0,1)
+  minDec.firstChild.data = '${minutes}';
 }
 
 function printSeconds() {
@@ -34,7 +36,7 @@ function clearSplits() {
 }
 
 function setStopBtn() {
-
+  
 }
 
 function setSplitBtn() {
@@ -50,11 +52,38 @@ function setResetBtn() {
 }
 
 // Start/Stop Button
-btnLeft.addEventListener('click', function () {
-
+btnLeft.addEventListener('click', function() {
+  this.classList.toggle("stop")
+  btnRight.classList.toggle("split")
+  if (this.firstChild.data == "START") 
+   {
+      chronometer.startClick()
+       this.firstChild.data = "STOP";
+       btnRight.firstChild.data = "SPLIT";
+   }
+   else 
+   {
+     this.firstChild.data = "START";
+     chronometer.stopClick()
+   }
+  console.log(`Funciona ${this}}`)
 });
 
 // Reset/Split Button
 btnRight.addEventListener('click', function () {
+  if (this.firstChild.data == "SPLIT") {   
+      this.classList.toggle("split")
+      this.firstChild.data = "RESET";   
+   }
+   else 
+   {
+     this.firstChild.data = "SPLIT";
+     this.classList.toggle("split")
+     btnLeft.classList.toggle("stop")
+     btnLeft.firstChild.data = "START";
+   }
+  console.log(`Funciona ${this}`)
+}
+  
+);
 
-});
