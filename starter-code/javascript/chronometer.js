7ab8d57eb91380884ef012d9ebf0189428c0dca0
marypztr <nutrimary.77@gmail.com>
2019-04-03 00:09:12 -0600

    DE MEX Maryjose

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,10 +1,26 @@
-class Chronometer {
-  constructor() {}
-  // startClick() {}
-  // getMinutes() {}
-  // getSeconds() {}
-  // twoDigitsNumber() {}
-  // stopClick() {}
-  // resetClick() {}
-  // splitClick() {}
-}
+
+class Chronometer { 
+  constructor() {     
+    this.currentTime = 0    
+    this.intervalId = undefined   }     
+startClick() {    
+     this.intervalId= setInterval(()=>{ 
+     this.currentTime  += 1;     
+     },1000)     }    
+ getMinutes() {    
+    return Math.floor(this.currentTime/60)  
+   }     
+   getSeconds() {    
+    this.seconds = this.currentTime >= 60 ? this.currentTime - 60 : this.currentTime   
+        return this.currentTime % 60    
+       }   
+ twoDigitsNumber(time) {    
+    return `${time}`.padStart(2 , '0')   }     
+ stopClick() {     
+     clearInterval()     }     
+ resetClick() {
+
+ }   
+ splitClick() {
+
+ } }
--- starter-code/javascript/main.js
+++ starter-code/javascript/main.js
@@ -14,15 +14,42 @@ function printTime() {
 }
 
 function printMinutes() {
-
+    setTimeout(()=>{
+        setInterval(()=>{
+            if(minUni.innerHTML < 9){
+                minUni.innerHTML++
+            } else {
+                minUni.innerHTML = 0;
+                minDec.innerHTML < 5 ? minDec.innerHTML++ : minDec.innerHTML=0   
+            }
+        }, 60000)
+    }) 
 }
 
 function printSeconds() {
-
+    setTimeout(()=>{
+        setInterval(()=>{
+            if(secUni.innerHTML < 9){
+                secUni.innerHTML++
+            } else {
+                secUni.innerHTML = 0;
+                secDec.innerHTML < 5 ? secDec.innerHTML++ : secDec.innerHTML=0   
+            }
+        }, 1000)
+    }) 
 }
 
 function printMilliseconds() {
-
+    setTimeout(()=>{
+        setInterval(()=>{
+            if(milUni.innerHTML < 9){
+                milUni.innerHTML++
+            } else {
+                milUni.innerHTML = 0;
+                milDec.innerHTML < 9 ? milDec.innerHTML++ : milDec.innerHTML=0   
+            }
+        }, 10)
+    }) 
 }
 
 function printSplit() {
@@ -51,10 +78,17 @@ function setResetBtn() {
 
 // Start/Stop Button
 btnLeft.addEventListener('click', function () {
-
+    document.getElementsByName(`${printSeconds()}`);
+    document.getElementsByName(`${printMilliseconds()}`);
+    document.getElementsByName (`${printMinutes()}`);
 });
 
 // Reset/Split Button
 btnRight.addEventListener('click', function () {
-
+    secUni.innerHTML = 0;
+    secDec.innerHTML = 0;
+    milUni.innerHTML = 0;
+    milDec.innerHTML = 0;
+    minDec.innerHTML = 0;
+    minUni.innerHTML = 0;
 });
