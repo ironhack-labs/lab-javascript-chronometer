98289e29d8b16a5dc3673739cd80077a47f7fd9d
gabaldonrico <gabaldon.1617@gmail.com>
2019-04-30 13:46:42 +0200

    Interation1:The logic

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,10 +1,32 @@
 class Chronometer {
-  constructor() {}
-  // startClick() {}
-  // getMinutes() {}
-  // getSeconds() {}
-  // twoDigitsNumber() {}
-  // stopClick() {}
-  // resetClick() {}
+  constructor() {
+    this.currentTime = 0, 
+    this.intervalId
+  }
+  startClick() {
+    const interval = ()=>{
+      this.currentTime ++
+    }
+    this.intervalId = setInterval(interval, 1000)  
+  }
+  getMinutes() {
+    return parseInt(this.currentTime / 60)
+  }
+  getSeconds() {
+    return this.currentTime % 60 
+  }
+  twoDigitsNumber(time) {
+    if(time < 10){
+      return `0${time}`
+    }else{
+      return `${time}`
+    }
+  }
+  stopClick() {
+    clearInterval(this.intervalId)
+  }
+  resetClick() {
+    this.currentTime =0
+  }
   // splitClick() {}
 }
