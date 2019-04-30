1fb09d16f7d03688c95de1932e0b144c0153a0dd
Teo <teodorolg130@gmail.com>
2019-04-30 16:25:16 +0200

    completado perfecto

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,10 +1,69 @@
 class Chronometer {
-  constructor() {}
-  // startClick() {}
-  // getMinutes() {}
-  // getSeconds() {}
-  // twoDigitsNumber() {}
-  // stopClick() {}
-  // resetClick() {}
+  constructor() {
+    this.currentTime = 0
+    this.currentMillSeconds = 0
+    this.intervalId
+    this.millSecondsId
+}
+
+  startClick() {
+    let timer = setInterval( () => {
+      this.currentTime += 1
+      printTime();
+      console.log(this.currentTime)
+    },1000)
+
+    let millTimer = setInterval(() => {
+      if(this.currentMillSeconds<100) {
+      this.currentMillSeconds += 1
+      printMilliseconds()
+    } else {
+      this.currentMillSeconds = 0
+    }
+    } ,10)
+
+    this.intervalId = timer
+    this.millSecondsId = millTimer
+  }
+  getMinutes() {
+    let seconds = this.currentTime
+    let minutes = parseInt(seconds/60)
+    return minutes
+  }
+
+  getSeconds() {
+    let seconds = this.currentTime
+    let minutes = parseInt(seconds/60)
+    let actualSeconds = this.currentTime - minutes*60
+
+    return actualSeconds
+  }
+
+  getMillSeconds() {
+    let mills = this.currentMillSeconds
+    return mills
+
+  }
+
+  
+  twoDigitsNumber(num) {
+    if (num <10) {
+      let string = num.toString()
+      let result = "0" + string
+      return result
+    } else if (num >= 10){ 
+      return num.toString()
+    }
+  }
+  
+  stopClick() {
+    clearInterval(this.intervalId)
+    clearInterval(this.millSecondsId)
+  }
+  
+  resetClick() { 
+    this.currentTime = 0
+    this.currentMillSeconds = 0
+  }
   // splitClick() {}
 }
