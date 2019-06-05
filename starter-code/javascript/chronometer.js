f6f478895655b8679152057a9f1f5615881c2dfd
elinlarsen <elin_larsen1@hotmail.fr>
2019-06-05 14:15:54 +0200

    logic done

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,10 +1,34 @@
 class Chronometer {
-  constructor() {}
-  // startClick() {}
-  // getMinutes() {}
-  // getSeconds() {}
-  // twoDigitsNumber() {}
-  // stopClick() {}
-  // resetClick() {}
-  // splitClick() {}
+  constructor() {
+    this.currentTime=0;
+
+  }
+   startClick() {
+     this.intervalId=setInterval( () => {this.currentTime ++}, 1000) 
+    
+   }
+
+   getMinutes() {
+     return parseInt(this.currentTime/60)
+   }
+   getSeconds() {
+    return parseInt(this.currentTime%60)
+   }
+
+
+   twoDigitsNumber(integer) {
+      if(integer<10)
+      {
+        return "0"+integer
+      }
+      else return integer.toString() /*toString(integer*/
+   }
+   stopClick() {
+      clearInterval(this.intervalId) 
+   }
+   
+   resetClick() {
+     this.currentTime=0;
+   }
+   splitClick() {}
 }
