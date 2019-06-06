58234f017b3adf5d2edca40a254bb8fecc297fba
Octave Stoufflet <octave@heetch.com>
2019-06-06 12:31:21 +0200

    javascript done / jasmine ok

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,10 +1,35 @@
 class Chronometer {
-  constructor() {}
-  // startClick() {}
-  // getMinutes() {}
-  // getSeconds() {}
-  // twoDigitsNumber() {}
-  // stopClick() {}
-  // resetClick() {}
+  constructor() {
+    this.currentTime = 0;
+    this.intervalId = 0;
+  }
+
+  startClick() {
+    const intervalId = setInterval(() => {
+      this.currentTime += 1;
+    }, 1000);
+  }
+  getMinutes() {
+    return Math.floor(this.currentTime / 60);
+  }
+  getSeconds() {
+    return this.currentTime % 60;
+  }
+  twoDigitsNumber(a) {
+    if (a === 0) {
+      return "00";
+    }
+    if (a < 10) {
+      return "0" + a;
+    }
+    if (a > 10) {
+      return a.toString();
+    }
+  }
+
+  stopClick() {
+    clearInterval(setInterval);
+  }
+  resetClick() {}
   // splitClick() {}
 }
