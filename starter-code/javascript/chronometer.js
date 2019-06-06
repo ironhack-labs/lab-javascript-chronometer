560a61d84dd3165ed56fcdd2aaad378c0151ff5b
Camille Jacques <camille.jacques75@gmail.com>
2019-06-06 14:14:14 +0200

    jasmine done, buttons somewhat functioning

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,10 +1,38 @@
 class Chronometer {
-  constructor() {}
-  // startClick() {}
-  // getMinutes() {}
-  // getSeconds() {}
-  // twoDigitsNumber() {}
-  // stopClick() {}
-  // resetClick() {}
-  // splitClick() {}
+  constructor() {
+    this.currentTime = 0;
+    this.intervalId = 0;
+  }
+  startClick() {
+    this.intervalId = setInterval(() => {
+      this.currentTime += 1;
+    }, 1000);
+  }
+
+  getMinutes() {
+    return Math.floor(this.currentTime / 60)
+  }
+
+  getSeconds() {
+    let minutes = Math.floor(this.currentTime / 60);
+    return this.currentTime - minutes * 60
+  }
+
+  twoDigitsNumber(digit) {
+    if (digit < 10) {
+      return `0${digit}`
+    } else if (digit >= 10) {
+      return `${digit}`
+    }
+  }
+
+  stopClick() {
+    clearInterval(this.intervalId);
+  }
+
+  resetClick() {
+    this.currentTime = 0;
+  }
+
+  splitClick() {}
 }
--- starter-code/javascript/main.js
+++ starter-code/javascript/main.js
@@ -9,6 +9,7 @@ var milDec      = document.getElementById('milDec');
 var milUni      = document.getElementById('milUni');
 
 
+
 function printTime() {
 
 }
@@ -34,27 +35,46 @@ function clearSplits() {
 }
 
 function setStopBtn() {
-
+  btnLeft.textContent = "STOP";
+  btnLeft.classList = "btn stop";
 }
 
 function setSplitBtn() {
-
+  btnRight.textContent = "SPLIT";
+  btnRight.classList = "btn split"
 }
 
 function setStartBtn() {
-
+  btnLeft.textContent = "START";
+  btnLeft.classList = "btn start";
 }
 
 function setResetBtn() {
-
+  btnRight.textContent = "RESET";
+  btnRight.classList = "btn reset";
 }
 
 // Start/Stop Button
-btnLeft.addEventListener('click', function () {
-
+btnLeft.addEventListener('click', function() {
+  if (btnLeft.classList.contains("stop") && btnLeft.textContent === "STOP") {
+    setStartBtn();
+    chronometer.stopClick();
+  } else if (btnLeft.classList.contains("start") && btnLeft.textContent === "START") {
+    setStopBtn();
+    chronometer.startClick();
+  }
+  console.log("click it to the left");
 });
 
 // Reset/Split Button
 btnRight.addEventListener('click', function () {
-
+  if (btnRight.classList.contains("split") && btnLeft.textContent === "SPLIT") {
+    setResetBtn();
+    chronometer.splitClick();
+  } else if (btnRight.classList.contains("reset") && btnLeft.textContent === "RESET") {
+    setSplitBtn();
+    chronometer.resetClick();
+  }
+  console.log("click it to the right");
 });
+
