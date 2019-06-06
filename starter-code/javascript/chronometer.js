3ceeecfc11a908e635ed4eded52ace2629232706
Yacine Berrada <yacineberrada@gmail.com>
2019-06-06 11:50:29 +0200

    first commit with the milliseconds

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,10 +1,54 @@
 class Chronometer {
-  constructor() {}
-  // startClick() {}
-  // getMinutes() {}
-  // getSeconds() {}
-  // twoDigitsNumber() {}
-  // stopClick() {}
-  // resetClick() {}
+
+
+  constructor() {
+
+    this.currentTime = 0;
+    this.intervalId = 0;
+
+  }
+
+
+  startClick() {
+
+    this.intervalId = setInterval(() => { this.currentTime++; }, 1000);
+    console.log("starting the chronometer");
+
+  }
+
+
+  getMinutes() {
+
+    return Math.floor(this.currentTime / 60);
+  }
+
+
+  getSeconds() {
+
+    return this.currentTime - this.getMinutes() * 60;
+
+  }
+
+
+  twoDigitsNumber(value) {
+
+    if (value < 10) return String("0" + String(value))
+    else return String(value);
+
+  }
+
+  stopClick() {
+
+    clearInterval(this.intervalId);
+    this.intervalId = 0;
+    console.log("stopping the chronometer");
+
+
+  }
+  resetClick() {
+
+    this.currentTime = 0;
+
+  }
   // splitClick() {}
 }
