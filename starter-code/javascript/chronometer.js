08a272e846c415b00262a292cdedd258e8af4694
rogerayra <roger.ayra@gmail.com>
2019-06-11 22:34:57 -0500

    La versión sin bonus (milisegundos) pasaba las pruebas de Jasmine. Al incluir los milisegundos, ciertas prubas de Jasmine dejan de funcionar

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,10 +1,52 @@
 class Chronometer {
-  constructor() {}
-  // startClick() {}
-  // getMinutes() {}
-  // getSeconds() {}
-  // twoDigitsNumber() {}
-  // stopClick() {}
-  // resetClick() {}
-  // splitClick() {}
+  constructor() {
+    this.currentTime = 0;
+    this.currentMiliseconds
+  }
+
+  startClick = () => {
+    this.intervalId = setInterval(() => this.currentTime++, 10);
+    // this.intervalId = setInterval(() => this.currentTime++, 1000);
+  };
+
+  getMinutes = () => {
+    // return Math.floor(this.currentTime / 60);
+    return Math.floor((this.currentTime / 100) / 60);
+  };
+
+  getSeconds = () => {
+    // return this.currentTime % 60;
+
+    return Math.floor(this.currentTime / 100) % 60;
+  };
+
+  getMilliseconds = () => {
+    return this.currentTime % 100;
+  };
+
+  twoDigitsNumber = num => {
+    let str = num.toString();
+    if (str.length === 1) {
+      str = "0" + str;
+    }
+
+    return str;
+  };
+
+  stopClick = () => {
+    clearInterval(this.intervalId);
+  };
+  resetClick = () => {
+    this.currentTime = 0;
+  };
+  splitClick() {
+    let milliseconds = chronometer.twoDigitsNumber(
+      chronometer.getMilliseconds()
+    );
+    let seconds = chronometer.twoDigitsNumber(chronometer.getSeconds());
+    let minutes = chronometer.twoDigitsNumber(chronometer.getMinutes());
+
+    return `${minutes}:${seconds}:${milliseconds}`;
+    // return `${minutes}:${seconds}`;
+  }
 }
