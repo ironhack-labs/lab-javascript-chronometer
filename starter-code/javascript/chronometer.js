deff664106f800c2ff854fd54f1e0f13f094e7c2
mariana-gamino <mariianagamino@gmail.com>
2019-06-11 22:13:55 -0500

    Primera iteración terminada

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,10 +1,35 @@
 class Chronometer {
-  constructor() {}
-  // startClick() {}
-  // getMinutes() {}
-  // getSeconds() {}
-  // twoDigitsNumber() {}
-  // stopClick() {}
-  // resetClick() {}
+  constructor() {
+    this.currentTime = 0;
+    this.intervalId = 0;
+  }
+  startClick() {
+    this.intervalId = setInterval(()=> {this.currentTime++}, 1000)
+  }
+  getMinutes() {
+    if(this.currentTime===0) return 0
+    return parseInt(this.currentTime/60)
+  }
+
+  getSeconds() {
+    if(this.currentTime===0) return 0
+    return parseInt(this.currentTime%60)
+  }
+
+  twoDigitsNumber(num) {
+    if(num.toString().length === 1) {
+      return "0" + num.toString()
+  } else {
+      return num.toString()
+    }
+  }
+
+  stopClick() {
+    clearInterval(this.intervalId)
+  }
+  
+  resetClick() {
+    this.currentTime = 0
+  }
   // splitClick() {}
 }
