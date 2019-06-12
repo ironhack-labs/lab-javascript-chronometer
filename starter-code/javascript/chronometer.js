1de4c0cf48dd501376dc0c1d23bd97c903d6acb1
Edgar Herera <contactoedgarherrera@gmail.com>
2019-06-11 23:30:02 -0500

    Test with Jasmine | Done.

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,10 +1,46 @@
 class Chronometer {
-  constructor() {}
-  // startClick() {}
-  // getMinutes() {}
-  // getSeconds() {}
-  // twoDigitsNumber() {}
-  // stopClick() {}
-  // resetClick() {}
+  constructor() {
+    this.currentTime = 0,
+    this.intervalId
+  }
+  startClick() {
+    this.intervalId = setInterval(() => {
+      console.log(this.count);
+      this.currentTime++;
+    }, 1000);
+  }
+
+  getMinutes() {
+    if (this.currentTime == 0) {
+      return 0
+    } else {
+      return Math.floor(this.currentTime / 60)
+    }
+  }
+
+  getSeconds() {
+    if (this.currentTime == 0) {
+      return 0
+    } else {
+      return this.currentTime - (this.getMinutes() * 60)
+    }
+  }
+
+
+  twoDigitsNumber(value) {
+    let string = value.toString()
+    if (string.length < 2) {
+      string = `0${string}`
+    }
+    return string
+  }
+  
+  stopClick() {
+    clearInterval(this.clearInterval)
+  }
+
+  resetClick() {
+    this.currentTime = 0
+  }
   // splitClick() {}
 }
