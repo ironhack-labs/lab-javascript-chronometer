f4ecdbd6e3fceb9103aa74def9de49b91a124ca9
bryanmej <bryanmej@gmail.com>
2019-06-12 21:27:23 -0500

    Este tema de constructores y OOP es complicado para mi tengo que trabajar mas en esto

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,9 +1,29 @@
 class Chronometer {
-  constructor() {}
-  // startClick() {}
-  // getMinutes() {}
-  // getSeconds() {}
-  // twoDigitsNumber() {}
+  constructor() {
+    this.currentTime = 0;
+    
+  }
+
+  startClick() {
+   this.intervalId = setInterval(()=> {
+     this.currentTime++
+   },1000)
+  }
+  getMinutes() {
+    return Math.floor(this.currentTime/60)
+  }
+   getSeconds() {
+     let sec = this.currentTime - (60 * Math.floor(this.currentTime/60))
+     return sec
+   }
+  twoDigitsNumber(value) {
+   let a= value.toString()
+    if(a == 1) {
+      return ("0"+ a)
+    } else {
+      return value
+      } 
+  }
   // stopClick() {}
   // resetClick() {}
   // splitClick() {}
