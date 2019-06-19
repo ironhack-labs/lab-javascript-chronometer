5fa9539d17fa5997ff3e92c556eabfaf048c82d8
arstrel <arstrel@gmail.com>
2019-06-19 14:32:33 -0400

    Iteration 4: Basic functionality works

--- starter-code/javascript/main.js
+++ starter-code/javascript/main.js
@@ -9,15 +9,22 @@ var milDec      = document.getElementById('milDec');
 var milUni      = document.getElementById('milUni');
 
 
+
 function printTime() {
 
 }
 
-function printMinutes() {
-
+function printMinutes(min) {
+  let twoDigitsMin = chronometer.twoDigitsNumber(min);
+  minDec.innerHTML = twoDigitsMin.slice(0,1)
+  minUni.innerHTML = twoDigitsMin.slice(1,2)
 }
 
-function printSeconds() {
+function printSeconds(sec) {
+  
+  let twoDigitsSec = chronometer.twoDigitsNumber(sec);
+  secDec.innerHTML = twoDigitsSec.slice(0,1)
+  secUni.innerHTML = twoDigitsSec.slice(1,2)
 
 }
 
@@ -26,41 +33,54 @@ function printMilliseconds() {
 }
 
 function printSplit() {
+  let minutes = chronometer.twoDigitsNumber(chronometer.getMinutes())
+  let seconds = chronometer.twoDigitsNumber(chronometer.getSeconds())
+  let newSplit = `<li>${minutes}:${seconds}</li>`
+  $('#splits').append(newSplit)
 
 }
 
 function clearSplits() {
-
+  $('#splits').html('');
 }
 
-function setStopBtn() {
 
-}
-
-function setSplitBtn() {
-
-}
-
-function setStartBtn() {
-
-}
-
-function setResetBtn() {
-
-}
+setInterval(()=> {
+  printSeconds(chronometer.getSeconds())
+  printMinutes(chronometer.getMinutes())},1000)
 
 // Start/Stop Button
 btnLeft.addEventListener('click', function () {
-  $(this).toggleClass('stop');
-  chronometer.isRunning = !chronometer.isRunning;
+  $(this).toggleClass('start stop');
+  $(btnRight).toggleClass('reset split');
+
+  //already running
   if(chronometer.isRunning) {
-    $(this).html('STOP')
-  } else {
+    chronometer.isRunning = !chronometer.isRunning;
+    chronometer.stopClick();
     $(this).html('START')
+    $(btnRight).html('RESET')
+
+
+    //not yet running
+  } else {
+    chronometer.isRunning = !chronometer.isRunning;
+    chronometer.startClick();
+    
+    $(this).html('STOP');
+    $(btnRight).html('SPLIT'); 
   }
 });
 
 // Reset/Split Button
 btnRight.addEventListener('click', function () {
-
+  if(chronometer.isRunning) {
+    //the button is "split"
+    printSplit()
+  }
+  else {
+    //the button is "reset"
+    chronometer.resetClick();
+    clearSplits();
+  }
 });
