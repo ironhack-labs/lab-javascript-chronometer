bf072b11addad2bae08d2eee9f000f0d63aff080
David Dornelles <daviddornelles@gmail.com>
2019-06-19 19:06:58 -0300

    done

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,10 +1,40 @@
 class Chronometer {
-  constructor() {}
-  // startClick() {}
-  // getMinutes() {}
-  // getSeconds() {}
-  // twoDigitsNumber() {}
-  // stopClick() {}
-  // resetClick() {}
+  constructor() {
+    this.currentTime = 0;
+    this.intervalId = 0;
+  }
+
+  startClick() {
+    this.intervalId = setInterval(() => {
+      this.currentTime += 1
+    }, 1000);
+
+  }
+
+  getMinutes() {
+    if (this.currentTime >= 60) {
+      return Math.floor(this.currentTime / 60)
+    }
+    return 0;
+  }
+
+  getSeconds() {
+    return this.currentTime % 60;
+  }
+
+  twoDigitsNumber(value) {
+    if (value < 10) {
+      return `0${value}`;
+    }
+      return `${value}`;
+  }
+
+  stopClick() {
+    clearInterval(this.intervalId);
+  }
+
+  resetClick() {
+    return this.currentTime = 0;
+  }
   // splitClick() {}
 }
--- starter-code/javascript/main.js
+++ starter-code/javascript/main.js
@@ -1,60 +1,87 @@
 var chronometer = new Chronometer();
-var btnLeft     = document.getElementById('btnLeft');
-var btnRight    = document.getElementById('btnRight');
-var minDec      = document.getElementById('minDec');
-var minUni      = document.getElementById('minUni');
-var secDec      = document.getElementById('secDec');
-var secUni      = document.getElementById('secUni');
-var milDec      = document.getElementById('milDec');
-var milUni      = document.getElementById('milUni');
-
+var btnLeft = document.getElementById("btnLeft");
+var btnRight = document.getElementById("btnRight");
+var minDec = document.getElementById("minDec");
+var minUni = document.getElementById("minUni");
+var secDec = document.getElementById("secDec");
+var secUni = document.getElementById("secUni");
+var milDec = document.getElementById("milDec");
+var milUni = document.getElementById("milUni");
 
 function printTime() {
-
+  printSeconds();
+  printMinutes();
 }
 
 function printMinutes() {
-
+  const minutes = chronometer.twoDigitsNumber(chronometer.getMinutes());
+  minUni.innerHTML = minutes[1];
+  minDec.innerHTML = minutes[0];
 }
 
 function printSeconds() {
-
+  const seconds = chronometer.twoDigitsNumber(chronometer.getSeconds());
+  secUni.innerHTML = seconds[1];
+  secDec.innerHTML = seconds[0];
 }
 
-function printMilliseconds() {
-
-}
+function printMilliseconds() {}
 
 function printSplit() {
+  const printer = printTime();
+  console.log(printer);
 
+  let list = document.createElement('li');
+  document.querySelector('#splits').appendChild(list);
+  list.innerHTML = `${minDec.innerHTML}${minUni.innerHTML}:${secDec.innerHTML}${secUni.innerHTML}`;
 }
 
-function clearSplits() {
-
-}
+function clearSplits() {}
 
 function setStopBtn() {
-
-}
-
-function setSplitBtn() {
-
+  btnLeft.classList.add("start");
+  btnLeft.classList.remove("stop");
+  btnRight.classList.remove("split");
+  btnRight.classList.add("reset");
+  btnLeft.innerHTML = "START";
+  btnRight.innerHTML = "RESET";
+  chronometer.stopClick();
 }
 
 function setStartBtn() {
-
+  btnLeft.classList.add("stop");
+  btnLeft.classList.remove("start");
+  btnRight.classList.remove("reset");
+  btnRight.classList.add("split");
+  btnLeft.innerHTML = "STOP";
+  btnRight.innerHTML = "SPLIT";
+  chronometer.startClick();
 }
 
-function setResetBtn() {
+function setSplitBtn() {
+    printSplit();
+}
 
+  function setResetBtn() {
+    chronometer.resetClick();
 }
 
 // Start/Stop Button
-btnLeft.addEventListener('click', function () {
-
+let counter;
+btnLeft.addEventListener("click", function() {
+  if (btnLeft.className === "btn start") {
+    setStartBtn();
+    counter = setInterval(() => printTime(), 1000);
+  } else {
+    setStopBtn();
+  }
 });
 
 // Reset/Split Button
-btnRight.addEventListener('click', function () {
-
+btnRight.addEventListener("click", function() {
+  if (btnRight.className === "btn split") {
+    setSplitBtn();
+  } else {
+    setResetBtn();
+  }
 });
