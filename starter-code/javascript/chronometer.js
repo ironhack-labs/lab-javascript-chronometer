4a054ffb3527867af6bce8416799b9f51668cf76
Victor Levy <victorlevygp@gmail.com>
2019-06-19 17:14:20 -0300

    done

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,10 +1,41 @@
 class Chronometer {
-  constructor() {}
-  // startClick() {}
-  // getMinutes() {}
-  // getSeconds() {}
-  // twoDigitsNumber() {}
-  // stopClick() {}
-  // resetClick() {}
-  // splitClick() {}
+  // classe construtora
+  constructor() {
+    this.currentTime = 0;
+    this.intervalId = 0; // guarda o currentTime quando clica no split
+  }
+  // incrementa o valor de currentTime, a cada 10ms. Atribui esse valor ao intervalId
+  startClick() {
+    this.intervalId = setInterval(() => {
+      this.currentTime += 1;
+    }, 10);
+    return this.intervalId
+  }
+  // transforma milisegundos em minutos
+  getMinutes() {
+    return Math.floor((this.currentTime / 100) / 60);
+  }
+  // transforma milisegundos em segundos. % 60 faz ele zerar a cada 60s
+  getSeconds() {
+    return Math.floor((this.currentTime / 100) % 60);
+  }
+  // transforma em string (toString) e adiciona um '0' no começo até a string ter 2 caracteres (padStart)
+  twoDigitsNumber(currentTime) {
+    return currentTime.toString().padStart(2, '0');
+  }
+  // pega os milisegundos e zera quando chega a 100
+  getMiliseconds() {
+    return this.currentTime % 100;
+  }
+  // quebra o setInterval e guarda o valor de currentTime no intervalId
+  stopClick() {
+    clearInterval(this.intervalId);
+    this.intervalId = this.currentTime;
+  }
+  // define o valor de currentTime para 0
+  resetClick() {
+    this.currentTime = 0;
+  }
 }
+
+
--- starter-code/javascript/main.js
+++ starter-code/javascript/main.js
@@ -1,60 +1,112 @@
-var chronometer = new Chronometer();
-var btnLeft     = document.getElementById('btnLeft');
-var btnRight    = document.getElementById('btnRight');
-var minDec      = document.getElementById('minDec');
-var minUni      = document.getElementById('minUni');
-var secDec      = document.getElementById('secDec');
-var secUni      = document.getElementById('secUni');
-var milDec      = document.getElementById('milDec');
-var milUni      = document.getElementById('milUni');
+// instancia um nova Chronometer
+const chronometer = new Chronometer();
 
+// pega os elementos da DOM
+const btnLeft = document.getElementById('btnLeft');
+const btnRight = document.getElementById('btnRight');
 
+const minDec = document.getElementById('minDec');
+const minUni = document.getElementById('minUni');
+
+const secDec = document.getElementById('secDec');
+const secUni = document.getElementById('secUni');
+
+const milDec = document.getElementById('milDec');
+const milUni = document.getElementById('milUni');
+
+// pega a ol onde vai inserir os splits e cria os elementos li
+const splitsOl = document.getElementById('splits');
+const splitsLi = document.createElement('li');
+
+// atualiza o valor do tempo decorrido a cada 10ms
 function printTime() {
-
+  setInterval(() => {
+    printMinutes();
+    printSeconds();
+    printMilliseconds();
+    this.currentTime += 1;
+  }, 10);
 }
 
 function printMinutes() {
-
+  minDec.innerHTML = chronometer.twoDigitsNumber(chronometer.getMinutes())[0];
+  minUni.innerHTML = chronometer.twoDigitsNumber(chronometer.getMinutes())[1];
 }
 
 function printSeconds() {
-
+  secDec.innerHTML = chronometer.twoDigitsNumber(chronometer.getSeconds())[0];
+  secUni.innerHTML = chronometer.twoDigitsNumber(chronometer.getSeconds())[1];
 }
 
 function printMilliseconds() {
+  milDec.innerHTML = chronometer.twoDigitsNumber(chronometer.getMiliseconds())[0];
+  milUni.innerHTML = chronometer.twoDigitsNumber(chronometer.getMiliseconds())[1];
 
 }
 
+// pega o valor do currentTime (minutos, segundos e milisegundos) e atribui o valor(textnode) à li(node), que é inserida na DOM
 function printSplit() {
-
+  const splitTime = `${chronometer.twoDigitsNumber(chronometer.getMinutes())}:${chronometer.twoDigitsNumber(chronometer.getSeconds())}:${chronometer.twoDigitsNumber(chronometer.getMiliseconds())}`
+  let node = document.createElement("li");
+  let textnode = document.createTextNode(splitTime);
+  node.appendChild(textnode);
+  document.getElementById("splits").appendChild(node);
 }
 
+// deixa um valor vazio dentro da ol
 function clearSplits() {
-
+  splitsOl.innerHTML = '';
 }
 
+// chama a função stopClick() e altera a classe e innerHTML na DOM
 function setStopBtn() {
-
+  chronometer.stopClick();
+  btnLeft.className = 'btn start';
+  btnLeft.innerHTML = 'START'
 }
 
+// altera classe e innerHTML na DOM
 function setSplitBtn() {
-
+  btnRight.className = 'btn split';
+  btnRight.innerHTML = 'SPLIT';
 }
 
+// inicia o cronômetro com startClick, começa a escrever os valores na tela com printTime e altera classe e innerHTML na DOM
 function setStartBtn() {
-
+  chronometer.startClick();
+  printTime();
+  btnLeft.className = 'btn stop';
+  btnLeft.innerHTML = 'STOP';
 }
 
+// altera classe e innerHTML na DOM
 function setResetBtn() {
-
+  btnRight.className = 'btn reset';
+  btnRight.innerHTML = 'RESET';
+  
 }
 
+// define um valor de estado em chronometerState
 // Start/Stop Button
+let chronometerState = false;
 btnLeft.addEventListener('click', function () {
-
+  chronometerState = !chronometerState; // alterna em true e false de chronometerState
+  if (chronometerState) { // se for verdateiro, inicia o cronômetro (setStartBtn) e alterna o botão direito para split (setSplitBtn)
+    setStartBtn();
+    setSplitBtn();
+  } else { // se for falso, para o cronômetro (setStopBtn) e alterna o botão direito para reset (setResetBtn)
+    setStopBtn();
+    setResetBtn();
+  }
 });
+
 
 // Reset/Split Button
 btnRight.addEventListener('click', function () {
-
+  if (chronometerState) { // se o cronômetro estiver rodadndo (chronometerState === true), ao clicar no botão direito, cria um split (printSplit)
+    printSplit();
+  } else { // se o cronômetro estiver parado (chronometerState === false), ao clicar no botão direito, reseta o valor (resetClick) e limpa a ol (clearSplits)
+    chronometer.resetClick();
+    clearSplits();
+  }
 });
