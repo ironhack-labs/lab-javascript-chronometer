e6b8ef3fe9e22656634473de0b0b1b2ad98db94f
Sergio Velikopoljski <sergiovelo17@gmail.com>
2019-06-19 10:25:27 -0400

    lab done with miliseconds

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,10 +1,40 @@
 class Chronometer {
-  constructor() {}
-  // startClick() {}
-  // getMinutes() {}
-  // getSeconds() {}
-  // twoDigitsNumber() {}
-  // stopClick() {}
-  // resetClick() {}
-  // splitClick() {}
+  constructor(){
+    this.currentTime = 0;
+    this.intervalId = 0;
+  }
+  startClick() {
+    this.intervalId = setInterval(() => this.currentTime++, 10);
+  }
+  getMinutes() {
+    let minutes = Math.floor((this.currentTime / 100)/60);
+    return minutes;
+  }
+  getSeconds() {
+    let seconds = Math.floor((this.currentTime / 100));
+    seconds = seconds % 60;
+    return seconds;
+  }
+  getMilli(){
+    let mili = this.currentTime;
+    mili = mili % 100;
+    return mili;
+    
+  }
+  twoDigitsNumber(number) {
+    let twoDigit = "";
+    if(number >= 10){
+      twoDigit += number;
+    }else{
+      twoDigit += `0${number}`;
+    }
+    return twoDigit;
+  }
+  stopClick() {
+    clearInterval(this.intervalId);
+  }
+  resetClick() {
+    this.currentTime = 0;
+  }
+  splitClick() {}
 }
--- starter-code/javascript/main.js
+++ starter-code/javascript/main.js
@@ -7,54 +7,95 @@ var secDec      = document.getElementById('secDec');
 var secUni      = document.getElementById('secUni');
 var milDec      = document.getElementById('milDec');
 var milUni      = document.getElementById('milUni');
+let currId = 0;
 
 
 function printTime() {
-
+  currId = setInterval(function() { 
+  printMinutes();
+  printSeconds();
+  printMilliseconds();
+  }, 10);
 }
 
 function printMinutes() {
-
+  let minutes = chronometer.getMinutes();
+  minutes = chronometer.twoDigitsNumber(minutes);
+  $('#minDec').text(minutes[0]);
+  $('#minUni').text(minutes[1]);
 }
 
 function printSeconds() {
-
+  let seconds = chronometer.getSeconds();
+  seconds = chronometer.twoDigitsNumber(seconds);
+  $('#secDec').text(seconds[0]);
+  $('#secUni').text(seconds[1]);
 }
 
 function printMilliseconds() {
-
+  let miliSec = chronometer.getMilli();
+  miliSec = chronometer.twoDigitsNumber(miliSec);
+  $('#milDec').text(miliSec[0]);
+  $('#milDec').text(miliSec[1]);
 }
 
 function printSplit() {
-
+  let time = "";
+  let theMinutes = chronometer.getMinutes();
+  let theSeconds = chronometer.getSeconds();
+  let theMili = chronometer.getMilli();
+  theMinutes = chronometer.twoDigitsNumber(theMinutes);
+  theSeconds = chronometer.twoDigitsNumber(theSeconds);
+  theMili = chronometer.twoDigitsNumber(theMili);
+  time = time + theMinutes + ":" + theSeconds + ":" + theMili;
+  $('#splits').append(`<li>${time}</li>`);
 }
 
 function clearSplits() {
-
+  $('#splits').empty();
 }
 
 function setStopBtn() {
-
+  $('#btnLeft').text('STOP');
+  $('#btnLeft').addClass('stop').removeClass('start');
 }
 
 function setSplitBtn() {
+  $('#btnRight').text('SPLIT');
+  $('#btnRight').addClass('split').removeClass('reset');
 
 }
 
 function setStartBtn() {
-
+  $('#btnLeft').text('START');
+  $('#btnLeft').addClass('start').removeClass('stop');
 }
 
 function setResetBtn() {
-
+  $('#btnRight').text('RESET');
+  $('#btnRight').addClass('reset').removeClass('split');
 }
 
 // Start/Stop Button
 btnLeft.addEventListener('click', function () {
-
+  if($('#btnLeft').text() === 'START'){
+    chronometer.startClick();
+    setStopBtn();
+    setSplitBtn();
+    printTime();
+  }else{
+    chronometer.stopClick();
+    setStartBtn();
+    setResetBtn();
+  }
 });
 
 // Reset/Split Button
 btnRight.addEventListener('click', function () {
-
+    if($('#btnRight').text() === 'SPLIT'){
+      printSplit();
+    }else{
+      chronometer.resetClick();
+      clearSplits();
+    }
 });
