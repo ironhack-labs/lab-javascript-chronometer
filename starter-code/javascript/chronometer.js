e1013bff60fe21894ef4859f467a8e3cde402ed6
Mark Trevail <trevail.m@gmail.com>
2019-07-02 14:28:53 +0200

    Done v1

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,56 +1,68 @@
 class Chronometer {
 
   constructor(chronometerDisplayObj) {
-    this.currentTime = 0;
+    this.currentTimeSecs = 0;
+    this.currentTimeMilliSecs = 0;
     this.intervalId = 0;
+
     this.currentMins = 0;
     this.currentSecs = 0;
+    this.currentMilliSecs = 0;
     this.chronometerDisplay = chronometerDisplayObj;
   }
 
   startClick() {
     this.intervalId = setInterval(() => { 
-      this.currentTime++;
+      this.currentTimeSecs += 0.16 ;     // Speed up by 16x for debugging (should be 0.01)
+      this.currentTimeMilliSecs += 160 ;    // Speed up by 16x for debugging (should be 10)
       this.setTime();
-      this.chronometerDisplay.printTime(this.currentMins, this.currentSecs);
-      console.log(this.currentTime, this.currentSecs, this.currentMins);
-    }, 100);
+      this.chronometerDisplay.printTime(this.currentMins, this.currentSecs, this.currentMilliSecs);
+    }, 10);  // Count every 10ms
   };
 
   stopClick() {
     clearInterval(this.intervalId);
-  }
+  };
 
   resetClick() {
-    this.currentTime = 0;
+    this.currentTimeSecs = 0;
+    this.currentTimeMilliSecs = 0;
     this.setTime();
-    this.chronometerDisplay.printTime(this.currentMins, this.currentSecs);
-  }
+    this.chronometerDisplay.printTime(this.currentMins, this.currentSecs, this.currentMilliSecs);
+    this.chronometerDisplay.clearSplits();
+  };
 
-  getCurrentTimeSecs() {
-    return this.currentTime % 60;
-  }
+  splitClick() {
+    this.chronometerDisplay.printSplit(this.currentMins, this.currentSecs, this.currentMilliSecs);
+  };
 
   getCurrentTimeMins() {
-    return Math.floor( this.currentTime / 60);
+    return Math.floor(this.currentTimeSecs / 60);
+  };
+
+  getCurrentTimeSecs() {
+    return Math.floor(this.currentTimeSecs) % 60;
+  };
+
+  getCurrentTimeMilliSecs() {
+    return Math.floor(this.currentTimeMilliSecs / 10);  // Display shows just first 2 digits of the 3 digit number
   };
 
   twoDigitsNumber(number) {
-    return ("0" + number.toString()).slice(-2);
-  }
+    if(number.toString().length === 1) {
+      return "0" + number.toString();
+    } else if (number.toString().length > 2) {
+      return number.toString().slice(-2);
+    } else {
+      return number.toString();
+    }
+    };
 
   setTime() {
     this.currentMins = this.twoDigitsNumber(this.getCurrentTimeMins());
     this.currentSecs = this.twoDigitsNumber(this.getCurrentTimeSecs());
-  }
+    this.currentMilliSecs = this.twoDigitsNumber(this.getCurrentTimeMilliSecs());  // Need to fix this
+  };
+
 
 };
-
-  
-// // Testing
-
-// myChrono = new Chronometer();
-// myChrono.startClick();
-
-// // myChrono.stopClick()
-// // myChrono.resetClick()
--- starter-code/javascript/chronometerDisplay.js
+++ starter-code/javascript/chronometerDisplay.js
@@ -9,28 +9,34 @@ class ChronometerDisplay {
     this.secUni = document.getElementById('secUni');
     this.milDec = document.getElementById('milDec');
     this.milUni = document.getElementById('milUni');
-  }  
+  }; 
 
-  printTime(mins, secs) {
-    document.getElementById("minDec").innerHTML = mins[0];
-    document.getElementById("minUni").innerHTML = mins[1];
-    document.getElementById("secDec").innerHTML = secs[0];
-    document.getElementById("secUni").innerHTML = secs[1];
-  }
+  printTime(mins, secs, ms) {
+    this.minDec.innerHTML = mins[0];
+    this.minUni.innerHTML = mins[1];
+    this.secDec.innerHTML = secs[0];
+    this.secUni.innerHTML = secs[1];
+    this.milDec.innerHTML = ms[0];
+    this.milUni.innerHTML = ms[1];
+  };
+
+  printSplit(mins, secs, ms) {
+    var newSplitLi = document.createElement('li');
+    newSplitLi.innerHTML = `${mins}:${secs}:${ms}`;
+    document.getElementById("splits").appendChild(newSplitLi);
+  };
+
+  clearSplits() {
+    let splitsList = document.getElementById("splits");
+    while (splitsList.firstChild) {
+      splitsList.removeChild(splitsList.firstChild);
+    };
+  };
+
 };
   
 
-// function printMinutes() {
 
-// }
-
-// function printSeconds() {
-
-// }
-
-// function printMilliseconds() {
-
-// }
 
 // function printSplit() {
 
