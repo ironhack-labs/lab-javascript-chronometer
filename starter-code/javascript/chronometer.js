4d0febca505255e737e7b2e3725de4505d46d12f
Tatijana Rajcic <tatijana.rajcic@essec.edu>
2019-07-02 14:06:53 +0200

    first version without bonuses

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,10 +1,75 @@
-class Chronometer {
-  constructor() {}
-  // startClick() {}
-  // getMinutes() {}
-  // getSeconds() {}
-  // twoDigitsNumber() {}
-  // stopClick() {}
-  // resetClick() {}
-  // splitClick() {}
+function Chronometer() {
+
+  let fixThis = this; 
+  //at this point of the function: "this" refers to the object Chronometer
+
+  this.currentTime = 0;
+  this.minutes = 0;
+  this.seconds = 0;
+  this.intervalId;
+  this.timeFormat;
+
+  // startClick function: keeps track of the time by updating currentTime
+  this.startClick = function(){
+    fixThis.intervalId = setInterval(function(){
+      fixThis.currentTime++; 
+      //we use "fixThis" because using "this" here would refer to the scope of setInterval which is window
+      fixThis.timeFormat = fixThis.setTime(fixThis.setMinutes(),fixThis.setSeconds());
+      printTime(fixThis.timeFormat);
+      return fixThis.timeFormat;
+    },1000)
+  };
+
+  // setMinutes function: converts currentTime into minutes
+  this.setMinutes = function(){
+    if (fixThis.currentTime % 60 === 0) {
+      this.minutes++
+    }
+    return this.minutes;
+  };
+
+  // setSeconds function: converts currentTime into seconds
+  this.setSeconds = function(){
+    if (fixThis.currentTime % 60 === 0) {
+      this.seconds = 0
+    } else {
+      this.seconds++
+    }
+    return this.seconds;
+  };
+
+
+  // twoDigitsNumber function: converts a number into a string with the right format
+  this.twoDigitsNumber = function (number) {
+    if (number >= 0 && number <= 9) {
+      let firstDigit = "0";
+      number = firstDigit + number.toString();
+    } else {
+      number = number.toString();
+    }
+    return number;
+  };
+
+  // setTime function: converts the currentTime into a string with the right format
+  this.setTime = function(minutes,seconds) {
+    let time = [];
+    let minutesString = fixThis.twoDigitsNumber(minutes);
+    let secondsString = fixThis.twoDigitsNumber(seconds);
+    time.push(minutesString, secondsString);
+    return time;
+  };
+
+  // stopClick function: stops the setInterval function
+  this.stopClick = function() {
+    clearInterval(fixThis.intervalId);
+  };
+
+  // resetClick function: resets the setInterval function (= the intervalId stays the same)
+  this.resetClick = function(){
+    fixThis.currentTime = 0;
+    fixThis.seconds = 0;
+    fixThis.minutes = 0;
+    printTime(["00","00"]);
+
+  };
 }
--- starter-code/javascript/main.js
+++ starter-code/javascript/main.js
@@ -1,4 +1,5 @@
 var chronometer = new Chronometer();
+
 var btnLeft     = document.getElementById('btnLeft');
 var btnRight    = document.getElementById('btnRight');
 var minDec      = document.getElementById('minDec');
@@ -7,30 +8,68 @@ var secDec      = document.getElementById('secDec');
 var secUni      = document.getElementById('secUni');
 var milDec      = document.getElementById('milDec');
 var milUni      = document.getElementById('milUni');
+var mySplits    = document.getElementById("splits");
 
 
-function printTime() {
+// Start/Stop Button
+btnLeft.addEventListener('click', function () {
+  if (btnLeft.getAttribute("class") === "btn stop"){
+    btnLeft.innerHTML="START";
+    btnLeft.setAttribute("class","btn start");
+    btnRight.innerHTML = "RESET";
+    btnRight.setAttribute("class","btn reset");
+    chronometer.stopClick();
+  } else {
+    btnLeft.innerHTML="STOP";
+    btnLeft.setAttribute("class","btn stop");
+    btnRight.innerHTML = "SPLIT";
+    btnRight.setAttribute("class","btn split");
+    chronometer.startClick();
+  }
+});
 
+// Reset/Split Button
+btnRight.addEventListener('click', function () {
+  if (btnRight.getAttribute("class") === "btn reset"){
+    chronometer.resetClick();
+    clearSplits();
+  } else {
+    printSplit(`${chronometer.timeFormat[0]}:${chronometer.timeFormat[1]}`);
+  }
+});
+
+function printMinutes(arrayTime) {
+  let minutes = arrayTime[0];
+  minDec.innerHTML = minutes.split('')[0];
+  minUni.innerHTML = minutes.split('')[1];
 }
 
-function printMinutes() {
-
+function printSeconds(arrayTime) {
+  let seconds = arrayTime[1];
+  secDec.innerHTML = seconds.split('')[0];
+  secUni.innerHTML = seconds.split('')[1];
 }
 
-function printSeconds() {
+// function printMilliseconds() {
 
+// }
+
+function printTime(arrayTime) {
+  printMinutes(arrayTime);
+  printSeconds(arrayTime);
+  // printMilliseconds(arrayTime);
 }
 
-function printMilliseconds() {
-
-}
-
-function printSplit() {
-
+function printSplit(string) {
+  let myNewSplit = document.createElement("li");
+  myNewSplit.innerHTML = string;
+  mySplits.appendChild(myNewSplit);
 }
 
 function clearSplits() {
-
+  for (let i=mySplits.children.length-1; i>=0; i--) {
+    mySplits.removeChild(mySplits.children[i]);
+  }
 }
 
 function setStopBtn() {
@@ -48,13 +87,3 @@ function setStartBtn() {
 function setResetBtn() {
 
 }
-
-// Start/Stop Button
-btnLeft.addEventListener('click', function () {
-
-});
-
-// Reset/Split Button
-btnRight.addEventListener('click', function () {
-
-});
