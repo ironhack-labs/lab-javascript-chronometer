5751f8689fdf569ef7effa3c0c594176dda93d31
ioav <ioavlivneh@gmail.com>
2019-07-02 17:19:16 +0200

    ioav-livneh

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,10 +1,50 @@
-class Chronometer {
-  constructor() {}
-  // startClick() {}
-  // getMinutes() {}
-  // getSeconds() {}
-  // twoDigitsNumber() {}
-  // stopClick() {}
-  // resetClick() {}
-  // splitClick() {}
+function Chronometer () {
+  
+  this.currentTime = 0;
+  this.minutesintervalId = "";
+  this.mincounter = 0;
+
+  this.startClick = function() {
+    this.intervalId = setInterval(() => {
+        this.currentTime++
+        printTime()
+      }, 1000)
+  }
+  
+  this.twoDigitsNumber = function(num){
+    if(num<10){
+    return "0" + num.toString();
+    } else{
+      return num.toString();
+    }
+  }
+
+  this.setTime = function(){
+    var minutes = this.twoDigitsNumber((this.currentTime - (this.currentTime % 60))/60);
+    var seconds = this.twoDigitsNumber(this.currentTime % 60);
+    return minutes+seconds;
+  }
+
+  this.stopCLick = function(){
+    clearInterval(this.intervalId);
+  }
+
+  this.resetClick = function(){
+    //reset timer
+    this.currentTime = 0;
+    
+    //clear timer
+    minUni.innerHTML = "0";
+    minDec.innerHTML = "0";
+    secUni.innerHTML = "0";
+    secDec.innerHTML = "0";
+
+    //clear splits
+    var splits = document.getElementById("splits")
+    while (splits.hasChildNodes()) {
+       splits.removeChild(splits.firstChild);
+    }
+ }
+  
 }
+// this.splitClick = functino() {}
--- starter-code/javascript/main.js
+++ starter-code/javascript/main.js
@@ -10,51 +10,41 @@ var milUni      = document.getElementById('milUni');
 
 
 function printTime() {
-
-}
-
-function printMinutes() {
-
-}
-
-function printSeconds() {
-
-}
-
-function printMilliseconds() {
-
-}
-
-function printSplit() {
-
-}
-
-function clearSplits() {
-
-}
-
-function setStopBtn() {
-
-}
-
-function setSplitBtn() {
-
-}
-
-function setStartBtn() {
-
-}
-
-function setResetBtn() {
-
+    var timeToPrint = chronometer.setTime()
+    minUni.innerHTML = timeToPrint[1];
+    minDec.innerHTML = timeToPrint[0];
+    secUni.innerHTML = timeToPrint[3];
+    secDec.innerHTML = timeToPrint[2];
 }
 
 // Start/Stop Button
 btnLeft.addEventListener('click', function () {
-
+    if(document.getElementsByClassName("start")[0]){ //stopped->running
+        btnLeft.innerHTML = "STOP";
+        btnLeft.className = "btn stop";
+        btnRight.innerHTML = "SPLIT";
+        btnRight.className = "btn split";
+        chronometer.startClick()
+    }
+    else{ //running->stopped
+        chronometer.stopCLick()
+        btnLeft.innerHTML = "START";
+        btnLeft.className = "btn start";
+        btnRight.innerHTML = "RESET";
+        btnRight.className = "btn reset"
+    }
 });
 
 // Reset/Split Button
 btnRight.addEventListener('click', function () {
+    if(document.getElementsByClassName("btn reset")[0]){
+        chronometer.resetClick()
+    }
+    else{
+        var newSplit = document.createElement("li");                 // Create a <li> node
+        var splitTime = document.createTextNode(`${chronometer.setTime()[0]+chronometer.setTime()[1]}:${chronometer.setTime()[2]+chronometer.setTime()[3]}`);         // Create a text node
+        newSplit.appendChild(splitTime);                              // Append the text to <li>
+        document.getElementById("splits").appendChild(newSplit);     // Append <li> to <ul> with id="splits"
+    }
 
 });
