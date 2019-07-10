e292d34f1ca0e77c69c60c4a94a95e195273af12
Rebeca Lopez <rebecalopezsaelices@MacBook-Air-de-REBECA.local>
2019-07-10 08:44:07 +0200

    cambios

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,10 +1,42 @@
 class Chronometer {
-  constructor() {}
-  // startClick() {}
-  // getMinutes() {}
-  // getSeconds() {}
-  // twoDigitsNumber() {}
-  // stopClick() {}
-  // resetClick() {}
-  // splitClick() {}
-}
+  constructor() {
+    this.currentTime = 0
+    this.intervalId = ""
+  }
+  startClick() {
+    this.intervalId =  setInterval (()=> {
+       this.currentTime += 1
+    },1000) 
+  } 
+  getMinutes() {
+  if(this.currentTime === 0){
+    return 0
+  }
+    else if(this.currentTime > 60){
+    return parseInt(this.currentTime / 60) //serán minutos sin decimal
+  }
+  }
+  getSeconds() {
+    if(this.currentTime % 60 == 0){ //los números son múltiplos
+      return 0
+    }
+    else{
+      return this.currentTime % 60 
+    }
+  }
+  twoDigitsNumber(num1) {
+    if(num1 <= 9){
+    let a = '0' + num1.toString()
+      return a
+    }
+    return num1.toString()
+  }
+  stopClick() {
+    clearInterval (this.intervalId)
+  }
+  resetClick() {
+    this.currentTime = 0
+  }
+ } 
+
+//   // splitClick() {}
--- starter-code/javascript/main.js
+++ starter-code/javascript/main.js
@@ -51,10 +51,34 @@ function setResetBtn() {
 
 // Start/Stop Button
 btnLeft.addEventListener('click', function () {
+    let leftButton = document.getElementsByClassName("btn start")
+    leftButton[0].setAttribute('class', 'btn stop')
+   
+    let leftStop =document.getElementsByClassName("btn stop")
+    leftStop[0].textContent = "STOP"
 
+    let rightButton = document.getElementsByClassName("btn reset")
+    rightButton[0].setAttribute('class', 'btn split')
+    
+    let rightSplit =document.getElementsByClassName("btn split")
+    rightSplit[0].textContent = "SPLIT"
+/*--*/
+
+    
+    // let leftButton2 = document.getElementsByClassName("btn stop")
+    // leftButton2[0].setAttribute('class', 'btn start')
+
+    // let leftStop2 =document.getElementsByClassName("btn start")
+    // leftStop2[0].textContent = "START"
+
+    // let rightButton2 = document.getElementsByClassName("btn split")
+    // rightButton2[0].setAttribute('class', 'btn reset')
+    
+    // let rightSplit2 =document.getElementsByClassName("btn reset")
+    // rightSplit2[0].textContent = "RESET"
 });
 
 // Reset/Split Button
 btnRight.addEventListener('click', function () {
-
+    
 });
