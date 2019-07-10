1fc52fd0f26a0f29a3e85bc952fdc5ec104ba27f
Iváne <iromcan1984@gmail.com>
2019-07-10 09:05:31 +0200

    primer commit

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,10 +1,35 @@
 class Chronometer {
-  constructor() {}
-  // startClick() {}
-  // getMinutes() {}
-  // getSeconds() {}
-  // twoDigitsNumber() {}
-  // stopClick() {}
-  // resetClick() {}
-  // splitClick() {}
+  constructor() {
+    this.currentTime = 0
+    this.intervalId = ""
+  }
+  startClick() {
+    this.intervalId = setInterval(() => {
+      this.currentTime++
+    }, 1000)
+
+  }
+  getMinutes() {
+    let minutes = this.currentTime / 60000
+    return Math.floor(minutes)
+  }
+  getSeconds() {
+    let seconds = this.currentTime / 1000
+    return seconds
+  }
+  twoDigitsNumber(number) {
+    alert(typeof number)
+    // var str = 
+    if (number < 10) { return (`0${number}`) }
+    else return number
+  }
+  stopClick() {
+
+  }
+  resetClick() {
+    this.currentTime = 0
+  }
+  splitClick() {
+
+  }
 }
--- starter-code/javascript/main.js
+++ starter-code/javascript/main.js
@@ -1,12 +1,12 @@
 var chronometer = new Chronometer();
-var btnLeft     = document.getElementById('btnLeft');
-var btnRight    = document.getElementById('btnRight');
-var minDec      = document.getElementById('minDec');
-var minUni      = document.getElementById('minUni');
-var secDec      = document.getElementById('secDec');
-var secUni      = document.getElementById('secUni');
-var milDec      = document.getElementById('milDec');
-var milUni      = document.getElementById('milUni');
+var btnLeft = document.getElementById('btnLeft');
+var btnRight = document.getElementById('btnRight');
+var minDec = document.getElementById('minDec');
+var minUni = document.getElementById('minUni');
+var secDec = document.getElementById('secDec');
+var secUni = document.getElementById('secUni');
+var milDec = document.getElementById('milDec');
+var milUni = document.getElementById('milUni');
 
 
 function printTime() {
@@ -14,11 +14,16 @@ function printTime() {
 }
 
 function printMinutes() {
+    let dis_minutes = chronometer.getMinutes()
+    console.log(dis_minutes)
+    minUni.innerHTML = dis_minutes
 
 }
 
 function printSeconds() {
-
+    let dis_seconds = chronometer.getSeconds()
+    console.log(dis_seconds)
+    secUni.innerHTML = dis_seconds
 }
 
 function printMilliseconds() {
@@ -26,7 +31,10 @@ function printMilliseconds() {
 }
 
 function printSplit() {
-
+    let right = document.getElementById("btnRight")
+    document.getElementById("btnRight").className
+    right.setAttribute("class", "btn split")
+    right.innerHTML = "SPLIT"
 }
 
 function clearSplits() {
@@ -46,15 +54,50 @@ function setStartBtn() {
 }
 
 function setResetBtn() {
-
+    let right = document.getElementById("btnRight")
+    document.getElementById("btnRight").className
+    right.setAttribute("class", "btn reset")
+    right.innerHTML = "RESET"
 }
 
 // Start/Stop Button
 btnLeft.addEventListener('click', function () {
+    let left = document.getElementById("btnLeft")
+    let class_name = document.getElementById("btnLeft").className
+    if (class_name === "btn start") {
+        left.setAttribute("class", "btn stop")
+        left.innerHTML = "STOP"
+        printSplit()
+        chronometer.startClick()
+        printMinutes()
+        printSeconds()
+
+    }
+    else
+        if (class_name === "btn stop") {
+            left.setAttribute("class", "btn start")
+            left.innerHTML = "START"
+            setResetBtn()
+            chronometer.stopClick()
+        }
+
+
+
 
 });
 
 // Reset/Split Button
 btnRight.addEventListener('click', function () {
+    let right = document.getElementById("btnRight")
+    let class_name = document.getElementById("btnRight").className
+    if (class_name === "btn reset") {
+        right.setAttribute("class", "btn split")
+        right.innerHTML = "SPLIT"
 
+    }
+    else
+        if (class_name === "btn split") {
+            right.setAttribute("class", "btn reset")
+            right.innerHTML = "RESET"
+        }
 });
