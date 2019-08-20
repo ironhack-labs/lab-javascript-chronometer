2f07af265cfb28987b0bcbed600cd74f0069ebb7
Elisa <elisam.esquivel@gmail.com>
2019-08-20 18:11:43 -0500

    Cronometro listo

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,10 +1,37 @@
-class Chronometer {
-  constructor() {}
-  // startClick() {}
-  // getMinutes() {}
-  // getSeconds() {}
-  // twoDigitsNumber() {}
-  // stopClick() {}
-  // resetClick() {}
+function Chronometer() {
+  this.currentTime=0
+  this.intervalId=0
+  this.startClick=function() {
+    this.intervalId = setInterval( () => {
+      return this.currentTime++
+    },1000)
+  }
+  this.getMinutes = function() {
+    const minutes = Math.floor(this.currentTime/60)
+    return minutes;
+  }
+  this.getSeconds= function (){
+    if (this.currentTime < 60){
+      return this.currentTime
+    } else{
+      const restingSec = Math.floor(this.currentTime%60)
+      return restingSec
+    }
+  }
+  this.twoDigitsNumber = function(value) {
+    if (value == 0){
+      return `00`
+    } else if (value < 10){
+      return `0${value}`
+    } else{
+      return `${value}`
+    }
+  }
+  this.stopClick = function(){
+    clearInterval(this.intervalId)
+  }
+  this.resetClick = function(){
+    this.currentTime = 0
+  }
   // splitClick() {}
 }
--- starter-code/javascript/main.js
+++ starter-code/javascript/main.js
@@ -1,4 +1,9 @@
-var chronometer = new Chronometer();
+//var chronometer = new Chronometer()
+//Chronometer.prototype = Object.create(Chronometer.prototype)
+//Chronometer.prototype.constructor = Chronometer
+const cronometro = new Chronometer()
+console.log(cronometro)
+
 var btnLeft     = document.getElementById('btnLeft');
 var btnRight    = document.getElementById('btnRight');
 var minDec      = document.getElementById('minDec');
@@ -10,51 +15,92 @@ var milUni      = document.getElementById('milUni');
 
 
 function printTime() {
-
+    printSeconds()
+    printMinutes()
 }
 
 function printMinutes() {
-
+    const minutes = cronometro.getMinutes()
+    if (minutes < 10){
+        minUni.innerHTML = cronometro.getMinutes()
+    } else{
+        minDec.innerHTML = cronometro.getMinutes()
+        minUni.innerHTML = ''
+    }
 }
 
 function printSeconds() {
-
+    const seconds = cronometro.getSeconds()
+    if (seconds < 10){
+        secDec.innerHTML = 0
+        secUni.innerHTML = cronometro.getSeconds()
+    }
+    else{
+        secDec.innerHTML = cronometro.getSeconds()
+        secUni.innerHTML = ''
+    }
 }
 
 function printMilliseconds() {
-
+     
 }
 
 function printSplit() {
-
+    const splitList = document.getElementById('splits')
+    const splitElement = document.createElement('li')
+    const minutes = cronometro.twoDigitsNumber(cronometro.getMinutes())
+    const seconds = cronometro.twoDigitsNumber(cronometro.getSeconds())
+    splitElement.innerHTML = `${minutes}:${seconds}`
+    splitList.appendChild(splitElement)
 }
 
 function clearSplits() {
-
+    cronometro.resetClick()
+    const splitList = document.getElementById('splits')
+    splitList.innerHTML = ''
 }
 
 function setStopBtn() {
-
+    btnLeft.setAttribute('class','btn stop')
+    btnLeft.innerHTML = 'STOP'
 }
 
 function setSplitBtn() {
-
+    btnRight.setAttribute('class','btn split')
+    btnRight.innerHTML = 'SPLIT'
 }
 
 function setStartBtn() {
-
+    btnLeft.setAttribute('class','btn start')
+    btnLeft.innerHTML = 'START'
 }
 
 function setResetBtn() {
-
+    btnRight.setAttribute('class','btn reset')
+    btnRight.innerHTML = 'RESET'
 }
 
 // Start/Stop Button
 btnLeft.addEventListener('click', function () {
-
+    const intervalId = setInterval(() => {
+        printTime()
+    })
+    if(btnLeft.className == 'btn start'){
+        setStopBtn()
+        setSplitBtn()
+        cronometro.startClick()
+    } else{
+        setStartBtn()
+        setResetBtn()
+        cronometro.stopClick()
+    }
 });
 
 // Reset/Split Button
 btnRight.addEventListener('click', function () {
-
+    if(btnRight.className == 'btn reset'){
+        clearSplits()
+    } else{
+        printSplit()
+    }
 });
