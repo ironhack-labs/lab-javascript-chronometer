54e1be9a0ff58a13af5b43942ad47f07f39086ba
Juan Vidal <juanvidaldom@gmail.com>
2019-08-20 22:10:19 -0500

    first commit

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,10 +1,33 @@
-class Chronometer {
-  constructor() {}
-  // startClick() {}
-  // getMinutes() {}
-  // getSeconds() {}
-  // twoDigitsNumber() {}
-  // stopClick() {}
-  // resetClick() {}
+function Chronometer() {
+  this.currentTime = 0
+  this.intervalId = 0
+
+  this.startClick = function() {
+    this.intervalId = setInterval(() => {
+      this.currentTime++
+    }, 1000)
+  }
+  this.getMinutes = function(){
+    const minutes = Math.floor(this.currentTime/60)
+    return minutes
+  }
+  this.getSeconds= function(){
+    const seconds = Math.floor(this.currentTime % 60)
+    return seconds
+  }
+  this.twoDigitsNumber = function(num){
+    if(num<10){
+      let str = `0` + `${num}`
+      return str
+    } else {
+      return num.toString()
+    }
+  }
+  this.stopClick = function(){
+    clearInterval(this.intervalId)
+  }
+  this.resetClick = function(){
+    this.currentTime = 0
+  }
   // splitClick() {}
 }
--- starter-code/javascript/main.js
+++ starter-code/javascript/main.js
@@ -8,53 +8,95 @@ var secUni      = document.getElementById('secUni');
 var milDec      = document.getElementById('milDec');
 var milUni      = document.getElementById('milUni');
 
-
 function printTime() {
+    printMinutes()
+    printSeconds()
 
 }
 
 function printMinutes() {
-
+    setInterval(() => {
+        minDec.innerHTML = chronometer.twoDigitsNumber(chronometer.getMinutes())[0]
+        minUni.innerHTML = chronometer.twoDigitsNumber(chronometer.getMinutes())[1]
+    },6000)
 }
+
+//PORQUE PTM SON 6000 EN LUGAR DE 60000????????????????????????
 
 function printSeconds() {
-
-}
-
-function printMilliseconds() {
-
-}
-
-function printSplit() {
-
+    setInterval(() => {
+        secDec.innerHTML = chronometer.twoDigitsNumber(chronometer.getSeconds())[0]
+        secUni.innerHTML = chronometer.twoDigitsNumber(chronometer.getSeconds())[1]
+    },1000);
 }
 
 function clearSplits() {
-
-}
-
-function setStopBtn() {
-
+    let olElement = document.getElementById('splits')
+    olElement.innerHTML = ""
 }
 
 function setSplitBtn() {
-
-}
-
-function setStartBtn() {
-
+    let olElement = document.getElementById('splits')
+    let liElement = document.createElement('li')
+    olElement.appendChild(liElement)
+    let itemElement = document.createTextNode(`${minDec.innerHTML}`+`${minUni.innerHTML}` + ` : ` +`${secDec.innerHTML}`+`${secUni.innerHTML}`)
+    liElement.appendChild(itemElement)
 }
 
 function setResetBtn() {
-
+    chronometer.resetClick()
 }
 
 // Start/Stop Button
 btnLeft.addEventListener('click', function () {
 
+    if(btnLeft.innerHTML === 'START'){
+        btnLeft.innerHTML = 'STOP'
+        btnLeft.classList.remove('start')
+        btnLeft.classList.add('stop')
+
+        btnRight.innerHTML = 'SPLIT'
+        btnRight.classList.remove('reset')
+        btnRight.classList.add('split')
+
+        chronometer.startClick()
+        printTime()
+
+    } else {
+        btnLeft.innerHTML = 'START'
+        btnLeft.classList.remove('stop')
+        btnLeft.classList.add('start')
+
+        btnRight.innerHTML = 'RESET'
+        btnRight.classList.remove('split')
+        btnRight.classList.add('reset')
+
+        chronometer.stopClick()
+
+    }
+
 });
+
 
 // Reset/Split Button
 btnRight.addEventListener('click', function () {
+    if(btnRight.innerHTML === 'RESET'){
+        setResetBtn()
+        clearSplits()
+    } else if(btnRight.innerHTML === 'SPLIT'){
+        setSplitBtn()
+    }
+    
 
 });
+
+// if(btnLeft.innerHTML === 'RESET'){
+    //     btnRight.innerHTML = 'SPLIT'
+    //     btnRight.classList.remove('reset')
+    //     btnRight.classList.add('split')
+
+    // } else {
+    //     btnRight.innerHTML = 'RESET'
+    //     btnRight.classList.remove('split')
+    //     btnRight.classList.add('reset')
+    // }
