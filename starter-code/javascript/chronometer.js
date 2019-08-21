061389566f32054ce96b65468ceaf1b4285817df
Ro <rodrigo_betan@hotmail.com>
2019-08-20 22:13:25 -0500

    al fin acabado

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,10 +1,42 @@
-class Chronometer {
-  constructor() {}
-  // startClick() {}
-  // getMinutes() {}
-  // getSeconds() {}
-  // twoDigitsNumber() {}
-  // stopClick() {}
-  // resetClick() {}
-  // splitClick() {}
+function Chronometer() {
+  this.currentTime = 0
+  this.intervalId = 0
+
+  this.startClick = function () {
+    this.intervalId = setInterval(() => {
+      return this.currentTime++}, 1000)
+  }
+
+  this.getMinutes = function () {
+    let minutes = Math.floor(this.currentTime / 60)
+    return minutes
+  }
+
+  this.getSeconds = function () {
+    let seconds = this.currentTime % 60
+    return seconds
+  }
+
+  this.twoDigitsNumber = function (number) {
+    if (number < 10) {
+      let string = `0${ number }`
+      return string} 
+    
+    else {return number.toString()  }
+  }
+
+  // ya esta bien
+  this.stopClick = function (number) {
+      clearInterval(this.intervalId)
+  }
+
+  // ya esta bien 
+  this.resetClick = function (number) {
+    this.currentTime = 0
+  }
+
+  this.splitClick = function (number) {
+
+  }
+
 }
