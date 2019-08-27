b16a166d887471b8b0278cafe2fcc632a7c0d614
pauromeropau <pauromeropau@gmail.com>
2019-08-27 17:44:38 +0200

    Iteration 1

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,10 +1,39 @@
 class Chronometer {
-  constructor() {}
-  // startClick() {}
-  // getMinutes() {}
-  // getSeconds() {}
-  // twoDigitsNumber() {}
-  // stopClick() {}
-  // resetClick() {}
+  constructor() {
+    this.currentTime = 0;
+    this.minutes = 0;
+    this.seconds = 0;
+    this.intervalId;
+    this.twoDigitsNumber;
+    this.stopClick;
+  }
+  startClick() {
+    this.intervalId = setInterval(() => {
+      this.currentTime += 1;
+    }, 1000);
+  }
+  getMinutes() {
+    this.minutes = Math.floor(this.currentTime / 60);
+    return this.minutes;
+  }
+  getSeconds() {
+    this.seconds = this.currentTime % 60;
+    return this.seconds;
+  }
+  twoDigitsNumber(number) {
+    let numTwo = number.toString();
+    if (numTwo.length > 1) {
+      return numTwo;
+    }
+    return "0" + numTwo;
+  }
+  stopClick() {
+    clearInterval(this.intervalId);
+  }
+  resetClick() {
+    
+  }
+
+
   // splitClick() {}
 }
