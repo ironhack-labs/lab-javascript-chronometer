f375f1bf88da617ecdbb763f83f0fa40483783e8
Ester-Martinez <martinez.ester@gmail.com>
2019-08-27 16:44:28 +0200

    chronometer finished

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,10 +1,41 @@
 class Chronometer {
-  constructor() {}
-  // startClick() {}
-  // getMinutes() {}
-  // getSeconds() {}
-  // twoDigitsNumber() {}
-  // stopClick() {}
-  // resetClick() {}
+  constructor() {
+    this.currentTime = 0;
+    this.intervalId;
+  }
+
+  startClick() {
+    this.intervalId = setInterval(() => {
+      this.currentTime++;
+    }, 1000);
+  }
+  getMinutes() {
+    if (this.currentTime === 0) {
+      return 0;
+    }
+    if (this.currentTime > 60) {
+      return Math.floor(this.currentTime / 60);
+    }
+  }
+  getSeconds() {
+    return this.currentTime % 60;
+  }
+  twoDigitsNumber(time) {
+    let twoDigits = time.toString();
+
+    if (twoDigits.length === 1) {
+      return "0" + twoDigits;
+    }
+    else {
+      return twoDigits;
+    }
+  }
+  stopClick() {
+
+    this.intervalId = clearInterval();
+  }
+  resetClick() {
+    this.currentTime = 0;
+  }
   // splitClick() {}
 }
