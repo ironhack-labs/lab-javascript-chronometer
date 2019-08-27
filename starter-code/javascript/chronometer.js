155ca14130c33c469c925af863f637cc234db2bf
Fernando de la Torre <fer@MacBook-Pro-de-Fer-2.local>
2019-08-27 19:26:14 +0200

    [MAD-PP] Christopher Benavides, Fernando de la Torre

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,10 +1,32 @@
 class Chronometer {
-  constructor() {}
-  // startClick() {}
-  // getMinutes() {}
-  // getSeconds() {}
-  // twoDigitsNumber() {}
-  // stopClick() {}
-  // resetClick() {}
+  constructor() {
+
+    this.currentTime=0
+    this.intervalId
+    this.miliSeconds=0
+  }
+  startClick() {
+    this.intervalId= setInterval(() => {this.currentTime++}, 1000);
+    setInterval(() => {this.miliSeconds++}, 10);
+
+ 
+  };
+  getMinutes() {
+    return Math.floor(this.currentTime/60);
+  }
+  getSeconds() {
+    return this.currentTime%60;
+  }
+  twoDigitsNumber(number) {
+    return number<10? number=`0${number}` : number.toString();
+  }
+  stopClick() {
+    clearInterval(this.intervalId);
+    clearInterval(this.miliSeconds);
+
+  }
+  resetClick() {
+    this.currentTime=0;
+  }
   // splitClick() {}
-}
+};
--- starter-code/javascript/main.js
+++ starter-code/javascript/main.js
@@ -10,19 +10,35 @@ var milUni      = document.getElementById('milUni');
 
 
 function printTime() {
-
+  setInterval(()=>{
+    printMinutes();
+    printSeconds();
+  },1000);
+  setInterval(()=>{
+    if (chronometer.miliSeconds<100) {
+      printMilliseconds()
+    }
+    else {
+      chronometer.miliSeconds=0;
+    }
+    
+  },10);
+  
 }
 
 function printMinutes() {
-
+  minDec.innerHTML= chronometer.twoDigitsNumber(chronometer.getMinutes())[0];
+  minUni.innerHTML= chronometer.twoDigitsNumber(chronometer.getMinutes())[1];
 }
 
 function printSeconds() {
-
+  secUni.innerHTML= chronometer.twoDigitsNumber(chronometer.getSeconds())[1];
+  secDec.innerHTML= chronometer.twoDigitsNumber(chronometer.getSeconds())[0];
 }
 
 function printMilliseconds() {
-
+  milDec.innerHTML=chronometer.twoDigitsNumber(chronometer.miliSeconds)[0];
+  milUni.innerHTML=chronometer.twoDigitsNumber(chronometer.miliSeconds)[1];
 }
 
 function printSplit() {
@@ -49,12 +65,43 @@ function setResetBtn() {
 
 }
 
+let isAnimationActive = true;
 // Start/Stop Button
 btnLeft.addEventListener('click', function () {
-
+  isAnimationActive = !isAnimationActive;
+  if(isAnimationActive) {
+    btnLeft.innerHTML="START";
+    btnLeft.className="btn start";
+  
+    btnRight.innerHTML="RESET";
+    btnRight.className="btn reset";
+    chronometer.stopClick();
+  }
+  else {
+    chronometer.startClick();
+    printTime();
+    btnRight.innerHTML="SPLIT";
+    btnRight.className="btn split";
+    btnLeft.innerHTML="STOP";
+    btnLeft.className="btn stop";
+    
+  }
 });
+let newSplit= document.querySelector("#splits");
 
 // Reset/Split Button
 btnRight.addEventListener('click', function () {
-
+  if(btnRight.innerHTML=="SPLIT") {
+    // document.querySelector("body").style.backgroundColor = "red";
+    newSplit.appendChild(document.createElement("li")).innerHTML= `${chronometer.twoDigitsNumber(chronometer.getMinutes())}:${chronometer.twoDigitsNumber(chronometer.getSeconds())}:00`
+  }
+  else {
+    chronometer.currentTime= 0;
+    printMinutes();
+    printSeconds();
+    while (newSplit.firstChild) {
+      newSplit.removeChild(newSplit.firstChild);
+    }
+  }
+  
 });
