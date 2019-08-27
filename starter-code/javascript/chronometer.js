25508d67de42e7df481218701437c3448d96324e
Antonio Llamas <antoniollamasln@gmail.com>
2019-08-27 16:35:09 +0200

    Add first iteration

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,10 +1,20 @@
 class Chronometer {
-  constructor() {}
-  // startClick() {}
-  // getMinutes() {}
-  // getSeconds() {}
-  // twoDigitsNumber() {}
-  // stopClick() {}
-  // resetClick() {}
+  constructor() {
+
+    this.currentTime = 0;
+    this.intervalId;
+  }
+
+  startClick() { this.intervalId = setInterval(() => { this.currentTime++ }, 1000) }
+
+  getMinutes() { return Math.floor(this.currentTime / 60) }
+
+  getSeconds() { return this.currentTime % 60 }
+
+  twoDigitsNumber(digitToFormat) { return (digitToFormat < 10) ? `0${digitToFormat}` : `${digitToFormat}` }
+
+  stopClick() { clearInterval(this.intervalId) }
+
+  resetClick() { this.currentTime = 0 }
   // splitClick() {}
 }
