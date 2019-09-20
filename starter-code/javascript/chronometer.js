3465a304d25bfb7e4297dd0652987119b12e476c
Gabriel Zsigmond <gaazsig@gmail.com>
2019-09-20 11:48:16 -0300

    timer functions working and getting minutes and sec

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,9 +1,23 @@
 class Chronometer {
-  constructor() {}
-  // startClick() {}
-  // getMinutes() {}
-  // getSeconds() {}
-  // twoDigitsNumber() {}
+  constructor() {
+    this.currentTime = 0;
+    this.intervalId;
+  }
+  startClick() {
+    this.intervalId = setInterval(() => {
+      this.currentTime += 1;
+      console.log("add");
+    }, 1000);
+  }
+  getMinutes() {
+    return Math.floor(this.currentTime / 60);
+  }
+  getSeconds() {
+    return Math.floor(this.currentTime % 60);
+  }
+  twoDigitsNumber(num) {
+    return num.toString().length < 2 ? "0"+num : num.toString()
+  }
   // stopClick() {}
   // resetClick() {}
   // splitClick() {}
