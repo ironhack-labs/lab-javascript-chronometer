6801b516f89b66ec9fffc72612aefc720239623c
unknown <rober.araya.gil@gmail.com>
2019-09-21 14:59:53 +0200

    Done in seconds

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,10 +1,35 @@
 class Chronometer {
-  constructor() {}
-  // startClick() {}
-  // getMinutes() {}
-  // getSeconds() {}
-  // twoDigitsNumber() {}
-  // stopClick() {}
-  // resetClick() {}
+  constructor() {
+    this.currentTime = 0
+    this.intervalId = 0
+  }
+  startClick() {
+    this.intervalId = setInterval(() => {
+      this.currentTime += 1
+    }, 1000);
+  }
+  getMinutes() {
+      const minutes = Number((Math.floor(this.currentTime / 60)).toFixed(0))
+      return minutes
+  }
+  getSeconds() {
+      return this.currentTime % 60
+  }
+  twoDigitsNumber(item) {
+    let result = ''
+    if (item <10) {
+      result = '0' + item
+    }else{
+      result = item.toString(10)
+    }
+
+    return result
+  }
+  stopClick() {
+    clearInterval(this.intervalId)
+  }
+  resetClick() {
+    this.currentTime = 0
+  }
   // splitClick() {}
 }
--- starter-code/javascript/main.js
+++ starter-code/javascript/main.js
@@ -7,18 +7,30 @@ var secDec      = document.getElementById('secDec');
 var secUni      = document.getElementById('secUni');
 var milDec      = document.getElementById('milDec');
 var milUni      = document.getElementById('milUni');
+const ol        = document.querySelector('#splits')
 
 
 function printTime() {
-
+    let print = setInterval(()=> {
+        printMinutes()
+        printSeconds()
+    },1000)
+    
+    
 }
 
 function printMinutes() {
-
+    let minutes = chronometer.getMinutes()
+    minutes = chronometer.twoDigitsNumber(minutes)
+    minUni.innerHTML = minutes[1]
+    minDec.innerHTML = minutes[0]
 }
 
 function printSeconds() {
-
+    let seconds = chronometer.getSeconds()
+    seconds =  chronometer.twoDigitsNumber(seconds)
+    secUni.innerHTML = seconds[1]
+    secDec.innerHTML = seconds[0]
 }
 
 function printMilliseconds() {
@@ -26,35 +38,72 @@ function printMilliseconds() {
 }
 
 function printSplit() {
+    let minutes = chronometer.getMinutes()
+    minutes = chronometer.twoDigitsNumber(minutes)
+    let seconds = chronometer.getSeconds()
+    seconds =  chronometer.twoDigitsNumber(seconds)
+    let nowTime = `${minutes}:${seconds}`    
+    let line = document.createElement('li')
+    line.innerHTML = ` ${nowTime}`
+    ol.appendChild(line)
 
 }
 
 function clearSplits() {
-
+    let intervals = ol.querySelectorAll('li')
+    for (i = 0; i < intervals.length; i++){
+        intervals[i].remove()
+    }
 }
 
 function setStopBtn() {
-
+    btnLeft.classList.add('stop')
+    btnLeft.classList.remove('start')
+    btnLeft.innerHTML = 'STOP'
 }
 
 function setSplitBtn() {
-
+    btnRight.classList.add('split')
+    btnRight.classList.remove('reset')
+    btnRight.innerHTML = 'SPLIT'
 }
 
 function setStartBtn() {
-
+    btnLeft.classList.add('start')
+    btnLeft.classList.remove('stop')
+    btnLeft.innerHTML = 'START'
 }
 
 function setResetBtn() {
-
+    btnRight.classList.add('reset')
+    btnRight.classList.remove('split')
+    btnRight.innerHTML = 'RESET'
 }
 
 // Start/Stop Button
 btnLeft.addEventListener('click', function () {
+    if (chronometer.currentTime === 0){
+        setStopBtn()
+        setSplitBtn()
+        chronometer.startClick()
+        printTime()
+    }else{
+        setStartBtn()
+        setResetBtn()
+        chronometer.stopClick()
+    }
 
 });
 
 // Reset/Split Button
 btnRight.addEventListener('click', function () {
+    
+    if (btnRight.classList[1] === 'reset'){
+        chronometer.resetClick()
+        clearSplits()
+    }else{
+        printSplit()
 
+    }
+   
 });
