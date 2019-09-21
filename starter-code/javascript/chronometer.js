501437a68dd3e05a16232272d1c9f7e3ed3197f9
Gabriel Zsigmond <gaazsig@gmail.com>
2019-09-21 13:09:28 -0300

    everything working smoothly :)

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,13 +1,18 @@
 class Chronometer {
   constructor() {
     this.currentTime = 0;
+    this.mili = 0;
     this.intervalId;
+    this.miliIntervalId;
   }
   startClick() {
     this.intervalId = setInterval(() => {
       this.currentTime += 1;
-      console.log("add");
     }, 1000);
+
+    this.miliIntervalId = setInterval(() => {
+      this.mili < 100 ? (this.mili += 1) : (this.mili = 0);
+    }, 10);
   }
   getMinutes() {
     return Math.floor(this.currentTime / 60);
@@ -15,10 +20,19 @@ class Chronometer {
   getSeconds() {
     return Math.floor(this.currentTime % 60);
   }
-  twoDigitsNumber(num) {
-    return num.toString().length < 2 ? "0"+num : num.toString()
+  getMilisecondes() {
+    return this.mili;
   }
-  // stopClick() {}
-  // resetClick() {}
-  // splitClick() {}
+  twoDigitsNumber(num) {
+    return num.toString().length < 2 ? "0" + num : num.toString();
+  }
+  stopClick() {
+    clearInterval(this.intervalId);
+    clearInterval(this.miliIntervalId);
+  }
+  resetClick() {
+    this.currentTime = 0;
+    this.mili = 0;
+  }
+  splitClick() {}
 }
