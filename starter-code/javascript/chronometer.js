c81097a85b86e6a148201b8d87ee99ef7f1f9e24
Evgeny <esryabchenkov@gmail.com>
2019-10-24 15:36:39 +0200

    done

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,10 +1,36 @@
 class Chronometer {
-  constructor() {}
-  // startClick() {}
-  // getMinutes() {}
-  // getSeconds() {}
-  // twoDigitsNumber() {}
-  // stopClick() {}
-  // resetClick() {}
-  // splitClick() {}
+  constructor() {
+    this.currentTime = 0
+  } 
+  startClick(){
+      let cronObj = this
+      this.intervalId = setInterval(function(){
+        cronObj.currentTime += 1
+      },10)
+    }
+
+  getMinutes() {
+    return Math.floor(this.currentTime/6000)
+  }
+  getSeconds() {
+    return (Math.floor(this.currentTime/100)%60)
+  }
+  getMilSeconds() {
+    return Math.floor(this.currentTime%100)
+  }
+  twoDigitsNumber(number) {
+    return (number<10 ? `0` : '') + number
+  }
+  stopClick() {
+    return clearInterval(this.intervalId)
+  }
+
+  resetClick() {
+    this.currentTime=0
+  }
+
+
+
+
+
 }
--- starter-code/javascript/main.js
+++ starter-code/javascript/main.js
@@ -7,54 +7,122 @@ var secDec      = document.getElementById('secDec');
 var secUni      = document.getElementById('secUni');
 var milDec      = document.getElementById('milDec');
 var milUni      = document.getElementById('milUni');
+var splits      = document.getElementById('splits');
+var splitList = []
 
+var state = {
+    isStart: false,
+  }
 
 function printTime() {
-
+    if(state.isStart){
+        secInt = setInterval(function(){
+            printMinutes()
+            printSeconds()
+            printMilliseconds()
+        },10)
+    }
+    else if (!state.isStart) clearInterval(secInt)
 }
 
 function printMinutes() {
-
+    let a = chronometer.twoDigitsNumber(chronometer.getMinutes())
+    minDec.innerHTML = a[0]
+    minUni.innerHTML = a[1]
 }
 
 function printSeconds() {
-
+    let a = chronometer.twoDigitsNumber(chronometer.getSeconds())
+    secDec.innerHTML = a[0]
+    secUni.innerHTML = a[1]
 }
 
 function printMilliseconds() {
-
+    let a = chronometer.twoDigitsNumber(chronometer.getMilSeconds())
+    milDec.innerHTML = a[0]
+    milUni.innerHTML = a[1]
 }
 
 function printSplit() {
-
+    clearSplits()
+    splitList.forEach(elem => splits.innerHTML += `<li>${elem}</li>`)
 }
 
 function clearSplits() {
-
+    splits.innerHTML = ''
 }
 
 function setStopBtn() {
-
+if (state.isStart)
+    {
+        btnLeft.innerHTML = "STOP"
+        btnLeft.classList.remove("start")
+        btnLeft.classList.add("stop")
+        chronometer.startClick()
+        console.log(chronometer)
+    }
 }
 
 function setSplitBtn() {
-
+if (state.isStart)
+    {
+        btnRight.innerHTML = "SPLIT"
+        btnRight.classList.remove("reset")
+        btnRight.classList.add("split")
+    }
 }
 
 function setStartBtn() {
-
+if (!state.isStart)
+    {
+        btnLeft.innerHTML = "START"
+        btnLeft.classList.remove("stop")
+        btnLeft.classList.add("start")
+        state.isReset = true
+        chronometer.stopClick()
+    }
 }
 
 function setResetBtn() {
-
+if (!state.isStart)
+    {
+        btnRight.innerHTML = "RESET"
+        btnRight.classList.remove("split")
+        btnRight.classList.add("reset")
+    }
 }
 
 // Start/Stop Button
 btnLeft.addEventListener('click', function () {
-
+    state.isStart = !state.isStart
+    printTime()
+    setStartBtn()
+    setStopBtn()
+    setSplitBtn()
+    setResetBtn()
 });
 
 // Reset/Split Button
 btnRight.addEventListener('click', function () {
-
-});
+    if(state.isStart)
+    {
+        let m = chronometer.twoDigitsNumber(chronometer.getMinutes())
+        let s = chronometer.twoDigitsNumber(chronometer.getSeconds())
+        let ms = chronometer.twoDigitsNumber(chronometer.getMilSeconds())
+        splitList.push(` ${m} ${s} ${ms}`)
+        console.log(splitList)
+        printSplit()
+    }
+    else
+    {
+        clearSplits()
+        chronometer.resetClick()
+        minDec.innerHTML = 0
+        minUni.innerHTML = 0
+        secDec.innerHTML = 0
+        secUni.innerHTML = 0
+        milDec.innerHTML = 0
+        milUni.innerHTML = 0
+        splitList = []
+    }
+})
