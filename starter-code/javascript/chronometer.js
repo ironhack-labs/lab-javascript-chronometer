8aab90b6eb375d079f245f13b0c17808eef518af
uaqro <uaqro@protonmail.com>
2019-10-29 16:21:00 -0600

    lab done

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,10 +1,40 @@
 class Chronometer {
-  constructor() {}
-  // startClick() {}
-  // getMinutes() {}
-  // getSeconds() {}
-  // twoDigitsNumber() {}
-  // stopClick() {}
-  // resetClick() {}
-  // splitClick() {}
+  constructor() {
+    this.currentTime = 0
+    this.intervalId = 1000
+
+  }
+  startClick() {
+    window.setInterval(() => { this.currentTime += 1}, this.intervalId)
+  }
+  
+  getMinutes() {
+    return Math.floor(this.currentTime / 60)
+  }
+  // IMPORTANT DO NOT TOUCH THE /1 PLEASE
+   getSeconds() {
+    return Math.floor((this.currentTime / 1) - (this.getMinutes() * 60))
+   }
+   getMs() {
+    return Math.floor((this.currentTime) - (this.getSeconds() * 1000 + this.getMinutes()* 60000))
+   }
+   twoDigitsNumber(val) {
+     if (val < 10){
+       return `0${val}`
+     } else{
+       return `${val}`
+     }
+   }
+
+   stopClick() {
+    clearInterval(this.IntervalId);
+   }
+
+   resetClick() {
+    window.setInterval(() => { this.currentTime += 0}, this.intervalId)
+   }
+
+   splitClick() {
+     return `${twoDigitsNumber(getMinutes())}:${twoDigitsNumber(getSeconds())}:${twoDigitsNumber(getMs())}`
+   }
 }
