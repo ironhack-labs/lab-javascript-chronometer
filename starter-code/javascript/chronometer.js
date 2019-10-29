b7ee289dd8f62eab8077bb9478de3e823d4487be
Laura <laura.vivas.gamboa@gmail.com>
2019-10-29 17:04:22 +0100

    Fisrt commit Chronometer

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,10 +1,39 @@
 class Chronometer {
-  constructor() {}
-  // startClick() {}
-  // getMinutes() {}
-  // getSeconds() {}
-  // twoDigitsNumber() {}
-  // stopClick() {}
-  // resetClick() {}
+  constructor() {
+    this.currentTime = 0;
+    this.intervalId = 0; 
+  } 
+
+  startClick() {
+   setInterval(() => {
+    this.currentTime += 1;
+    }, 1000);
+  };
+
+  getMinutes() {
+    return Math.floor(this.currentTime / 60);
+  };
+
+   getSeconds(time) {
+    let secondsInMinutes= this.currentTime-time*60;
+    return secondsInMinutes;
+  }
+
+  twoDigitsNumber(num) {
+    if (num < 10){
+      return `0${String(num)}`;   
+    } else {
+      return String(num);
+    }  
+  }
+
+  stopClick() {
+      clearInterval(this.intervalId)
+  };
+
+  resetClick() {
+    this.currentTime = 0;
+  }
+  }
+
   // splitClick() {}
-}
