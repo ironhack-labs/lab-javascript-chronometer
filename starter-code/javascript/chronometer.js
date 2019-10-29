51c6d47b4a599f4c47b7b66f7dbe00ed3765f948
javiview <javieragudo3d@gmail.com>
2019-10-29 17:19:46 +0100

    Jasmine approved

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,10 +1,35 @@
 class Chronometer {
-  constructor() {}
-  // startClick() {}
-  // getMinutes() {}
-  // getSeconds() {}
-  // twoDigitsNumber() {}
-  // stopClick() {}
-  // resetClick() {}
+  constructor() {
+    this.currentTime = 0;
+    this.intervalId;
+  }
+  startClick() {  
+    this.intervalId = setInterval(()=>{
+     this.currentTime++;
+    },1000)    
+  }
+  getMinutes() {
+    return Math.floor(this.currentTime/60);
+  }
+  getSeconds() {
+    return this.currentTime % 60;
+  }
+  twoDigitsNumber(time) {
+
+    let timeToString = time.toString();
+
+    if (timeToString.length === 1){
+      return "0" + timeToString;
+    }else{
+      return timeToString;
+    }
+    
+  }
+  stopClick() {
+    return clearInterval(this.intervalId)
+  }
+  resetClick() {
+    return this.currentTime = 0;
+  }
   // splitClick() {}
 }
