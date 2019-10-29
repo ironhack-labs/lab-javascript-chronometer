aea24305be29a3ac3537dd63ab7735f105d7dab5
pulpocabezon bonnet molina <pulpocabezon@MacBook-Pro-de-pulpocabezon.local>
2019-10-29 19:39:57 +0100

    cronometro

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,10 +1,31 @@
 class Chronometer {
-  constructor() {}
-  // startClick() {}
-  // getMinutes() {}
-  // getSeconds() {}
-  // twoDigitsNumber() {}
-  // stopClick() {}
-  // resetClick() {}
+  constructor() {
+    this.currentTime = 0;
+    this.intervalId;
+  }
+  startClick() {
+    this.intervalId = setInterval(() => {
+      this.currentTime += 1;
+    }, 1000);
+  }
+  getMinutes() {
+    return Math.floor(this.currentTime / 60);
+  }
+  getSeconds() {
+    return this.currentTime % 60;
+  }
+  twoDigitsNumber(crazyClock) {
+    let crazyP = crazyClock.toString();
+    if (crazyP.length <= 1) {
+      return (crazyP = 0 + crazyP);
+    }
+    return crazyP;
+  }
+  stopClick() {
+    clearInterval(this.intervalId);
+  }
+  resetClick() {
+    this.currentTime == 0;
+  }
   // splitClick() {}
 }
--- starter-code/javascript/main.js
+++ starter-code/javascript/main.js
@@ -1,60 +1,60 @@
 var chronometer = new Chronometer();
-var btnLeft     = document.getElementById('btnLeft');
-var btnRight    = document.getElementById('btnRight');
-var minDec      = document.getElementById('minDec');
-var minUni      = document.getElementById('minUni');
-var secDec      = document.getElementById('secDec');
-var secUni      = document.getElementById('secUni');
-var milDec      = document.getElementById('milDec');
-var milUni      = document.getElementById('milUni');
-
+var btnLeft = document.getElementById("btnLeft");
+var btnRight = document.getElementById("btnRight");
+var minDec = document.getElementById("minDec");
+var minUni = document.getElementById("minUni");
+var secDec = document.getElementById("secDec");
+var secUni = document.getElementById("secUni");
+var milDec = document.getElementById("milDec");
+var milUni = document.getElementById("milUni");
 
 function printTime() {
-
+  let minunts = printMinutes();
+  minDec.innerHTML = minunts.charAt;
+  let minunts2 = printMinutes();
+  minUni.innerHTML = minunts2.charAt;
+  let secnunts = printSeconds();
+  secDec.innerHTML = secnunts.charAt;
+  let secnunts2 = printSeconds();
+  secUni.innerHTML = secnunts2.charAt;
 }
-
 function printMinutes() {
-
+  return chronometer.twoDigitsNumber(chronometer.getMinutes());
 }
 
 function printSeconds() {
-
+  return chronometer.twoDigitsNumber(chronometer.getSeconds());
 }
 
-function printMilliseconds() {
+function printMilliseconds() {}
 
-}
+function printSplit() {}
 
-function printSplit() {
+function clearSplits() {}
 
-}
+function setStopBtn() {}
 
-function clearSplits() {
+function setSplitBtn() {}
 
-}
+function setStartBtn() {}
 
-function setStopBtn() {
-
-}
-
-function setSplitBtn() {
-
-}
-
-function setStartBtn() {
-
-}
-
-function setResetBtn() {
-
-}
+function setResetBtn() {}
 
 // Start/Stop Button
-btnLeft.addEventListener('click', function () {
+btnLeft.addEventListener("click", function() {
+  if (btnLeft.innerText == "START") {
+    btnLeft.classList.replace("Start", "Stop");
+    btnLeft.textContent = "STOP";
+    setStartBtn();
+  } else {
+    btnLeft.classList.replace("Stop", "Start");
+    btnLeft.textContent = "START";
+    setStopBtn();
+  }
 
+  //start chron
+  //change style of button
 });
 
 // Reset/Split Button
-btnRight.addEventListener('click', function () {
-
-});
+btnRight.addEventListener("click", function() {});
