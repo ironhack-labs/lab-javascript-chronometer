6fc6adb6982ce2d6caabf366289e52e1d78a34d8
eduardo <eduardo@DESKTOP-LPB6IGQ.localdomain>
2019-10-29 16:02:38 +0100

    primeros avances

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,10 +1,26 @@
 class Chronometer {
-  constructor() {}
-  // startClick() {}
-  // getMinutes() {}
+  constructor() {
+    this.currentTime = 0;
+    this.intervalId;
+  }
+  startClick() {
+    this.intervalId = setInterval(() => { 
+      this.currentTime++;
+    }, 1000);
+  }
+  
+  getMinutes() {
+    return Math.floor(this.currentTime/60);
+  }
   // getSeconds() {}
   // twoDigitsNumber() {}
   // stopClick() {}
   // resetClick() {}
   // splitClick() {}
 }
+
+const newChrono = new Chronometer;
+
+newChrono.currentTime = 50210;
+
+console.log(newChrono.getMinutes());
