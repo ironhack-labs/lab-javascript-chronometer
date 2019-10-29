cc07f44b7703326b77290b0c3160b7b89ccb5496
Flor Urbinati <flor29600@hotmail.com>
2019-10-29 19:48:10 +0100

    completed excercise ;)

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,10 +1,38 @@
 class Chronometer {
-  constructor() {}
-  // startClick() {}
-  // getMinutes() {}
-  // getSeconds() {}
-  // twoDigitsNumber() {}
-  // stopClick() {}
-  // resetClick() {}
-  // splitClick() {}
+  constructor() {
+    this.currentTime = 0;
+    this.intervalId = 0;  
+  }
+  startClick() {
+    this.intervalId = setInterval(() => {  
+      this.currentTime = this.currentTime + 1;
+      printTime()
+    }, 1000);
+     
+  }
+  getMinutes() {
+    return Math.floor(this.currentTime / 60);
+  }
+  getSeconds() {
+    return this.currentTime % 60;
+  }
+  twoDigitsNumber(val) {
+    val = `${val}`;
+    if (val.length === 1){
+     return `0${val}`;
+     
+    } else {
+      return val;
+    }
+  }
+  stopClick() {
+    clearInterval(this.intervalId);
+  }
+  resetClick() {
+    return this.currentTime = 0;
+  }
+  splitClick() {
+    return `${this.twoDigitsNumber(this.getMinutes())} : ${this.twoDigitsNumber(this.getSeconds())}`;
+    
+  }
 }
--- starter-code/javascript/main.js
+++ starter-code/javascript/main.js
@@ -1,24 +1,33 @@
 var chronometer = new Chronometer();
-var btnLeft     = document.getElementById('btnLeft');
-var btnRight    = document.getElementById('btnRight');
-var minDec      = document.getElementById('minDec');
-var minUni      = document.getElementById('minUni');
-var secDec      = document.getElementById('secDec');
-var secUni      = document.getElementById('secUni');
-var milDec      = document.getElementById('milDec');
-var milUni      = document.getElementById('milUni');
-
+console.log(chronometer);
+var btnLeft = document.getElementById('btnLeft');
+var btnRight = document.getElementById('btnRight');
+var minDec = document.getElementById('minDec');
+var minUni = document.getElementById('minUni');
+var secDec = document.getElementById('secDec');
+var secUni = document.getElementById('secUni');
+var milDec = document.getElementById('milDec');
+var milUni = document.getElementById('milUni');
+let splits = document.getElementById('splits');
 
 function printTime() {
-
+    printSeconds();
+    printMinutes();
 }
 
 function printMinutes() {
+    let mins = chronometer.twoDigitsNumber(chronometer.getMinutes());
+    console.log(mins);
+    minDec.innerHTML = mins[0];
+    minUni.innerHTML = mins[1];
 
 }
 
 function printSeconds() {
-
+    let sec = chronometer.twoDigitsNumber(chronometer.getSeconds());
+    console.log(sec);
+    secDec.innerHTML = sec[0];
+    secUni.innerHTML = sec[1];
 }
 
 function printMilliseconds() {
@@ -26,35 +35,68 @@ function printMilliseconds() {
 }
 
 function printSplit() {
+    return chronometer.splitClick()
 
 }
 
 function clearSplits() {
+    splits.innerHTML = '';
 
 }
 
 function setStopBtn() {
+    btnLeft.className = ("btn stop");
+    btnLeft.innerHTML = ("STOP");
+    chronometer.startClick(printTime);
 
 }
 
 function setSplitBtn() {
+    btnRight.className = ("btn split");
+    btnRight.innerHTML = ("SPLIT");
+
 
 }
 
 function setStartBtn() {
+    btnLeft.className = ("btn start");
+    btnLeft.innerHTML = ("START");
+    chronometer.stopClick(printTime);
 
 }
 
 function setResetBtn() {
+    btnRight.className = ("btn reset");
+    btnRight.innerHTML = ("RESET");
+
 
 }
-
+let isClicked = true;
 // Start/Stop Button
 btnLeft.addEventListener('click', function () {
+    isClicked = !isClicked;
+
+    if (isClicked) {
+        setStopBtn();
+        setSplitBtn();
+    } else {
+        setStartBtn();
+        setResetBtn();
+
+    }
 
 });
 
 // Reset/Split Button
-btnRight.addEventListener('click', function () {
 
+btnRight.addEventListener('click', function () {
+    if (isClicked) {
+        let listItemDoEl = document.createElement("li");
+        listItemDoEl.innerHTML = printSplit();
+        splits.appendChild(listItemDoEl);
+    } else {
+        clearSplits();
+        chronometer.resetClick();
+        printTime();
+    }
 });
