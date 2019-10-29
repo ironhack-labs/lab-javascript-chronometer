78a273a3a9f088c30ff0b8197ab0680240a699fc
Enrique Montaño Ocaña <enrique@Enriques-MacBook-Pro.local>
2019-10-29 17:11:59 +0100

    iteration 1 finished

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,10 +1,42 @@
 class Chronometer {
-  constructor() {}
-  // startClick() {}
-  // getMinutes() {}
-  // getSeconds() {}
-  // twoDigitsNumber() {}
-  // stopClick() {}
-  // resetClick() {}
-  // splitClick() {}
+  constructor() {
+    this.currentTime = 0;
+    this.intervalId;
+  }
+
+
+startClick() {
+  this.intervalId = setInterval(() => {  
+  this.currentTime += 1;
+  }, 1000)
+}
+
+getMinutes() {
+  return Math.floor(this.currentTime / 60);
+}
+
+getSeconds() {
+  return this.currentTime % 60; 
+}
+
+twoDigitsNumber(time) {
+  // time = this.currentTime;
+  let timeString = time.toString();
+
+  if (timeString.length == 1){
+    return '0' + timeString;
+  } else {
+    return timeString;
+  }
+
+}
+
+stopClick() {
+  clearInterval(this.intervalId)
+}
+resetClick() {
+  this.currentTime=0
+ 
+}
+// splitClick() {}
 }
