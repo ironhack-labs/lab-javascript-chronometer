fa1651d89639214d2fbd5f195b7983957ace7584
Daniel <2raederdev@gmail.com>
2019-10-29 19:22:29 +0100

    Button texts and colors changed

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,10 +1,47 @@
 class Chronometer {
-  constructor() {}
-  // startClick() {}
-  // getMinutes() {}
-  // getSeconds() {}
-  // twoDigitsNumber() {}
-  // stopClick() {}
-  // resetClick() {}
+  constructor() {
+    this.currentTime = 0
+    this.intervalId = 0
+  }
+
+  startClick() {
+    
+    const callbackFunction = () => {
+
+      this.currentTime += 1
+      }
+  
+    const interval = setInterval(callbackFunction, 1000)
+  
+  }
+
+  getMinutes() {
+    let convertToMinutes = this.currentTime / 60
+    let minutes = parseInt(convertToMinutes)
+    return minutes
+  }
+
+  getSeconds() {
+    let seconds = this.currentTime % 60
+    return seconds
+  }
+
+  twoDigitsNumber(number) {
+    if (number<10) {
+      return "0" + number
+    } else {
+      return ""+number
+    }
+  }
+
+  stopClick() {
+    clearInterval(this.intervalId)
+  }
+
+  resetClick() {
+    clearInterval(this.currentTime)
+
+  }
+
   // splitClick() {}
 }
--- starter-code/javascript/main.js
+++ starter-code/javascript/main.js
@@ -51,10 +51,38 @@ function setResetBtn() {
 
 // Start/Stop Button
 btnLeft.addEventListener('click', function () {
-
+    
+    if (btnLeft.textContent == "START") {
+            btnLeft.classList.replace('start','stop')
+            btnLeft.textContent = "STOP"
+            btnRight.textContent = "SPLIT"
+            btnRight.classList.replace('reset','split')
+    } else {
+        btnLeft.classList.replace('stop','start')
+        btnLeft.textContent = "START"
+        btnRight.classList.replace('split','reset')
+        btnRight.textContent = "RESET"
+    }
 });
+
+
+/*
+When the left button is clicked while the chronometer is stopped we need to:
+
+Set the btnLeft button with the text STOP, and the class btn stop.
+Set the btnRight button with the text SPLIT, and the class btn split.
+*/
+
+
 
 // Reset/Split Button
 btnRight.addEventListener('click', function () {
 
-});
+    if (btnRight.textContent == "RESET") {
+
+        } else {
+
+    }
+
+
+})
