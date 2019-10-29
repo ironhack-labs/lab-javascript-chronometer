d9df2482956d7b5f06646166b2634c58061c9a34
Fran <fmjerez@icloud.com>
2019-10-29 17:22:23 +0100

    first Chronometer

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,10 +1,39 @@
 class Chronometer {
-  constructor() {}
-  // startClick() {}
-  // getMinutes() {}
-  // getSeconds() {}
-  // twoDigitsNumber() {}
-  // stopClick() {}
-  // resetClick() {}
-  // splitClick() {}
+  constructor() {
+    this.currentTime = 0,
+      this.intervalId = 0
+  }
+  startClick() {
+    this.intervalId = setInterval(() => {
+      this.currentTime++
+    }, 1000)
+  }
+  
+  getMinutes() {
+   return Math.floor(this.currentTime/60)  
+  }
+  getSeconds() {
+    return this.currentTime-this.getMinutes()*60
+  }
+  twoDigitsNumber(value) {
+    if(value<10){
+     return ('0' + value).slice(-2)
+    }else return value.toString()
+  }
+  stopClick() {
+    clearInterval(this.intervalId)
+  }
+  resetClick() {
+    this.currentTime = 0
+  }
 }
+//let fran = new Chronometer()
+//console.log(fran.startClick())
+
+
+
+
+
+
+
+// splitClick() {}
