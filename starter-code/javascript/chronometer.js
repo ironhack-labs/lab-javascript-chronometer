952d036f5f9e64bfc9f756583770f84cd4c5bb46
Ana Segarra <anasegarraber@gmail.com>
2019-11-23 19:44:15 +0100

    Add startClick, getMinutes and getSeconds methods

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,10 +1,21 @@
 class Chronometer {
-  constructor() {}
-  // startClick() {}
-  // getMinutes() {}
-  // getSeconds() {}
-  // twoDigitsNumber() {}
-  // stopClick() {}
-  // resetClick() {}
-  // splitClick() {}
+	constructor() {
+		this.currentTime = 0;
+		this.intervalId = this.startClick();
+	}
+	startClick() {
+		return setInterval(() => {
+			this.currentTime++;
+		}, 1000);
+	}
+	getMinutes() {
+		return Math.floor(this.currentTime / 60);
+	}
+	getSeconds() {
+		return this.currentTime % 60;
+	}
+	// twoDigitsNumber() {}
+	// stopClick() {}
+	// resetClick() {}
+	// splitClick() {}
 }
