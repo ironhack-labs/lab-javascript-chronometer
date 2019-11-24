7ef32dd86220a6e6fc5525d539992e8b75ba5f3f
Gonzalo Hernández <gonzalohernandez@icloud.com>
2019-11-24 20:55:33 +0100

    iteration 2 finished

--- starter-code/javascript/main.js
+++ starter-code/javascript/main.js
@@ -8,17 +8,19 @@ var secUni      = document.getElementById('secUni');
 var milDec      = document.getElementById('milDec');
 var milUni      = document.getElementById('milUni');
 
-
-function printTime() {
-
+function printTime(minutes, seconds) {
+  printMinutes(minutes);
+  printSeconds(seconds);
 }
 
-function printMinutes() {
-
+function printMinutes(minutes) {
+  minDec.innerHTML = minutes.charAt(0);
+  minUni.innerHTML = minutes.charAt(1);
 }
 
-function printSeconds() {
-
+function printSeconds(seconds) {
+  secDec.innerHTML = seconds.charAt(0);
+  secUni.innerHTML = seconds.charAt(1);
 }
 
 function printMilliseconds() {
@@ -34,27 +36,44 @@ function clearSplits() {
 }
 
 function setStopBtn() {
-
+  btnLeft.className = btnLeft.className.replace(/start/, 'stop');
+  btnLeft.innerHTML = 'STOP';
 }
 
 function setSplitBtn() {
-
+  btnRight.className = btnRight.className.replace(/reset/, 'split');
+  btnRight.innerHTML = 'SPLIT';
 }
 
 function setStartBtn() {
-
+  btnLeft.className = btnLeft.className.replace(/stop/, 'start');
+  btnLeft.innerHTML = 'START';
 }
 
 function setResetBtn() {
-
+  btnRight.className = btnRight.className.replace(/split/, 'reset');
+  btnRight.innerHTML = 'RESET';
 }
 
 // Start/Stop Button
 btnLeft.addEventListener('click', function () {
-
+  if (btnLeft.classList.contains('start')){
+    setStopBtn();
+    setSplitBtn();
+    chronometer.startClick();
+    intervalId = setInterval(() => {
+      let minutes = chronometer.twoDigitsNumber(chronometer.getMinutes());
+      let seconds = chronometer.twoDigitsNumber(chronometer.getSeconds());
+      printTime(minutes, seconds);
+    }, 10);
+  } else {
+    setStartBtn();
+    setResetBtn();
+    chronometer.stopClick();
+    clearInterval(intervalId);
+  }
 });
 
 // Reset/Split Button
 btnRight.addEventListener('click', function () {
-
 });
