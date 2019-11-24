617ce80f81f38cf5f24dece679a2e1fb2c7ce5c0
Veronica <veronica1506@gmail.com>
2019-11-24 12:24:18 -0500

    startClick getSecond and getMInutes done

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,9 +1,38 @@
 class Chronometer {
-  constructor() {}
-  // startClick() {}
-  // getMinutes() {}
-  // getSeconds() {}
-  // twoDigitsNumber() {}
+  constructor() {
+    this.currentTime = 0;
+    this.intervalId = undefined
+  }
+
+
+
+
+  startClick() {
+    this.intervalId = setInterval(() => {
+      this.currentTime++
+    }, 1000)
+  }
+
+  getMinutes() {
+    let number = this.currentTime / 60
+    return Math.floor(number)
+  }
+
+  // const minutes = getMinutes();
+
+  getSeconds() {
+    if (this.currentTime < 60) {
+      return this.currentTime
+    } else {
+      return this.currentTime % 60
+    }
+  }
+  twoDigitsNumber(value) {
+    if (value.length === 1 || value.length === 0) {
+      return `0${value}`
+    }
+    return `${value}`
+  }
   // stopClick() {}
   // resetClick() {}
   // splitClick() {}
