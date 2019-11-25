095f03fd3ec278b1fb0d014644f2c3a33cae7fc5
Ruben Vaquero <rvaquero@elpublicamaleon.com>
2019-11-25 18:35:09 +0100

    itertation 1 finish

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,10 +1,29 @@
 class Chronometer {
-  constructor() {}
-  // startClick() {}
-  // getMinutes() {}
-  // getSeconds() {}
-  // twoDigitsNumber() {}
+  constructor() {
+    this.currentTime = 0;
+    this.intervalId = 0;
+  }
+  startClick() {
+    let that = this;
+    this.intervalId = setInterval(function(){
+      that.currentTime+=1;
+    }, 1000)
+  }
+  getMinutes() {
+    return Math.floor(this.currentTime/60);
+  }
+  getSeconds() {
+    return Math.floor(this.currentTime%60)
+  }
+  twoDigitsNumber(value){
+    value = new String(value);
+    if(value.length < 2) return '0' + value;
+    return value
+  }
   // stopClick() {}
-  // resetClick() {}
+  resetClick() {
+    this.currentTime = 0;
+  }
   // splitClick() {}
+
 }
