ce7b419f5f2d84552a04476aa3b5abe44c73b121
pelinski <teresapelinski@gmail.com>
2019-11-25 13:04:29 +0100

    finished iteration-1

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,10 +1,35 @@
 class Chronometer {
-  constructor() {}
-  // startClick() {}
-  // getMinutes() {}
-  // getSeconds() {}
-  // twoDigitsNumber() {}
-  // stopClick() {}
-  // resetClick() {}
+  constructor() {
+    this.currentTime = 0;
+    this.intervalId = 0;
+  }
+
+  startClick() {
+    var chrono = this;
+    this.intervalId = setInterval(function() {
+      chrono.currentTime++;
+    }, 1000);
+  }
+
+  getMinutes() {
+    return Math.floor(this.currentTime / 60);
+  }
+  getSeconds() {
+    return this.currentTime % 60;
+  }
+
+  twoDigitsNumber(num) {
+    if (num < 10) {
+      return "0" + num;
+    }
+    return num.toString();
+  }
+   stopClick() {
+     clearInterval(this.intervalId);
+   }
+  
+  resetClick() {
+    this.currentTime = 0;
+  }
   // splitClick() {}
 }
