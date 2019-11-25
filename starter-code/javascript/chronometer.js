b112cc0231a01fad15e0dc93366f36c8090d0701
vctorChamizo <victor.chamizo96@gmail.com>
2019-11-25 18:50:32 +0100

    startClick method done

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,6 +1,13 @@
 class Chronometer {
-  constructor() {}
-  // startClick() {}
+  constructor() {
+    this.currentTime = 0;
+    this.intervalID = "";
+  }
+  startClick() {
+    this.intervalID = setInterval(() => {
+      this.currentTime++;
+    }, 1000);
+  }
   // getMinutes() {}
   // getSeconds() {}
   // twoDigitsNumber() {}
