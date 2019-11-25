b358fa4bb388fd2fa2c02220e25f9b1ee550ce95
mdehoogh <m.p.a.j.dehoogh@tudelft.nl>
2019-11-25 22:29:38 +0100

    functional without checking for test conformance

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,10 +1,81 @@
 class Chronometer {
-  constructor() {}
-  // startClick() {}
-  // getMinutes() {}
-  // getSeconds() {}
-  // twoDigitsNumber() {}
-  // stopClick() {}
-  // resetClick() {}
-  // splitClick() {}
+  constructor() {
+    this._interval=1000;
+    this._intervalId=null;
+    this._ontick=null; // no ticker yet registered
+    this.startTime=null; // when the chronometer started
+  }
+  
+  // getTime() returns the time in millsecond precision (when running), 0 otherwie
+  getTime() { return(this.startTime?(new Date()).getTime()-this.startTime:0); }
+  
+  // currentTime returns the time in seconds (when running), zero otherwise
+  get currentTime(){return(this.startTime?Math.floor(this.getTime())/1000:0);}
+
+  get interval(){return this._interval;}
+  set interval(value){this._interval=Math.max(1,value);}
+
+  // for registering the ticker
+  get ontick(){return this._ontick;}
+  set ontick(_ontick){
+    this._ontick=_ontick;
+    console.log(this._ontick?"On tick handler set!":"On tick handler removed!");
+  }
+  tick(){
+    //////console.log("Chronometer: ",this);
+    // this.currentTime+=1000;
+    if(this._ontick!=null)this._ontick(this);
+    else console.log("No ontick callback!"); // passing the chronometer as well
+  }
+
+  // isRunning() will tell us when the chronometer is running
+  isRunning(){return(this._intervalId!=null);}
+  syncStartTime(){
+    // NOTE not nulling this.startTime if stopped, i.e. stopping is treated as pausing!!!
+    if(this._intervalId!=null){
+      // NOTE only when we do not have a start date do we create a new start date
+      if(this.startTime==null){
+        this.startTime=new Date().getTime();
+        /////console.log("New start time: "+this.startTime+".");
+      }
+    }
+    // replacing: this.startTime=(this.intervalId!=null?new Date().getTime():null);
+    if(this.startTime!=null)console.log("Start time: "+this.startTime+".");
+    /////else console.log("No start time!");
+  }
+
+  get intervalId(){return this._intervalId;}
+  set intervalId(_intervalId){
+    this._intervalId=_intervalId;
+    this.syncStartTime();
+  }
+  
+  getMinutes() {return Math.floor(this.currentTime/60);}
+  getSeconds() {return Math.floor(this.currentTime%60);}
+  getMilliseconds() { return Math.floor(this.getTime()%1000);}
+
+  twoDigitsNumber(value) {return String(value).padStart(2,'0');}
+  threeDigitsNumber(value) {return String(value).padStart(3,'0');}
+
+  startClick() {
+    if(this.isRunning())return;
+    // get an interval id if we haven't got one yet
+    this.intervalId=setInterval(this.tick.bind(this),this._interval); // NOTE took a long time to find out how to do `this`
+  }
+  stopClick() {
+    if(!this.isRunning())return;
+    clearTimeout(this._intervalId);
+    this.intervalId=null;
+    // do NOT force a tick until resetClick() is called!!!
+  }
+  resetClick() {
+    this.stopClick(); // will null this.startTime()
+    this.startTime=null; // this will force the time to zero!!!!
+    this.tick(); // force an update of the current time (in seconds) showing
+  }
+  splitClick() {
+    var ms=this.getMilliseconds();
+    var seconds=this.currentTime;
+    return this.twoDigitsNumber(Math.floor(seconds/60))+":"+this.twoDigitsNumber(Math.floor(seconds%60))+"."+this.threeDigitsNumber(ms%1000);
+  }
 }
--- starter-code/javascript/main.js
+++ starter-code/javascript/main.js
@@ -8,7 +8,7 @@ var secUni      = document.getElementById('secUni');
 var milDec      = document.getElementById('milDec');
 var milUni      = document.getElementById('milUni');
 
-
+/*
 function printTime() {
 
 }
@@ -48,13 +48,93 @@ function setStartBtn() {
 function setResetBtn() {
 
 }
+*/
+
+function respondToTick(chronometer){
+    // update the chronometer view on every tick
+    ///console.log("Responding to tick!");
+    var min=chronometer.twoDigitsNumber(chronometer.getMinutes());
+    var sec=chronometer.twoDigitsNumber(chronometer.getSeconds());
+    var ms=chronometer.threeDigitsNumber(chronometer.getMilliseconds());
+    ///console.log("Minutes: "+min+" - seconds: "+sec+".");
+    minDec.innerHTML=min[0];
+    minUni.innerHTML=min[1];
+    secDec.innerHTML=sec[0];
+    secUni.innerHTML=sec[1];
+    milDec.innerHTML=ms[0];
+    milUni.innerHTML=ms[1];
+}
+
+// create the chronometer instance
+window.onload=function(){
+    chronometer.interval=17; // tick every 17 ms, so we can see milliseconds!!
+    chronometer.ontick=respondToTick;
+}
+
+function syncButtons(){
+    console.log("Syncing buttons.");
+    // button left
+    if(btnLeft.classList.contains("start")){
+        console.log("Syncing start button");
+        if(chronometer.isRunning()){
+            btnLeft.classList.remove("start");
+            btnLeft.classList.add("stop");
+            btnLeft.innerText="STOP";
+        }
+    }else
+    if(btnLeft.classList.contains("stop")){
+        console.log("Syncing stop button");
+        if(!chronometer.isRunning()){
+            btnLeft.classList.remove("stop");
+            btnLeft.classList.add("start");
+            btnLeft.innerText="START";
+        }
+    }
+    // button right
+    if(btnRight.classList.contains("reset")){
+        console.log("Syncing reset button");
+        if(chronometer.isRunning()){
+            btnRight.classList.remove("reset");
+            btnRight.classList.add("split");
+            btnRight.innerText="SPLIT";
+        }
+    }else
+    if(btnRight.classList.contains("split")){
+        console.log("Syncing split button");
+        if(!chronometer.isRunning()){
+            btnRight.classList.remove("split");
+            btnRight.classList.add("reset");
+            btnRight.innerText="RESET";
+        }
+    }
+}
+
+function split(){
+    // ASSERT only to be called when running!!!!
+    // grab the current time and add it to the splits list
+    var newSplit=document.createElement("li");
+    newSplit.innerHTML=chronometer.splitClick();
+    document.getElementById("splits").appendChild(newSplit);
+}
 
 // Start/Stop Button
 btnLeft.addEventListener('click', function () {
-
+    // the chronometer knows best whether it us running or not
+    if(chronometer.isRunning())
+        chronometer.stopClick();
+    else
+        chronometer.startClick();
+    syncButtons();
 });
 
 // Reset/Split Button
 btnRight.addEventListener('click', function () {
-
+    if(!chronometer.isRunning()){
+        chronometer.resetClick();
+        syncButtons();
+        // remove all li elements
+        var splitslist=document.getElementById("splits");
+        while(splitslist.firstChild)splitslist.removeChild(splitslist.firstChild);
+    }else
+        split();
 });
