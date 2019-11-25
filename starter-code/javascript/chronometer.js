591d6b7202e5b6976da20edc42a77910aa3f8d8d
mario010203 <mariojose1490@hotmail.com>
2019-11-26 00:11:25 +0100

    first ite done

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,6 +1,14 @@
 class Chronometer {
-  constructor() {}
-  // startClick() {}
+  constructor() {
+    this.currentTime = 0;
+    this.intervalId = 0;
+  }
+
+  startClick() {
+  
+    setInterval(this.intervalId, 1);
+    this.currentTime= +1;
+  }
   // getMinutes() {}
   // getSeconds() {}
   // twoDigitsNumber() {}
