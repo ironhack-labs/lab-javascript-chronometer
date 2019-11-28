31c7ac1de47ac9f781c6b46947c56871ff33b07f
Ignacio Garcia Barrero <igarciabarrero@outlook.com>
2019-11-28 22:31:43 +0100

    Completed Chronometer

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,12 +1,31 @@
 class Chronometer {
-  currentTime = 0;
-  intervalId = 0;
-  constructor() {}
-  startClick() {}
-  // getMinutes() {}
-  // getSeconds() {}
-  // twoDigitsNumber() {}
-  // stopClick() {}
-  // resetClick() {}
+  constructor() {
+    this.currentTime = 0;
+    this.intervalId;
+  }
+
+  startClick() {
+    this.intervalId = setInterval(function() {
+      this.currentTime++;
+    }, 1000);
+  }
+
+  getMinutes() {
+    //60 seconds in 1 minute
+    return Math.floor(this.currentTime / 60);
+  }
+  getSeconds() {
+    //Gives us remainder of seconds
+    return Math.floor(this.currentTime % 60);
+  }
+  twoDigitsNumber(value) {
+    return ("0" + value).slice(-2);
+  }
+  stopClick() {
+    return (this.intervalId = 0);
+  }
+  resetClick() {
+    this.currentTime = 0;
+  }
   // splitClick() {}
 }
