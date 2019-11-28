4125d9f6e6937447273b09aa9b7f9f2945190797
Borja Domínguez <borja.d.atienza@gmail.com>
2019-11-28 20:15:31 +0100

    1º Interation DONE

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,10 +1,34 @@
 class Chronometer {
-  constructor() {}
-  // startClick() {}
-  // getMinutes() {}
-  // getSeconds() {}
-  // twoDigitsNumber() {}
-  // stopClick() {}
-  // resetClick() {}
+  constructor() {
+    this.currentTime = 0;
+    this.intervalId = 0;
+  }
+  
+  startClick () {
+    this.intervalId = setInterval(() => {
+      this.currentTime += 1;      
+  }, 1000); 
+   
+  }
+  getMinutes() {
+    return Math.floor(this.currentTime / 60)
+  }
+  getSeconds() {
+    return this.currentTime - (60*this.getMinutes()) 
+  }
+  twoDigitsNumber(digits) {
+    if(digits.toString().length < 2){
+      return "0"+digits.toString()
+    }
+
+    return digits.toString()
+  }
+   stopClick() {
+     clearInterval(this.intervalId)
+     this.intervalId = 0
+   }
+   resetClick() {
+     this.currentTime = 0
+   }
   // splitClick() {}
 }
