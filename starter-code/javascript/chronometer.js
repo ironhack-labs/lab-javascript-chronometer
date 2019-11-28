d209e0a906d981111b566627d56a2a729f481c91
vctorChamizo <victor.chamizo96@gmail.com>
2019-11-28 22:09:30 +0100

    Logic done

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,10 +1,58 @@
 class Chronometer {
-  constructor() {}
-  // startClick() {}
-  // getMinutes() {}
-  // getSeconds() {}
-  // twoDigitsNumber() {}
-  // stopClick() {}
-  // resetClick() {}
-  // splitClick() {}
+  constructor() {
+    this.currentTime = 0;
+    this.milisecondsTime = 0;
+    this.intervalId;
+    this.intervalIdMilisecods;
+  }
+
+  startClick() {
+    this.intervalId = setInterval(() => {
+      this.currentTime++;
+    }, 1000);
+
+    this.intervalIdMilisecods = setInterval(() => {
+      if (this.milisecondsTime >= 99) {
+        this.milisecondsTime = 0;
+      } else {
+        this.milisecondsTime++;
+      }
+    }, 1);
+  }
+
+  getMinutes() {
+    return parseInt(this.currentTime / 60);
+  }
+
+  getSeconds() {
+    return parseInt(this.currentTime % 60);
+  }
+
+  getMiliseconds() {
+    return this.milisecondsTime;
+  }
+
+  twoDigitsNumber(number) {
+    return number.toString().padStart(2, 0);
+  }
+
+  stopClick() {
+    clearInterval(this.intervalId);
+    clearInterval(this.intervalIdMilisecods);
+  }
+
+  resetClick() {
+    this.currentTime = 0;
+    this.milisecondsTime = 0;
+  }
+
+  splitClick() {
+    return (
+      this.twoDigitsNumber(this.getMinutes()) +
+      ":" +
+      this.twoDigitsNumber(this.getSeconds()) +
+      ":" +
+      this.twoDigitsNumber(this.milisecondsTime)
+    );
+  }
 }
