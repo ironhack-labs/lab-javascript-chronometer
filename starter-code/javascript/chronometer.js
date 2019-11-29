f4c81a1f6ea4868e013ff9628e988f404afe3f7c
Miguel <alonsogalalrdomiguel@gmail.com>
2019-11-29 23:37:38 +0100

    faltan2err

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,10 +1,48 @@
 class Chronometer {
-  constructor() {}
-  // startClick() {}
-  // getMinutes() {}
-  // getSeconds() {}
-  // twoDigitsNumber() {}
-  // stopClick() {}
-  // resetClick() {}
-  // splitClick() {}
+  constructor() {
+    this.currentTime = 0;
+    this.intervalId = 0;
+  }
+   startClick() {
+
+   this.intervalId = setInterval(function() {
+      this.currentTime++;
+    }.bind(this), 1000);
+  }
+  
+  getMinutes() {
+    return parseInt(this.currentTime / 60)
+
+  }
+  getSeconds() {
+    this.currentTime = Math.round(this.currentTime * 60)
+    return this.currentTime;
+  }
+
+
+
+  twoDigitsNumber(num) {
+    return (num +"").padStart(2,0)
+
+    /*
+    function twoDigits(num){
+      num = num.toString();
+      if (num.length === 1){
+        num = "0".concat(num);
+        return parseInt(num,10);
+    }
+    return parseInt(num,10);
+    }
+    */
+  }
+
+
+  stopClick() {
+    clearInterval(this.intervalId);
+  }
+    
+  resetClick() {
+    this.currentTime = 0;
+  }
+  //splitClick() {}
 }
