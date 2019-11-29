994e995afffb4d1fa9e303a7ee54cda02a871391
Javi L <jlopezro@gmail.com>
2019-11-29 18:49:54 +0100

    Iteration 1 (The logic) done

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,10 +1,39 @@
 class Chronometer {
-  constructor() {}
-  // startClick() {}
-  // getMinutes() {}
-  // getSeconds() {}
-  // twoDigitsNumber() {}
+  constructor() {
+    this.currentTime = 0;
+    this.intervalId = 0;
+  }
+  startClick() {
+    this.intervalId = setInterval(
+      function() {
+        this.currentTime++;
+      }.bind(this),
+      1000
+    );
+  }
+  getMinutes() {
+    this.minutes = Math.floor(this.currentTime / 60);
+    return this.minutes;
+  }
+  getSeconds() {
+    this.minutesRemoved = 0;
+    if (this.currentTime % 60 != 0)
+      //{
+      this.minutesRemoved = this.currentTime % 60;
+    return this.minutesRemoved;
+  }
+  twoDigitsNumber(number) {
+    this.numToStr = number.toString();
+    if (this.numToStr.length < 2) {
+      this.twoDigits = "0" + this.numToStr;
+    } else {
+      this.twoDigits = this.numToStr;
+    }
+    return this.twoDigits;
+  }
   // stopClick() {}
-  // resetClick() {}
+  resetClick() {
+    this.currentTime = 0;
+  }
   // splitClick() {}
 }
