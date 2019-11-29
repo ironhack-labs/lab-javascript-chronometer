7259b284ffb0ffbef26bca77538ba4a56ce5f5dd
Gaby Zapata <thegabyzapata@gmail.com>
2019-11-29 19:28:17 +0100

    Do iteration 1

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,6 +1,18 @@
 class Chronometer {
-  constructor() {}
+  constructor() {
+    this.currentTime = 0;
+    this.intervalId;
+  }
+
+  
   // startClick() {}
+  startClick() {
+    this.intervalId = setInterval(() => {
+      this.currentTime++;
+      printSeconds();
+    }, 1000);
+  }
+
   // getMinutes() {}
   // getSeconds() {}
   // twoDigitsNumber() {}
