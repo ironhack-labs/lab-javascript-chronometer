27e15ebc406dfd92975d2003036d47dd32c12d82
Ivan Esteban <ivan.estebanisabel@gmail.com>
2019-11-29 19:06:48 +0100

    feature1

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,13 +1,32 @@
 class Chronometer {
   constructor() {
-    this.currentTime 
-    this.intevalId
+    this.currentTime = 0;
+    this.intevalId;
   }
-  startClick() {}
-  // getMinutes() {}
-  // getSeconds() {}
-  // twoDigitsNumber() {}
-  // stopClick() {}
+  startClick() {
+    this.intervalId = setInterval(function() {
+      this.currentTime++;
+    }, 1000);
+  }
+
+  getMinutes() {
+    var minutes = Math.floor(this.currentTime/60)
+    return minutes;
+  }
+  getSeconds() {
+    var seconds = this.currentTime%60
+    return seconds;
+  }
+  twoDigitsNumber(value) {
+    if(value<10){
+      return "0" + value;
+    }else{
+      return value;
+    }
+  }
+  stopClick() {
+    clearInterval(this.intervalId)
+  }
   // resetClick() {}
   // splitClick() {}
 }
