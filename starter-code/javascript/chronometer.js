1a41d6e486bd764434d865d1876133864b834d26
carballo <carballet@gmail.com>
2019-11-30 00:08:10 +0100

    Iteracion 1 casi completada

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,10 +1,36 @@
 class Chronometer {
-  constructor() {}
-  // startClick() {}
-  // getMinutes() {}
-  // getSeconds() {}
-  // twoDigitsNumber() {}
-  // stopClick() {}
-  // resetClick() {}
-  // splitClick() {}
+  constructor() {
+    this.currentTime = 0;
+    this.intervalId;
+  }
+
+  startClick(){
+    this.intervalId = setInterval(function(){
+      this.currentTime++;
+    }.bind(this), 1000)  // <-- Este bind es "el truco" porque si no no actualiza this.currentTime
+  }
+
+  getMinutes() {
+    return Math.floor( this.currentTime / 60 );
+  }
+
+
+  getSeconds() {
+    return this.currentTime - (this.getMinutes()*60);
+  }
+
+  twoDigitsNumber() {}
+
+
+  stopClick() {
+    clearInterval(this.intervalId);
+  }
+
+
+  resetClick() {
+    this.currentTime = 0;
+  }
+
+
+  splitClick() {}
 }
