6bce88d8944b489b14f488b26f4d8e6b2359ccac
Iván Herranz <ihcuesta@gmail.com>
2019-11-29 22:42:13 +0100

    Iteration1: Done - All tests passed

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,15 +1,39 @@
 class Chronometer {
+  //Constructor
   constructor() {
-    this.currentTime;
+    this.currentTime = 0;
     this.intervalId;
   }
 
+  //Methods
+  startClick() {
+    let that = this;
+    this.intervalId = setInterval(function () {
+      that.currentTime++;
+    }, 1000)
+  }
+
+  getMinutes() {
+    return Math.floor(this.currentTime / 60);
+  }
   
-  // startClick() {}
-  // getMinutes() {}
-  // getSeconds() {}
-  // twoDigitsNumber() {}
-  // stopClick() {}
-  // resetClick() {}
+  getSeconds() {
+    return this.currentTime % 60;
+  }
+  
+  twoDigitsNumber(number) {
+    number = String(number);
+    number = number.padStart(2, '0');
+    return number;
+  }
+  
+  stopClick() {
+    clearInterval(this.intervalId)
+  }
+
+  resetClick() {
+    this.currentTime = 0;
+  }
+
   // splitClick() {}
 }
