e7de1755387d96bdb98d1c416f34d093f929dbe1
Román Méndez <mendezbeck@gmail.com>
2019-11-29 17:47:14 +0100

    miliseconds done

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,12 +1,18 @@
 class Chronometer {
   constructor() {
-    this.currentTime = 2476;
+    this.currentTime = 0;
+    this.miliseconds = 0;
     this.intervalId;
+    this.milInterval;
   }
   startClick(){
     this.intervalId = setInterval(function(){
       this.currentTime += 1;
     }.bind(this), 1000)
+    this.milInterval = setInterval(function(){
+      this.miliseconds += 1;
+    }.bind(this), 1)
+    
   }
   getMinutes(){
     var minutes = Math.floor(this.currentTime / 60)
@@ -15,6 +21,10 @@ class Chronometer {
   getSeconds(){
     var seconds = this.currentTime % 60
     return seconds
+  }
+  getMiliseconds(){
+    var miliseconds = Math.floor(this.miliseconds % 100)
+    return miliseconds
   }
   twoDigitsNumber(value){
     if (value < 10){
@@ -25,9 +35,11 @@ class Chronometer {
   }
   stopClick(){
     clearInterval(this.intervalId)
+    clearInterval(this.milInterval)
   }
   resetClick(){
     this.currentTime = 0;
+    this.miliseconds = 0;
   }
   // splitClick() {}
 }
