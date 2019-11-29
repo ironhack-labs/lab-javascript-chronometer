0e881994b12ecc01c72231fc2685ad943c17ab62
Pilar <p.garciacampo@gmail.com>
2019-11-30 00:47:19 +0100

    Add iteration 1 - the logic

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,10 +1,30 @@
 class Chronometer {
-  constructor() {}
-  // startClick() {}
-  // getMinutes() {}
-  // getSeconds() {}
-  // twoDigitsNumber() {}
-  // stopClick() {}
-  // resetClick() {}
+  constructor() {
+    this.currentTime = 0;
+    this.intervalId;
+  }
+  startClick() {
+    this.intervalId = setInterval(increaseOneSecond, 1000);
+    function increaseOneSecond() {
+      return this.currentTime++;
+    }
+  }
+  getMinutes() {
+    return parseInt(this.currentTime / 60, 10);
+  }
+  getSeconds() {
+    return parseInt(this.currentTime % 60, 10);
+  }
+  twoDigitsNumber(num) {
+    num = Math.floor(num);
+    var formatedNum = ("0" + num).slice(-2);
+    return formatedNum;
+  }
+  stopClick() {
+    clearInterval(this.intervalId);
+  }
+  resetClick() {
+    this.currentTime = 0;
+  }
   // splitClick() {}
 }
