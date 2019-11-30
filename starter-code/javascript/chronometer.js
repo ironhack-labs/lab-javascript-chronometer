ecb26026d2d3aec42f7287a632776644804fc864
Borja del Amo <borjadao@gmail.com>
2019-11-30 09:02:41 +0100

    completada solo la primera iteracion por no saber hacer mas

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,9 +1,27 @@
 class Chronometer {
-  constructor() {}
+    constructor() {
+      this.currentTime = 0;
+      this.intervalId = 0;
+    }
+    startClick() {
+      this.intervalId = setInterval(() => (this.currentTime += 1), 1000);
+    }
+    getMinutes() {
+      return Math.floor(this.currentTime / 60);
+    }
+    getSeconds() {
+      let minutes = Math.floor(this.currentTime / 60);
+      let seconds = this.currentTime - minutes * 60;
+      if (seconds >= 60) {
+        seconds = 0;
+      }
+      return seconds;
+    let currentTime
+  }
   // startClick() {}
   // getMinutes() {}
   // getSeconds() {}
-  // twoDigitsNumber() {}
+  twoDigitsNumber() {}
   // stopClick() {}
   // resetClick() {}
   // splitClick() {}
