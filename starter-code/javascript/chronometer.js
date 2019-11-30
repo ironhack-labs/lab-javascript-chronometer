44f046530a793d724f8971bd349dc81cb4afc5d3
Miguel <alonsogalalrdomiguel@gmail.com>
2019-11-30 10:17:35 +0100

    iteration1_complete

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -15,25 +15,13 @@ class Chronometer {
 
   }
   getSeconds() {
-    this.currentTime = Math.round(this.currentTime * 60)
-    return this.currentTime;
+    return this.currentTime % 60;
   }
 
 
 
   twoDigitsNumber(num) {
     return (num +"").padStart(2,0)
-
-    /*
-    function twoDigits(num){
-      num = num.toString();
-      if (num.length === 1){
-        num = "0".concat(num);
-        return parseInt(num,10);
-    }
-    return parseInt(num,10);
-    }
-    */
   }
 
 
