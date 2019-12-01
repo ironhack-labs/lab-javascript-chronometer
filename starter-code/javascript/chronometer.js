7f1904534ed0c28caa12a44ff4b0bf2dde1c239a
militroncho7 <victor@MacBook-Air-de-Victor.local>
2019-12-01 11:54:22 +0100

    Iteration 1: Done

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,10 +1,45 @@
 class Chronometer {
-  constructor() {}
-  startClick() {}
-  // getMinutes() {}
-  // getSeconds() {}
-  // twoDigitsNumber() {}
-  // stopClick() {}
-  // resetClick() {}
-  // splitClick() {}
+  constructor() {
+    this.currentTime = 0;
+    this.intervalId;
+    this.currentMilis = 0;
+    this.instervalMilisId;
+  }
+
+  startClick() {
+    this.intervalId = setInterval(function () {
+      this.currentMilis++;
+      if (this.currentMilis % 100 == 0) this.currentTime++;
+    }.bind(this), 10);
+  }
+
+  getMinutes() {
+    return Math.floor(this.currentTime / 60);
+  }
+
+  getSeconds() {
+    return Math.floor(this.currentTime % 60);
+  }
+
+  getMiliSeconds() {
+    return Math.floor(this.currentMilis % 100);
+  }
+
+  twoDigitsNumber(value) {
+    return (value < 10) ? "0" + value : value.toString();
+  }
+
+  stopClick() {
+    clearInterval(this.intervalId);
+    clearInterval(this.instervalMilisId);
+  }
+
+  resetClick() {
+    this.currentTime = 0;
+    this.currentMilis = 0;
+  }
+
+  splitClick() {
+    return `${this.twoDigitsNumber(this.getMinutes())} : ${this.twoDigitsNumber(this.getSeconds())} : ${this.twoDigitsNumber(this.getMiliSeconds())}`;
+  }
 }
