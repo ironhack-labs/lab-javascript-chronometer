c047f53eabaef2e80679202d8cfdc509ac5ef195
zante <dza.devandcomputing@gmail.com>
2019-12-02 11:12:33 +0100

    first iteration completed

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,10 +1,42 @@
 class Chronometer {
-  constructor() {}
-  // startClick() {}
-  // getMinutes() {}
-  // getSeconds() {}
-  // twoDigitsNumber() {}
-  // stopClick() {}
-  // resetClick() {}
-  // splitClick() {}
+
+  constructor() {
+    this.currentTime = 0;
+    this.intervalId;
+  }
+
+  startClick() {
+    this.intervalId = setInterval(function(){
+        this.currentTime++;
+      }.bind(this), 1000) // por qué el contexto? ver esto. --> si no no supera los tests
+  }
+
+  getMinutes() {
+    return Math.floor(this.currentTime / 60);
+  }
+
+  getSeconds() {
+    return this.currentTime % 60
+  }
+
+  twoDigitsNumber(number) {
+    if(number < 10) {
+      return '0' + number
+    }else{
+      return number.toString();
+    }
+  }
+
+  stopClick() {
+    clearInterval(this.intervalId);
+  }
+
+  resetClick() {
+    this.currentTime = 0;
+  }
+
+  splitClick() {
+
+  }
+
 }
