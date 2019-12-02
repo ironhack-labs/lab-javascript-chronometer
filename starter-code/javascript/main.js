deb352ae018bb92f769e4952c3d1785c91d72387
mario010203 <mariojose1490@hotmail.com>
2019-12-02 16:41:19 +0100

    logic done

--- starter-code/javascript/main.js
+++ starter-code/javascript/main.js
@@ -1,63 +1,39 @@
 var chronometer = new Chronometer();
-var btnLeft     = document.getElementById('btnLeft');
-var btnRight    = document.getElementById('btnRight');
-var minDec      = document.getElementById('minDec');
-var minUni      = document.getElementById('minUni');
-var secDec      = document.getElementById('secDec');
-var secUni      = document.getElementById('secUni');
-var milDec      = document.getElementById('milDec');
-var milUni      = document.getElementById('milUni');
-
+var btnLeft = document.getElementById("btnLeft");
+var btnRight = document.getElementById("btnRight");
+var minDec = document.getElementById("minDec");
+var minUni = document.getElementById("minUni");
+var secDec = document.getElementById("secDec");
+var secUni = document.getElementById("secUni");
+var milDec = document.getElementById("milDec");
+var milUni = document.getElementById("milUni");
 
 function printTime() {
-    printMinutes()
-    printSeconds()
-    printMilliseconds()
-
+  printMinutes();
+  printSeconds();
+  printMilliseconds();
 }
 
-function printMinutes() {
+function printMinutes() {}
 
-}
+function printSeconds() {}
 
-function printSeconds() {
+function printMilliseconds() {}
 
-}
+function printSplit() {}
 
-function printMilliseconds() {
+function clearSplits() {}
 
-}
+function setStopBtn() {}
 
-function printSplit() {
+function setSplitBtn() {}
 
-}
+function setStartBtn() {}
 
-function clearSplits() {
-
-}
-
-function setStopBtn() {
-
-}
-
-function setSplitBtn() {
-
-}
-
-function setStartBtn() {
-
-}
-
-function setResetBtn() {
-
-}
+function setResetBtn() {}
 
 // Start/Stop Button
-btnLeft.addEventListener('click', function () {
-
-});
+btnLeft.addEventListener("click", function() {});
 
 // Reset/Split Button
-btnRight.addEventListener('click', function () {
-
-});
+btnRight.addEventListener("click", function() {});
