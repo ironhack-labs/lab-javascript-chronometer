3757e1155338749e4cf4d4ac155971cac602354c
Charly <cgtright@gmail.com>
2020-01-14 15:44:13 -0600

    Iteracion 1: the logic

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,10 +1,34 @@
 class Chronometer {
-  constructor() {}
-  // startClick() {}
-  // getMinutes() {}
-  // getSeconds() {}
-  // twoDigitsNumber() {}
-  // stopClick() {}
-  // resetClick() {}
+  constructor() {
+    this.currentTime = 0
+    this.intervalId = null
+  }
+   startClick() {
+     this.intervalId = setInterval(() => {
+       this.currentTime++
+     },1000)
+   }
+
+
+   getMinutes() {
+     return Math.floor(this.currentTime / 60)
+   }
+
+   getSeconds() {
+
+     return this.currentTime - (this.getMinutes() * 60)
+
+   }
+   twoDigitsNumber(val) {
+     val = (val <= 9) ? `0${val}`: val
+     return  `${val}`
+   }
+   stopClick() {
+     clearInterval(this.intervalId)
+   }
+
+   resetClick() {
+     this.currentTime = 0
+   }
   // splitClick() {}
 }
