81a7a2ab1cf66e47ccfcd0dbf111f586e6960fe5
Esteban <esteb.gtz.mtz@hotmail.com>
2020-01-14 19:48:14 -0600

    PP-WDFT-MEX-Esteban-Gutierrez-Silvana-Gomez
    Signed-off-by: Esteban <esteb.gtz.mtz@hotmail.com>

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,10 +1,37 @@
 class Chronometer {
-  constructor() {}
-  // startClick() {}
-  // getMinutes() {}
-  // getSeconds() {}
-  // twoDigitsNumber() {}
-  // stopClick() {}
-  // resetClick() {}
-  // splitClick() {}
+    constructor() {
+        this.currentTime = 0;
+        this.intervalId = 0;
+    }
+    startClick() {
+        this.intervalId = setInterval(() => this.currentTime++, 1000)
+            // console.log(this.currentTime)
+    }
+    getMinutes() {
+        return parseInt(this.currentTime / 60);
+    }
+
+    getSeconds() {
+        return parseInt(this.currentTime % 60);
+    }
+
+    twoDigitsNumber(value) {
+        if (value <= 9) {
+            return `0${value}`;
+        } else {
+            return value.toString(10);
+        }
+    }
+
+    stopClick() {
+        clearInterval(this.intervalId);
+    }
+
+    resetClick() {
+        this.currentTime = 0;
+    }
+
+    splitClick() {
+        return this.twoDigitsNumber(this.getMinutes()) + ": " + this.twoDigitsNumber(this.getSeconds());
+    }
 }
--- starter-code/javascript/main.js
+++ starter-code/javascript/main.js
@@ -1,36 +1,46 @@
 var chronometer = new Chronometer();
-var btnLeft     = document.getElementById('btnLeft');
-var btnRight    = document.getElementById('btnRight');
-var minDec      = document.getElementById('minDec');
-var minUni      = document.getElementById('minUni');
-var secDec      = document.getElementById('secDec');
-var secUni      = document.getElementById('secUni');
-var milDec      = document.getElementById('milDec');
-var milUni      = document.getElementById('milUni');
+var btnLeft = document.getElementById('btnLeft');
+var btnRight = document.getElementById('btnRight');
+var minDec = document.getElementById('minDec');
+var minUni = document.getElementById('minUni');
+var secDec = document.getElementById('secDec');
+var secUni = document.getElementById('secUni');
+var milDec = document.getElementById('milDec');
+var milUni = document.getElementById('milUni');
+var splits = document.getElementById('splits');
 
 
 function printTime() {
-
+    printSeconds();
+    printMinutes();
 }
 
 function printMinutes() {
-
+    let minutos = chronometer.getMinutes();
+    let twoDigits = chronometer.twoDigitsNumber(minutos);
+    //console.log(twoDigits)
+    minDec.innerText = twoDigits[0];
+    minUni.innerText = twoDigits[1];
 }
 
 function printSeconds() {
-
+    let seconds = chronometer.getSeconds();
+    let twoDigits = chronometer.twoDigitsNumber(seconds);
+    //console.log(twoDigits)
+    secDec.innerText = twoDigits[0];
+    secUni.innerText = twoDigits[1]
 }
 
-function printMilliseconds() {
-
-}
+function printMilliseconds() {}
 
 function printSplit() {
-
+    let li = document.createElement('li');
+    li.innerText = chronometer.splitClick();
+    splits.append(li);
 }
 
 function clearSplits() {
-
+    splits.innerHTML = '';
 }
 
 function setStopBtn() {
@@ -46,15 +56,33 @@ function setStartBtn() {
 }
 
 function setResetBtn() {
-
+    chronometer.resetClick();
 }
 
 // Start/Stop Button
-btnLeft.addEventListener('click', function () {
-
+btnLeft.addEventListener('click', function() {
+    if (btnLeft.className === 'btn start') {
+        btnLeft.setAttribute('class', 'btn stop')
+        btnLeft.innerText = 'STOP';
+        chronometer.startClick();
+        let newInterval = setInterval((printTime), 1000);
+        btnRight.innerText = 'SPLIT';
+        btnRight.setAttribute('class', 'btn split')
+    } else {
+        btnLeft.setAttribute('class', 'btn start');
+        btnLeft.innerText = 'START';
+        chronometer.stopClick();
+        btnRight.setAttribute('class', 'btn reset')
+        btnRight.innerText = 'RESET'
+    }
 });
 
 // Reset/Split Button
-btnRight.addEventListener('click', function () {
-
+btnRight.addEventListener('click', function() {
+    if (btnRight.className === 'btn split') {
+        printSplit();
+    } else {
+        setResetBtn();
+        clearSplits();
+    }
 });
