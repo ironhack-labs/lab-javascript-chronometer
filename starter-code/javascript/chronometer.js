1f1311b215d446d4769e8ebf33dc07647af93bfb
Marcel <marcel.riek@googlemail.com>
2020-01-16 12:06:57 +0100

    iteration 1 done

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,10 +1,50 @@
 class Chronometer {
-  constructor() {}
-  // startClick() {}
-  // getMinutes() {}
-  // getSeconds() {}
-  // twoDigitsNumber() {}
-  // stopClick() {}
-  // resetClick() {}
+  constructor() {
+    this.currentTime = 0;
+    this.intervalId = 0;
+  }
+
+  startClick() {
+    this.intervalId = setInterval(
+      function() {
+        this.currentTime++;
+        // use bind to make the this keyword refer to the class Chronometer instead of the window
+        // alternative: use arrow function above
+      }.bind(this),
+      1000
+    );
+  }
+
+  getMinutes() {
+    let currentMinutes = Math.floor(this.currentTime / 60);
+    return currentMinutes;
+  }
+
+  getSeconds() {
+    // let currentSeconds = Math.floor(this.currentTime)
+    let remaining = this.currentTime % 60;
+    return remaining;
+    // if (this.currentTime === 60) {
+    //   currentMinutes += 1;
+    //   currentSeconds - 60;
+  }
+
+  twoDigitsNumber(number) {
+    if (number < 10) {
+      return "0" + number;
+    } else {
+      return number.toString();
+    }
+  }
+
+  stopClick() {
+    clearInterval(this.intervalId);
+  }
+
+  resetClick() {
+    clearInterval(this.currentTime);
+  }
   // splitClick() {}
 }
+
+//
