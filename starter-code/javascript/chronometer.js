e42640b758194e3066930b689d64672f84cdff7d
Martin Schmal <martinschmal@gmail.com>
2020-01-17 17:52:33 +0100

    done

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,10 +1,49 @@
 class Chronometer {
-  constructor() {}
-  // startClick() {}
-  // getMinutes() {}
-  // getSeconds() {}
-  // twoDigitsNumber() {}
-  // stopClick() {}
-  // resetClick() {}
-  // splitClick() {}
+	constructor() {
+		this.currentTime = 0;
+		this.intervalId;
+	}
+
+	startClick() {
+		this.intervalId = setInterval(
+			function() {
+				this.currentTime++;
+				console.log("currentTime");
+			}.bind(this),
+			1000
+		);
+	}
+
+	getMinutes() {
+		return Math.floor(this.currentTime / 60);
+	}
+
+	getSeconds() {
+		console.log("Seconds: " + Math.floor(this.currentTime % 60));
+		return Math.floor(this.currentTime % 60);
+	}
+
+	twoDigitsNumber(value) {
+		let stringValue = value.toString();
+		value = value.toString();
+
+		if (stringValue < 9) {
+			stringValue = "0" + stringValue;
+			// console.log("stringValue " + stringValue);
+			return stringValue;
+		} else {
+			// console.log("value " + value);
+			return value;
+		}
+	}
+	stopClick() {
+		clearInterval(this.intervalId);
+
+		//return this.intervalId;
+	}
+	resetClick() {
+		this.currentTime = 0;
+	}
+
+	// splitClick() {}
 }
