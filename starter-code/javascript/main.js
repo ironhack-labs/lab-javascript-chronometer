37ebb4a3b9061053440397580b6a50e0e79cfc64
Paul Carillion <pcarillion@yahoo.fr>
2020-01-19 16:51:36 +0100

    done but last iterations are not working

--- starter-code/javascript/main.js
+++ starter-code/javascript/main.js
@@ -1,4 +1,7 @@
-var chronometer = new Chronometer();
+var chronometer = new Chronometer(
+
+);
+
 var btnLeft     = document.getElementById('btnLeft');
 var btnRight    = document.getElementById('btnRight');
 var minDec      = document.getElementById('minDec');
@@ -9,16 +12,19 @@ var milDec      = document.getElementById('milDec');
 var milUni      = document.getElementById('milUni');
 
 
-function printTime() {
-
+function printTime(minutes, seconds) {
+    printMinutes(minutes);
+    printSeconds(seconds)
 }
 
-function printMinutes() {
-
+function printMinutes(minutes) {
+    minDec.textContent = minutes.charAt(0);
+    minUni.textContent = minutes.charAt(1);
 }
 
-function printSeconds() {
-
+function printSeconds(seconds) {
+    secDec.textContent = seconds.charAt(0);
+    secUni.textContent = seconds.charAt(1)
 }
 
 function printMilliseconds() {
@@ -26,7 +32,9 @@ function printMilliseconds() {
 }
 
 function printSplit() {
-
+   var li = document.createElement('li');
+   li.textContent = `${printMinutes(minutes)} : ${printSeconds(seconds)}`;
+   document.getElementById("splits").appendChild(li);
 }
 
 function clearSplits() {
@@ -51,10 +59,29 @@ function setResetBtn() {
 
 // Start/Stop Button
 btnLeft.addEventListener('click', function () {
-
+    if (btnLeft.innerText === "START") {
+        btnLeft.className = "btn stop";
+        btnLeft.textContent = "STOP";
+        btnRight.className = "btn split";
+        btnRight.textContent = "SPLIT";
+        chronometer.startClick();
+    } else if (btnLeft.innerText === "STOP") {
+        btnLeft.className = " btn start";
+        btnLeft.textContent = "START";
+        btnRight.className = " btn reset";
+        btnRight.textContent = "RESET";
+        chronometer.stopClick()
+    };  
 });
+
 
 // Reset/Split Button
 btnRight.addEventListener('click', function () {
-
+    if (btnRight.innerContent === "SPLIT") {
+        printSplit()
+    } else if (btnRight.innerText === "RESET") {
+        printTime("00", "00");
+        document.getElementById("splits").removeChild();
+    }
 });
+
