77792afeb31321d0aa1a3f52bb4c1c0971719a56
CrisSugar <aguacero18@hotmail.com>
2020-01-21 17:50:56 +0100

    js-done

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,10 +1,32 @@
 class Chronometer {
-  constructor() {}
-  // startClick() {}
-  // getMinutes() {}
-  // getSeconds() {}
-  // twoDigitsNumber() {}
-  // stopClick() {}
-  // resetClick() {}
+  constructor() {
+    this.currentTime = 0
+    this.intervalId = 0
+  }
+  startClick() {
+    this.intervalId = setInterval(() => {
+      this.currentTime += 1;
+    }, 1000);
+  }
+
+  getMinutes() {
+    return Math.floor(this.currentTime / 60)
+  }
+  getSeconds() {
+    return (this.currentTime - (this.getMinutes() * 60));
+  }
+  twoDigitsNumber(x) {
+    if (x < 10) {
+      return (`0${x}`)
+    } else {
+      return (`${x}`) 
+      }
+  }
+  stopClick() {
+    clearInterval(this.currentTime)
+  }
+  resetClick() {
+    this.currentTime=0
+  }
   // splitClick() {}
 }
