04fbc02b9f72886ac6aa8ece1759cd90d9d8038c
Arturo G. Bruno <arturogbruno@gmail.com>
2020-01-21 17:09:19 +0100

    Iteration 1 finished

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,10 +1,34 @@
 class Chronometer {
-  constructor() {}
-  // startClick() {}
-  // getMinutes() {}
-  // getSeconds() {}
-  // twoDigitsNumber() {}
-  // stopClick() {}
-  // resetClick() {}
-  // splitClick() {}
+    constructor() {
+        this.currentTime = 0;
+        this.intervalId;
+    }
+
+    startClick() {
+        this.intervalId = setInterval(() => this.currentTime++, 1000);
+    }
+
+    getMinutes() {
+        return parseInt(this.currentTime / 60);
+    }
+    
+    getSeconds() {
+        return this.currentTime % 60;
+    }
+
+    twoDigitsNumber(value) {
+        value === 0 ? value = `00` : 
+        value < 10 ? value = `0${value}` : false;
+        return `${value}`;
+    }
+
+    stopClick() {
+        clearInterval(this.intervalId);
+    }
+
+    resetClick() {
+        this.currentTime = 0;
+    }
+
+    // splitClick() {s}
 }
