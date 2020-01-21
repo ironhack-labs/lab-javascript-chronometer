644c0f04be985f586fd200cbd27f9a18e386cb22
alejandrosz <alejandrosz@gmail.com>
2020-01-21 17:04:49 +0100

    jasmine va

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,10 +1,41 @@
 class Chronometer {
-  constructor() {}
-  // startClick() {}
-  // getMinutes() {}
-  // getSeconds() {}
-  // twoDigitsNumber() {}
-  // stopClick() {}
-  // resetClick() {}
-  // splitClick() {}
+  constructor() {
+    this.currentTime = 0;
+    this.intervalId = 0;
+  }
+  startClick() {
+    this.intervalId = setInterval(() => {
+      this.currentTime++;
+    }, 1000);
+  }
+
+  getMinutes() {
+    let minutes = parseInt(this.currentTime / 60);
+    return minutes;
+  }
+
+  getSeconds() {
+    let seconds = parseInt(this.currentTime % 60);
+    return seconds;
+  }
+
+  twoDigitsNumber(number) {
+    if (number === 0) {
+      return `00`;
+    } else if (number < 10) {
+      return `0${number}`;
+    } else {
+      return `${number}`;
+    }
+  }
+
+  stopClick() {
+    clearInterval()
+  }
+
+  resetClick() {
+    this.currentTime = 0
+  }
+
+  splitClick() {}
 }
