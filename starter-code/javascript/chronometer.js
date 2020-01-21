c4bc461225c22911017b7f251bf31a8430969762
arnoldosicilia <arnoldo.sicilia.labarquilla@gmail.com>
2020-01-21 17:08:11 +0100

    Jasmine liquidada

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,10 +1,34 @@
 class Chronometer {
-  constructor() {}
-  // startClick() {}
-  // getMinutes() {}
-  // getSeconds() {}
-  // twoDigitsNumber() {}
-  // stopClick() {}
-  // resetClick() {}
+  constructor() {
+    this.currentTime = 0
+    this.intervalId = 0
+  }
+
+  startClick() {
+    this.intervalId = setInterval(() => {
+      this.currentTime++
+    }, 1000)
+  }
+
+  getMinutes() {
+    return parseInt(this.currentTime / 60)
+  }
+
+  getSeconds() {
+    return parseInt(this.currentTime % 60)
+  }
+
+  twoDigitsNumber(number) {
+    return number < 10 ? `0${number.toString()}` : number.toString()
+  }
+
+  resetClick() {
+    this.currentTime = 0
+  }
+
+  stopClick() {
+    clearInterval(this.intervalId)
+  }
+  
   // splitClick() {}
 }
