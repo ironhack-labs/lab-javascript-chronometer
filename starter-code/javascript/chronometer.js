5ad476af19fdfb00fee702e196bf305b3d44f208
armetegithub <armomomete@protonmail.com>
2020-01-21 17:24:36 +0100

    Hecho startclick

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,10 +1,23 @@
 class Chronometer {
-  constructor() {}
-  // startClick() {}
+  constructor() {
+
+    this.currentTime = 0;
+    this.intervalId = setInterval(this.startClick, 1000);
+  }
+     startClick() {
+
+     setInterval(() => {
+
+       this.currentTime++;
+
+      },1000);
+
+     }
   // getMinutes() {}
   // getSeconds() {}
   // twoDigitsNumber() {}
   // stopClick() {}
   // resetClick() {}
   // splitClick() {}
-}
+    }
+  
