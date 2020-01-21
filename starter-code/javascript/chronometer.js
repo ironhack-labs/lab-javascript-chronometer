ef0801f07609acb1e4bfac205f53c5af9a20a092
Andres Atehortua <anderson17colombia@hotmail.com>
2020-01-21 19:00:46 +0100

    Done except miliseconds

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,10 +1,35 @@
 class Chronometer {
-  constructor() {}
-  // startClick() {}
-  // getMinutes() {}
-  // getSeconds() {}
-  // twoDigitsNumber() {}
-  // stopClick() {}
-  // resetClick() {}
-  // splitClick() {}
+  constructor() {
+    this.currentTime = 0
+    this.intervalId;
+  }
+
+  startClick = () => {
+    this.intervalId = setInterval(() => {
+      this.currentTime++
+    }, 1000)
+  }
+  getMinutes() {
+    let minutes = Math.floor(this.currentTime / 60)
+
+    return minutes
+  }
+  getSeconds() {
+    let seconds = this.currentTime % 60
+    return seconds
+  }
+  twoDigitsNumber(number) {
+    let str = (100 + number).toString()
+    let newStr = str.slice(-2)
+    return newStr
+  }
+  stopClick() {
+    clearInterval(this.intervalId)
+  }
+  resetClick() {
+    this.currentTime = 0
+  }
+  // splitClick() {
+
+  // }
 }
--- starter-code/javascript/main.js
+++ starter-code/javascript/main.js
@@ -1,37 +1,45 @@
 var chronometer = new Chronometer();
-var btnLeft     = document.getElementById('btnLeft');
-var btnRight    = document.getElementById('btnRight');
-var minDec      = document.getElementById('minDec');
-var minUni      = document.getElementById('minUni');
-var secDec      = document.getElementById('secDec');
-var secUni      = document.getElementById('secUni');
-var milDec      = document.getElementById('milDec');
-var milUni      = document.getElementById('milUni');
+var btnLeft = document.getElementById('btnLeft');
+var btnRight = document.getElementById('btnRight');
+var minDec = document.getElementById('minDec');
+var minUni = document.getElementById('minUni');
+var secDec = document.getElementById('secDec');
+var secUni = document.getElementById('secUni');
+var milDec = document.getElementById('milDec');
+var milUni = document.getElementById('milUni');
+let ol = document.getElementById("splits")
 
 
 function printTime() {
+    return printMinutes() + " : " + printSeconds()
 
 }
 
-function printMinutes() {
 
+function printMinutes() {
+    return chronometer.getMinutes()
 }
 
 function printSeconds() {
-
+    return chronometer.getSeconds()
 }
 
-function printMilliseconds() {
 
+function printMilliseconds() {
+    let miliseconds = chronometer.getSeconds / 1000
+    miliseconds.toString()
+    let milSec2 = miliseconds.slice(-2)
+    return milSec2
 }
 
 function printSplit() {
-
+    return chronometer.twoDigitsNumber(printMinutes()) + " : " + chronometer.twoDigitsNumber(printSeconds())
 }
 
 function clearSplits() {
-
+    ol.innerHTML = ""
 }
+clearSplits()
 
 function setStopBtn() {
 
@@ -51,10 +59,39 @@ function setResetBtn() {
 
 // Start/Stop Button
 btnLeft.addEventListener('click', function () {
+    if (btnLeft.innerHTML === "START") {
+        chronometer.startClick()
+        setInterval(() => {
+            secDec.innerHTML = chronometer.twoDigitsNumber(chronometer.getSeconds())
+            secUni.innerHTML = ""
+            minDec.innerHTML = chronometer.twoDigitsNumber(chronometer.getMinutes())
+            minUni.innerHTML = ""
+        }, 1000);
 
+        btnLeft.innerHTML = "STOP"
+        btnLeft.style.backgroundColor = "#f14949"
+        btnRight.innerHTML = "SPLIT"
+        btnRight.style.backgroundColor = "#0851ab"
+
+    } else {
+        chronometer.stopClick()
+        btnLeft.innerHTML = "START"
+        btnLeft.style.backgroundColor = "#5fca5f"
+        btnRight.innerHTML = "RESET"
+        btnRight.style.backgroundColor = "#908e8e"
+    }
 });
 
 // Reset/Split Button
 btnRight.addEventListener('click', function () {
-
+    if (btnRight.innerHTML === "SPLIT") {
+        let ol = document.getElementById("splits")
+        let li = document.createElement("li")
+        li.appendChild(document.createTextNode(printSplit()))
+        ol.appendChild(li)
+    } else {
+        chronometer.stopClick()
+        chronometer.resetClick()
+        clearSplits()
+    }
 });
