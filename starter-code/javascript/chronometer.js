7762b1332d88e9d312d94e8a5db56e24f461dde2
Jorge Sb <jorgesanchezblanco1989@gmail.com>
2020-02-26 00:17:18 +0100

    Chronometer completed, website still to do

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,10 +1,29 @@
 class Chronometer {
-  constructor() {}
-  // startClick() {}
-  // getMinutes() {}
-  // getSeconds() {}
-  // twoDigitsNumber() {}
-  // stopClick() {}
-  // resetClick() {}
-  // splitClick() {}
+  constructor() {
+    this.currentTime = 0
+    this.intervalId = 0
+  }
+
+
+startClick() {
+  this.intervalId = setInterval(()=> {this.currentTime++},1000);
+}
+getMinutes() {
+  return Math.floor(this.currentTime/60)
+}
+
+getSeconds() {
+  return (this.currentTime%60)
+}
+
+twoDigitsNumber(input) {
+let output = ("0" + input).slice(-2);
+return output
+}
+
+stopClick() {clearInterval(this.intervalId)}
+
+resetClick() {this.currentTime = 0}
+
+splitClick() {}
 }
--- starter-code/javascript/main.js
+++ starter-code/javascript/main.js
@@ -14,11 +14,11 @@ function printTime() {
 }
 
 function printMinutes() {
-
+// console.log(twoDigitsNumber(getMinutes()))
 }
 
 function printSeconds() {
-
+// console.log(twoDigitsNumber(getSeconds()))
 }
 
 function printMilliseconds() {
@@ -51,10 +51,24 @@ function setResetBtn() {
 
 // Start/Stop Button
 btnLeft.addEventListener('click', function () {
-
+if (btnLeft.classList.contains("stop")===true){
+    btnLeft.setAttribute("class", "btn start")
+    btnLeft.innerHTML=START
+    chronometer.startClick()}
+else {
+    btnLeft.setAttribute("class", "btn stop")
+    btnLeft.innerHTML=STOP
+    chronometer.stopClick()
+}
 });
 
 // Reset/Split Button
 btnRight.addEventListener('click', function () {
-
+    if btnLeft.classList.contains("stop")===true){
+        btnRight.setAttribute("class", "btn split")
+        btnRight.innerHTML=SPLIT}
+    else {
+        btnRight.setAttribute("class", "btn reset")
+        btnRight.innerHTML=RESET
+    }
 });
