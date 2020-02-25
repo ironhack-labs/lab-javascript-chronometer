97323a7658c773d03bb984851b6f1551443d81d6
joaquim pienda <joaquimpineda@gmail.com>
2020-02-26 00:16:54 +0100

    primer js

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,10 +1,36 @@
 class Chronometer {
-  constructor() {}
-  // startClick() {}
-  // getMinutes() {}
-  // getSeconds() {}
-  // twoDigitsNumber() {}
-  // stopClick() {}
-  // resetClick() {}
+  constructor() {
+    this.currentTime = 0
+    this.intervalId =""
+  }
+  startClick() {
+    this.intervalId = setInterval(() => {
+      this.currentTime++
+    }, 1000);
+
+  }
+  getMinutes() {
+    return Math.floor(this.currentTime/60)
+  }
+  getSeconds() {
+    return this.currentTime-Math.floor(this.currentTime/60)*60
+
+  }
+  twoDigitsNumber(param) {
+
+      let result = "0"+String(param)
+    if(result.length===3){
+      return String(param)
+    }
+    return result
+    //Return ("0"+input).slice(-2)
+  }
+   stopClick() {
+    clearInterval(this.intervalId)
+   }
+  resetClick() {
+    this.currentTime =0
+  }
   // splitClick() {}
 }
+
