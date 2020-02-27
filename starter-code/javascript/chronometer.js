6c5f9eeccf896824c01288c4031c133d07b9c610
Francis Bukenya <frankie.bukenya@gmail.com>
2020-02-27 14:52:54 +0100

    Add complete chrononmeter.js

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,10 +1,40 @@
 class Chronometer {
-  constructor() {}
+  constructor() {
+    this.currentTime = 0;
+  }
   // startClick() {}
+  startClick() {
+    let fixedThis = this;
+    this.intervalId = setInterval(function() {
+      fixedThis.currentTime++;
+    }, 1000);
+  }
   // getMinutes() {}
+  getMinutes() {
+    let min = this.currentTime / 60;
+    return Math.floor(min);
+  }
   // getSeconds() {}
+  getSeconds() {
+    let secs = this.currentTime % 60;
+    return Math.floor(secs);
+  }
   // twoDigitsNumber() {}
+  twoDigitsNumber(num) {
+    let ten = Math.floor(num / 10);
+    let unit = Math.floor(num % 10);
+
+    let convertedNum = `${ten}${unit}`;
+
+    return convertedNum;
+  }
   // stopClick() {}
+  stopClick() {
+    return clearInterval(this.intervalId);
+  }
   // resetClick() {}
+  resetClick() {
+    return (this.currentTime = 0);
+  }
   // splitClick() {}
 }
