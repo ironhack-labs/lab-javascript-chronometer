0b7d04c3f281219160d3d5237d185cafff3d2968
Neil Gursahani <gursahani.neil@gmail.com>
2020-03-02 17:35:56 +0100

    first iteration

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,10 +1,51 @@
 class Chronometer {
-  constructor() {}
-  // startClick() {}
-  // getMinutes() {}
-  // getSeconds() {}
-  // twoDigitsNumber() {}
+  constructor() {
+    this.currentTime = 0;
+  }
+
+  startClick(intervalId) {
+    this.intervalId = setInterval( () => {
+      this.currentTime += 1;
+    }, 1000);
+  }
+
+  getMinutes() {
+    let currentTimeInMinutes = this.currentTime/60
+    let currentTimeInMinutesRounded = Math.floor(currentTimeInMinutes)
+    return currentTimeInMinutesRounded;
+  
+  }
+  getSeconds() {
+    if (this.currentTime > 0) {
+      let currentTimeRemaining = (this.currentTimeInMinutes - this.currentTimeInMinutesRounded);
+      let currentTimeRemainingInSeconds = currentTimeRemaining * 60;
+      return currentTimeRemainingInSeconds;
+    } else {
+      return 0;
+    }
+    
+  }
+
+  twoDigitsNumber(number) {
+    if (this.currentTimeInMinutesRounded.length === 1 || this.currentTimeRemainingInSeconds.length === 1) {
+
+    }
+    if (typeof this.currentTimeInMinutesRounded === 'number' || typeof this.currentTimeRemainingInSeconds === 'number') {
+      let currentTimeInMinutesRoundedString = currentTimeInMinutesRounded.toString();
+      let currentTimeRemainingInSecondsString = currentTimeRemainingInSeconds.toString();
+
+      return currentTimeInMinutesRoundedString, currentTimeRemainingInSecondsString;
+
+    } else {
+      return 0;
+    }
+  }
+
+
+
   // stopClick() {}
   // resetClick() {}
   // splitClick() {}
 }
+
+
