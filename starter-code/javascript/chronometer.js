f738b1f603b0db0a38313071135259785f295c96
Justin Peeters <Peeters.justin@yahoo.com>
2020-03-02 16:14:07 +0100

    AMS WDFT FEB - Justin Peeters

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,10 +1,37 @@
 class Chronometer {
-  constructor() {}
-  // startClick() {}
-  // getMinutes() {}
-  // getSeconds() {}
-  // twoDigitsNumber() {}
-  // stopClick() {}
-  // resetClick() {}
-  // splitClick() {}
+    constructor() {
+        this.currentTime = 0,
+            this.intervalId = 0
+    }
+    startClick() {
+        var i = this;
+        this.intervalId = setInterval(function() {
+            i.currentTime++
+        }, 1000);
+
+    }
+    getMinutes() {
+        var minutes = Math.floor(this.currentTime / 60);
+        return minutes;
+    }
+    getSeconds() {
+        var seconds = Math.floor(this.currentTime % 60);
+        return seconds;
+    }
+    twoDigitsNumber(time) {
+        var str = time.toString();
+        if (str.length < 2) {
+            return 0 + str
+        } else return str;
+
+    }
+
+    stopClick() {
+        clearInterval(this.intervalId)
+    }
+    resetClick() {
+            clearInterval(this.currentTime)
+        }
+        // splitClick() {
+        // }
 }
--- starter-code/javascript/main.js
+++ starter-code/javascript/main.js
@@ -1,12 +1,14 @@
-var chronometer = new Chronometer();
-var btnLeft     = document.getElementById('btnLeft');
-var btnRight    = document.getElementById('btnRight');
-var minDec      = document.getElementById('minDec');
-var minUni      = document.getElementById('minUni');
-var secDec      = document.getElementById('secDec');
-var secUni      = document.getElementById('secUni');
-var milDec      = document.getElementById('milDec');
-var milUni      = document.getElementById('milUni');
+var chronometer = new Chronometer(
+
+);
+var btnLeft = document.getElementById('btnLeft');
+var btnRight = document.getElementById('btnRight');
+var minDec = document.getElementById('minDec');
+var minUni = document.getElementById('minUni');
+var secDec = document.getElementById('secDec');
+var secUni = document.getElementById('secUni');
+var milDec = document.getElementById('milDec');
+var milUni = document.getElementById('milUni');
 
 
 function printTime() {
@@ -14,10 +16,11 @@ function printTime() {
 }
 
 function printMinutes() {
-
+    minDec = Chronometer.getMinutes();
 }
 
 function printSeconds() {
+    secDec = Chronometer.getSeconds();
 
 }
 
@@ -50,11 +53,22 @@ function setResetBtn() {
 }
 
 // Start/Stop Button
-btnLeft.addEventListener('click', function () {
-
+btnLeft.addEventListener('click', function(event) {
+    if (btnLeft.innerHTML === "START") {
+        btnLeft.classList.remove("start");
+        btnLeft.classList.add("stop");
+        btnLeft.innerHTML = "STOP";
+    } else if (btnLeft.innerHTML === "STOP") {
+        btnLeft.classList.remove("stop");
+        btnLeft.classList.add("start");
+        btnLeft.innerHTML = "START";
+    };
 });
-
 // Reset/Split Button
-btnRight.addEventListener('click', function () {
-
+btnRight.addEventListener('click', function() {
+    if (btnRight.innerHTML === "RESET") {
+        btnRight.innerHTML = "SPLIT";
+    } else if (btnRight.innerHTML === "SPLIT") {
+        btnRight.innerHTML = "RESET";
+    };
 });
