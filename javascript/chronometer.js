e83dedae89814e5872a001490a5aceffeefebe6a
Rafael Freitas <rafaelborges1985@hotmail.com>
2020-03-18 15:25:24 -0300

    Create methods twoDigitNumber and stopClick

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -16,15 +16,22 @@ class Chronometer {
   }
   getSeconds() {
     // ... your code goes here
+    return this.currentTime % 60;
   }
-  twoDigitsNumber() {
+  twoDigitsNumber(number) {
     // ... your code goes here
+    return number < 10 ? 
+    '0' + number : 
+    number.toString();
   }
   stopClick() {
     // ... your code goes here
+    clearInterval(this.interval);
   }
   resetClick() {
     // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = 0;
   }
   splitClick() {
     // ... your code goes here
