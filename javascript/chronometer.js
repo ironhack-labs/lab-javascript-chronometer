8fc56826887438762d00aae97bc43530c8d268be
Rafael Freitas <rafaelborges1985@hotmail.com>
2020-03-18 14:34:26 -0300

    Finish constructor and startClick and getMinutes methods

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,12 +1,18 @@
 class Chronometer {
   constructor() {
     // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = 0;
   }
   startClick(callback) {
     // ... your code goes here
+    this.intervalId = setInterval(() => this.currentTime ++, 1000);
   }
   getMinutes() {
     // ... your code goes here
+    return Math.floor(this.currentTime / 60);
+
+
   }
   getSeconds() {
     // ... your code goes here
