d00147f899745237049d3effb07eb551f20b4314
donovanftl <donovan_torreslan@outlook.com>
2020-03-31 15:43:48 -0600

    cambio de turno

--- javascript/index.js
+++ javascript/index.js
@@ -1,17 +1,17 @@
-const chronometer = new Chronometer();
+const chronometer = new Chronometer()
 
 // get the buttons:
-const btnLeft = document.getElementById('btnLeft');
-const btnRight = document.getElementById('btnRight');
+const btnLeft = document.getElementById('btnLeft')
+const btnRight = document.getElementById('btnRight')
 
 // get the DOM elements that will serve us to display the time:
-let minDec = document.getElementById('minDec');
-let minUni = document.getElementById('minUni');
-let secDec = document.getElementById('secDec');
-let secUni = document.getElementById('secUni');
-let milDec = document.getElementById('milDec');
-let milUni = document.getElementById('milUni');
-let splits = document.getElementById('splits');
+let minDec = document.getElementById('minDec')
+let minUni = document.getElementById('minUni')
+let secDec = document.getElementById('secDec')
+let secUni = document.getElementById('secUni')
+let milDec = document.getElementById('milDec')
+let milUni = document.getElementById('milUni')
+let splits = document.getElementById('splits')
 
 function printTime() {
   // ... your code goes here
@@ -56,12 +56,20 @@ function setResetBtn() {
 
 // Start/Stop Button
 btnLeft.addEventListener('click', () => {
-  btnLeft.className === 'btn start' ? btnLeft.className = 'btn stop' : btnLeft.className = 'btn start';
-  btnLeft.className === 'btn start' ? btnLeft.innerText = 'START' : btnLeft.innerText = 'STOP';
-});
+  if (btnLeft.className === 'btn start') {
+    btnLeft.className = 'btn stop'
+    btnLeft.innerText = 'STOP'
+    btnRight.className = 'btn split'
+    btnRight.innerText = 'SPLIT'
+  } else {
+    btnLeft.className = 'btn start'
+    btnLeft.innerText = 'START'
+    btnRight.className = 'btn reset'
+    btnRight.innerText = 'RESET'
+  }
+})
 
 // Reset/Split Button
 btnRight.addEventListener('click', () => {
-  btnLeft.className === 'btn start' ? btnLeft.className = 'btn stop' : btnLeft.className = 'btn start';
-  btnLeft.className === 'btn start' ? btnLeft.innerText = 'START' : btnLeft.innerText = 'STOP';
-});
+    
+})
