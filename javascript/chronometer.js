0116922eee639d5cbda1a3f72d20c50f1a922634
Teddy Rogers <teddy.rogers.pro@outlook.com>
2020-04-05 18:51:30 +0200

    almost done...

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,26 +1,44 @@
 class Chronometer {
   constructor() {
     // ... your code goes here
+    this.currentTime = 0
+    this.intervalId = 0
   }
   startClick(callback) {
     // ... your code goes here
+    this.intervalId = setInterval( () => {
+      this.currentTime ++;
+    }, 1000);
   }
   getMinutes() {
     // ... your code goes here
+    let toMinutes = Math.floor(this.currentTime/60);
+    return toMinutes;
   }
   getSeconds() {
     // ... your code goes here
+    let toSeconds = this.currentTime % 60;
+    return toSeconds;
   }
-  twoDigitsNumber() {
+  twoDigitsNumber(oneDigit) {
     // ... your code goes here
-  }
+     if (oneDigit < 10) {
+       return oneDigit = `0${oneDigit}`;
+       
+     } else {
+       return oneDigit = `${oneDigit}`;
+     }
+   }
   stopClick() {
     // ... your code goes here
+    clearInterval(this.intervalId);
   }
   resetClick() {
     // ... your code goes here
+    this.currentTime = 0;
   }
   splitClick() {
     // ... your code goes here
+    return `${this.twoDigitsNumber(this.getMinutes())} : ${this.twoDigitsNumber(this.getSeconds())}`;
   }
 }
