766b345c9eafb1ec6e28f8d9116adb583c05fb77
Siiri Sikkilä <sisikkil@student.oulu.fi>
2020-04-08 19:18:52 +0300

    it's a mess :D

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,26 +1,53 @@
 class Chronometer {
   constructor() {
     // ... your code goes here
+    this.currentTime = 0; // this was hard to find out
+    this.intervalId = 0; // 
   }
   startClick(callback) {
-    // ... your code goes here
+    // use setInterval to add digits (well, 1) to intervalId every 1000 ms
+    this.intervalId = setInterval( () => {console.log("inside intervalId unnamed func", this.currentTime); this.currentTime += 1}, 1000); // y dis work like dis?????
+    // note to self:
+    // this.intervalId = setInterval( () => this.currentTime += 1, 1000);
+    // that's how it was before console.logs. nyt tuo concise arrow function on oikea syntaksi. eli ei turhia sulkeita tai keywordeja!
+    console.log("startClick was called");
   }
   getMinutes() {
-    // ... your code goes here
+    // now currentTime is added a digit every 1000 ms. Have to calculate the num of minutes
+    // return has to be wihtout decimals
+    let minutes = this.currentTime / 60; // only get full minutes
+    return Math.floor(minutes); // so if we have an hour or more, should i not return in minutes????
   }
   getSeconds() {
-    // ... your code goes here
+    // currentTime has all the seconds. getMinutes takes out full minutes. what is left, is only seconds
+    let secs = this.currentTime % 60;
+    return secs;
   }
-  twoDigitsNumber() {
-    // ... your code goes here
+  // borrowed function:
+  concatZero (num) {   // 1
+    return ('0' + num ).slice(-2);    // "01"
+  }
+  twoDigitsNumber(num) {
+    // output has to be 00 : 00
+    // borrowing code from Uros to get moving
+    let twoDigitOutput = ("0" + num).slice(-2);
+    return `${twoDigitOutput}`;
   }
   stopClick() {
     // ... your code goes here
+    // this.intervalId = clearInterval(this.intervalId); <- this way?  or like this:
+    clearInterval(this.intervalId);
   }
   resetClick() {
     // ... your code goes here
+    this.currentTime = 0;
   }
   splitClick() {
     // ... your code goes here
+//    let mins = this.getMinutes(); rather not do it This verbousely
+//    let secs = this.getSeconds();
+    let minout = this.twoDigitsNumber(this.getMinutes());
+    let secout = this.twoDigitsNumber(this.getSeconds());
+    return `${minout}:${secout}`;
   }
 }
--- javascript/index.js
+++ javascript/index.js
@@ -14,15 +14,26 @@ let milUni = document.getElementById('milUni');
 let splits = document.getElementById('splits');
 
 function printTime() {
-  // ... your code goes here
+  // ... regardless of event listeners, this should work all the time, right? and be called on main level?
+  // okay sos i would have probably solved this whole thing in a diff way and now i cant grasp anything bc i 
+  // have my own and the assigmnent-guided way of working all mixed up.. just cant pull it together :/
+  printSeconds();
+
+
 }
 
 function printMinutes() {
   // ... your code goes here
+  let minutes = chronometer.twoDigitsNumber(chronometer.getMinutes);
+  console.log(minutes);
 }
 
 function printSeconds() {
   // ... your code goes here
+  let seconds = chronometer.twoDigitsNumber(chronometer.getSeconds);
+  console.log(seconds);
+  secDec.innerHTML = seconds[0];
+  secUni.innerHTML = seconds[1];
 }
 
 // ==> BONUS
@@ -57,9 +68,32 @@ function setResetBtn() {
 // Start/Stop Button
 btnLeft.addEventListener('click', () => {
   // ... your code goes here
+  btnLeft.classList.toggle("start"); // btn start OR btn stop
+  btnLeft.classList.toggle("stop");  
+  console.log(btnLeft.className);
+  // start running time IF the class is stop
+  if (btnLeft.className === "btn stop") { // so yes this is the oppsite
+    // so i just didnt wrap this inside a function, ah bit messy i know :/
+    chronometer.startClick(printTime);
+    btnLeft.innerHTML = "STOP";  
+  } else {
+    chronometer.stopClick();
+    btnLeft.innerHTML = "START";
+  }
+  console.log(btnLeft.className);
+  //chronometer.startClick(someArgumentThatIsAFunc);
 });
 
 // Reset/Split Button
 btnRight.addEventListener('click', () => {
   // ... your code goes here
+  btnRight.classList.toggle("reset");
+  btnRight.classList.toggle("split");
+  if (btnRight.className === "btn split") {
+    console.log("btn right class såplit");
+    chronometer.innerHTML = "SPLIT";
+  } else {
+    btnRight.innerHTML = "RESET";
+    chronometer.resetClick();
+  }
 });
