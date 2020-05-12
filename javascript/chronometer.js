2d492480f831b9836fe886bea25818a481e0046e
James Dawson <63356869+polymurph13@users.noreply.github.com>
2020-05-12 13:08:22 +0100

    completed (except final bonus)

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,26 +1,32 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = 0;
   }
   startClick(callback) {
-    // ... your code goes here
+    this.intervalId = setInterval(() => {
+      this.currentTime += 1;
+    }, 1000);
   }
   getMinutes() {
-    // ... your code goes here
+    return Math.trunc(this.currentTime / 60);
   }
   getSeconds() {
-    // ... your code goes here
+    return this.currentTime % 60;
   }
-  twoDigitsNumber() {
-    // ... your code goes here
+  getMilliseconds() {
+    return this.currentTime; 
+  }
+  twoDigitsNumber(num) {
+    return num.toString().padStart(2, '0');
   }
   stopClick() {
-    // ... your code goes here
+    clearInterval(this.intervalId);
   }
   resetClick() {
-    // ... your code goes here
+    this.currentTime = 0;
   }
   splitClick() {
-    // ... your code goes here
+    return `${this.twoDigitsNumber(this.getMinutes())}:${this.twoDigitsNumber(this.getSeconds())}`;
   }
 }
--- javascript/index.js
+++ javascript/index.js
@@ -13,16 +13,24 @@ let milDec = document.getElementById('milDec');
 let milUni = document.getElementById('milUni');
 let splits = document.getElementById('splits');
 
+let timeLoop;
+
 function printTime() {
-  // ... your code goes here
+  timeLoop = setInterval(() => {
+    let timestr = chronometer.splitClick();
+    printMinutes(timestr.slice(0,2));
+    printSeconds(timestr.slice(3,5));
+  }, 1000);
 }
 
-function printMinutes() {
-  // ... your code goes here
+function printMinutes(mins) {
+  minDec.innerHTML = mins.slice(0,1);
+  minUni.innerHTML = mins.slice(1,2);
 }
 
-function printSeconds() {
-  // ... your code goes here
+function printSeconds(secs) {
+  secDec.innerHTML = secs.slice(0,1);
+  secUni.innerHTML = secs.slice(1,2);
 }
 
 // ==> BONUS
@@ -31,35 +39,79 @@ function printMilliseconds() {
 }
 
 function printSplit() {
-  // ... your code goes here
+  let splitList = document.getElementById('splits');
+  let newListItem = document.createElement('li');
+  newListItem.innerText = chronometer.splitClick();
+  newListItem.classList.add('split-item');
+  splitList.appendChild(newListItem);
 }
 
 function clearSplits() {
-  // ... your code goes here
+  let list = document.getElementById('splits');
+  var child = list.lastElementChild;  
+        while (child) { 
+            list.removeChild(child); 
+            child = list.lastElementChild; 
+        }
+  chronometer.stopClick();
+  chronometer.resetClick();
+  clearInterval(timeLoop);
+  printMinutes('00');
+  printSeconds('00');
 }
 
 function setStopBtn() {
-  // ... your code goes here
+    btnLeft.innerHTML = 'STOP';
+    btnLeft.classList.toggle('start');
+    btnLeft.classList.toggle('stop');
+    setResetBtn();
 }
 
 function setSplitBtn() {
-  // ... your code goes here
+  btnRight.innerHTML = 'RESET';
+  btnRight.classList.toggle('reset');
+  btnRight.classList.toggle('split');
 }
 
 function setStartBtn() {
-  // ... your code goes here
+    btnLeft.innerHTML = 'START';
+    btnLeft.classList.toggle('start');
+    btnLeft.classList.toggle('stop');
+    setSplitBtn();
 }
 
 function setResetBtn() {
-  // ... your code goes here
+  btnRight.innerHTML = 'SPLIT';
+  btnRight.classList.toggle('reset');
+  btnRight.classList.toggle('split');
 }
 
 // Start/Stop Button
 btnLeft.addEventListener('click', () => {
-  // ... your code goes here
+
+  if (btnLeft.classList.contains('start')) {
+    setStopBtn();
+    chronometer.startClick();
+    printTime();
+  } else if (btnLeft.classList.contains('stop')) {
+    setStartBtn();
+    chronometer.stopClick();
+  }
+  //chronometer.startClick();
+  //printTime();
+  //chronometer.stopClick();
+  //printSplit();
+
 });
 
 // Reset/Split Button
 btnRight.addEventListener('click', () => {
-  // ... your code goes here
+  if (btnRight.classList.contains('reset')) {
+      clearSplits();
+  }
+
+  if (btnRight.classList.contains('split')) {
+    printSplit();
+  }
+
 });
