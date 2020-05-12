aa66c8681aa21c6bc2c9330e22bb4040ed6b4570
Javier Asua <asuajavier@gmail.com>
2020-05-12 17:09:45 +0200

    Done except bonus

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,26 +1,42 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime=0;
+    this.intervalId =0;
+        // ... your code goes here
   }
   startClick(callback) {
+    this.intervalId = setInterval(()=>{
+      this.currentTime +=1;
+    },1000);
     // ... your code goes here
   }
   getMinutes() {
+    return Math.trunc(this.currentTime/60);
     // ... your code goes here
   }
   getSeconds() {
+    return this.currentTime%60;
     // ... your code goes here
   }
-  twoDigitsNumber() {
+  twoDigitsNumber(number) {
+    if(number.toString().length ===2){
+    console.log(number)
+    return number.toString()
+    
+    }else if(number.toString().length ===1){ return '0'+number.toString()
+    }else{ console.log('yeeeey'); return '00'}
     // ... your code goes here
   }
   stopClick() {
+    clearInterval(this.intervalId);
     // ... your code goes here
   }
   resetClick() {
+    this.currentTime =0;
     // ... your code goes here
   }
   splitClick() {
     // ... your code goes here
+    return `${this.twoDigitsNumber(this.getMinutes())}:${this.twoDigitsNumber(this.getSeconds())}`;
   }
 }
--- javascript/index.js
+++ javascript/index.js
@@ -14,14 +14,25 @@ let milUni = document.getElementById('milUni');
 let splits = document.getElementById('splits');
 
 function printTime() {
+  loop = setInterval(()=>{
+    let time = chronometer.splitClick();
+    console.log(time)
+    printMinutes(time.slice(0,2));
+    printSeconds(time.slice(3,5));
+  },1000);
   // ... your code goes here
 }
 
-function printMinutes() {
+function printMinutes(minutes) {
+  minDec.innerHTML = minutes.slice(0,1);
+  minUni.innerHTML=minutes.slice(1,2);
   // ... your code goes here
 }
 
-function printSeconds() {
+function printSeconds(seconds) {
+  secDec.innerHTML = seconds.slice(0,1)
+  console.log('print seconds')
+  secUni.innerHTML = seconds.slice(1,2);
   // ... your code goes here
 }
 
@@ -31,35 +42,81 @@ function printMilliseconds() {
 }
 
 function printSplit() {
+  let listSplits = document.getElementById('splits');
+  let newSplitForList = document.createElement('li');
+  newSplitForList.innerHTML=chronometer.splitClick();
+  listSplits.appendChild(newSplitForList);
+
   // ... your code goes here
 }
 
 function clearSplits() {
+  let list = document.getElementById('splits');
+  var last = list.lastElementChild;  
+      while (last) { 
+          list.removeChild(last); 
+          last = list.lastElementChild; 
+      }
+  chronometer.stopClick();
+  chronometer.resetClick();
+  clearInterval(loop);
+  printMinutes('00');
+  printSeconds('00');
   // ... your code goes here
 }
 
 function setStopBtn() {
+  btnLeft.innerHTML = 'STOP';
+  btnLeft.classList.toggle('start');
+  btnLeft.classList.toggle('stop');
+  setResetBtn();
   // ... your code goes here
 }
 
 function setSplitBtn() {
+  btnRight.innerHTML = 'RESET';
+  btnRight.classList.toggle('reset');
+  btnRight.classList.toggle('split');
   // ... your code goes here
 }
 
 function setStartBtn() {
+  btnLeft.innerHTML = 'START';
+    btnLeft.classList.toggle('start');
+    btnLeft.classList.toggle('stop');
+    setSplitBtn();
   // ... your code goes here
 }
 
 function setResetBtn() {
+  btnRight.innerHTML = 'SPLIT';
+  btnRight.classList.toggle('reset');
+  btnRight.classList.toggle('split');
   // ... your code goes here
 }
 
 // Start/Stop Button
 btnLeft.addEventListener('click', () => {
+  console.log('hello');
+  if (btnLeft.classList.contains('start')) {
+    setStopBtn();
+    chronometer.startClick();
+    printTime();
+  } else if (btnLeft.classList.contains('stop')) {
+    setStartBtn();
+    chronometer.stopClick();
+  }
   // ... your code goes here
 });
 
 // Reset/Split Button
 btnRight.addEventListener('click', () => {
+  if (btnRight.classList.contains('reset')) {
+    clearSplits();
+  }
+
+  if (btnRight.classList.contains('split')) {
+    printSplit();
+  }
   // ... your code goes here
 });
