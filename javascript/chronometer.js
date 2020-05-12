f6b836d2d60fbf4b031e777eca84db7480a149ca
nardokra <nardo.kra@gmail.com>
2020-05-12 16:59:08 +0200

    finished now working on the bonus

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,26 +1,58 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 21560;
+    this.intervalId = 0;
   }
+
   startClick(callback) {
-    // ... your code goes here
+    this.intervalId = setInterval(() => {
+      this.currentTime += 1;
+      callback();
+    }, 10);
   }
+
   getMinutes() {
-    // ... your code goes here
+    let theMinutes = Math.floor(this.currentTime / 6000);
+    return theMinutes;
   }
+
   getSeconds() {
-    // ... your code goes here
+    let theSeconds = Math.floor((this.currentTime - (this.getMinutes() * 6000)) / 100);
+    return theSeconds;
   }
-  twoDigitsNumber() {
-    // ... your code goes here
+
+  getMilliseconds(){
+    let theMiliseconds = Math.floor( (this.currentTime - ((this.getMinutes() * 6000) + (this.getSeconds()*100))));
+    return theMiliseconds;
   }
+
+  twoDigitsNumber(parameter) {
+    if (parameter < 10) {
+      return String(parameter = `0${parameter}`);
+    } else{
+      return String(parameter);
+    }
+  }
+
   stopClick() {
-    // ... your code goes here
+    clearInterval(this.intervalId);
   }
+
   resetClick() {
-    // ... your code goes here
+    this.currentTime = 0;
   }
+
   splitClick() {
-    // ... your code goes here
+    let min = this.getMinutes();
+    let sec = this.getSeconds();
+    if (min < 10 && sec < 10) {
+      return`${0}${min}:${0}${sec}`;
+    } else if (min < 10 && sec >= 10) {
+      return `${0}${min}:${sec}`;
+    } else if (min >= 10 && sec >= 10) {
+      return `${min}:${sec}`;
+    } else{
+      return `${min}:${sec}`;
+    }
   }
 }
--- javascript/index.js
+++ javascript/index.js
@@ -14,15 +14,18 @@ let milUni = document.getElementById('milUni');
 let splits = document.getElementById('splits');
 
 function printTime() {
-  // ... your code goes here
+  printMinutes();
+  printSeconds();
 }
 
 function printMinutes() {
-  // ... your code goes here
+  minDec.innerHTML = chronometer.twoDigitsNumber(chronometer.getMinutes())[0];
+  minUni.innerHTML = chronometer.twoDigitsNumber(chronometer.getMinutes())[1];
 }
 
 function printSeconds() {
-  // ... your code goes here
+  secDec.innerHTML = chronometer.twoDigitsNumber(chronometer.getSeconds())[0];
+  secUni.innerHTML = chronometer.twoDigitsNumber(chronometer.getSeconds())[1];
 }
 
 // ==> BONUS
@@ -31,35 +34,67 @@ function printMilliseconds() {
 }
 
 function printSplit() {
-  // ... your code goes here
+  let liSplit = document.createElement('li');
+  liSplit.innerHTML = chronometer.splitClick();
+  let placeSplitHere = document.querySelector('#splits');
+  placeSplitHere.appendChild(liSplit);
 }
 
 function clearSplits() {
-  // ... your code goes here
+  if (document.querySelectorAll('#splits li')[0]) {
+    let allSplitElements = document.querySelectorAll('#splits li');
+    let parent = document.querySelector('#splits');
+    for (let i = 0; i < allSplitElements.length; i++) {
+      parent.removeChild(allSplitElements[i]);
+    }
+  }
 }
 
 function setStopBtn() {
-  // ... your code goes here
+  btnLeft.style.backgroundColor = "red";
+  btnLeft.innerHTML = "STOP";
 }
 
 function setSplitBtn() {
-  // ... your code goes here
+  btnRight.style.backgroundColor = "#0851ab";
+  btnRight.innerHTML = "SPLIT"
 }
 
 function setStartBtn() {
-  // ... your code goes here
+  btnLeft.style.backgroundColor = "green";
+  btnLeft.innerHTML = "START";
 }
 
 function setResetBtn() {
-  // ... your code goes here
+  btnRight.style.backgroundColor = "#908e8e";
+  btnRight.innerHTML = "RESET";
 }
 
 // Start/Stop Button
 btnLeft.addEventListener('click', () => {
-  // ... your code goes here
+  if(btnLeft.innerText === "START"){
+    chronometer.startClick(printTime);
+    setStopBtn();
+    setSplitBtn();
+    printMinutes();
+    printSeconds();
+  } else {
+    chronometer.stopClick();
+    setStartBtn();
+    setResetBtn();
+  }
+  
 });
 
 // Reset/Split Button
 btnRight.addEventListener('click', () => {
-  // ... your code goes here
+  if(btnRight.innerHTML === "RESET"){
+    minDec.innerHTML = "0";
+    minUni.innerHTML = "0";
+    secDec.innerHTML = "0";
+    secUni.innerHTML = "0";
+    clearSplits();
+  } else {
+    printSplit();
+  }
 });
