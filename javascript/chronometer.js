93ae59634d5a38e6c24933f7453a3f777106d694
Nic Barnes <colorpulse@gmail.com>
2020-05-14 16:59:25 +0200

    DOM manipulation complete + Bonus

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,17 +1,28 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
      
-
   }
     currentTime = 0; 
+    currentTimeMilli = 0;
     intervalId = 0;
+    intervalIdMilli = 0;
+    
 
   startClick(callback) {
     this.intervalId = setInterval(() => {
       this.currentTime ++;
+      callback()
     }, 1000)
   }
+
+  startClickMilli(callback) {
+     this.intervalIdMilli = setInterval(() => {
+      this.currentTimeMilli ++;
+      callback();
+      console.log(this.currentTimeMilli)
+    }, 10)
+  }
+  
   getMinutes() {
     return Math.floor(this.currentTime / 60);
     
@@ -20,16 +31,29 @@ class Chronometer {
     return this.currentTime % 60;
     
   }
-  twoDigitsNumber(time) {
-    return String(time).padStart(2, 0)
+  getMilliSeconds() {
+    return this.currentTimeMilli;
   }
+  twoDigitsNumber(time) {
+    return String(time).padStart(2, 0);
+  }
+  twoDigitMilli(num){
+    
+    return String(num).slice(-2);
+  }
+
   stopClick() {
-     clearInterval();  
+     clearInterval(this.intervalId); 
+     clearInterval(this.intervalIdMilli);
+     
     }
   resetClick() {
     this.currentTime = 0;
+    this.currentTimeMilli = 0;
+    
   }
   splitClick(num1, num2) {
-    return `${this.twoDigitsNumber(this.getMinutes())}:${this.twoDigitsNumber(this.getSeconds())}`
+    return `${this.twoDigitsNumber(this.getMinutes())}:${this.twoDigitsNumber(this.getSeconds())}:${this.twoDigitMilli(this.getMilliSeconds())}`
   }
 }
+
--- javascript/index.js
+++ javascript/index.js
@@ -13,53 +13,120 @@ let milDec = document.getElementById('milDec');
 let milUni = document.getElementById('milUni');
 let splits = document.getElementById('splits');
 
+
+
 function printTime() {
   // ... your code goes here
+  printMinutes();
+  printSeconds();
+  
+}
+
+function printTimeMilli() {
+  // ... your code goes here
+  printMilliseconds();
 }
 
 function printMinutes() {
-  // ... your code goes here
+  
+  let myMinutes = chronometer.twoDigitsNumber(chronometer.getMinutes());
+  minDec.innerHTML = myMinutes[0];
+  minUni.innerHTML = myMinutes[1];
+  
 }
 
+
+
 function printSeconds() {
-  // ... your code goes here
+  let mySeconds = chronometer.twoDigitsNumber(chronometer.getSeconds());
+  secDec.innerHTML = mySeconds[0];
+  secUni.innerHTML = mySeconds[1];
+  
 }
+
 
 // ==> BONUS
 function printMilliseconds() {
   // ... your code goes here
+  let milli = chronometer.twoDigitMilli(chronometer.getMilliSeconds());
+  milDec.innerHTML = milli[0]
+  milUni.innerHTML = milli[1]
+  
+  
 }
 
 function printSplit() {
-  // ... your code goes here
+  
+  let li = document.createElement('li');
+  li.innerHTML = chronometer.splitClick()
+  splits.appendChild(li);
+  
+  
 }
 
 function clearSplits() {
   // ... your code goes here
+  splits.innerHTML = "";
 }
 
 function setStopBtn() {
   // ... your code goes here
+    btnLeft.innerHTML = "STOP";
+    btnLeft.className = 'btn stop';
+
+    
 }
 
 function setSplitBtn() {
   // ... your code goes here
+  btnRight.innerHTML = 'SPLIT'
+  btnRight.className ='btn split';
 }
 
 function setStartBtn() {
   // ... your code goes here
+  btnLeft.innerHTML = 'START';
+  btnLeft.className = 'btn start';
 }
 
 function setResetBtn() {
   // ... your code goes here
+  btnRight.innerHTML = 'RESET';
+  btnRight.className = 'btn reset';
+
 }
 
 // Start/Stop Button
 btnLeft.addEventListener('click', () => {
-  // ... your code goes here
+  if (btnLeft.classList.contains('start')){
+    chronometer.startClick(printTime);
+    chronometer.startClickMilli(printTimeMilli);
+    setStopBtn();
+    setSplitBtn();
+    
+
+  } else {
+    setStartBtn();
+    setResetBtn();
+    chronometer.stopClick();
+  }
+  
 });
 
 // Reset/Split Button
 btnRight.addEventListener('click', () => {
-  // ... your code goes here
+  if (btnRight.classList.contains('split')){
+    printSplit()
+  } else {
+    chronometer.resetClick();
+    printTime();
+    printTimeMilli();
+    clearSplits()
+    milDec.innerHTML = '0'
+    milUni.innerHTML = '0'
+  
+  }
+    
 });
+
+
