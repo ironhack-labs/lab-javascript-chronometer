e801d41de62d1d57bba7613b0f25cec191d97f9a
Irma <iblanc12@corp.vodafone.es>
2020-05-16 15:03:01 +0200

    First iteration in progress

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,12 +1,19 @@
 class Chronometer {
   constructor() {
     // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = 0;
   }
+
   startClick(callback) {
     // ... your code goes here
+    setInterval( () => this.currentTime++, 1000);
   }
+  
   getMinutes() {
     // ... your code goes here
+    const minutos = Math.floor(this.currentTime/60)
+    return Math.floor(this.currentTime/60);
   }
   getSeconds() {
     // ... your code goes here
