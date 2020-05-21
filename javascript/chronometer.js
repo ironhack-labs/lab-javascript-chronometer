08e01d5ff318dff17af2e2e900cbfaa28c160078
Irma <iblanc12@corp.vodafone.es>
2020-05-21 17:56:12 +0200

    feat: getSeconds()

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,23 +1,20 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
     this.currentTime = 0;
     this.intervalId = 0;
   }
 
   startClick(callback) {
-    // ... your code goes here
     setInterval( () => this.currentTime++, 1000);
   }
   
   getMinutes() {
-    // ... your code goes here
-    const minutos = Math.floor(this.currentTime/60)
     return Math.floor(this.currentTime/60);
   }
   getSeconds() {
-    // ... your code goes here
+    return Math.floor(this.currentTime - this.getMinutes(this.currentTime) * 60);
   }
+  
   twoDigitsNumber() {
     // ... your code goes here
   }
