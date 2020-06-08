8e4bb5e181f37baec79d75afca28cf2351fd5066
Jose Roberto <juniorferreirarob@gmail.com>
2020-06-08 18:40:42 -0300

    interecao-1-done

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,26 +1,32 @@
 class Chronometer {
-  constructor() {
+  constructor( ) {
     // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = 0;
   }
+
   startClick(callback) {
-    // ... your code goes here
+    this.intervalId = setInterval(() => this.currentTime++, 1000)
   }
   getMinutes() {
     // ... your code goes here
+    return Math.floor(this.currentTime / 60)  ;
+  
   }
   getSeconds() {
     // ... your code goes here
+    return (Math.floor(this.currentTime % 60));
   }
   twoDigitsNumber() {
-    // ... your code goes here
+    return (this.getMinutes()).toString() + (this.getSeconds()).toString();
   }
   stopClick() {
-    // ... your code goes here
+    clearInterval(this.intervalId);
   }
   resetClick() {
-    // ... your code goes here
+    this.currentTime = 0;
   }
   splitClick() {
-    // ... your code goes here
+    return this.twoDigitsNumber(this.getMinutes) + `:` + this.twoDigitsNumber(this.getSeconds); ;
   }
 }
