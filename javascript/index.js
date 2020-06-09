debf1021086a948423cfc291a015019e29b22418
Miguel Serrano <miguelserrano.sp@gmail.com>
2020-06-09 23:04:56 +0200

    done

--- javascript/index.js
+++ javascript/index.js
@@ -14,15 +14,27 @@ let milUni = document.getElementById('milUni');
 let splits = document.getElementById('splits');
 
 function printTime() {
-  // ... your code goes here
+  setInterval(() => {
+    printSeconds()
+    printMinutes()
+  }, 1000)
+  
 }
 
 function printMinutes() {
   // ... your code goes here
+  let mins = chronometer.getMinutes()
+  let minsStr = chronometer.twoDigitsNumber(mins) //instancia de clase chronometer
+  minDec.innerText = minsStr[0]
+  minUni.innerText = minsStr[1]
 }
 
 function printSeconds() {
   // ... your code goes here
+  let secs = chronometer.getSeconds()
+  let secsStr = chronometer.twoDigitsNumber(secs)
+  secDec.innerText = secsStr[0]
+  secUni.innerText = secsStr[1]
 }
 
 // ==> BONUS
@@ -36,30 +48,58 @@ function printSplit() {
 
 function clearSplits() {
   // ... your code goes here
+
 }
 
 function setStopBtn() {
   // ... your code goes here
+  btnLeft.classList.toggle('stop')
+  btnLeft.innerText = 'STOP'
 }
 
 function setSplitBtn() {
+  btnRight.classList.toggle('split')
+  btnRight.innerText = 'SPLIT'
+  
   // ... your code goes here
 }
 
 function setStartBtn() {
+  btnLeft.classList.remove('stop')
+  btnLeft.innerText = 'START'
   // ... your code goes here
 }
 
 function setResetBtn() {
   // ... your code goes here
+  btnRight.classList.remove('split')
+  btnRight.innerText = 'RESET'
 }
 
 // Start/Stop Button
 btnLeft.addEventListener('click', () => {
-  // ... your code goes here
-});
+  if(btnLeft.className === 'btn start') {
+    setStopBtn()
+    setSplitBtn()
+    chronometer.startClick()
+    printTime()
+  } else {
 
+    chronometer.stopClick()
+    setStartBtn()
+    setResetBtn()
+
+    
+  }
+})
 // Reset/Split Button
 btnRight.addEventListener('click', () => {
-  // ... your code goes here
-});
+  if(btnRight.className === 'btn reset') {
+
+    setResetBtn()
+    chronometer.resetClick()
+    
+  } else {
+    printTime()
+  }  
+})
