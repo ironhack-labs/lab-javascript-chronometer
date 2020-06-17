4577ba26746822cc0ecd40b95bd934146b2a0827
Renzo Pederzoli <rdp73@miami.edu>
2020-06-17 11:04:02 -0400

    finished-logic

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,26 +1,35 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0
+    this.intervalId = 0
   }
   startClick(callback) {
-    // ... your code goes here
+    this.intervalId = setInterval(() => {
+      this.currentTime++
+    }, 1000)
   }
   getMinutes() {
-    // ... your code goes here
+    let mins = this.currentTime / 60
+    return Math.floor(mins)
   }
   getSeconds() {
-    // ... your code goes here
+    let min = this.getMinutes()
+    return this.currentTime - min*60
   }
-  twoDigitsNumber() {
-    // ... your code goes here
+  twoDigitsNumber(num) {
+    if (num < 10)
+      return "0" + num.toString()
+    return num.toString()
   }
   stopClick() {
-    // ... your code goes here
+    clearInterval(this.intervalId)
   }
   resetClick() {
-    // ... your code goes here
+    this.currentTime = 0
   }
   splitClick() {
-    // ... your code goes here
+    let min = this.twoDigitsNumber(this.getMinutes())
+    let sec = this.twoDigitsNumber(this.getSeconds())
+    return `${min}:${sec}`
   }
 }
