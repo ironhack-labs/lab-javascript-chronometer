f28139b5236cda359f47d3bb7981f44283c27ec4
Manon Morgaut <manonmorgaut@gmail.com>
2020-06-18 16:48:21 +0200

    Finished exercise

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,26 +1,43 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = 0;
   }
   startClick(callback) {
-    // ... your code goes here
+     this.intervalId = setInterval(() => {
+      this.currentTime++;
+      console.log(this.currentTime);
+    }, 1000);
   }
   getMinutes() {
-    // ... your code goes here
+    return Math.round(this.currentTime / 60);
   }
   getSeconds() {
-    // ... your code goes here
+    return this.currentTime % 60;
   }
-  twoDigitsNumber() {
-    // ... your code goes here
+  twoDigitsNumber(number) {
+    if (number < 10) return (number = `0${number}`);
   }
   stopClick() {
-    // ... your code goes here
+    clearInterval(this.intervalId);
   }
   resetClick() {
-    // ... your code goes here
+    this.currentTime = 0;
   }
-  splitClick() {
-    // ... your code goes here
+  splitClick(m, s) {
+    let min = this.twoDigitsNumber(m);
+    let sec = this.twoDigitsNumber(s);
+    return `${min}:${sec}`;
   }
 }
+
+// LEARNINGS 
+// Btn.onclick.function; attention a ne pas APPELER la fonction (pas de parentheses)
+// Pour initialiser une constante il faut l'initialiser avec this. dans le constructor sans arguments 
+// Ceci permet d'utiliser cette constante a plusieurs endroits dans la classe (dans les fonctions)
+// Il est possible d'appeler plusieurs fonctions avec onclick: 
+// btnLeft.onclick = function () {
+//   changeLeftBtn();
+//   leftClick()
+//   printTime();
+// };
--- javascript/index.js
+++ javascript/index.js
@@ -1,65 +1,72 @@
 const chronometer = new Chronometer();
 
 // get the buttons:
-const btnLeft = document.getElementById('btnLeft');
-const btnRight = document.getElementById('btnRight');
+const btnLeft = document.getElementById("btnLeft");
+const btnRight = document.getElementById("btnRight");
+
+function changeLeftBtn() {
+  if (btnLeft.classList.contains("start")) {
+    btnLeft.textContent = "STOP";
+    btnLeft.classList.replace("start", "stop");
+    btnRight.textContent = "SPLIT";
+    btnRight.classList.replace("reset", "split");
+  } else {
+    btnLeft.textContent = "START";
+    btnLeft.classList.replace("stop", "start");
+    btnRight.textContent = "RESET";
+    btnRight.classList.replace("split", "reset");
+  }
+}
+
+function leftClick() {
+  if (btnLeft.classList.contains("stop")) {
+    return chronometer.startClick();
+  } else return chronometer.stopClick();
+}
+
+btnLeft.onclick = function () {
+  changeLeftBtn();
+  leftClick()
+  printTime();
+};
 
 // get the DOM elements that will serve us to display the time:
-let minDec = document.getElementById('minDec');
-let minUni = document.getElementById('minUni');
-let secDec = document.getElementById('secDec');
-let secUni = document.getElementById('secUni');
-let milDec = document.getElementById('milDec');
-let milUni = document.getElementById('milUni');
-let splits = document.getElementById('splits');
+let minDec = document.getElementById("minDec");
+let minUni = document.getElementById("minUni");
+let secDec = document.getElementById("secDec");
+let secUni = document.getElementById("secUni");
+let milDec = document.getElementById("milDec");
+let milUni = document.getElementById("milUni");
+let splits = document.getElementById("splits");
 
 function printTime() {
-  // ... your code goes here
-}
-
-function printMinutes() {
-  // ... your code goes here
-}
-
-function printSeconds() {
-  // ... your code goes here
-}
-
-// ==> BONUS
-function printMilliseconds() {
-  // ... your code goes here
+  let minutes = chronometer.getMinutes();
+  let seconds = chronometer.getSeconds();
+  minutes = chronometer.twoDigitsNumber(minutes);
+  seconds = chronometer.twoDigitsNumber(seconds);
+  minDec.textContent = minutes[0];
+  minUni.textContent = minutes[1];
+  secDec.textContent = seconds[0];
+  secUni.textContent = seconds[1];
 }
 
 function printSplit() {
-  // ... your code goes here
+  let minutes = chronometer.getMinutes();
+  let seconds = chronometer.getSeconds();
+  let time = chronometer.splitClick(minutes,seconds);
+  splits.innerHTML+=`<li>${time}</li>`;
 }
 
-function clearSplits() {
-  // ... your code goes here
+function clear() {
+    chronometer.currentTime=0;
+    printTime();
+    splits.innerHTML="";
 }
 
-function setStopBtn() {
-  // ... your code goes here
-}
-
-function setSplitBtn() {
-  // ... your code goes here
-}
-
-function setStartBtn() {
-  // ... your code goes here
-}
-
-function setResetBtn() {
-  // ... your code goes here
-}
-
-// Start/Stop Button
-btnLeft.addEventListener('click', () => {
-  // ... your code goes here
-});
-
-// Reset/Split Button
-btnRight.addEventListener('click', () => {
-  // ... your code goes here
-});
+btnRight.onclick = function () {
+  if (btnRight.classList.contains("split")) 
+    printSplit();
+  
+  else 
+  clear();
+  }
