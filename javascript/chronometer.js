b776df8faf753dfa973b8fe17bb63d2fd7c48e5f
Alejandro Garcia <agarc371@gmail.com>
2020-06-20 16:59:02 -0400

    add logic for getMinutes method

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -9,7 +9,7 @@ class Chronometer {
     }, 1000)
   }
   getMinutes() {
-    // ... your code goes here
+    return Math.floor((this.currentTime / 60));
   }
   getSeconds() {
     // ... your code goes here
