6d861972177ac4ff90048e4a5b61c685339f9105
Kenouly Lo Van <kenouly.lovan@gmail.com>
2020-07-18 14:37:18 +0200

    done

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,26 +1,56 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = 0;
   }
+
+   // increment by 1 the curremtTime property every 1 second
   startClick(callback) {
-    // ... your code goes here
-  }
+    this.intervalId = setInterval(() => {
+      this.currentTime ++;
+      if(callback) {
+        callback();
+      }
+    }, 1000);
+    }
+
+  // return the number of minutes = value of seconds in currentTime
   getMinutes() {
-    // ... your code goes here
+    return Math.floor(this.currentTime / 60);
   }
+
+  // return the seconds left after calculating the round number of minutes
   getSeconds() {
-    // ... your code goes here
+    return Math.floor(this.currentTime %60);
   }
-  twoDigitsNumber() {
-    // ... your code goes here
+
+  //  return 2 digits-number when functions return 1 digit-number
+  twoDigitsNumber(timeNumber) {
+    if(timeNumber < 10) {
+      return `${0}${timeNumber}`;
+    } else {
+      return `${timeNumber}`;
+    }
   }
-  stopClick() {
-    // ... your code goes here
+
+  // stop the chronometer
+stopClick() {
+  clearInterval(this.intervalId);
   }
-  resetClick() {
-    // ... your code goes here
+
+  // set curremtTime to 0
+resetClick() {
+  this.currentTime = 0;
+}
+
+// put in valid format
+splitClick() {
+  let sec = this.getSeconds();
+  let min = this.getMinutes();
+  if (min < 10 || sec < 10) {
+    return `${0}${min}:${0}${sec}`;
+  } else {
+    return `${min}:${sec}`;
   }
-  splitClick() {
-    // ... your code goes here
-  }
+}
 }
