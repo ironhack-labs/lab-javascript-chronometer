96618bbd091b53fab1d07836780d560aa162447d
unimatrix2 <dnny.nis@gmail.com>
2020-07-25 18:00:58 -0300

    finished iteration 1

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,26 +1,40 @@
 class Chronometer {
-  constructor() {
-    // ... your code goes here
-  }
-  startClick(callback) {
-    // ... your code goes here
-  }
-  getMinutes() {
-    // ... your code goes here
-  }
-  getSeconds() {
-    // ... your code goes here
-  }
-  twoDigitsNumber() {
-    // ... your code goes here
-  }
-  stopClick() {
-    // ... your code goes here
-  }
-  resetClick() {
-    // ... your code goes here
-  }
-  splitClick() {
-    // ... your code goes here
-  }
+    constructor() {
+        this.currentTime = 0;
+        this.intervalId = 0;
+    }
+
+    startClick(callback) {
+        this.intervalId = setInterval(() => { this.currentTime += 1; }, 1000);
+    }
+
+    getMinutes() {
+        return Math.floor(this.currentTime / 60);
+    }
+
+    getSeconds() {
+        if (this.currentTime === 0) return 0;
+        return this.currentTime % 60;
+    }
+
+    twoDigitsNumber(num) {
+        const digit = JSON.stringify(num);
+        if (digit.length === 1) { return 0 + digit; }
+        return digit;
+    }
+
+    stopClick() {
+        clearInterval(this.intervalID);
+    }
+
+    resetClick() {
+        this.currentTime = 0;
+    }
+
+    splitClick() {
+        const min = this.twoDigitsNumber(this.getMinutes());
+        const sec = this.twoDigitsNumber(this.getSeconds());
+        const result = `${min}:${sec}`;
+        return result;
+    }
 }
--- tests/chronometer.spec.js
+++ tests/chronometer.spec.js
@@ -123,18 +123,15 @@ describe('Chronometer', () => {
     it('should always return a string of length 2', () => {
       expect(chronometer.twoDigitsNumber(7).length).toEqual(2);
     });
-
-    // it("should return '00' when the value is 0", () => {
-    //   expect(chronometer.twoDigitsNumber(0)).toEqual('00');
-    // });
-
-    // it("should return '15' when the value is 15", ()=> {
-    //   expect(chronometer.twoDigitsNumber(15)).toEqual('15');
-    // });
-
-    // it("Should return '03' when the value is 3", function() {
-    //   expect(chronometer.twoDigitsNumber(3)).toEqual('03');
-    // });
+     it("should return '00' when the value is 0", () => {
+       expect(chronometer.twoDigitsNumber(0)).toEqual('00');
+     });
+     it("should return '15' when the value is 15", ()=> {
+       expect(chronometer.twoDigitsNumber(15)).toEqual('15');
+     });
+     it("Should return '03' when the value is 3", function() {
+       expect(chronometer.twoDigitsNumber(3)).toEqual('03');
+     });
   });
 
   describe('stopClick method', () => {
