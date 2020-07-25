bcd818eac4a72ff1580ba949faba6b099441adcf
Rodrigo Muniz <ramunizbh@yahoo.com.br>
2020-07-25 15:46:57 -0300

    done

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,26 +1,65 @@
 class Chronometer {
   constructor() {
     // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = 0;
   }
+
   startClick(callback) {
     // ... your code goes here
+    this.intervalId = setInterval(() => {
+      this.currentTime += 1; 
+    },1000)
   }
+  
   getMinutes() {
     // ... your code goes here
+    let minutes = this.currentTime / 60;
+    return Math.floor(minutes);
   }
   getSeconds() {
     // ... your code goes here
+    let minutes = this.currentTime / 60;
+    minutes =  Math.floor(minutes); 
+
+    let seconds =  this.currentTime - (minutes * 60);
+    return seconds;
   }
-  twoDigitsNumber() {
-    // ... your code goes here
+
+  twoDigitsNumber(value) {
+    let strRet = '';
+
+    if (value.length > 1 ){
+      strRet += String(value);
+    }else {
+      strRet += '0'+String(value);
+    }
+
+    return strRet;
   }
   stopClick() {
     // ... your code goes here
+    clearInterval(this.intervalId);
   }
   resetClick() {
     // ... your code goes here
+    this.currentTime = 0;
   }
-  splitClick() {
+  splitClick(minutos, segundos) {
     // ... your code goes here
+    if (! minutos ){
+      minutos = 0;
+    }
+
+    if (! segundos ){
+      segundos = 0;
+    }
+
+    let strRetorno = '';
+
+    strRetorno = this.twoDigitsNumber(minutos) + ':' + this.twoDigitsNumber(segundos)
+
+    return strRetorno;
+
   }
 }
