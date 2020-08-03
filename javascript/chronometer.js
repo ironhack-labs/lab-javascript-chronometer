e9a9a80ede74c4640395f59de0574858e35d6462
Alex Anthopoulos <zephoxcodese@gmail.com>
2020-08-03 18:15:06 +0200

    done but one jasmine error

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,26 +1,37 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0 
+    this.intervalId = 0
   }
   startClick(callback) {
-    // ... your code goes here
+    this.intervalId = setInterval(() => this.currentTime +=1, 1000)
+    if(callback) {
+      callback()
+    }
   }
   getMinutes() {
-    // ... your code goes here
+   return Math.floor(this.currentTime / 60)
   }
   getSeconds() {
-    // ... your code goes here
+    let seconds = this.currentTime - this.getMinutes() * 60
+    return seconds
   }
   twoDigitsNumber() {
-    // ... your code goes here
+    if(this.getMinutes()< 10) {
+      return `0 + ${this.getMinutes()}`;
+    }
+    if(this.getSeconds() < 10) {
+      return `0 + ${this.getSeconds()}`;
+    }
   }
+
   stopClick() {
-    // ... your code goes here
-  }
-  resetClick() {
-    // ... your code goes here
-  }
+     return clearInterval(this.intervalId)
+   }
+   resetClick() {
+    return this.currentTime = 0;
+   }
   splitClick() {
-    // ... your code goes here
-  }
+     return `${this.twoDigitsNumber(this.getMinutes())}:${this.twoDigitsNumber(this.getSeconds())}`
+ }
 }
