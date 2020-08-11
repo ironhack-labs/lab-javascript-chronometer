5e54f5c2c3febe224b8704f7f85aa8f6d8964edb
Yvan Menuet <yvan.menuet@gmail.com>
2020-08-11 16:29:01 -0500

    chronometer.js done

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,26 +1,34 @@
 class Chronometer {
-  constructor() {
-    // ... your code goes here
-  }
-  startClick(callback) {
-    // ... your code goes here
-  }
-  getMinutes() {
-    // ... your code goes here
-  }
-  getSeconds() {
-    // ... your code goes here
-  }
-  twoDigitsNumber() {
-    // ... your code goes here
-  }
-  stopClick() {
-    // ... your code goes here
-  }
-  resetClick() {
-    // ... your code goes here
-  }
-  splitClick() {
-    // ... your code goes here
-  }
+    constructor() {
+        this.currentTime = 0;
+        this.intervalId = 0;
+    }
+    startClick(callback) {
+        // callback()
+        setInterval(() => {
+            this.currentTime++
+        }, 1000)
+    }
+    getMinutes() {
+        return Math.floor(this.currentTime / 60)
+    }
+    getSeconds() {
+        return this.currentTime % 60
+    }
+    twoDigitsNumber(number) {
+        let stringNumber = number.toString()
+        if (stringNumber.length < 2) {
+            stringNumber = "0" + stringNumber
+        }
+        return stringNumber
+    }
+    stopClick() {
+        clearInterval();
+    }
+    resetClick() {
+        this.currentTime = 0;
+    }
+    splitClick() {
+        return `${this.twoDigitsNumber(this.getMinutes())}:${this.twoDigitsNumber(this.getSeconds())}`
+    }
 }
