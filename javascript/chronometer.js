58aa5cdc05fcf5eb240dac4432286f466b1a1f85
Raphaelle Olivo <rapholivo@hotmail.fr>
2020-09-02 16:16:38 +0200

    Raph first driver done until get minutes

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,13 +1,18 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = 0;
   }
   startClick(callback) {
-    // ... your code goes here
+    const intervalId = setInterval(() => {
+      this.currentTime++;
+      // console.log(this.currentTime)
+    }, 1000);
   }
-  getMinutes() {
-    // ... your code goes here
-  }
+
+
+  getMinutes = () => Math.floor(this.currentTime / 60);
+
   getSeconds() {
     // ... your code goes here
   }
