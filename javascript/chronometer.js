6d4a159ead61f89840a132405835065cdfdaa4ac
sechs <philippe.buteau@skema.edu>
2020-09-02 16:46:13 +0200

    Iteration 1 completed

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,26 +1,47 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = 0;
   }
+
   startClick(callback) {
-    // ... your code goes here
+    this.intervalId = setInterval(() => this.currentTime++, 1000);
   }
+
   getMinutes() {
-    // ... your code goes here
+    return Math.trunc(this.currentTime / 60);
   }
+
   getSeconds() {
-    // ... your code goes here
+    return Number(this.currentTime % 60);
   }
-  twoDigitsNumber() {
-    // ... your code goes here
+
+  twoDigitsNumber(value) {
+    if (!isNaN(value)) {
+      if (value.length === 1) {
+        value = "0" + value;
+        return value;
+      }
+    } else {
+      console.log("This is not a number");
+    }
+    return "00";
   }
+
   stopClick() {
-    // ... your code goes here
+    clearInterval(this.intervalId);
   }
+
   resetClick() {
-    // ... your code goes here
+    this.currentTime = 0;
   }
+
   splitClick() {
-    // ... your code goes here
+    var result = "";
+    result += this.twoDigitsNumber(this.getMinutes());
+    result += ":";
+    result += this.twoDigitsNumber(this.getSeconds());
+  
+    return result;
   }
 }
