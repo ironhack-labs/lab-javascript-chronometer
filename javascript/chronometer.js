4d2174cd021d6ccabea45c29f7dc0caf87e6df08
Ute Wiemer <upwiemer@gmail.com>
2020-09-12 15:10:14 +0200

    lab submit

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,26 +1,37 @@
-class Chronometer {
+class Chronometer  {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = 0;
   }
-  startClick(callback) {
-    // ... your code goes here
+  
+startClick(callback) {
+  setInterval(() => {
+      this.currentTime += 1
+    }, 1000);
   }
+
   getMinutes() {
-    // ... your code goes here
+  return Math.floor(this.currentTime / 60)
   }
+
   getSeconds() {
-    // ... your code goes here
+    return this.currentTime%60
   }
-  twoDigitsNumber() {
-    // ... your code goes here
+
+  twoDigitsNumber(number) {
+   if (number<10) return "0" + number;
+   else return number
   }
+
   stopClick() {
-    // ... your code goes here
+    clearInterval(this.currentTime)
   }
+
   resetClick() {
-    // ... your code goes here
+ this.currentTime = 0
   }
+
   splitClick() {
-    // ... your code goes here
+   return this.twoDigitsNumber(this.getMinutes()) + ":" + this.twoDigitsNumber(this.getSeconds())
   }
 }
