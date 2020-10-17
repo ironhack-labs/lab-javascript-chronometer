937ad4ada8413f9572b124d5ecd287ed52d6f39d
Marc Lopez Pujol <marclopez@MacBook-Pro-de-Marc-2.local>
2020-10-17 15:39:50 +0200

    Primeras funciones

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,15 +1,18 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = 0;
   }
   startClick(callback) {
-    // ... your code goes here
+    this.intervalId = setInterval(() => this.currentTime++, 1000);
   }
   getMinutes() {
-    // ... your code goes here
+    let minutes = Math.floor(this.currentTime / 60);
+    return minutes;
   }
   getSeconds() {
-    // ... your code goes here
+    let seconds = this.currentTime - (this.getMinutes() * 60);
+    return seconds
   }
   twoDigitsNumber() {
     // ... your code goes here
