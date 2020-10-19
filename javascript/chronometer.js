221ff8310ff19938428fc2642d821f7a59dc0ff8
Nina van den Braak <ninavandenbraak@gmail.com>
2020-10-19 17:10:27 +0200

    did the first iteration, although not sure why splitclick fails

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,26 +1,35 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0
+    this.intervalId = 0
   }
   startClick(callback) {
-    // ... your code goes here
+    setInterval(() =>
+      this.currentTime++, 1000)
   }
   getMinutes() {
-    // ... your code goes here
+    return Math.floor(this.currentTime / 60)
   }
   getSeconds() {
-    // ... your code goes here
+    return (this.currentTime - Math.floor(this.getMinutes() * 60))
   }
-  twoDigitsNumber() {
-    // ... your code goes here
+  twoDigitsNumber(number) {
+    number = number.toString()
+    if (number.length < 2) {
+      return `0${number}`
+    } else {
+      return number
+    }
   }
   stopClick() {
-    // ... your code goes here
+    clearInterval(this.IntervalId)
   }
   resetClick() {
-    // ... your code goes here
+    this.currentTime = 0
   }
   splitClick() {
-    // ... your code goes here
+    let min = this.twoDigitsNumber(this.getMinutes)
+    let sec = this.twoDigitsNumber(this.getSeconds)
+    return `${min}:${sec}`
   }
 }
