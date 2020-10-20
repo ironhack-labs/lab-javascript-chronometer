4e2c33584de5e6efd378520e2622cf4d89dcf3de
shtesh <vshtefanuka@gmail.com>
2020-10-20 21:14:08 +0200

    Iteration 1

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,26 +1,49 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = 0;
   }
+
   startClick(callback) {
-    // ... your code goes here
+    this.intervalId = setInterval( () => {
+      this.currentTime++;
+      if (callback) callback();
+   }, 1000);
   }
+
   getMinutes() {
-    // ... your code goes here
+    let minutes = Math.floor(this.currentTime / 60);
+    return minutes; 
   }
+
   getSeconds() {
-    // ... your code goes here
+    let seconds = Math.floor(this.currentTime % 60);
+    return seconds;
   }
+
   twoDigitsNumber() {
-    // ... your code goes here
+   return number < 10 ? '0' + number : number;
   }
+
+  // twoDigitsNumber(value) {
+//if (value < 10) return `0${value}`;
+//return `${value}`;
+// OR return ("0" + value).slice(-2);
+  //}
+
   stopClick() {
-    // ... your code goes here
+    clearInterval(this.intervalId);
   }
+
   resetClick() {
-    // ... your code goes here
+    this.currentTime = 0;
   }
   splitClick() {
-    // ... your code goes here
+    return `${this.twoDigitsNumber(this.getMinutes())}:${this.twoDigitsNumber(this.getSeconds())}`;
   }
 }
+//let minutes = this.twoDigitsNumber(this.getMinutes());
+//let seconds = this.twoDigitsNumber(this.getSeconds());
+//return `${minutes}:${seconds}`;
+
+
