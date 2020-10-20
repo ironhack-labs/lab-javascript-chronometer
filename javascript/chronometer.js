98282a0fcc9bbd4be8fa0311bc509f267035bd03
Ceyda Ulubas <ceyda.ulubas@gmail.com>
2020-10-20 19:43:41 +0300

    completed Iteration1

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,26 +1,35 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = 0;
   }
-  startClick(callback) {
-    // ... your code goes here
+
+  startClick(){
+    this.intervalId= setInterval(() => { 
+      this.currentTime ++;
+    },1000);
   }
+
   getMinutes() {
-    // ... your code goes here
+    return Math.floor(this.currentTime / 60);
   }
   getSeconds() {
-    // ... your code goes here
+    return this.currentTime % 60;
   }
   twoDigitsNumber() {
-    // ... your code goes here
+    return this.currentTime.toString( ).padStart(2, '0');
   }
   stopClick() {
-    // ... your code goes here
+    clearInterval(this.intervalId)
   }
   resetClick() {
-    // ... your code goes here
+    this.currentTime=0
   }
   splitClick() {
-    // ... your code goes here
+    return this.twoDigitsNumber(this.getMinutes()) + ":" + this.twoDigitsNumber(this.getSeconds());
   }
 }
+
+
+
+ 
