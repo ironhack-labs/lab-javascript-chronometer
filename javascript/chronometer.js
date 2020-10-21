d9cb8bd1966bcfb549aec3aafa6bc016ffb10d99
ElfGod <albertogx@icloud.com>
2020-10-20 19:07:25 -0500

    lab chronometer

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,26 +1,60 @@
 class Chronometer {
   constructor() {
     // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = 0;
+
+    this.currentMilliseconds = 0;
+    this.intervalMilliseconds = 0;
   }
   startClick(callback) {
     // ... your code goes here
+    this.intervalId = setInterval(() => {
+      this.currentTime++;
+    }, 1000);
+
+    this.intervalMillisecondsId = setInterval(() => {
+      this.currentMilliseconds++;
+      callback ? callback() : null;
+    }, 10);
   }
   getMinutes() {
     // ... your code goes here
+    return Math.floor(this.currentTime / 60);
   }
   getSeconds() {
     // ... your code goes here
+    return this.currentTime % 60;
   }
-  twoDigitsNumber() {
+  getMilliseconds() {
+    return this.currentMilliseconds % 100;
+  }
+  twoDigitsNumber(number) {
     // ... your code goes here
+    const string = number.toString();
+    if (string.length === 1) {
+      return `0${string}`;
+    } else {
+      return string;
+    }
   }
   stopClick() {
+    clearInterval(this.intervalId);
+    clearInterval(this.intervalMillisecondsId);
     // ... your code goes here
   }
   resetClick() {
     // ... your code goes here
+    this.currentTime = 0;
+    this.currentmilliseconds = 0;
   }
+
   splitClick() {
-    // ... your code goes here
+    const min = this.getMinutes();
+    const sec = this.getSeconds();
+    const mili = this.getMilliseconds();
+    return `${this.twoDigitsNumber(min)}:${this.twoDigitsNumber(
+      sec
+    )}:${this.twoDigitsNumber(mili)}`;
   }
 }
