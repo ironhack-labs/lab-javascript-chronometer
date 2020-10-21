ae9dfc9e5cd25271a7e3056b0962a1e5d58b7314
mahavinay <mahavinay28@gmail.com>
2020-10-21 20:35:19 +0200

    Completed till Iteration4 except clearing the child elements and after stop reset is not done

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,26 +1,58 @@
 class Chronometer {
   constructor() {
     // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = 0
   }
   startClick(callback) {
+    this.intervalId = setInterval(
+      () => {
+        this.currentTime++;
+            if (callback) {
+                callback()
+            }
+    }, 1000)
+    //console.log("this.startClick")
+}
+
+    
     // ... your code goes here
-  }
+    
+  
+  //setInterval = (startClick() => {),1000}
   getMinutes() {
     // ... your code goes here
+    const minutes = Math.floor(this.currentTime / 60);
+    return minutes
+
   }
   getSeconds() {
-    // ... your code goes here
+    return Math.floor(this.currentTime % 60);
+ 
   }
-  twoDigitsNumber() {
+  twoDigitsNumber(number) {
     // ... your code goes here
+    
+  
+    return number > 9 ? String(number) : `0`+ number;
+    
+    
   }
   stopClick() {
     // ... your code goes here
+    clearInterval(this.intervalId)
   }
   resetClick() {
     // ... your code goes here
+    this.currentTime=0;
   }
   splitClick() {
     // ... your code goes here
-  }
+    let min= this.twoDigitsNumber(this.getMinutes());
+    let sec=this.twoDigitsNumber(this.getSeconds());
+    return `${min}:${sec}`;
+   
+    } 
 }
+
+//Iteration 2: DOM Manipulation
--- javascript/index.js
+++ javascript/index.js
@@ -15,14 +15,34 @@ let splits = document.getElementById('splits');
 
 function printTime() {
   // ... your code goes here
+
+  // WHat you want in the end is to call both printMinutes
+  // and printSeconds in here
+
+  // printMinutes();
+  let fetchedSec= printSeconds();
+  secDec.innerHTML=fetchedSec[0];
+  secUni.innerHTML=fetchedSec[1];
+  let fetchedMin=printMinutes();
+  minDec.innerHTML=fetchedMin[0];
+  minUni.innerHTML=fetchedMin[1];
+  
+  
 }
 
 function printMinutes() {
   // ... your code goes here
+ 
+  let fetchMin= chronometer.twoDigitsNumber(chronometer.getMinutes());
+  return fetchMin;
+  
 }
 
 function printSeconds() {
   // ... your code goes here
+  //chronometer.getSeconds();
+  let fetchSec= chronometer.twoDigitsNumber(chronometer.getSeconds());
+  return fetchSec;
 }
 
 // ==> BONUS
@@ -55,11 +75,62 @@ function setResetBtn() {
 }
 
 // Start/Stop Button
-btnLeft.addEventListener('click', () => {
-  // ... your code goes here
-});
+
+ document.getElementById('btnLeft').addEventListener("click", () => {
+  const btnLeftClassName = document.getElementById('btnLeft').className;
+  if(btnLeftClassName==="btn start"){
+   chronometer.startClick(printTime);
+  btnLeft.innerHTML="STOP";
+  btnLeft.className="btn stop"
+  btnRight.className="btn split"
+  btnRight.innerHTML="SPLIT"
+  //;
+   }
+
+ if(btnLeftClassName==="btn stop"){
+  btnLeft.innerHTML="STOP";
+  btnLeft.className="btn stop"
+  btnRight.className="btn reset"
+  chronometer.stopClick()
+  btnRight.innerHTML="RESET"
+ }
+  });
+
+
+
+
 
 // Reset/Split Button
 btnRight.addEventListener('click', () => {
   // ... your code goes here
+  const btnRighttClassName = document.getElementById('btnRight').className;
+  if(btnRighttClassName==="btn reset"){
+  btnRight.innerHTML="SPLIT";
+  btnLeft.className="btn stop";
+  btnRight.className="btn split";
+  btnLeft.innerHTML="STOP"
+  
+ // var selectULElement=document.querySelector("#splits")
+  
+//document.getElementById("myList").appendChild(node);
+ // var textnode = selectULElement.createTextNode(chronometer.splitClick());
+ 
+  //console.log(chronometer.resetClick());
+  chronometer.resetClick();
+ 
+  }
+  if(btnRighttClassName==="btn split"){
+    btnRight.innerHTML="RESET";
+    btnLeft.className="btn start"
+    btnRight.className="btn reset"
+    var childElement = document.createElement("LI");
+
+    childElement.innerHTML = chronometer.splitClick()
+    
+
+    splits.appendChild(childElement);
+
+  
+   }
+
 });
