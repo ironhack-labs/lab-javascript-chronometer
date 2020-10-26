ec74f62f973bf8531d18b8eac001200dc6649970
gtretow <guilherme.tretow@gmail.com>
2020-10-26 18:01:49 -0300

    StartClick, Minutes,Seconds and TwoDigits finished

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -2,17 +2,30 @@ class Chronometer {
   constructor() {
     // ... your code goes here
   }
+  currentTime = 0;
+  intervalId = 0;
+
   startClick(callback) {
     // ... your code goes here
+    //setIntervall agora adiciona +1 a cada segundo (1000milisec) no currentTime do cronometro
+    setInterval(() => {
+      this.currentTime += 1;
+    }, 1000);
   }
   getMinutes() {
     // ... your code goes here
+    let minutes = Math.floor((this.currentTime % 3600) / 60);
+    return minutes;
   }
   getSeconds() {
     // ... your code goes here
+    let seconds = Math.floor((this.currentTime % 3600) % 60);
+    return seconds;
   }
-  twoDigitsNumber() {
+  twoDigitsNumber(numbers) {
     // ... your code goes here
+    if (numbers < 9) return "0" + numbers;
+    else return numbers;
   }
   stopClick() {
     // ... your code goes here
