c6ca8c335a439f3d0ab82e15387b177c5f449691
Sebastian Montoya <seb246@gmail.com>
2020-10-25 20:43:11 -0400

    finished iteration 1

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,26 +1,47 @@
 class Chronometer {
   constructor() {
+    this.currentTime = 0;
+    this.intervalId = 0;
+    
     // ... your code goes here
   }
   startClick(callback) {
+    setInterval(() => {
+      this.currentTime += 1;
+
+    }, 1000);
     // ... your code goes here
   }
+  
   getMinutes() {
+    return Math.floor(this.currentTime / 60);
+  
     // ... your code goes here
   }
   getSeconds() {
+    return this.currentTime % 60;
     // ... your code goes here
   }
   twoDigitsNumber() {
+    if(this.currentTime.length === 1){
+      return `0${this.currentTime.toString()}`;
+    }else {
+      return this.currentTime.toString();
+    }
     // ... your code goes here
   }
   stopClick() {
+    clearInterval(this.intervalId);
     // ... your code goes here
   }
   resetClick() {
+    this.currentTime = 0;
     // ... your code goes here
   }
   splitClick() {
+    let minutes = getMinutes();
+    let seconds = getSeconds();
+    return `${minutes}:${seconds}`;
     // ... your code goes here
   }
 }
