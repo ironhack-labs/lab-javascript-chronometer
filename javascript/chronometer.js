084538c2df2f19cd07d7bef436c3ed7857e30929
gtretow <guilherme.tretow@gmail.com>
2020-10-27 19:31:47 -0300

    split done

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,42 +1,33 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = 0;
   }
-  currentTime = 0;
-  intervalId = 0;
-
   startClick(callback) {
-    // ... your code goes here
-    //setIntervall agora adiciona +1 a cada segundo (1000milisec) no currentTime do cronometro
-    setInterval(() => {
-      this.currentTime += 1;
+    this.intervalId = setInterval(() => {
+      this.currentTime++;
+      if (callback) {
+        callback();
+      }
     }, 1000);
   }
   getMinutes() {
-    // ... your code goes here
     return Math.floor(this.currentTime / 60);
   }
   getSeconds() {
-    // ... your code goes here
-    let seconds = Math.floor((this.currentTime % 3600) % 60);
-    return seconds;
+    return this.currentTime % 60;
   }
-  twoDigitsNumber(numbers) {
-    // ... your code goes here
-    if (numbers < 9) return "0" + numbers;
-    else return numbers;
+  twoDigitsNumber(value) {
+    if (value < 10) return `0${value}`;
+    return `${value}`;
   }
   stopClick() {
-    // ... your code goes here
     clearInterval(this.intervalId);
   }
-
   resetClick() {
-    // ... your code goes here
     this.currentTime = 0;
   }
   splitClick() {
-    // ... your code goes here
     return `${this.twoDigitsNumber(this.getMinutes())}:${this.twoDigitsNumber(
       this.getSeconds()
     )}`;
