0dc128ea71479cf303286c6b6a4696b2fcdcda24
DenisM20 <denis.gmta@gmail.com>
2020-10-27 10:34:01 +0000

    done

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,26 +1,30 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0
+    this.intervalId = 0
   }
   startClick(callback) {
-    // ... your code goes here
+    this.intervalId = setInterval(() => this.currentTime++, 1000);
   }
   getMinutes() {
-    // ... your code goes here
+    let minutes = Math.floor(this.currentTime / 60);
+    return minutes;
   }
   getSeconds() {
-    // ... your code goes here
+    let seconds = this.currentTime % 60;
+    return seconds;
   }
   twoDigitsNumber() {
-    // ... your code goes here
+    return number < 10 ? '0' + number : number;
   }
   stopClick() {
-    // ... your code goes here
+    clearInterval(this.intervalId);
   }
   resetClick() {
-    // ... your code goes here
+    this.currentTime === 0;
   }
   splitClick() {
-    // ... your code goes here
+    return `${this.twoDigitsNumber(this.getMinutes())}:${this.twoDigitsNumber(this.getSeconds())}`;
+  
   }
 }
