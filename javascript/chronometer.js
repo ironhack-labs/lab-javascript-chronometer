4a748e2fe5a13aecd133cd9fa5df0defae36327f
TalB <tal.burdman@gmail.com>
2020-10-27 17:31:03 +0100

    done

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,17 +1,28 @@
 class Chronometer {
   constructor() {
+    this.currentTime = 0;
+    this.intervalId = 0;
     // ... your code goes here
   }
   startClick(callback) {
+    const intervalId = setInterval(() => {
+      this.currentTime++
+    }, 1000)
     // ... your code goes here
   }
   getMinutes() {
+
+    return Math.floor(this.currentTime / 60);
     // ... your code goes here
   }
   getSeconds() {
+
+    return Math.floor(this.currentTime % 60);
     // ... your code goes here
   }
-  twoDigitsNumber() {
+  twoDigitsNumber(number) {
+
+    return number.toString();
     // ... your code goes here
   }
   stopClick() {
