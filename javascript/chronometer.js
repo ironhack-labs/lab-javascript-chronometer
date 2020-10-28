f8f8ce16e2ccc306ca05015e09c2cf340b4f13e7
Laura Ferraz <lauraferraz5@gmail.com>
2020-10-28 11:09:53 -0300

    final version

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -2,33 +2,42 @@ class Chronometer {
   constructor() {
     this.currentTime = 0;
     this.intervalId = 0;
+    this.milliseconds = 0;
+    this.intervalMilliSec = 0;
   }
   startClick(callback) {
-    const intervalId = setInterval(() => {
-      this.currentTime += 1;
-    }, 1000);
+    this.intervalId = setInterval(() => {
+      this.currentTime++;
+      if (callback) {
+        callback();
+      }
+    }, 10);
   }
   getMinutes() {
-    return Math.floor(this.currentTime / 60);
+    return Math.floor(this.currentTime / 6000);
   }
   getSeconds() {
-    return Math.floor(this.currentTime % 60);
+    return Math.floor(this.currentTime / 100) % 60;
   }
-  twoDigitsNumber(number) {
-    if (number.toString().length === 1) {
-      return `0${number}`;
-    }
-    return number.toString();
+  getMilliseconds() {
+    return this.currentTime % 100;
+  }
+
+  twoDigitsNumber(value) {
+    if (value < 10) return `0${value}`;
+    return `${value}`;
   }
   stopClick() {
     clearInterval(this.intervalId);
+    clearInterval(this.intervalMilliSec);
   }
   resetClick() {
     this.currentTime = 0;
+    this.milliseconds = 0;
   }
   splitClick() {
     return `${this.twoDigitsNumber(this.getMinutes())}:${this.twoDigitsNumber(
       this.getSeconds()
-    )}`;
+    )}:${this.twoDigitsNumber(this.getMilliseconds())}`;
   }
 }
