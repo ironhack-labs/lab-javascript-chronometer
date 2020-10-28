347124344e436cd9570a49e613986d2cfd14bd78
Fellype <fellype.ads@gmail.com>
2020-10-28 12:13:56 -0300

    atualizado startClick para 1 contador

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,9 +1,7 @@
 class Chronometer {
   constructor() {
     this.currentTime = 0;
-    this.miliSeconds = 0;
     this.intervalId = 0;
-    this.intervalMiliSec = 0;
   }
   startClick(callback) {
     this.intervalId = setInterval(() => {
@@ -11,26 +9,20 @@ class Chronometer {
       if (callback) {
         callback();
       }
-    }, 1000);
-    this.intervalMiliSec = setInterval(() => {
-      this.miliSeconds++;
-      if (callback) {
-        callback();
-      }
     }, 10);
   }
   getMinutes() {
-    this.minutes = Math.floor(this.currentTime / 60);
+    this.minutes = Math.floor(this.currentTime / 6000);
     return this.minutes;
   }
   getSeconds() {
     if (this.currentTime === 0) {
       return 0;
     }
-    return Number(this.currentTime - this.getMinutes() * 60);
+    return Math.floor(this.currentTime / 100) % 60;
   }
   getMilliseconds() {
-    return this.miliSeconds - this.currentTime * 100;
+    return this.currentTime % 100;
   }
   twoDigitsNumber(currentTimeDigit) {
     return currentTimeDigit < 10
@@ -39,11 +31,9 @@ class Chronometer {
   }
   stopClick() {
     clearInterval(this.intervalId);
-    clearInterval(this.intervalMiliSec);
   }
   resetClick() {
     this.currentTime = 0;
-    this.miliSeconds = 0;
   }
   splitClick() {
     return `${this.twoDigitsNumber(this.getMinutes())}:${this.twoDigitsNumber(
