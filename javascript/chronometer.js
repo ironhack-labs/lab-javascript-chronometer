45103e802cb04a5c187af38f1d37c073eac56e19
Helene Stephan <helene.stephan@protonmail.com>
2020-10-28 13:03:54 +0100

    firstand second iterations

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,12 +1,15 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = 0;
   }
   startClick(callback) {
-    // ... your code goes here
+    this.intervalId = setInterval(() => {
+      this.currentTime++;
+    }, 1000);
   }
   getMinutes() {
-    // ... your code goes here
+    return parseInt(this.currentTime / 60);
   }
   getSeconds() {
     // ... your code goes here
