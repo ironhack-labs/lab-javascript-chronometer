2196fd5ee0d51f7c2e51b356fd194b633f9243ac
Ana Ortez <anaortez23@gmail.com>
2020-11-08 16:16:17 -0500

    Write code for OOP part of the Chronometer class

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,26 +1,29 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
-  }
+    this.currentTime = 0;
+    this.intervalId = 0;
+
+    }
   startClick(callback) {
-    // ... your code goes here
+    setInterval(() => this.currentTime++ , 1000);    
   }
   getMinutes() {
-    // ... your code goes here
+  return Math.floor(this.currentTime / 60);
   }
   getSeconds() {
-    // ... your code goes here
+  return (this.currentTime % 60);
   }
   twoDigitsNumber() {
-    // ... your code goes here
+    if (this.currentTime < 10) return `0${this.currentTime}`;
+    else return this.currentTime;
   }
   stopClick() {
-    // ... your code goes here
+  clearInterval(this.intervalId);
   }
   resetClick() {
-    // ... your code goes here
+    this.currentTime = 0;
   }
   splitClick() {
-    // ... your code goes here
+    return (`${this.twoDigitsNumber(this.getMinutes())}:${this.twoDigitsNumber(this.getSeconds())}`);
   }
 }
