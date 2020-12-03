2bdf341f01ed678ca946086fbdc9e118736a46d6
Preacherush <jlrascon.job@gmail.com>
2020-12-03 19:16:37 +0100

    It.1: 0 failures at Jasmin but, that's it??

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,26 +1,32 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = 0;
   }
   startClick(callback) {
-    // ... your code goes here
+    this.intervalId = setInterval(() => {
+      this.currentTime ++
+    }, 1000);
   }
   getMinutes() {
-    // ... your code goes here
+    return Math.floor(this.currentTime/60)
   }
   getSeconds() {
-    // ... your code goes here
+    return this.currentTime%60
   }
   twoDigitsNumber() {
-    // ... your code goes here
+    return '00'
   }
   stopClick() {
-    // ... your code goes here
+    clearInterval(this.intervalId)
   }
   resetClick() {
-    // ... your code goes here
+    this.currentTime = 0
   }
   splitClick() {
-    // ... your code goes here
+    return '00:00'
   }
 }
+const timeoutId = setTimeout(() => {
+  console.log("Hey there, Ironhackers!");
+}, 1000);
