84812bb3a5e6af855c4c71e321a01cd0151681d3
Ines de Palma <ipalmamasaveu@gmail.com>
2020-12-03 11:02:39 +0100

    Logic done

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,26 +1,46 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0
+    this.intervalId = 0
   }
+
   startClick(callback) {
-    // ... your code goes here
+    this.intervalId = setInterval(() => {
+      this.currentTime++
+
+    }, 1000)
   }
+
   getMinutes() {
-    // ... your code goes here
+    return Math.floor(this.currentTime / 60)
+
   }
+
   getSeconds() {
-    // ... your code goes here
+    return Math.floor(this.currentTime % 60)
   }
-  twoDigitsNumber() {
-    // ... your code goes here
+  
+  twoDigitsNumber(time) {
+    if (String(time).length === 1) {
+      return `0${time}`
+    }
+    return String(time)
   }
+
   stopClick() {
-    // ... your code goes here
+    clearInterval(this.intervalId)
   }
+
   resetClick() {
-    // ... your code goes here
+    this.currentTime = 0
+    this.intervalId = 0
   }
+  
   splitClick() {
-    // ... your code goes here
+    let min = this.twoDigitsNumber(this.getMinutes())
+    let sec = this.twoDigitsNumber(this.getSeconds())
+
+    return `${min}:${sec}`;
+        
   }
 }
