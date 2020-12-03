2ceac512b90d724dd28befd3a6e1ddc2673adfd7
Alicia Parra <alicia@redparra.com>
2020-12-03 19:14:46 +0100

    Do iteration 1. The logic. All Jasmine tests working fine

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,26 +1,36 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
-  }
+    this.currentTime=0;
+    this.intervalId=0;
+    }
   startClick(callback) {
-    // ... your code goes here
+    this.intervalId= setInterval(()=> {this.currentTime++},1000);
   }
   getMinutes() {
-    // ... your code goes here
+    let minutes=0;
+    minutes= this.currentTime/60;
+    return Math.trunc(minutes);
   }
   getSeconds() {
-    // ... your code goes here
+    let seconds=0;
+    seconds= this.currentTime - this.getMinutes() * 60;
+    return seconds;
   }
-  twoDigitsNumber() {
-    // ... your code goes here
+  twoDigitsNumber(num) {
+    let convertedNum=num.toString();
+      if(convertedNum.length<2){
+        return '0' + num;
+      }
+    return num;
+    
   }
   stopClick() {
-    // ... your code goes here
+    clearInterval(this.intervalId);
   }
   resetClick() {
-    // ... your code goes here
+    this.currentTime=0;
   }
   splitClick() {
-    // ... your code goes here
+    return `${this.twoDigitsNumber(this.getMinutes())}:${this.twoDigitsNumber(this.getSeconds())}`
   }
 }
