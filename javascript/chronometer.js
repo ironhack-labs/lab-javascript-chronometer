272cfbec8c36cf5de5b039c85f24995b3a808c9d
Sotiria Dialeti <soti.dialeti@gmail.com>
2021-01-18 18:21:19 +0100

    Final

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,26 +1,37 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = 0;
   }
   startClick(callback) {
-    // ... your code goes here
+    this.intervalId = setInterval(() => {
+      this.currentTime += 1;
+    }, 1000);
   }
+
   getMinutes() {
-    // ... your code goes here
+    return Number(Math.trunc(this.currentTime / 60));
   }
   getSeconds() {
-    // ... your code goes here
+    return Number(Math.round(this.currentTime % 60));
   }
-  twoDigitsNumber() {
-    // ... your code goes here
+  twoDigitsNumber(num) {
+    if (num < 10) {
+      return `0${num}`;
+    } else {
+      return `${num}`;
+    }
   }
+
   stopClick() {
-    // ... your code goes here
+    clearInterval(this.intervalId);
   }
   resetClick() {
-    // ... your code goes here
+    this.currentTime = 0;
   }
   splitClick() {
-    // ... your code goes here
+    return `${this.twoDigitsNumber(this.getMinutes())}:${this.twoDigitsNumber(
+      this.getSeconds()
+    )}`;
   }
 }
