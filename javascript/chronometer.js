212df5eec1c20388a081d202f8aa5080023b5837
Driele An <drielean@gmail.com>
2021-01-19 12:50:10 -0300

    master

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -2,37 +2,31 @@ class Chronometer {
   constructor() {
     this.currentTime = 0;
     this.intervalId = 0;
-    // ... your code goes here
   }
   startClick(callback) {
     this.setIntervalId = setInterval(()=> {
       this.currentTime++;
+      if (callback) callback();
     }, 1000);
-    // ... your code goes here
   }
   getMinutes() {
     return Math.floor(this.currentTime / 60);
-    // ... your code goes here
   }
   getSeconds() {
     return this.currentTime % 60;
-    // ... your code goes here
   }
-  twoDigitsNumber() {
-    const minutes = String(this.getMinutes()).padStart(2, '0');
-    const seconds = String(this.getSeconds()).padStart(2, '0');
-    return `${minutes}:${seconds}`;
-    // ... your code goes here
+  twoDigitsNumber(value) {
+   if(value < 10) return `0${value}`;
+   return `${value}`;
   }
   stopClick() {
     return clearInterval(this.intervalId);
-    // ... your code goes here
   }
   resetClick() {
     this.currentTime = 0;
-    // ... your code goes here
   }
   splitClick() {
-    // ... your code goes here
+    return `${this.twoDigitsNumber(this.getMinutes())}:${this.twoDigitsNumber(this.getSeconds()
+      )}`;
   }
 }
