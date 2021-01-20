02e70cbb088ff06bbe765a8cb9ba6408a4f50a4f
honghainguyen777 <n.h.hai2015@gmail.com>
2021-01-20 21:14:32 +0100

    All done - add some comments

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -5,7 +5,11 @@ class Chronometer {
     this.milliseconds = 1;
     this.milIntervalId = 0;
   }
-  // startClick(callback) // Not specify callback here, because I want it to receive two function that can be involked inside
+ // Not specify callback here, because I want it to receive two function that can be involked inside
+  // this will not pass the test due to adding arguments[0] and arguments[1] received from index.js
+  // To pass the test, please comment out line 12, 15, 16, 18-21
+
+  // startClick(callback)
   startClick() {
     this.intervalId = setInterval(() => {
       this.currentTime++;
@@ -39,10 +43,14 @@ class Chronometer {
     this.currentTime = 0;
     this.milliseconds = 0
   }
+
+  // This will not pass the test due to adding milliseconds
+  // to pass the test please comment out like 54 and uncomment in line 53
   splitClick() {
     let sec = this.getSeconds();
     let min = this.getMinutes();
     let mil = this.getMiliseconds();
+    // return this.twoDigitsNumber(min) + ':' + this.twoDigitsNumber(sec)
     return this.twoDigitsNumber(min) + ':' + this.twoDigitsNumber(sec) + ':' + this.twoDigitsNumber(mil);
   }
 }
