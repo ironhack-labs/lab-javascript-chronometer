a83ab726a7b95daa62c824d9d4f895bee77630ed
Mafe Bracho <mafe.bracho@gmail.com>
2021-01-31 19:49:52 +0100

    done without bonus

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,26 +1,50 @@
 class Chronometer {
   constructor() {
     // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = 0;
   }
   startClick(callback) {
     // ... your code goes here
+    this.intervalId = setInterval(() => {
+      this.currentTime++;
+      callback();
+    }, 10);
+    console.log(this.currentTime);
   }
+  // getHours() {
+  //     return Math.floor(this.currentTime / 360000);
+  // }
   getMinutes() {
     // ... your code goes here
+    return Math.floor((this.currentTime / 6000) % 60);
   }
   getSeconds() {
     // ... your code goes here
+    return Math.floor((this.currentTime / 100) % 60);
   }
-  twoDigitsNumber() {
+  getMilliseconds() {
+    return this.currentTime % 100;
+  }
+  twoDigitsNumber(num) {
     // ... your code goes here
+    return String(num).padStart(2, "0");
   }
   stopClick() {
     // ... your code goes here
+    clearInterval(this.intervalId);
   }
   resetClick() {
     // ... your code goes here
+    this.currentTime = 0;
   }
   splitClick() {
     // ... your code goes here
+    // let hour = this.twoDigitsNumber(this.getHours());
+    let min = this.twoDigitsNumber(this.getMinutes());
+    let sec = this.twoDigitsNumber(this.getSeconds());
+    let milliSec = this.twoDigitsNumber(this.getMilliseconds());
+    let string = `${min}:${sec}:${milliSec}`;
+    return string;
   }
 }
--- javascript/index.js
+++ javascript/index.js
@@ -1,28 +1,50 @@
 const chronometer = new Chronometer();
 
-// get the buttons:
-const btnLeft = document.getElementById('btnLeft');
-const btnRight = document.getElementById('btnRight');
+ // enumerators
 
+ const BTN_TEXT_START = "START";
+ const BTN_TEXT_RESET = "RESET";
+
+ // get the buttons:
+ const btnLeft = document.getElementById("btnLeft");
+ const btnRight = document.getElementById("btnRight");
 // get the DOM elements that will serve us to display the time:
-let minDec = document.getElementById('minDec');
-let minUni = document.getElementById('minUni');
-let secDec = document.getElementById('secDec');
-let secUni = document.getElementById('secUni');
-let milDec = document.getElementById('milDec');
-let milUni = document.getElementById('milUni');
-let splits = document.getElementById('splits');
+// let hourDec = document.getElementById("hourDec");
+// let hourUni = document.getElementById("hourUni");
+let minDec = document.getElementById("minDec");
+let minUni = document.getElementById("minUni");
+let secDec = document.getElementById("secDec");
+let secUni = document.getElementById("secUni");
+let milDec = document.getElementById("milDec");
+let milUni = document.getElementById("milUni");
+ let splits = document.getElementById("splits");
 
-function printTime() {
+ function printTime() {
   // ... your code goes here
-}
+  printMinutes();
+  printSeconds();
+  printMilliseconds();
+ }
+
+ function printHours() {
+  let hours = chronometer.twoDigitsNumber(chronometer.getHours());
+  hourDec.innerText = hours[0];
+  hourUni.innerText = hours[1];
+ }
 
 function printMinutes() {
   // ... your code goes here
+  let minutes = chronometer.twoDigitsNumber(chronometer.getMinutes());
+  minDec.innerText = minutes[0];
+  minUni.innerText = minutes[1];
+
 }
 
 function printSeconds() {
   // ... your code goes here
+  let seconds = chronometer.twoDigitsNumber(chronometer.getSeconds());
+  secDec.innerText = seconds[0];
+  secUni.innerText = seconds[1];
 }
 
 // ==> BONUS
