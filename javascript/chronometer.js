20615f9a6b98e0b396d6f7df51a21371fe18ae0b
mmanso <mayara.manso@mercadolivre.com>
2021-02-02 22:45:58 -0300

    done class Chronometer

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,26 +1,62 @@
 class Chronometer {
   constructor() {
     // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = 0;
   }
   startClick(callback) {
     // ... your code goes here
+    this.intervalId = setInterval(() => {
+      this.currentTime += 1;
+    }, 1000)
   }
   getMinutes() {
     // ... your code goes here
+    let minutes = Math.floor(this.currentTime / 60)
+    if (minutes >= 1) {
+      return minutes
+    } else {
+      return minutes = 0;
+    }
+
   }
   getSeconds() {
     // ... your code goes here
+    if (this.currentTime === 0) return this.currentTime;
+    return this.currentTime % 60;
   }
-  twoDigitsNumber() {
+  twoDigitsNumber(number) {
     // ... your code goes here
+    let newNumber = number.toString();
+
+    if (newNumber.length === 1) {
+      return `0${newNumber}`
+    } else {
+      return newNumber
+    }
   }
   stopClick() {
     // ... your code goes here
+    clearInterval(this.intervalId);
   }
   resetClick() {
     // ... your code goes here
+    this.currentTime = 0;
   }
   splitClick() {
     // ... your code goes here
+    let min = this.getMinutes();
+    let sec = this.getSeconds();
+
+    switch (true) {
+      case (min < 10 && sec < 10):
+        return `${0}${min}:${0}${sec}`;
+      case (min < 10 && sec > 10):
+        return `0${min}:${sec}`;
+      case (min > 10 && sec < 10):
+        return `${min}:0${sec}`;
+      default:
+        return `${min}:${sec}`
+    }
   }
 }
