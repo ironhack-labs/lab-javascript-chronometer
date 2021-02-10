9dfdd23158707f401edf1277791c938df03ce107
Mohammed Adib <adibmohammed95@gmail.com>
2021-02-10 16:44:36 +0100

    Chronometer Lab | Mohammed

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,15 +1,38 @@
 class Chronometer {
+  
   constructor() {
     // ... your code goes here
+      this.currentTime = 0;
+      this.intervalId = 0;
   }
   startClick(callback) {
     // ... your code goes here
+    setInterval(() => {
+      this.currentTime += 1;
+    }, 1000
+
+    ) 
+
   }
   getMinutes() {
-    // ... your code goes here
+    
+    let minutesConverter = this.currentTime / 60 ;
+    
+
+    // if(this.currentTime === 60){
+    //   retu
+    // }
+    return Number(minutesConverter.toFixed(0)) ;
   }
   getSeconds() {
     // ... your code goes here
+
+    if(this.currentTime === 0){
+      return 0;
+    }
+     let secondsConverter = this.currentTime ;
+
+     return Number(secondsConverter);
   }
   twoDigitsNumber() {
     // ... your code goes here
