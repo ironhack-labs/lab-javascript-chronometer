e0e1454ee37658cb466a1f8919405e0698b9c3d1
Mathieu Jacob <mathieu.jacob@hotmail.fr>
2021-02-10 18:39:11 +0100

    chronometer part done, DOM en cours

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,26 +1,51 @@
 class Chronometer {
   constructor() {
+    this.currentTime = 0;
+    this.intervalId = 0;
     // ... your code goes here
   }
+
+  
   startClick(callback) {
+   // let counter=0
+   setInterval(() => {
+      this.currentTime++;
+      console.log (this.currentTime);
+    }, 1000);
+
     // ... your code goes here
   }
+
   getMinutes() {
-    // ... your code goes here
+   return Math.floor(this.currentTime/60);
   }
+
+    // ... your code goes here
+  //}
   getSeconds() {
+
+ return Math.floor(this.currentTime%60);
     // ... your code goes here
   }
-  twoDigitsNumber() {
+  twoDigitsNumber(a) {
     // ... your code goes here
+    return `0${a}`.slice(-2);
+
   }
   stopClick() {
     // ... your code goes here
+    return clearInterval(this.intervalId)
   }
   resetClick() {
     // ... your code goes here
+    return this.currentTime=0;
   }
   splitClick() {
+    var min = this.getMinutes();
+    var sec = this.getSeconds();
+    return (this.twoDigitsNumber(min) + ":" + this.twoDigitsNumber(sec));
     // ... your code goes here
+    
   }
+ 
 }
