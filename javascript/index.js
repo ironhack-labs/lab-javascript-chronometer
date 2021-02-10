852fe0f5613aed1252b9aca275e82c5a498e59f8
Rowan Williams <rmtwilliams@hotmail.co.uk>
2021-02-10 18:19:35 +0100

    Rowans

--- javascript/index.js
+++ javascript/index.js
@@ -1,4 +1,5 @@
 const chronometer = new Chronometer();
+const newChronometer = new Chronometer();
 
 // get the buttons:
 const btnLeft = document.getElementById('btnLeft');
@@ -19,10 +20,16 @@ function printTime() {
 
 function printMinutes() {
   // ... your code goes here
+  let mins = newChronometer.twoDigitsNumber(getMinutes()).split(''); 
+  minDec.innerText = mins[0];
+  minUni.innerText = mins[1];
 }
 
 function printSeconds() {
   // ... your code goes here
+  let secs = newChronometer.twoDigitsNumber(getSecconds()).split(''); 
+  secDec.innerText = secs[0];
+  secUni.innerText = secs[1];
 }
 
 // ==> BONUS
@@ -40,6 +47,7 @@ function clearSplits() {
 
 function setStopBtn() {
   // ... your code goes here
+  if (btnLeft.classList.contains("stop")) newChronometer.stopClick();
 }
 
 function setSplitBtn() {
@@ -48,6 +56,7 @@ function setSplitBtn() {
 
 function setStartBtn() {
   // ... your code goes here
+ if (btnLeft.classList.contains("start")) newChronometer.startClick();
 }
 
 function setResetBtn() {
@@ -57,9 +66,29 @@ function setResetBtn() {
 // Start/Stop Button
 btnLeft.addEventListener('click', () => {
   // ... your code goes here
+  btnLeft.classList.toggle("start");
+  btnLeft.classList.toggle("stop");
+  btnRight.classList.toggle("split");
+  btnRight.classList.toggle("reset");
+  
+
+  if (btnLeft.classList.contains("stop")) {
+    btnLeft.innerText = "STOP";
+    btnRight.innerText = "SPLIT";
+
+    } else {
+    btnLeft.innerText = "START";
+    btnRight.innerText = "RESET";
+  }
 });
 
 // Reset/Split Button
 btnRight.addEventListener('click', () => {
   // ... your code goes here
+
 });
+
+
+
+
+
