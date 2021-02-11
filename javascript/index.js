6f3b7108efb552cc286da6d15ebdf087eba5aa3d
Amalia Zebi <amalia.zebi@b4finance.com>
2021-02-11 11:15:08 +0100

    Last changes

--- javascript/index.js
+++ javascript/index.js
@@ -84,24 +84,42 @@ function setResetBtn() {
 //   btnRight.classList.toggle("split");
 //   return setStartBtn();
 // });
+
+
 //OR
+
+
 btnLeft.addEventListener("click", () => {
   if (btnLeft.classList.contains("start")) {
     btnLeft.innerHTML = "STOP";
     btnLeft.classList.add("stop");
     btnLeft.classList.remove("start");
     chronometer.startClick(printTime);
+    btnRight.innerHTML = "SPLIT";
+    btnRight.classList.add("split");
+    btnRight.classList.remove("reset");
   } else {
     btnLeft.innerHTML = "START";
     btnLeft.classList.add("start");
     btnLeft.classList.remove("stop");
     chronometer.stopClick(printTime);
+    btnRight.innerHTML = "RESET";
+    btnRight.classList.add("reset");
+    btnRight.classList.remove("split");
   }
 });
 
 // Reset/Split Button
 btnRight.addEventListener("click", () => {
-  // btnRight.classList.toggle("reset");
-  // btnRight.classList.toggle("split");
-  // return setResetBtn();
+  if (btnRight.classList.contains("reset")) {
+    chronometer.resetClick(printTime);
+    minDec.innerHTML = "0";
+    minUni.innerHTML = "0";
+    secDec.innerHTML = "0";
+    secUni.innerHTML = "0";
+    splits.innerHTML = "";
+
+  } else {
+    splits.innerHTML += `<li>${chronometer.splitClick()}</li>`
+  }
 });
