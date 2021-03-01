c7131b3f6190d3912d85cd2a1cd9eeecb90ca0af
Lena Araújo <lenaaraujo92@gmail.com>
2021-03-01 18:24:35 -0300

    iteration 1 completed

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,24 +1,33 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = 0;
   }
   startClick(callback) {
-    // ... your code goes here
+    setInterval(() => {
+      this.currentTime++;
+    }, 1000);
   }
   getMinutes() {
-    // ... your code goes here
+    return Math.floor(this.currentTime / 60);
   }
   getSeconds() {
-    // ... your code goes here
+    return Math.floor(this.currentTime % 60);
   }
-  twoDigitsNumber() {
-    // ... your code goes here
+  twoDigitsNumber(number) {
+    let returnSting = "";
+
+    if (number < 10) {
+      returnSting += "0";
+    }
+
+    return returnSting + number;
   }
   stopClick() {
-    // ... your code goes here
+    clearInterval(this.intervalId);
   }
   resetClick() {
-    // ... your code goes here
+    this.currentTime = 0;
   }
   splitClick() {
     // ... your code goes here
