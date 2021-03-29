1a86c79f05ab4eb0e021a03260a74c7f621eeca6
Raquel Rodriguez <srtademaciel@gmail.com>
2021-03-29 18:37:34 +0200

    only  splitClick left

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -12,18 +12,29 @@ class Chronometer {
 
   getMinutes() {
     // ... your code goes here
+   return Math.floor(this.currentTime / 60)
+
   }
   getSeconds() {
     // ... your code goes here
+  return this.currentTime % 60
   }
-  twoDigitsNumber() {
+  twoDigitsNumber(num) {
     // ... your code goes here
-  }
+    return num<10 ? `0${num}`: `${num}`
+    /* if(num<10){
+      return `0${num}`
+    } else {
+      return `${num}`} */
+    }
+
   stopClick() {
     // ... your code goes here
+    return clearInterval(this.intervalId)
   }
   resetClick() {
     // ... your code goes here
+    return this.currentTime = 0
   }
   splitClick() {
     // ... your code goes here
