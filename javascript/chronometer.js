cc3d8478e925b9af2ce30275e1890ebff8400aaa
root <root@PCR-GAMING.localdomain>
2021-03-29 17:36:46 +0200

    Iteration 1 completed

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,26 +1,31 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0
+    this.intervalId = 0
   }
   startClick(callback) {
-    // ... your code goes here
+    setInterval(()=>{
+      this.currentTime++
+    }, 1000)
   }
   getMinutes() {
-    // ... your code goes here
+    return Math.floor(this.currentTime/60)
   }
   getSeconds() {
-    // ... your code goes here
+    return this.currentTime%60
   }
-  twoDigitsNumber() {
-    // ... your code goes here
+  twoDigitsNumber(value) {
+    if (value < 10){
+      return '0'+ value
+    }
   }
   stopClick() {
-    // ... your code goes here
+    clearInterval(this.interalId)
   }
   resetClick() {
-    // ... your code goes here
+    this.currentTime = 0
   }
   splitClick() {
-    // ... your code goes here
+    return `${this.twoDigitsNumber(this.getMinutes())}:${this.twoDigitsNumber(this.getSeconds())}`
   }
 }
