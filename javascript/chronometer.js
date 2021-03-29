4527ca72239a108294b3d3120e16f2cbca15cc4e
Bea Avis <bea.avis.p@gmail.com>
2021-03-29 16:30:38 +0200

    Add twoDigitsNumber() to class Chronometer

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -16,8 +16,12 @@ class Chronometer {
     let seconds = Math.floor(this.currentTime - this.getMinutes()*60);
     return seconds;
   }
-  twoDigitsNumber() {
-    // Our chronometer has a super cool screen that needs two digits number to display minutes and seconds, but sometimes getMinutes() and getSeconds() returns a single-digit number. Let's create a super simple algorithm that will turn into two-digits number any received value. Example: if the value of the currentTime property is 36 seconds, it should return 00 for minutes and 36 for seconds; if the currentTime is 5 min and 43 sec, it should give us back 05 for minutes and 43 for seconds. At the same time, if the currentTime is 17 min and 13 sec, it should give us back 17 for minutes and 13 for seconds.
+  twoDigitsNumber(number) {
+    if (number < 10){
+      return `0${number}`;
+    } else if (number <= 60){
+      return `${number}`;
+    }
   }
   stopClick() {
     // When invoked, the stopClick() method should clear the intervalId. Simple as that. 💡 Hint: Use clearInterval.
