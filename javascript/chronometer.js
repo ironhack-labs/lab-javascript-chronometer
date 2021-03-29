2463ab7cfe38305eb5be3d0fcb860c1e5480e3ec
Lorgio Roda <lorgioroda@Lorgios-MacBook-Air.local>
2021-03-29 11:08:55 -0400

    done

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,26 +1,32 @@
 class Chronometer {
-  constructor() {
-    // ... your code goes here
+  constructor(){
+    this.currentTime = 0;
+    this.intervalId = 0;
   }
+  
   startClick(callback) {
-    // ... your code goes here
+      this.intervalId =  setInterval(() => {
+      this.currentTime++;
+    },1000)
   }
+
   getMinutes() {
-    // ... your code goes here
+    return Math.floor(this.currentTime / 60)
   }
   getSeconds() {
-    // ... your code goes here
+    return Math.floor(this.currentTime % 60)
   }
   twoDigitsNumber() {
-    // ... your code goes here
+    return '0' + this.currentTime.toString().slice(-2)
   }
   stopClick() {
-    // ... your code goes here
+    debugger
+    clearInterval(this.intervalId)
   }
   resetClick() {
-    // ... your code goes here
+    this.currentTime = 0
   }
   splitClick() {
-    // ... your code goes here
+    return `0${this.getMinutes()}:0${this.getSeconds()}`
   }
 }
