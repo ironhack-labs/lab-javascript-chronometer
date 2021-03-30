d779fd62a31282ee6bf194220338a58ad79ddb60
Mario Ortiz <ortiz.mario22@gmail.com>
2021-03-30 16:40:54 +0200

    nos ha faltado el asincrono

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,26 +1,80 @@
 class Chronometer {
   constructor() {
     // ... your code goes here
+    this.currentTime = 0
+    this.intervalId = 0
   }
+
   startClick(callback) {
     // ... your code goes here
+    this.intervalId = setInterval(() => {
+      this.currentTime += 1
+    }, 1000)
+
   }
   getMinutes() {
     // ... your code goes here
+    return Math.floor(this.currentTime / 60)
+
   }
   getSeconds() {
     // ... your code goes here
+    if (this.currentTime === 0) {
+      return 0
+
+    } else {
+      return this.currentTime - this.getMinutes() * 60
+
+    }
   }
-  twoDigitsNumber() {
+
+  twoDigitsNumber(value) {
     // ... your code goes here
+    if (value.toString().length === 1) {
+      let valueTest = '0' + value
+      return valueTest
+    } else {
+      return value.toString()
+    }
   }
+
   stopClick() {
     // ... your code goes here
+
+    window.clearInterval(this.intervalId)
+
+
   }
   resetClick() {
     // ... your code goes here
+    this.currentTime = 0
   }
   splitClick() {
     // ... your code goes here
+    let min = this.getMinutes();
+    let sec = this.getSeconds();
+    return `${this.twoDigitsNumber(min)}:${this.twoDigitsNumber(sec)}`
+
+    // let formatTime
+    // if (min < 10 && sec < 10) {
+    //   formatTime = `${0}${min}:${0}${sec}`
+    //   console.log(formatTime);
+    //   return formatTime
+
+    // } else if (min < 10 && sec > 10) {
+    //   formatTime = `0${min}:${sec}`
+    //   console.log(formatTime);
+    //   return formatTime
+
+    // } else if (min > 10 && sec < 10) {
+    //   formatTime = `${min}:0${sec}`
+    //   console.log(formatTime);
+    //   return formatTime
+
+    // } else {
+    //   formatTime = `${min}:${sec}`
+    //   console.log(formatTime);
+    //   return formatTime
+    // }
   }
 }
--- javascript/index.js
+++ javascript/index.js
@@ -15,14 +15,35 @@ let splits = document.getElementById('splits');
 
 function printTime() {
   // ... your code goes here
+  printSeconds()
+  printMinutes()
+
 }
 
 function printMinutes() {
   // ... your code goes here
+  minutes = chronometer.getMinutes()
+  minText = chronometer.twoDigitsNumber(seconds)
+
+  minText1 = minText[0]
+  minText2 = minText[1]
+
+  document.querySelector("#secDec").textContent = minText1
+  document.querySelector("#secUni").textContent = minText2
 }
 
 function printSeconds() {
   // ... your code goes here
+  seconds = chronometer.currentTime
+  secText = chronometer.twoDigitsNumber(seconds)
+
+  secText1 = secText[0]
+  secText2 = secText[1]
+  console.log(secText2);
+
+  document.querySelector("#secDec").textContent = secText1
+  document.querySelector("#secUni").textContent = secText2
+
 }
 
 // ==> BONUS
@@ -40,26 +61,55 @@ function clearSplits() {
 
 function setStopBtn() {
   // ... your code goes here
+  const selectButton = document.querySelector("#btnLeft")
+  selectButton.className = "btn stop"
+  selectButton.textContent = "STOP"
 }
 
 function setSplitBtn() {
   // ... your code goes here
+  const selectButton = document.querySelector("#btnRight")
+  selectButton.className = "btn split"
+  selectButton.textContent = "SPLIT"
 }
 
 function setStartBtn() {
   // ... your code goes here
+  const selectButton = document.querySelector("#btnLeft")
+  selectButton.className = "btn start"
+  selectButton.textContent = "START"
 }
 
 function setResetBtn() {
   // ... your code goes here
+  const selectButton = document.querySelector("#btnRight")
+  selectButton.className = "btn reset"
+  selectButton.textContent = "RESET"
 }
 
 // Start/Stop Button
+let count = 0
 btnLeft.addEventListener('click', () => {
   // ... your code goes here
+  if (count === 0 || count % 2 === 0) {
+    chronometer.startClick()
+    setStopBtn()
+    setSplitBtn()
+    count++
+
+  } else {
+    chronometer.stopClick()
+    setStartBtn()
+    setResetBtn()
+    count++
+
+  }
+  printTime()
+
 });
 
 // Reset/Split Button
 btnRight.addEventListener('click', () => {
   // ... your code goes here
+
 });
