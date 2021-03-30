d6864e489061baa8596f073841bdd3a61eff6553
silvia rodriguez <silrodcar@gmail.com>
2021-03-30 14:47:18 +0200

    iteration 1

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,26 +1,60 @@
+
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this. currentTime = 0;
+    this.intervalId = 0;
+
+
   }
+
+
   startClick(callback) {
-    // ... your code goes here
+    setInterval (() => {
+      this.currentTime +=1;}, 1000);
   }
+
   getMinutes() {
-    // ... your code goes here
+    if (this.currentTime!= 0) {
+      return (Math.floor(this.currentTime/60))
+    } else {return (0)}
+    
   }
+
+
   getSeconds() {
-    // ... your code goes here
+    if (this.currentTime!= 0) {
+
+      return (this.currentTime%60)
+    } else {return (0)}
   }
+
+
   twoDigitsNumber() {
-    // ... your code goes here
+
+    if (this.currentTime < 10) {
+      return `0${this.currentTime}`
+    }
+
   }
+
+
+
   stopClick() {
-    // ... your code goes here
+    clearInterval(this.intervalId);
   }
+
+
   resetClick() {
-    // ... your code goes here
+    this. currentTime = 0;
+
   }
+
   splitClick() {
-    // ... your code goes here
+    let min= this.getMinutes()
+    let sec= this.getSeconds()
+
+      return (`${this.twoDigitsNumber(min)}:${this.twoDigitsNumber(sec)}`)
+    
   }
+
 }
