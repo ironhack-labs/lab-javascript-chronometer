868acfcdc763a9c44c86aefeb50ed64c3c3b1081
Alkin Maystorov <alkin971@gmail.com>
2021-03-31 12:39:25 +0200

    Iteration 1 Done

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,26 +1,40 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = 0;
   }
   startClick(callback) {
-    // ... your code goes here
+    this.intervalId = setInterval(() => { // Why does it need to be an arrow functoin?
+      this.currentTime++;
+    }, 1000);
   }
   getMinutes() {
-    // ... your code goes here
+    if (this.currentTime === 0) {
+      return 0;
+    }
+    return Math.floor(this.currentTime / 60);
   }
   getSeconds() {
-    // ... your code goes here
+    if (this.currentTime === 0) {
+      return 0;
+    }
+    return this.currentTime % 60;
   }
   twoDigitsNumber() {
-    // ... your code goes here
+    if (this.currentTime < 10) {
+      return `0${this.currentTime}`;
+    }
+    return this.currentTime;
   }
   stopClick() {
-    // ... your code goes here
+    clearInterval(this.intervalId);
   }
   resetClick() {
-    // ... your code goes here
+    return this.currentTime = 0;
   }
   splitClick() {
-    // ... your code goes here
+    let minutes = this.twoDigitsNumber(this.getMinutes());
+    let seconds = this.twoDigitsNumber(this.getSeconds());
+    return `${minutes}:${seconds}`;
   }
 }
