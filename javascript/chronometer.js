73d9c9ae6b5f0220c83772c20708271da90fa44d
May-Lu <helenetrommelen@gmail.com>
2021-03-31 23:16:26 +0200

    completed iteration one: logic

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,26 +1,58 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+   this.currentTime = 0;
+   this.intervalId = 0;
+   //console.log(this.currentTime);
+
   }
-  startClick(callback) {
-    // ... your code goes here
+  startClick() {
+    this.intervalId = setInterval(() => {
+      this.currentTime++;
+      //console.log(this.currentTime); //this is the log for the incrementing seconds
+    }, 1000
+    );
+     //console.log(setInterval(`The current time is ${this.intervalID}`, 5000)); // why does it call this function 4 times?
   }
   getMinutes() {
-    // ... your code goes here
+    return Math.floor(this.currentTime/60);
   }
-  getSeconds() {
-    // ... your code goes here
+
+  getSeconds() { //why do we not have to pass an argument here?
+    return this.currentTime % 60;
   }
-  twoDigitsNumber() {
-    // ... your code goes here
+
+  twoDigitsNumber(num) { //why don't we have to do anything with getMinutes or getSeconds here?
+    if (num < 10){
+      return `0${num}`
+    } else {return num}  
   }
+
   stopClick() {
-    // ... your code goes here
+    clearInterval(this.intervalId);
   }
+
   resetClick() {
-    // ... your code goes here
+    this.currentTime = 0;
   }
-  splitClick() {
-    // ... your code goes here
+
+  splitClick(min, sec) {
+    min = this.twoDigitsNumber(this.getMinutes()); // why not:  min.twoDigitsNumber(min)??
+    sec = this.twoDigitsNumber(this.getSeconds()); //why not: sec.twoDigitsNumber(sec);??
+    return `${min}:${sec}`;
   }
+  
 }
+
+myChronometer = new Chronometer();
+//console.log(myChronometer.getMinutes(num))
+myChronometer.startClick();
+myChronometer.getMinutes();
+myChronometer.getSeconds();
+myChronometer.twoDigitsNumber();
+
+
+
+
+
+
+
