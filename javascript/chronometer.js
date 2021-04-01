8e20c2c9d515865e3b0cc928598b0b216c7ebfeb
Sullivan <adressecours0.1@gmail.com>
2021-04-01 22:02:02 +0200

    done incomplete

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,26 +1,34 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0,
+    this.intervalId = 0;
   }
   startClick(callback) {
-    // ... your code goes here
+
+    this.intervalId = setInterval(() => {
+      this.currentTime ++
+    }, 1000)
+
   }
   getMinutes() {
-    // ... your code goes here
+    return Math.floor(this.currentTime / 60);
+
   }
   getSeconds() {
-    // ... your code goes here
+    return this.currentTime - this.getMinutes() * 60;
   }
   twoDigitsNumber() {
-    // ... your code goes here
+    return this.getMinutes() + this.currentTime.toLocaleString()
+
+    //why do we have a length of 2 here ?
   }
   stopClick() {
-    // ... your code goes here
+    clearInterval(this.intervalId);
   }
   resetClick() {
-    // ... your code goes here
+    return this.currentTime = 0;
   }
   splitClick() {
-    // ... your code goes here
+    this.twoDigitsNumber.toLocaleString()
   }
 }
