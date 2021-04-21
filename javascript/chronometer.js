75d55552daf4255af36d0b1005dcda2efaf98424
Aurelie75FR <aurelielo.vi@gmail.com>
2021-04-21 20:16:45 +0200

    I commented my entire code in indexjs because there's an error new chronometeris undefined

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,26 +1,50 @@
 class Chronometer {
   constructor() {
     // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = 0;
   }
   startClick(callback) {
-    // ... your code goes here
+ 
+    this.intervalId = setInterval(() => (this.currentTime += 1), 1000);
   }
   getMinutes() {
     // ... your code goes here
+    return Math.floor(this.currentTime / 60);
   }
   getSeconds() {
     // ... your code goes here
+    return this.currentTime % 60;
   }
-  twoDigitsNumber() {
-    // ... your code goes here
+  twoDigitsNumber(number) {
+    // short version
+    // return ("0" + number).slice(-2);
+
+    //long version
+     if(number >= 10){
+     return (number)
+     } else {
+      return "0" + number
+     }
+     return;
   }
   stopClick() {
     // ... your code goes here
+    return clearInterval(this.intervalId);
   }
   resetClick() {
     // ... your code goes here
+    return (this.currentTime = 0);
   }
   splitClick() {
-    // ... your code goes here
+    // two versions 
+    //the "function in function"
+    let minutes = this.twoDigitsNumber(this.getMinutes());
+    
+    //the variable in function
+    let seconds = this.getSeconds();
+
+
+    return `${minutes}:${this.twoDigitsNumber(seconds)}`
   }
 }
--- javascript/index.js
+++ javascript/index.js
@@ -1,28 +1,39 @@
 const chronometer = new Chronometer();
 
 // get the buttons:
-const btnLeft = document.getElementById('btnLeft');
-const btnRight = document.getElementById('btnRight');
+const btnLeft = document.getElementById("btnLeft");
+const btnRight = document.getElementById("btnRight");
 
 // get the DOM elements that will serve us to display the time:
-let minDec = document.getElementById('minDec');
-let minUni = document.getElementById('minUni');
-let secDec = document.getElementById('secDec');
-let secUni = document.getElementById('secUni');
-let milDec = document.getElementById('milDec');
-let milUni = document.getElementById('milUni');
-let splits = document.getElementById('splits');
+let minDec = document.getElementById("minDec");
+let minUni = document.getElementById("minUni");
+let secDec = document.getElementById("secDec");
+let secUni = document.getElementById("secUni");
+let milDec = document.getElementById("milDec");
+let milUni = document.getElementById("milUni");
+let splits = document.getElementById("splits");
+
+
+//error at new chronometer (is undefined) so I commented my code entirely and didn't know how debug this.
 
 function printTime() {
   // ... your code goes here
+  // printMinutes();
+  // printSeconds();
 }
 
 function printMinutes() {
   // ... your code goes here
+  // let i = chronometer.twoDigitsNumber(chronometer.getMinutes());
+  // minDec.innerText = i[0];
+  // minUni.innerText = i[1];
 }
 
 function printSeconds() {
   // ... your code goes here
+  // let j = chronometer.twoDigitsNumber(chronometer.getSeconds());
+  // secDec.innerHTML = j[0];
+  // secUni.innerHTML = j[1];
 }
 
 // ==> BONUS
@@ -32,34 +43,67 @@ function printMilliseconds() {
 
 function printSplit() {
   // ... your code goes here
+  // splits.innnerHTML = `<li>${chronometer.splitClick()}</li>`;
 }
 
 function clearSplits() {
   // ... your code goes here
+  // splits.innerHTML = "";
 }
 
 function setStopBtn() {
   // ... your code goes here
+  // btnLeft.innerHTML = "STOP";
+  // btnLeft.className = "btn stop";
+  // chronometer.stopClick();
 }
 
 function setSplitBtn() {
   // ... your code goes here
+  // btnRight.innerHTML = "SPLIT";
+  // btnRight.className = "btn split";
+  // chronometer.splitClick();
 }
 
 function setStartBtn() {
   // ... your code goes here
+  // btnLeft.innerHTML = "START";
+  // btnLeft.className = "btn start";
+  // chronometer.startClick();
 }
 
 function setResetBtn() {
   // ... your code goes here
+  // btnRight.innerHTML = "RESET";
+  // btnRight.className = "btn reset";
+  // chronometer.resetClick();
 }
 
 // Start/Stop Button
-btnLeft.addEventListener('click', () => {
+btnLeft.addEventListener("click", () => {
   // ... your code goes here
+
+  // if (btnLeft.innerHTML == "START") {
+  //   chronometer.startClick(chronometer);
+  //   setStopBtn();
+  //   setSplitBtn();
+  // } else {
+  //   chronometer.stopClick();
+  //   setStartBtn();
+  //   setResetBtn();
+  // }
+
 });
 
 // Reset/Split Button
-btnRight.addEventListener('click', () => {
+btnRight.addEventListener("click", () => {
   // ... your code goes here
+
+  // if (btnRight.innerHTML == "RESET") {
+  //   chronometer.resetClick();
+  //   clearSplits();
+  //   printTime();
+  // } else {
+  //   printSplit();
+  // }
 });
