7256e273ae6d303463921d08c705dff702a923ff
Lucia Sanchez <luci.sanchez.ortiz@gmail.com>
2021-05-08 14:31:24 +0200

    Iteration1

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,26 +1,59 @@
+let currentTime = 0;
+let currentMinute = 0;
+let twoDigitsMinutes;
+let twoDigitsSeconds;
+
 class Chronometer {
-  constructor() {
-    // ... your code goes here
-  }
-  startClick(callback) {
-    // ... your code goes here
+  constructor(){
+    this.currentTime = 0;
+    this.intervalId = 0;
+ }
+
+  startClick() {    
+    const intervalId = setInterval(() => {
+      this.currentTime += 1;
+      console.log(this.currentTime);
+    }, 1000);
   }
   getMinutes() {
-    // ... your code goes here
+        const minutes = Math.floor(this.currentTime/60);
+      return minutes;
   }
   getSeconds() {
-    // ... your code goes here
+    let seconds;
+    if (this.currentTime < 60) {
+       seconds = this.currentTime;
+      } else {
+        seconds = (this.currentTime - (this.getMinutes()*60));
+      }
+      return seconds
   }
-  twoDigitsNumber() {
-    // ... your code goes here
+  twoDigitsNumber(time) {
+    let stringTime = time.toString();
+    if ( stringTime.length < 2) {
+      stringTime = "0" + stringTime;
+    }
+    return stringTime;
   }
   stopClick() {
-    // ... your code goes here
+    clearInterval(this.IntervalId);
   }
   resetClick() {
-    // ... your code goes here
+    this.currentTime = 0;
   }
   splitClick() {
-    // ... your code goes here
+    const minutes = this.getMinutes();
+    const seconds = this.getSeconds();
+      return this.twoDigitsNumber(minutes) + `:` + this.twoDigitsNumber(seconds);
   }
 }
+
+
+//TEST
+const chrono = new Chronometer()
+console.log(chrono.startClick())
+console.log(chrono.getMinutes())
+console.log(chrono.getSeconds())
+console.log(chrono.twoDigitsNumber(8))
+console.log(chrono.splitClick())
+
