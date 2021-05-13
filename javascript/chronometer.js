e354719b48743bc9b1c9908dccdd4fd87ca1261d
Elizabeth Colastra <elizabethcolastra@hotmail.es>
2021-05-13 14:00:26 +0200

    More updates

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -21,34 +21,29 @@ class Chronometer {
       if (seconds % 60 != 0) {
         //Devuelve currentTime en minutos. Truncamos porque en este caso es decimal el cociente
         //Los segundos restantes los calculamos en el metodo getSeconds
-        let minutes = Math.trunc(seconds / 60)
-        return minutes
+        return this.currentTime = Math.trunc(seconds / 60)
       } else {
         //Devuelve currentTime en minutos
-        let minutes = seconds / 60
-        return minutes
+        return this.currentTime = seconds / 60
       }
 
     }
   }
-  getSeconds() {
+  getSeconds(value) {
     // ... your code goes here
     let seconds = this.currentTime
     if (seconds === 0) {
       return 0
     }
     //Segundos del tiempo combinado (minutos,segundos)
-    let seconds2 = seconds % 60
-    return seconds2
+    return seconds % 60
   }
-  twoDigitsNumber() {
+  twoDigitsNumber(value) {
     // ... your code goes here
-   
-    if (String(minutes).length < 2) {
-      return `0${minutes}`
-    }
-    if (String(seconds).length < 2) {
-      return `0${seconds}`
+    if ((String(value)).length < 2) {
+      return `0${value}`
+    } else {
+      return value
     }
   }
   stopClick() {
