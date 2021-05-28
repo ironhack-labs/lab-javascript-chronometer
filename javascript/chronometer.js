562a24f65258097304d9242b7d6ebd981adca2b8
ross-u <cirkovic.uros.srb@gmail.com>
2021-05-28 02:32:43 +0200

    Add comments

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -32,5 +32,8 @@ class Chronometer {
   }
 }
 
-// The following is required for our automated tests, ignore it for now.
-if (typeof module !== 'undefined') module.exports = Chronometer;
+// The following is required to make unit tests work.
+/* Environment setup. Do not modify the below code. */
+if (typeof module !== 'undefined') {
+  module.exports = Chronometer;
+}
