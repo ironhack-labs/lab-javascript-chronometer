e4240b192640ca74ef600f3c6ee43e3b258d89c9
P4tt1XD <patti@outlook.com.br>
2021-06-06 09:24:50 -0300

    Update chronometer.js

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,35 +1,59 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = '';
+    this.currentMilisecond = 0;
   }
 
   start(callback) {
-    // ... your code goes here
-  }
+    let that = this;
+    this.intervalId = setInterval (function(){
+      that.currentTime += 1;
+      that.setTime();
+  }, 1000);
+    
+    this.milisecondIntervalId - setInterval(function(){
+      if(that.currentMiliseconds === 99){
+        that.currentMiliseconds = 0;
+      }
+      that.setMiliseconds();
+      that.currentMiliseconds += 1;
+    }, 10);
+  };
 
   getMinutes() {
-    // ... your code goes here
-  }
+    return Math.floor(this.currentTime /60);
+  };
 
   getSeconds() {
-    // ... your code goes here
-  }
+    return Math.floor(this.currentTime * 60);
+  };
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
-  }
+    return ('0' + value).slice(-2);
+    
+    let minutes = this.computeTwoDigitsNumber(this.getMinutes());
+    let seconds - this.computeTwoDigitsNumber(this.getSeconds(minutes));
+  };
 
   stop() {
-    // ... your code goes here
+    clearInterval(this.intervalId);
+    clearInterval(this.milisecondIntervalId);
   }
 
   reset() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.currentMiliseconds = 0;
+    this.setTime();
+    this.setMilisecond();
+    
   }
 
   split() {
-    // ... your code goes here
-  }
+    let minutes = this.computeTwoDigitsNumber(this.getMinutes());
+    let seconds = this.computeTwoDigitsNumber(this.getSeconds(minutes));
+    printSplit(minutes, seconds);
+  };
 }
 
 // The following is required to make unit tests work.
