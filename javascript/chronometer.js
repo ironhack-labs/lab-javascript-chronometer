552eec71d7b456990acc5431d90fce71e7b0e636
Jhonatan Veras <jveras@leroymerlin.com.br>
2021-06-07 18:20:21 -0300

    Solved lab

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,35 +1,43 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
   }
 
-  start(callback) {
-    // ... your code goes here
+  start () {
+    this.intervalId = setInterval(() => {
+    this.currentTime++;
+    console.log(this.currentTime);
+  },1000);
   }
 
   getMinutes() {
-    // ... your code goes here
+    return parseInt(Math.floor(this.currentTime / 60));
   }
 
   getSeconds() {
-    // ... your code goes here
+    return parseInt(this.currentTime % 60);
   }
 
-  computeTwoDigitNumber(value) {
-    // ... your code goes here
-  }
+  computeTwoDigitNumber(valor) {
+    return (`0` + valor).slice(-2);
+   }
 
   stop() {
-    // ... your code goes here
+    clearInterval(this.intervalId);
   }
 
   reset() {
-    // ... your code goes here
+    this.currentTime = 0;
   }
 
   split() {
-    // ... your code goes here
+    let min = this.computeTwoDigitNumber(this.getMinutes());
+    let sec = this.computeTwoDigitNumber(this.getSeconds());
+
+    return `${min}:${sec}`;
   }
+  
 }
 
 // The following is required to make unit tests work.
