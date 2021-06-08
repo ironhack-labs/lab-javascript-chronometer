340573a8244ae8a0958e83df76dfa0b079c79a78
Morgane Deschamps <morgane.deschamps@gmail.com>
2021-06-09 00:43:35 +0200

    final version - no bonus

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,36 +1,66 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
   }
 
   start(callback) {
-    // ... your code goes here
+    this.intervalId = setInterval(() => {
+      if(callback) {callback};
+      this.currentTime += 1;
+    }, 1000) 
   }
 
+  
   getMinutes() {
-    // ... your code goes here
+    return Math.floor(this.currentTime / 60);
   }
+
 
   getSeconds() {
-    // ... your code goes here
+    let seconds = 0;
+    if (this.currentTime < 60) {
+      seconds += this.currentTime;
+    } else {
+    seconds += this.currentTime % 60;
+    }
+    return seconds;
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    let twoDigits;
+    let valueString = value.toString();
+    if (valueString.length === 1) {
+      twoDigits = "0" + valueString;
+    } else {
+      twoDigits = valueString;
+    }
+    return twoDigits
+    // return value as string "0value" if value is 1num or "value"
   }
 
   stop() {
+    clearInterval(this.intervalId);
     // ... your code goes here
   }
 
   reset() {
+    this.currentTime = 0;
     // ... your code goes here
   }
 
   split() {
-    // ... your code goes here
+    let splitTime;
+    let min = this.getMinutes() // 0
+    let sec = this.getSeconds() // 5
+
+    splitTime = this.computeTwoDigitNumber(min) + ":" + this.computeTwoDigitNumber(sec);
+
+    return splitTime;
+    // return "mm:ss"
   }
 }
+
 
 // The following is required to make unit tests work.
 /* Environment setup. Do not modify the below code. */
--- javascript/index.js
+++ javascript/index.js
@@ -4,6 +4,69 @@ const chronometer = new Chronometer();
 const btnLeftElement = document.getElementById('btnLeft');
 const btnRightElement = document.getElementById('btnRight');
 
+
+// Start/Stop Button
+btnLeftElement.addEventListener('click', () => {
+  let interval;
+  if(btnLeftElement.classList.contains("start")) {
+    //start class => running
+    btnLeftElement.classList.add("stop");
+    btnLeftElement.classList.remove("start");
+    btnLeftElement.innerHTML = "STOP";
+
+    btnRightElement.classList.add("split");
+    btnRightElement.classList.remove("reset");
+    btnRightElement.innerHTML = "SPLIT";
+
+    chronometer.start();
+
+    interval = setInterval(this.printTime, 500);
+
+  } else {
+    //stop class => Stopped
+    btnLeftElement.classList.add("start")
+    btnLeftElement.classList.remove("stop")
+    btnLeftElement.innerHTML = "START"
+
+    btnRightElement.classList.add("reset")
+    btnRightElement.classList.remove("split")
+    btnRightElement.innerHTML = "RESET"
+
+    chronometer.stop();
+
+    clearInterval(interval);
+  }
+  // ... your code goes here
+});
+
+
+// Reset/Split Button
+btnRightElement.addEventListener('click', () => {
+  let listEl = document.createElement("li");
+  let splitNow = document.createTextNode(chronometer.split());
+
+  if(btnRightElement.classList.contains("split")) {
+  listEl.appendChild(splitNow);
+  splitsElement.appendChild(listEl);
+  } else {
+    chronometer.reset();
+    this.printTime()
+    while(splitsElement.firstChild) {
+      splitsElement.removeChild(splitsElement.firstChild);
+    }
+  }
+  
+
+
+
+
+
+  // ... your code goes here
+});
+
+//  if(splitsElement.hasChildNodes()) 
+
+
 // get the DOM elements that will serve us to display the time:
 const minDecElement = document.getElementById('minDec');
 const minUniElement = document.getElementById('minUni');
@@ -13,7 +76,21 @@ const milDecElement = document.getElementById('milDec');
 const milUniElement = document.getElementById('milUni');
 const splitsElement = document.getElementById('splits');
 
+
 function printTime() {
+  let minutes = chronometer.computeTwoDigitNumber(chronometer.getMinutes());
+  let seconds = chronometer.computeTwoDigitNumber(chronometer.getSeconds());
+
+  let time = minutes + seconds;
+  time = time.split("");
+
+  secUniElement.innerHTML = time[3];
+  secDecElement.innerHTML = time[2];
+
+  minUniElement.innerHTML = time[1];
+  minDecElement.innerHTML = time[0];
+
+
   // ... your code goes here
 }
 
@@ -53,13 +130,3 @@ function setStartBtn() {
 function setResetBtn() {
   // ... your code goes here
 }
-
-// Start/Stop Button
-btnLeftElement.addEventListener('click', () => {
-  // ... your code goes here
-});
-
-// Reset/Split Button
-btnRightElement.addEventListener('click', () => {
-  // ... your code goes here
-});
