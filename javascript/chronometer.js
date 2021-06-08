744c55114a6a765a7d0314aa5bc9793d55784620
Flavio-Pestana <flaviopestan@gmail.com>
2021-06-07 21:42:40 -0300

    Finalizando

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,30 +1,43 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
+   
   }
 
   start(callback) {
-    // ... your code goes here
+    this.intervalId = setInterval(() => {
+      this.currentTime =+1;
+      console.log(this.currentTime);
+    },1000);
+
+    
+    
   }
 
   getMinutes() {
-    // ... your code goes here
+    return parseInt(Math.floor(this.currentTime / 60));
+    
   }
 
   getSeconds() {
-    // ... your code goes here
+    return parseInt(this.currentTime % 60);
+    
   }
 
   computeTwoDigitNumber(value) {
+    
     // ... your code goes here
   }
 
   stop() {
-    // ... your code goes here
+    clearInterval(this.intervalId);
+    
   }
 
   reset() {
-    // ... your code goes here
+    this.currentTime = 0;
+    
   }
 
   split() {
