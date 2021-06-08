dfa87fa47c7323592716ddfba827a5ce45e6e3e7
sebastiangomezzz <sebastiangomezzz@gmail.com>
2021-06-08 21:48:12 +0200

    "1st iteration done"

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,34 +1,54 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0,
+    this.intervalId = null
   }
 
   start(callback) {
-    // ... your code goes here
-  }
+    this.intervalId = setInterval(() => { //setInterval it's a loop that gets executed periodically with a specified time lapse
+      if (callback) callback(); //if there's a callback, callback gets executed
+      this.currentTime = this.currentTime +1;
+    }, 1000);
+  }//this returns currentTime + 1
 
   getMinutes() {
-    // ... your code goes here
+  
+    return Math.floor(this.currentTime / 60);
   }
-
   getSeconds() {
-    // ... your code goes here
+    return this.currentTime % 60;
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    //let minString = this.getMinutes.toString();
+    //let secString = this.getSeconds.toString();
+    //if (minString.length < 2 ) {
+    //  minString =  `0+${minString}`
+    //}
+    //if (secString.length < 2) {
+    //  secString =  `0+${secString}`
+    //}
+    let valueToString = value.toString();
+    let totalValue
+    if (valueToString.length === 1) {
+      totalValue = "0" + valueToString;
+    }else 
+      totalValue = valueToString 
+    
+    return totalValue;
   }
 
   stop() {
-    // ... your code goes here
+    clearInterval(this.intervalId);
   }
 
   reset() {
-    // ... your code goes here
+    this.currentTime = 0;
   }
 
   split() {
-    // ... your code goes here
+    let splitTime = this.computeTwoDigitNumber(this.getMinutes()) + ":" + this.computeTwoDigitNumber(this.getSeconds());
+    return splitTime;
   }
 }
 
