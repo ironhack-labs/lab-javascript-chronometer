d30228be977320a608b932303ffdf5231caa677d
P4tt1XD <patti@outlook.com.br>
2021-06-07 21:24:13 -0300

    Update chronometer.js

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,25 +1,19 @@
 class Chronometer {
   constructor() {
-    this.currentTime = 0;
-    this.intervalId = '';
-    this.currentMilisecond = 0;
+    this.currentTime = 58;
+    this.intervalId = 0;
   }
 
   start(callback) {
-    let that = this;
-    this.intervalId = setInterval (function(){
-      that.currentTime += 1;
-      that.setTime();
-  }, 1000);
-    
-    this.milisecondIntervalId = setInterval(function(){
-      if(that.currentMiliseconds === 99){
-        that.currentMiliseconds = 0;
+    this.intervalId = setInterval(() =>{
+      this.currentTime++;
+
+      if(callback !== undefined){
+        callback();
       }
-      that.setMiliseconds();
-      that.currentMiliseconds += 1;
-    }, 10);
-  };
+    }, 1000);
+  }
+    
 
   getMinutes() {
     return Math.floor(this.currentTime /60);
@@ -33,26 +27,21 @@ class Chronometer {
     return ('0' + value).slice(-2);
     
     let minutes = this.computeTwoDigitsNumber(this.getMinutes());
-    let seconds = this.computeTwoDigitsNumber(this.getSeconds(minutes));
+    let seconds = this.computeTwoDigitsNumber(this.getSeconds());
   };
 
   stop() {
-    clearInterval(this.intervalId);
-    clearInterval(this.milisecondIntervalId);
+    return clearInterval(this.intervalId);
   }
 
   reset() {
     this.currentTime = 0;
-    this.currentMiliseconds = 0;
-    this.setTime();
-    this.setMilisecond();
-    
   }
 
   split() {
-    let minutes = this.computeTwoDigitsNumber(this.getMinutes());
-    let seconds = this.computeTwoDigitsNumber(this.getSeconds(minutes));
-    printSplit(minutes, seconds);
+     this.computeTwoDigitsNumber(this.getMinutes()); +
+    ":" +
+    this.computeTwoDigitsNumber(this.getSeconds());
   };
 }
 
@@ -62,44 +51,3 @@ if (typeof module !== 'undefined') {
   module.exports = Chronometer;
 };
 
-  getMinutes() {
-    return Math.floor(this.currentTime /60);
-  };
-
-  getSeconds() {
-    return Math.floor(this.currentTime * 60);
-  };
-
-  computeTwoDigitNumber(value) {
-    return ('0' + value).slice(-2);
-    
-    let minutes = this.computeTwoDigitsNumber(this.getMinutes());
-    let seconds = this.computeTwoDigitsNumber(this.getSeconds(minutes));
-  };
-
-  stop() {
-    clearInterval(this.intervalId);
-    clearInterval(this.milisecondIntervalId);
-  };
-
-  reset() {
-    this.currentTime = 0;
-    this.currentMiliseconds = 0;
-    this.setTime();
-    this.setMilisecond();
-    
-  };
-
-  split() {
-    let minutes = this.computeTwoDigitsNumber(this.getMinutes());
-    let seconds = this.computeTwoDigitsNumber(this.getSeconds(minutes));
-    printSplit(minutes, seconds);
-  };
-
-
-
-// The following is required to make unit tests work.
-/* Environment setup. Do not modify the below code. */
-if (typeof module !== 'undefined') {
-  module.exports = Chronometer;
-}
