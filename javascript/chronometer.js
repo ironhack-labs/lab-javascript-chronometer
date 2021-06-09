ce38652fc58d20876299bfa942ed78da228c89c4
Andrew Oppo <andrew.oppo@gmail.com>
2021-06-09 18:25:00 +0200

    finished iteration 1

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,34 +1,48 @@
 class Chronometer {
   constructor() {
     // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
   }
 
   start(callback) {
     // ... your code goes here
+    this.intervalId = setInterval(() => {
+      this.currentTime++;
+    }, 1000)
   }
 
   getMinutes() {
     // ... your code goes here
+    return Math.floor(this.currentTime / 60);
   }
 
   getSeconds() {
     // ... your code goes here
+    return this.currentTime % 60;
   }
 
   computeTwoDigitNumber(value) {
     // ... your code goes here
+    if (value.toString().length === 1) {
+      return '0' + value.toString();
+    } 
+    return value.toString();
   }
 
   stop() {
     // ... your code goes here
+    clearInterval(this.intervalId)
   }
 
   reset() {
     // ... your code goes here
+    this.currentTime = 0;
   }
 
   split() {
     // ... your code goes here
+    return this.computeTwoDigitNumber(this.getMinutes()).toString() + ":" + this.computeTwoDigitNumber(this.getSeconds()).toString()
   }
 }
 
