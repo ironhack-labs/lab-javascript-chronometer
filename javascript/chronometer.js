fa8b27831faac03b0e5c1a1adcf692ff6898b595
Sinah Scholz <s.scholz-hehn@outlook.de>
2021-06-10 08:19:48 +0200

    Solved lab

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,36 +1,146 @@
+
+//add the logic here!
+
+
+//"The constructor method shouldn't expect any arguments.
+// It should initialize two properties of the chronometer:
+//currentTime, which should start of as the number 0.
+// intervalId, which should start as null."
+
 class Chronometer {
-  constructor() {
-    // ... your code goes here
+  constructor() {    
+   
+
+    this.currentTime=0;
+    this.intervalId = null;  //store intervalID here to access it outside func as well
+
+//store my other variables here to access them anywhere inside class???
   }
 
-  start(callback) {
+
+  start((callback)=> {   //arrow function--> "this" refers to the Chronometer class
+//name argument v start ("callback") nicht ändern.
+
+ 
+this.intervalId = setInterval(()=>{
+
+  if (callback) {     //check if callback was passed (if-condition), if yes- execute it
+    
+    callback();
+  
+  
+  }
+  this.currentTime++;  //...else ignore it and just do this
+}, 1000)   //interval = 1 sec = 1000 millisecs
+  
+}
+
+
+
+
+//Method getMinutes
+
+  getMinutes() {    //NO ARGUMENT
     // ... your code goes here
+
+   let passedMinutes= this.currentTime/60;
+
+   //rounding the number to integer with toFixed()
+   
+  let myIntMinutes= Number(passedMinutes.toFixed());   //store int number in variable just in case
+  
+
+
+return myIntMinutes;     ////function scope???? access later with this????
+ 
   }
 
-  getMinutes() {
-    // ... your code goes here
-  }
+
 
   getSeconds() {
     // ... your code goes here
+
+let secondsAftercurrentMin = this.currentTime % 60;   
+
+return secondsAftercurrentMin;    ////function scope???? access later with this????
+
   }
+
 
   computeTwoDigitNumber(value) {
     // ... your code goes here
+
+//stringify number with toString??
+    
+let myStringNum= value.toString(); 
+ 
+
+if(value <10){
+
+myStringNum= "0".concat(myStringNum);
+
+    }  
+
+return myStringNum;
+ 
+
+
   }
+
+  //Later, we'll use the computeTwoDigitNumber method to format
+  // the values returned by getMinutes and getSeconds and display them in our chronometer.
 
   stop() {
     // ... your code goes here
+
+    clearInterval(this.intervalId);  
   }
 
   reset() {
     // ... your code goes here
+this.currentTime=0;
+
   }
 
-  split() {
+  split() {  //no arguments
     // ... your code goes here
+
+
+//use my minutes  "myIntMinutes" followed by my secs since minute started "secondsAftercurrentMin"
+//turn both into strings with computeTwoDigitNumber
+//and concatenate them together with ":"
+
+//meine sekunden seit minute: secondsAftercurrentMin
+
+//let mySplitNum = (myIntMinutes.computeTwoDigitNumber()).concat(":", secondsAftercurrentMin.computeTwoDigitNumber())
+
+//str1.concat(str2, str3, str4.......strn);
+
+
+
+//pass my numbers (min and secs) as parameters to computeTwoDigitNumber
+//&& store results in variable
+
+
+let myIntMinutes = this.getMinutes();    
+ //else myIntMinutes from above not accessible here?
+ 
+let  secondsAftercurrentMin= this.getSeconds();
+
+
+let myStringMin = computeTwoDigitNumber(myIntMinutes);     
+
+
+let myStringSecs = computeTwoDigitNumber(secondsAftercurrentMin);
+
+let mySplitNum = myStringMin.concat(":", myStringSecs);
+ 
+return mySplitNum;
+
+
+
   }
-}
+
 
 // The following is required to make unit tests work.
 /* Environment setup. Do not modify the below code. */
--- javascript/index.js
+++ javascript/index.js
@@ -1,4 +1,12 @@
+
+//DO THE INTERFACE DOM MANIPULATION HERE
+
+
+
+
 const chronometer = new Chronometer();
+
+let myNewchronometer = new Chronometer();
 
 // get the buttons:
 const btnLeftElement = document.getElementById('btnLeft');
@@ -57,9 +65,48 @@ function setResetBtn() {
 // Start/Stop Button
 btnLeftElement.addEventListener('click', () => {
   // ... your code goes here
+//um verhalten der knöpfe zu ändern, müssen wir Klasse umschalten
+
+
+// to change status, toggle class. Toggle method:
+//
+//btnLeftElement.classList.toggle("");
+
+
+//does this work as well???
+if(btnLeftElement.className === 'btn start'){
+
+
+  btnLeftElement.className = 'btn stop';
+  btnRightElement.className = 'btn split';
+
+
+  //set the btnLeft/right text to STOP here:
+  btnLeftElement.innerText    = "STOP";
+btnRightElement.innerText   = "SPLIT";
+
+} else {
+  btnLeftElemnt.className = 'btn start';
+  btnRightElement.className = 'btn reset';
+
+  btnLeftElement.innerText = "START";
+  btnRightElement.innerText = "RESET"
+
+
+}
+
 });
 
 // Reset/Split Button
 btnRightElement.addEventListener('click', () => {
   // ... your code goes here
+
+
+
+
 });
+
+
+//Create the necessary code in the index.js to call the 
+//Chronometer's start method if the button has the start class, 
+//or the stop method if the button has the stop class applied.
