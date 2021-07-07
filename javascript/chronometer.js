9f0ad61834b67ec0085f1f74822493ff3946f2e0
jxxjxxlxx <92627.p@gmail.com>
2021-07-08 00:46:07 +0200

    286 jinjoo
    done BUT the chronometer doesn't work at all ....... I read the code several times and inspected on chrome, still can' find what is wrong .......

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,34 +1,51 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId= null;
   }
 
   start(callback) {
-    // ... your code goes here
+    this. intervalId = setInterval(()=>{
+      if (callback !== undefined) {
+        callback()
+      }
+    this.currentTime += 1;
+    }, 1000);
   }
 
   getMinutes() {
-    // ... your code goes here
+    return Math.floor(this.currentTime / 60);
   }
 
   getSeconds() {
-    // ... your code goes here
+    return this.currentTime % 60
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    if (value < 10) {
+      return '0' + value
+    } else {
+      return value.toString();
+      // return '' + value;
+    }
   }
 
   stop() {
-    // ... your code goes here
+    clearInterval (this.intervalId);
   }
 
   reset() {
-    // ... your code goes here
+    this.currentTime = 0;
   }
 
   split() {
-    // ... your code goes here
+    let seconds = this.getSeconds();
+    let minutes = this.getMinutes();
+
+    let secondsAsString = this.computeTwoDigitNumber(seconds);
+    let minutesAsString = this.computeTwoDigitNumber(minutes);
+
+    return `${minutesAsString}:${secondsAsString}`
   }
 }
 
