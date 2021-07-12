ded6d97023a256d48c0ca80a805aa0abc7499406
Karina Narkeviciute <karina.narkeviciute@gmail.com>
2021-07-12 21:51:10 +0300

    implement getSeconds method

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -11,11 +11,11 @@ class Chronometer {
   }
 
   getMinutes() {
-    // ... your code goes here
+    return Math.floor(this.currentTime / 60);
   }
 
   getSeconds() {
-    // ... your code goes here
+    return this.currentTime % 60;
   }
 
   computeTwoDigitNumber(value) {
