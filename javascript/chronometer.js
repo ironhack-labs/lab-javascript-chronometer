0a51b41393148d00cd6e2973824935597c95e7f2
Karina Narkeviciute <karina.narkeviciute@gmail.com>
2021-07-12 23:10:52 +0300

    Solved lab

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -19,19 +19,27 @@ class Chronometer {
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    let valueString = value.toString();
+
+    if (valueString.length === 1) {
+      return '0' + valueString;
+    } else {
+      return valueString;
+    }
   }
 
   stop() {
-    // ... your code goes here
+    clearInterval(this.intervalId);
   }
 
   reset() {
-    // ... your code goes here
+    this.currentTime = 0;
   }
 
   split() {
-    // ... your code goes here
+    return `${this.computeTwoDigitNumber(
+      this.getMinutes()
+    )}:${this.computeTwoDigitNumber(this.getSeconds())}`;
   }
 }
 
