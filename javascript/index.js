914cec2b0ec2f2a423619fcc975fedb907c1b219
TrellinXp <Peter_My@gmx.de>
2021-07-12 19:37:07 +0200

    Stop Clock

--- javascript/index.js
+++ javascript/index.js
@@ -12,12 +12,15 @@ const secUniElement = document.getElementById('secUni');
 const milDecElement = document.getElementById('milDec');
 const milUniElement = document.getElementById('milUni');
 const splitsElement = document.getElementById('splits');
+var isRunning = false;
 
 function printTime() {
-  minDecElement.textContent = printMinutes()[0];
-  minUniElement.textContent  = printMinutes()[1];
-  secDecElement.textContent = printSeconds()[0];
-  secUniElement.textContent = printSeconds()[1];
+  if(isRunning) {
+    minDecElement.textContent = printMinutes()[0];
+    minUniElement.textContent  = printMinutes()[1];
+    secDecElement.textContent = printSeconds()[0];
+    secUniElement.textContent = printSeconds()[1];
+  }
 }
 
 function printMinutes() {
@@ -26,6 +29,14 @@ function printMinutes() {
 
 function printSeconds() {
   return chronometer.computeTwoDigitNumber(chronometer.getSeconds());
+}
+
+function clearTime() {
+  minDecElement.textContent = "0";
+  minUniElement.textContent  = "0";
+  minDecElement.textContent = "0"; 
+  secUniElement.textContent = "0"
+  chronometer.reset();
 }
 
 // ==> BONUS
@@ -60,27 +71,27 @@ function setResetBtn() {
 // Start/Stop Button
 
 btnLeftElement.addEventListener('click', () => {
+  let intervalId = null;
   if(btnLeftElement.textContent === 'START') {
     setStopBtn();
     setSplitBtn();
+    isRunning = true;
     chronometer.start();
+
+    intervalId = setInterval(function()
+    { 
+      printTime();
+    }, 1000);
   } else {
     setStartBtn();
     setResetBtn();
+    isRunning = false;
     chronometer.stop();
+    clearInterval(intervalId);
   }
-  
-  this.intervalId = setInterval(function()
-  { 
-    printTime();
-  }, 1000);
-
 });
 
 // Reset/Split Button
 btnRightElement.addEventListener('click', () => {
-  minDecElement.textContent = "0";
-  minUniElement.textContent  = "0";
-  minDecElement.textContent = "0"; 
-  secUniElement.textContent = "0";
+  clearTime();
 });
