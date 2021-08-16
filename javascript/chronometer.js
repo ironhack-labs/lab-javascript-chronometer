5366254a411fc1ffa72380d7bb507d6bf124971e
jaumebt90 <jaume.mx90@gmail.com>
2021-08-16 18:17:54 +0200

    Update chronometer.js

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,34 +1,59 @@
 class Chronometer {
   constructor() {
     // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
+
+      
+  }
+  start(callback) {
+
+    // ... your code goes here
+  this.intervalId = setInterval(() => {this.currentTime++},1000)
   }
 
-  start(callback) {
-    // ... your code goes here
-  }
 
   getMinutes() {
     // ... your code goes here
+    return Math.floor(this.currentTime/60)
   }
 
   getSeconds() {
     // ... your code goes here
+    return this.currentTime%60
+
   }
 
   computeTwoDigitNumber(value) {
     // ... your code goes here
+    if(value === 0){
+      return '00'
+    }
+
+    let valueToString = value.toString()
+    
+    if(value < 10){
+      return `0${value}`
+    }
+    return valueToString
+
   }
 
   stop() {
     // ... your code goes here
+    clearInterval(this.intervalId)
   }
 
   reset() {
     // ... your code goes here
+    this.currentTime = 0
   }
 
   split() {
     // ... your code goes here
+    const min = this.computeTwoDigitNumber (this.getMinutes());
+    const seg = this.computeTwoDigitNumber (this.getSeconds());
+    return `${min}:${seg}`
   }
 }
 
@@ -37,3 +62,4 @@ class Chronometer {
 if (typeof module !== 'undefined') {
   module.exports = Chronometer;
 }
+
