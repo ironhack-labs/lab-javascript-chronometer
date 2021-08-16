abedc14cf8b7f05830108242a9fa107173d2d617
jdavesa <86001040+jdavesa@users.noreply.github.com>
2021-08-16 17:02:43 +0200

    Update chronometer.js

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,34 +1,65 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.currentMiliseconds = 0;
+    this.intervalId = null;
+    this.miliSecondId = null;
   }
-
+  
   start(callback) {
-    // ... your code goes here
+    if(!callback){
+    this.intervalId = setInterval(()=>{ 
+      this.currentTime++;
+    },1000);
+    this.miliSecondId = setInterval(()=>{
+      this.currentMiliseconds ++;
+    }, 1);}
+    else {
+      this.intervalId = setInterval(callback(),1000);
+      this.miliSecondId = setInterval(callback(),1);
+    }
   }
 
   getMinutes() {
-    // ... your code goes here
+    let minutes = 0;
+    minutes =  Math.floor(this.currentTime / 60 ); 
+    return (minutes);
+  }
+  getSeconds() {
+    let seconds = 0;
+    seconds = this.currentTime % 60;
+    return (seconds);
   }
 
-  getSeconds() {
-    // ... your code goes here
+  getMiliseconds() {
+    let miliseconds = 0;
+    miliseconds = this.currentMiliseconds % 1000;
+    return (miliseconds);
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    let numString = value.toString();
+    if(numString.length < 2) {
+      numString = '0'+numString}
+    return (numString)
   }
 
   stop() {
-    // ... your code goes here
+    clearInterval(this.intervalId);
   }
 
   reset() {
-    // ... your code goes here
+    this.currentTime = 0;
   }
 
   split() {
-    // ... your code goes here
+    let message = "";
+    let minutes = this.getMinutes ();
+    let seconds = this.getSeconds ();
+    minutes = this.computeTwoDigitNumber (minutes);
+    seconds = this.computeTwoDigitNumber (seconds);
+    message = minutes + ":" + seconds;
+    return (message);
   }
 }
 
