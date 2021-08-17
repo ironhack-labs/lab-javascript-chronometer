5f07acb48de468ef9ad011f57ab3a17cb9d1920f
Juan De Santis <juanbds66@gmail.com>
2021-08-17 20:41:22 +0200

    [WEB MAD DE] JUAN DE SANTIS

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,35 +1,67 @@
 class Chronometer {
   constructor() {
     // ... your code goes here
+    this.currentTime = 0
+    this.intervalId = null
+    
   }
 
   start(callback) {
-    // ... your code goes here
+
+    this.intervalId = setInterval(() => {
+       this.currentTime ++
+     }, 1000);
   }
 
   getMinutes() {
     // ... your code goes here
+    
+     
+
+     return Math.floor(this.currentTime / 60)
+    
+    
+
+    
   }
 
   getSeconds() {
     // ... your code goes here
+  
+    return Math.floor(this.currentTime % 60)
   }
 
   computeTwoDigitNumber(value) {
     // ... your code goes here
+    
+    if(value < 10){
+
+      return 0 + value.toString();
+    } else {
+
+      return value.toString();
+    }
+     
+
+    
   }
 
   stop() {
     // ... your code goes here
+    clearInterval(this.intervalId)
   }
 
   reset() {
     // ... your code goes here
+    this.currentTime = 0
   }
 
   split() {
     // ... your code goes here
-  }
+   let split = this.currentTime +  this.computeTwoDigitNumber + this.getMinutes +  ':' + this.getSeconds
+
+    return split
+}
 }
 
 // The following is required to make unit tests work.
