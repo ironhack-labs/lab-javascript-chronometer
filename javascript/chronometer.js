3ff0879ee1efe7e3a5273c06145e47a548309667
Adrian Fernandez Carmona <adrianfdezbcn@gmail.com>
2021-08-17 18:13:24 +0200

    WD2021 - Adrian Fernandez
    con Ivan y Julian

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -10,23 +10,32 @@ class Chronometer {
     }, 1000);
   }
 
- 
 
   getMinutes() {
+
+    // Divide 60 and we get a whole number which is the minutes
     return Math.floor(this.currentTime / 60);
   }
 
   getSeconds() {
+
+    // %60 returns the extra minutes
     return this.currentTime % 60
   }
 
   computeTwoDigitNumber(value) {
+
+    // We need a 0 in fron of all the numbers from 0 to 9.
+    // If value is <10 we add a string 0 and sum the value (num) which turns into a string.
     if (value < 10){
       return "0" + value
-    }  else { return value.toString()};
+    }  else { 
+    //else already show number, and transform it to sting.
+      return value.toString()};
   }
 
   stop() {
+    //https://www.w3schools.com/jsref/met_win_clearinterval.asp it stops the time
    clearInterval(this.intervalId);
   }
 
@@ -35,12 +44,11 @@ class Chronometer {
   }
 
   split() {
+    //We get the minutes and the seconds and add them as a string divided by the :
    const min = this.computeTwoDigitNumber (this.getMinutes());
    const sec = this.computeTwoDigitNumber (this.getSeconds());
    const time = (`${min}:${sec}`)
    return time;
-
-
   }
 }
 
--- javascript/index.js
+++ javascript/index.js
@@ -14,15 +14,24 @@ const milUniElement = document.getElementById('milUni');
 const splitsElement = document.getElementById('splits');
 
 function printTime() {
-  // ... your code goes here
+  //We call the functions below
+  printMinutes();
+  printSeconds();
 }
 
 function printMinutes() {
-  // ... your code goes here
+  //Minutes need to have two digits, so we call the function .computeTwoDigitNumber() from the chronometer.js file with the getMinutes() as a parameter
+  let minutes = chronometer.computeTwoDigitNumber(chronometer.getMinutes());
+  minUniElement.innerHTML = `${minutes[1]}`; //Second number
+  minDecElement.innerHTML = `${minutes[0]}`; //First number (so it shows 0)
 }
 
 function printSeconds() {
-  // ... your code goes here
+  //Seconds need to have two digits, so we call the function .computeTwoDigitNumber() from the chronometer.js file with the getSeconds() as a parameter
+
+  let seconds = chronometer.computeTwoDigitNumber(chronometer.getSeconds());
+  secUniElement.innerHTML = `${seconds[1]}`;
+  secDecElement.innerHTML = `${seconds[0]}`;
 }
 
 // ==> BONUS
@@ -31,35 +40,64 @@ function printMilliseconds() {
 }
 
 function printSplit() {
-  // ... your code goes here
+  let minutes = chronometer.computeTwoDigitNumber(chronometer.getMinutes());
+  let seconds = chronometer.computeTwoDigitNumber(chronometer.getSeconds());
+  splitsElement.innerHTML += '<li>' + minutes + ':' + seconds + '</li>'; //???????? Is there another way like we saw in class?
 }
-
+//`<li>${minutes}:${seconds}</li>`;
 function clearSplits() {
-  // ... your code goes here
+  splitsElement.innerHTML = '';
 }
-
+//------------>If we press botton:
 function setStopBtn() {
-  // ... your code goes here
+  btnLeftElement.className = 'btn stop'; //class changes the color (because of the class in css)
+  btnLeftElement.innerHTML = 'STOP'; //changes the text
 }
 
 function setSplitBtn() {
-  // ... your code goes here
+  btnRightElement.className = 'btn split';
+  btnRightElement.innerHTML = 'SPLIT';
 }
 
 function setStartBtn() {
-  // ... your code goes here
+  btnLeftElement.className = 'btn start';
+  btnLeftElement.innerHTML = 'START';
 }
 
 function setResetBtn() {
-  // ... your code goes here
+  btnRightElement.className = 'btn reset';
+  btnRightElement.innerHTML = 'RESET';
 }
 
-// Start/Stop Button
+// Start / Stop -- Button
 btnLeftElement.addEventListener('click', () => {
-  // ... your code goes here
+  switch (btnLeftElement.className) {
+    case 'btn start':
+      setStopBtn();
+      setSplitBtn();
+      chronometer.start();
+      setInterval(() => printTime(), 10);
+      break;
+    case 'btn stop':
+      setStartBtn();
+      setResetBtn();
+      chronometer.stop();
+      break;
+  }
 });
 
 // Reset/Split Button
 btnRightElement.addEventListener('click', () => {
-  // ... your code goes here
+  switch (btnRightElement.className) {
+    case 'btn reset':
+      setResetBtn();
+      chronometer.reset();
+      clearSplits();
+      break;
+    case 'btn split':
+      setSplitBtn();
+      chronometer.split();
+      printSplit();
+      break;
+  }
 });
