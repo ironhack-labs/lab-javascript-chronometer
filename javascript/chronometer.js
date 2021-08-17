63dba216cd0de0d36282161a571e1e0fa9495e8e
Scott Stossel <stoss96@gmail.com>
2021-08-17 16:57:39 -0400

    commit but no push

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,34 +1,47 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
   }
 
   start(callback) {
-    // ... your code goes here
-  }
+      this.intervalId = setInterval(() => {
+        this.currentTime++;
+        if (callback) {
+        callback();
+        }
+      }, 1000);
+      return this.intervalId;
+    }
+
 
   getMinutes() {
-    // ... your code goes here
+    return Math.floor(this.currentTime / 60);
   }
 
   getSeconds() {
-    // ... your code goes here
+    return this.currentTime % 60;
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    if (value < 10) {
+      return "0" + value;
+    }
+    return `${value}`;
   }
 
   stop() {
-    // ... your code goes here
+    clearInterval(this.intervalId);
   }
 
   reset() {
-    // ... your code goes here
+    this.currentTime = 0;
   }
 
   split() {
-    // ... your code goes here
+    const currentMinutes = this.computeTwoDigitNumber(this.getMinutes());
+    const currentSeconds = this.computeTwoDigitNumber(this.getSeconds());
+    return `${currentMinutes}:${currentSeconds}`;
   }
 }
 
