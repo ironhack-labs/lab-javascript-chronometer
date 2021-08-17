5a303715279be59850419dcf829263ca18205d60
Sara Mansori <sara.mansori.amar@gmail.com>
2021-08-17 18:45:09 +0200

    iteration 1 js done

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,34 +1,54 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
   }
 
   start(callback) {
-    // ... your code goes here
+    if (callback) {
+      this.intervalId = setInterval(() => {
+        callback;
+      }, 1000);
+    } else {
+      this.intervalId = setInterval(() => {
+        this.currentTime++;
+      }, 1000);
+    }
   }
 
   getMinutes() {
-    // ... your code goes here
+    return parseInt(this.currentTime / 60);
   }
 
   getSeconds() {
-    // ... your code goes here
+    if (this.currentTime != 0) {
+      return parseInt(this.currentTime % 60);
+    } else {
+      return 0;
+    }
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    let string = value.toString();
+    if (string.length < 2) {
+      return `0${string}`;
+    } else {
+      return string;
+    }
   }
 
   stop() {
-    // ... your code goes here
+    clearInterval(this.intervalId);
   }
 
   reset() {
-    // ... your code goes here
+    this.currentTime = 0;
   }
 
   split() {
-    // ... your code goes here
+    const minutes = this.computeTwoDigitNumber(this.getMinutes());
+    const seconds = this.computeTwoDigitNumber(this.getSeconds());
+    return `${minutes}:${seconds}`;
   }
 }
 
