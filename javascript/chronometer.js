c4c71f29706356ada48187d69c247d217ca711de
Monika Geiger <geigermonika@hotmail.hu>
2021-08-18 13:56:16 +0200

    start method done

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,14 +1,22 @@
 class Chronometer {
   constructor() {
     // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null
   }
 
   start(callback) {
     // ... your code goes here
+    if (callback) {
+      this.intervalId = setInterval(callback, 1000)
+    } else {
+      this.intervalId = setInterval(() => this.currentTime++, 1000)
+    }
   }
 
   getMinutes() {
     // ... your code goes here
+    // return Math.floor(this.currentTime / 60)
   }
 
   getSeconds() {
