4535ae856af5cbce59f7426b3d2e1a0309d32197
Jorge LRangel <jorgelrgarduno@gmail.com>
2021-09-01 08:07:52 -0500

    lab 2 semana 2 chtonometer

--- javascript/index.js
+++ javascript/index.js
@@ -1,5 +1,5 @@
 const chronometer = new Chronometer();
-
+let intervalID2=null;
 // get the buttons:
 const btnLeftElement = document.getElementById('btnLeft');
 const btnRightElement = document.getElementById('btnRight');
@@ -14,15 +14,37 @@ const milUniElement = document.getElementById('milUni');
 const splitsElement = document.getElementById('splits');
 
 function printTime() {
-  // ... your code goes here
+
+  
+ 
+    
+    printSeconds()
+    printMinutes()
+
 }
 
 function printMinutes() {
+  
   // ... your code goes here
+  let imprimirM=chronometer.computeTwoDigitNumber(chronometer.getMinutes())
+  let arrM=imprimirM.split("")
+  console.log(arrM)
+  minDecElement.innerHTML=arrM[0];
+  minUniElement.innerHTML=arrM[1];
 }
 
 function printSeconds() {
   // ... your code goes here
+  let imprimirS=chronometer.computeTwoDigitNumber(chronometer.getSeconds())
+  //console.log(imprimirS)
+  //hacemos un array con SPlit para partir el string
+ let arrS= imprimirS.split("");
+ //console.log(arrS)
+
+ secDecElement.innerHTML=arrS[0];
+ secUniElement.innerHTML=arrS[1];
+
+
 }
 
 // ==> BONUS
@@ -32,6 +54,13 @@ function printMilliseconds() {
 
 function printSplit() {
   // ... your code goes here
+  let nuevoTiempo=chronometer.split()
+  //PAra preservar el valor anterior +el nuevo y asi agrega un elemeneto LI con el tiempo 
+  splitsElement.innerHTML+=`
+  <li>${nuevoTiempo}</li>
+  `
+
+
 }
 
 function clearSplits() {
@@ -39,27 +68,77 @@ function clearSplits() {
 }
 
 function setStopBtn() {
-  // ... your code goes here
+  //Class list te trae todas las clases del objeto
+  //removemos la classe de start del boton
+ btnLeftElement.classList.remove('start');
+ //agregamos el texto STOP
+ btnLeftElement.innerHTML=`STOP`;
+ //agregamos la clase stop 
+ btnLeftElement.classList.add('stop');
 }
 
 function setSplitBtn() {
   // ... your code goes here
+  //removemos la clase reset
+  btnRightElement.classList.remove('reset');
+  //agregamos el texto split
+  btnRightElement.innerHTML='SPLIT';
+  //agregamos la clase split
+  btnRightElement.classList.add('split')
 }
 
 function setStartBtn() {
-  // ... your code goes here
+  btnLeftElement.classList.remove('stop');
+  //agregamos el texto STOP
+  btnLeftElement.innerHTML=`START`;
+  //agregamos la clase stop 
+  btnLeftElement.classList.add('start');
 }
 
 function setResetBtn() {
-  // ... your code goes here
+  btnRightElement.classList.remove('split');
+  //agregamos el texto split
+  btnRightElement.innerHTML='RESET';
+  //agregamos la clase split
+  btnRightElement.classList.add('reset')
 }
 
 // Start/Stop Button
-btnLeftElement.addEventListener('click', () => {
-  // ... your code goes here
+btnLeftElement.addEventListener('click', (event) => {
+  //al objeto que se refiere, btn left element 
+  //Nos referimos al evento que estamos seleciconando
+  if (event.target.innerText=="START"){
+    //invocamos las funciones
+    setStopBtn();
+    setSplitBtn();
+    
+    //llamamos a la clase choronometro con el methodo start
+    chronometer.start();
+    //hacemos un intervalo para que print Time se invoque cada 1 Segunndo
+    //la variable intervalID2 esta hasta arriba como variable global para poder accerder a ella
+    intervalID2=setInterval(printTime,1000)
+    //printTime();
+  } else if(event.target.innerText=="STOP"){
+    setStartBtn();
+    setResetBtn();
+    chronometer.stop();
+    //
+    clearInterval(intervalID2);
+  }
+
 });
 
 // Reset/Split Button
-btnRightElement.addEventListener('click', () => {
-  // ... your code goes here
+btnRightElement.addEventListener('click', (event) => {
+  if(event.target.innerText=="SPLIT"){
+    printSplit();
+  } else{
+    chronometer.reset()
+    //ejecutarlo una ultimavez, ya que arriba se limpia el intervalo en el stop
+    printTime()
+    //LIMPIA EL LI
+    splitsElement.innerHTML=``
+
+  }
+  
 });
