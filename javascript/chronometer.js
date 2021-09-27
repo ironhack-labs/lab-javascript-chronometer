7f4c2b7958d1d0dba1f042b98ef4d80a342af76f
jeannette.valencia <jfatima.valencia@gmail.com>
2021-09-27 20:27:12 +0200

    iteration 1.0

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,30 +1,39 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
   }
 
   start(callback) {
-    // ... your code goes here
+    this.intervalId = setInterval (()=>{
+      this.currentTime ++;
+    },1000);
   }
 
   getMinutes() {
-    // ... your code goes here
+    return (Math.floor(this.currentTime/60));
   }
 
   getSeconds() {
-    // ... your code goes here
+    return (this.currentTime%60);
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    string = value.toString();
+    if (string.length<1){
+      console.log(`0 ${string}`);
+    } else {
+      console.log(string);
+    }
   }
 
   stop() {
-    // ... your code goes here
+   clearInterval(this.intervalId); 
   }
 
   reset() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
   }
 
   split() {
