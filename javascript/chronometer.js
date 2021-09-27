810e0002f9c873a6b48e0fb2121626f089135014
Dominika Lipka <dominika.lipka96@gmail.com>
2021-09-27 18:42:02 +0200

    done

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,34 +1,49 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
   }
 
   start(callback) {
-    // ... your code goes here
+    if (callback) {
+      callback();
+    } else {
+      this.intervalId = setInterval(() => {
+      this.currentTime++;
+    }, 1000)
+    }
   }
 
   getMinutes() {
-    // ... your code goes here
+    let minutes = this.currentTime / 60;
+    return Math.floor(minutes);
   }
 
   getSeconds() {
-    // ... your code goes here
+    let seconds = this.currentTime % 60;
+    return seconds;
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    if (value < 10) {
+      return '0' + value;
+    } else {
+      return '' + value;
+    }
   }
 
   stop() {
-    // ... your code goes here
+    clearInterval(this.intervalId);
   }
 
   reset() {
-    // ... your code goes here
+    return this.currentTime = 0;
   }
 
   split() {
-    // ... your code goes here
+    let minutesClock = this.computeTwoDigitNumber(this.getMinutes());
+    let secondsClock = this.computeTwoDigitNumber(this.getSeconds());
+    return minutesClock + ':' + secondsClock;
   }
 }
 
