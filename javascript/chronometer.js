5cd39570c5718b9163821ccfd323b59f1bf2dd85
Nekruz Avgani <nekruz.avgani@gmail.com>
2021-09-28 10:54:50 +0100

    Done

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,34 +1,38 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+      this.currentTime = 0;
+      this.intervalId = null;
+
   }
 
   start(callback) {
-    // ... your code goes here
+    const intervalId = setInterval(() => {
+       this.currentTime++
+    }, 1000)
   }
 
   getMinutes() {
-    // ... your code goes here
+    return Math.floor(this.currentTime / 60)
   }
 
   getSeconds() {
-    // ... your code goes here
+    return Math.floor(this.currentTime % 60)
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    return (value.toString().length === 1) ? `0${value}` : value.toString();
   }
 
   stop() {
-    // ... your code goes here
+    clearInterval(this.intervalId)
   }
 
   reset() {
-    // ... your code goes here
+    return this.currentTime = 0;
   }
 
   split() {
-    // ... your code goes here
+    let time = `${this.minutes}:${this.seconds}`;
   }
 }
 
