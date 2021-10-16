220fe6926453d44a65f4d12b6ab55fba4ed36000
SerchPro <proyects_serch@hotmail.com>
2021-10-16 16:56:18 -0500

    lab solved

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,39 +1,69 @@
 class Chronometer {
-  constructor() {
-    // ... your code goes here
-  }
+    constructor() {
+        // ... your code goes here
+        this.currentTime = 0;
+        this.intervalId = null;
+    }
 
-  start(callback) {
-    // ... your code goes here
-  }
+    start(callback) {
+        // ... your code goes here
+        this.intervalId = setInterval(() => {
+            console.log(this.currentTime);
 
-  getMinutes() {
-    // ... your code goes here
-  }
+            this.currentTime++;
+            //this.getMinutes()
+        }, 1000);
 
-  getSeconds() {
-    // ... your code goes here
-  }
+        return this.intervalId;
+    }
 
-  computeTwoDigitNumber(value) {
-    // ... your code goes here
-  }
+    getMinutes() {
+        // ... your code goes here
+        let minutes = Number((Math.floor(this.currentTime / 60).toFixed(0)));
+        console.log(minutes);
+        return minutes;
+    }
 
-  stop() {
-    // ... your code goes here
-  }
+    getSeconds() {
+        return (this.currentTime % 60);
+        // ... your code goes here
+    }
 
-  reset() {
-    // ... your code goes here
-  }
+    computeTwoDigitNumber(value) {
+        // ... your code goes here
+        let numberStr = '';
 
-  split() {
-    // ... your code goes here
-  }
+        numberStr = value.toString();
+        if (numberStr.length < 2) {
+            numberStr = `0${numberStr}`;
+
+        }
+        return numberStr;
+    }
+
+    stop() {
+        // ... your code goes here
+        //this.currentTime = null;
+        clearInterval(this.intervalId);
+    }
+
+    reset() {
+        // ... your code goes here
+        this.currentTime = 0;
+    }
+
+    split() {
+        // ... your code goes here
+        const minutes = this.computeTwoDigitNumber(this.getMinutes());
+        const seconds = this.computeTwoDigitNumber(this.getSeconds());
+
+        return `${minutes}:${seconds}`
+
+    }
 }
 
 // The following is required to make unit tests work.
 /* Environment setup. Do not modify the below code. */
 if (typeof module !== 'undefined') {
-  module.exports = Chronometer;
+    module.exports = Chronometer;
 }
