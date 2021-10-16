f45ae1f5732513caea75a3e293bca1f1016a8ec6
Oswaldo Aviles <oswaldoavilesib[1;7rgmail.com>
2021-10-16 17:01:24 -0500

    3 iterations complete

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,18 +1,25 @@
 class Chronometer {
+  currentTime = 0; 
+  intervalId = null;
   constructor() {
-    // ... your code goes here
+
   }
 
   start(callback) {
-    // ... your code goes here
+    let intervalId = setInterval(()=>{
+      this.currentTime += 1;
+      if(callback){
+        callback()
+      } else {}
+    },1000)
   }
 
   getMinutes() {
-    // ... your code goes here
+    return Math.floor(this.currentTime/60)
   }
 
   getSeconds() {
-    // ... your code goes here
+    return this.currentTime % 60;
   }
 
   computeTwoDigitNumber(value) {
