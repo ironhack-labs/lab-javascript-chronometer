51d545c741e565316beb7af9c81cd2c59ef143e6
Natali Riera <nataliriera19@gmail.com>
2021-10-18 14:37:13 -0400

    Done

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,34 +1,55 @@
 class Chronometer {
+  currentTime = 0;
+  intervalId = null;
+
   constructor() {
     // ... your code goes here
   }
 
   start(callback) {
     // ... your code goes here
+    this.intervalId = setInterval(()=>{
+      this.currentTime ++; 
+    },1000);
   }
 
   getMinutes() {
     // ... your code goes here
+    let minutes = this.currentTime / 60;
+    return Math.floor(minutes);
   }
 
   getSeconds() {
     // ... your code goes here
+    let seconds = this.currentTime % 60;
+    return seconds;
   }
 
   computeTwoDigitNumber(value) {
     // ... your code goes here
+    if (value < 10){
+      return "0" + value;
+    }else {
+      return value.toString();
+    }
   }
 
   stop() {
     // ... your code goes here
+    clearInterval(this.intervalId);
   }
 
   reset() {
     // ... your code goes here
+    this.currentTime = 0;
   }
 
   split() {
     // ... your code goes here
+    let minutes= this.computeTwoDigitNumber(this.getMinutes());
+    let seconds= this.computeTwoDigitNumber(this.getSeconds());
+
+    return `${minutes}:${seconds}`
   }
 }
 
