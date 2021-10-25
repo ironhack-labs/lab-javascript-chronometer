cc35b64be8b6d2ffda92e51d3206723b0ad51075
Rox <you@example.com>
2021-10-25 19:41:55 +0200

    iteracion_1

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,34 +1,60 @@
 class Chronometer {
   constructor() {
     // ... your code goes here
+    this.currentTime = 0
+    this.intervalId = null
+
   }
 
   start(callback) {
-    // ... your code goes here
+
+    setTimeout(()=>{  
+      
+      console.log(this.currentTime +=1)
+    }, 1000) 
+
+//-----------------------------------
+    // // if(currentTime === 3) {
+    // //   return 3
+    // }
   }
 
+
+
+
   getMinutes() {
-    // ... your code goes here
+    
+    return Math.floor(this.currentTime / 60)
   }
 
   getSeconds() {
-    // ... your code goes here
+    
+    return this.currentTime % 60;
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    // revisar, por que?
+
+    if (value < 10) return `0${value}`;
+    return `${value}`;
   }
 
   stop() {
-    // ... your code goes here
+    clearInterval(this.intervalId)
   }
 
+
+
   reset() {
-    // ... your code goes here
+    this.currentTime=0
   }
+
 
   split() {
     // ... your code goes here
+    
+
+
   }
 }
 
