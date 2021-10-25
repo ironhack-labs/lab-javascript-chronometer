6798e67b76dbf408493129c9abb75b024f334a91
JoseLValdes <jolvaldgo@gmail.com>
2021-10-25 21:13:31 +0200

    Iteration 1 Done

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -37,11 +37,11 @@ class Chronometer {
   }
 
   reset() {
-    // ... your code goes here
+    this.currentTime = 0;
   }
 
   split() {
-    // ... your code goes here
+    return `${this.computeTwoDigitNumber(this.getMinutes())}:${this.computeTwoDigitNumber(this.getSeconds())}`
   }
 }
 
