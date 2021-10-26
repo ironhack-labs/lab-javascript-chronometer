e693c1d8d03e3c91ef23c863f58949ffa7096b4b
Lisa Medina Gutiérrez <lisa.medinag@gmail.com>
2021-10-26 17:19:33 +0200

    Iteration 1 Finished

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,34 +1,49 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
   }
 
+
+
   start(callback) {
-    // ... your code goes here
+    this.intervalId = setInterval(() => {
+        this.currentTime++;
+        if (callback){ callback};
+      }, 1000);
   }
 
   getMinutes() {
-    // ... your code goes here
+    return Math.floor(this.currentTime/60);
   }
 
   getSeconds() {
-    // ... your code goes here
+    return (this.currentTime % 60);
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    if (value > 10 ){
+      return ""+value;
+    } else {
+      return "0" +value;
+    }
   }
 
   stop() {
-    // ... your code goes here
+    clearInterval(this.intervalId);
   }
 
   reset() {
-    // ... your code goes here
+    this.currentTime = 0;
   }
 
   split() {
-    // ... your code goes here
+    let totalMin = this.getMinutes(this.currentTime);
+    let totalSec = this.getSeconds(this.currentTime);
+    totalMin = this.computeTwoDigitNumber(totalMin);
+    totalSec = this.computeTwoDigitNumber(totalSec);
+    let totalReturn = `${totalMin}:${totalSec}`
+    return totalReturn;
   }
 }
 
