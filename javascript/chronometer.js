7a115f9eb3b3a0948d4804ba97cd2e85fd5792f9
Monica Saiz <moni.sm@gmail.com>
2021-10-27 15:20:13 +0200

    chronometer exercise

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,34 +1,40 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0
+    this.intervalId = null
   }
 
   start(callback) {
-    // ... your code goes here
+    this.intervalId = setInterval(() => {
+      if (callback) callback();
+      this.currentTime++;
+    }, 1000);
   }
 
   getMinutes() {
-    // ... your code goes here
+    return parseInt(this.currentTime / 60)
   }
 
   getSeconds() {
-    // ... your code goes here
+    return this.currentTime % 60
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    return (value < 10) ? `0${value}` : `${value}` 
   }
 
   stop() {
-    // ... your code goes here
+    clearInterval(this.intervalId)
   }
 
   reset() {
-    // ... your code goes here
+    this.currentTime = 0
   }
 
   split() {
-    // ... your code goes here
+    let mins = this.computeTwoDigitNumber(this.getMinutes())
+    let secs = this.computeTwoDigitNumber(this.getSeconds())
+    return `${mins}:${secs}`
   }
 }
 
