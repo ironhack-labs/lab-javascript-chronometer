184bea73e39ab458ceaeb7d3052a520badaa9d17
Josh McBroom <mcbroomjc@gmail.com>
2021-10-29 20:09:25 -0700

    Lab Iteration 1 complete (days ago, forgot to push)

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,34 +1,41 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
   }
 
   start(callback) {
-    // ... your code goes here
+    this.intervalId = setInterval(() => {this.currentTime += 1; if (callback) callback();}, 1000)
   }
+  // console.log(this.currentTime)
 
   getMinutes() {
-    // ... your code goes here
+    return Math.floor(this.currentTime / 60);
   }
 
   getSeconds() {
-    // ... your code goes here
+    return this.currentTime % 60;
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    if (value >= 10) {
+      return `${value}`;
+    } else {
+      return `0${value}`;
+    }
   }
 
   stop() {
-    // ... your code goes here
+    // console.log(`nanana`, this.interval);
+    clearInterval(this.intervalId);
   }
 
   reset() {
-    // ... your code goes here
+    this.currentTime = 0;
   }
 
   split() {
-    // ... your code goes here
+    return this.computeTwoDigitNumber(this.getMinutes()) + ":" + this.computeTwoDigitNumber(this.getSeconds());
   }
 }
 
