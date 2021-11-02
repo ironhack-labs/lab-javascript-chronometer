f4b3fc7f582419aaaf708849062d01268498cf47
Edgar Uco <edgarin@gmail.com>
2021-11-01 21:14:13 -0500

    Lab10 done

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,34 +1,53 @@
 class Chronometer {
   constructor() {
     // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
   }
+
 
   start(callback) {
     // ... your code goes here
+    this.intervalId = setInterval(() => {
+      this.currentTime++;
+    }, 1000)
+    setTimeout(() => {
+      clearInterval(this.intervalId);
+    }, 3000);
   }
 
   getMinutes() {
     // ... your code goes here
+    return Math.floor((this.currentTime / 60));
   }
 
   getSeconds() {
     // ... your code goes here
+    return this.currentTime % 60;
   }
 
   computeTwoDigitNumber(value) {
     // ... your code goes here
+    return ('0' + value).slice(-2);
   }
 
   stop() {
     // ... your code goes here
+    return clearInterval(this.intervalId);
+    
   }
 
   reset() {
     // ... your code goes here
+    clearInterval(this.intervalId);
+    this.currentTime = 0;
   }
 
   split() {
     // ... your code goes here
+    const mm = this.computeTwoDigitNumber(this.getMinutes());
+        const ss = this.computeTwoDigitNumber(this.getSeconds());
+    return `${mm}:${ss}`;
   }
 }
 
