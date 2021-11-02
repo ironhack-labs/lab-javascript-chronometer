0fea98c8741796a66b114270ca8e3356a650060d
Miguel Garcia <gunsandgamer@hotmail.com>
2021-11-01 20:17:18 -0600

    todas las iteraciones completas

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,39 +1,59 @@
 class Chronometer {
-  constructor() {
-    // ... your code goes here
-  }
+    constructor() {
+        this.currentTime = 0;
+        this.intervalId = null;
 
-  start(callback) {
-    // ... your code goes here
-  }
+        // ... your code goes here
+    }
+    start(callback) {
+        // ... your code goes here
+        this.intervalId = setInterval(() => {
+            this.currentTime++;
+        }, 1000)
+        setTimeout(() => {
+            clearInterval(this.intervalId);
+        }, 3000);
+    }
 
-  getMinutes() {
-    // ... your code goes here
-  }
+    getMinutes() {
+        // ... your code goes here
+        return Math.floor((this.currentTime / 60));
 
-  getSeconds() {
-    // ... your code goes here
-  }
+    }
 
-  computeTwoDigitNumber(value) {
-    // ... your code goes here
-  }
+    getSeconds() {
+        // ... your code goes here
+        return this.currentTime % 60
+    }
 
-  stop() {
-    // ... your code goes here
-  }
+    computeTwoDigitNumber(value) {
+        // ... your code goes here
+        return ('0' + value).slice(-2);
 
-  reset() {
-    // ... your code goes here
-  }
+    }
+    stop() {
+        // ... your code goes here
+        clearInterval(this.intervalId);
 
-  split() {
-    // ... your code goes here
-  }
+    }
+
+    reset() {
+        // ... your code goes here
+        clearInterval(this.intervalId);
+        this.currentTime = 0
+    }
+
+    split() {
+        // ... your code goes here
+        const ss = this.computeTwoDigitNumber(this.getSeconds());
+        const mm = this.computeTwoDigitNumber(this.getMinutes());
+
+        return `${mm}:${ss}`;
+    }
 }
 
 // The following is required to make unit tests work.
 /* Environment setup. Do not modify the below code. */
 if (typeof module !== 'undefined') {
-  module.exports = Chronometer;
+    module.exports = Chronometer;
 }
