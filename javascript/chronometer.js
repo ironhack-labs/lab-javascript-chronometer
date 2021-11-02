c86e8ba553dcef1e04d3f02cda0032531d2a5019
Rodrigo Samaniego <rodrigosamaniegopiza@gmail.com>
2021-11-01 20:13:43 -0600

    solo falto el ultimo punto

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,34 +1,56 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
+    
+
   }
 
   start(callback) {
-    // ... your code goes here
+   this.intervalId = setInterval (() => {
+     
+   
+    this.currentTime ++
+    },1000)
   }
 
   getMinutes() {
-    // ... your code goes here
+   return Math.floor(this.currentTime / 60)
   }
+    
 
   getSeconds() {
-    // ... your code goes here
+    if (Math.floor(this.currentTime % 60) > 0) {
+      return this.currentTime % 60
+    } else if ( !this.currentTime){
+      return 0
+    }
   }
 
+
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    
+    if (value <= 9) {
+      return `0${value}`
+      
+    }else if ( value > 9){
+      return `${value}`
+    }
+
   }
 
   stop() {
-    // ... your code goes here
-  }
+    return clearInterval(this.intervalId)
+  
+}
 
   reset() {
-    // ... your code goes here
+    return this.currentTime = 0;
   }
 
   split() {
-    // ... your code goes here
+    return `${this.computeTwoDigitNumber(this.getMinutes())}:${this.computeTwoDigitNumber(this.getSeconds())}`
+    
   }
 }
 
