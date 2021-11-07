1d06121b3f347593cfef8a02cfdb63ca51380b66
Fer Ortega <fernando.ortega79@gmail.com>
2021-11-07 20:05:54 +0100

    lab done

--- javascript/index.js
+++ javascript/index.js
@@ -13,20 +13,30 @@ const secDecElement = document.getElementById('secDec');
 const secUniElement = document.getElementById('secUni');
 //const milDecElement = document.getElementById('milDec');
 //const milUniElement = document.getElementById('milUni');
-//const splitsElement = document.getElementById('splits');
+const splitsElement = document.getElementById('splits');
 
 function printTime() {
-  
+  setInterval(() => {
+    printMinutes()
+    printSeconds() 
+    printMilliseconds()
+  }, 10);
 }
 
 function printMinutes() {
- let seeMinutes = chronometer.getMinutes()
-  console.log(seeMinutes)
-  return seeMinutes
+  let min = chronometer.getMinutes()
+  const newmin = chronometer.computeTwoDigitNumber(min)
+  minDecElement.textContent = newmin[0]
+  minUniElement.textContent = newmin[1]
+  return min
 }
 
 function printSeconds() {
-  
+  let sec = chronometer.getSeconds()
+  const newsec = chronometer.computeTwoDigitNumber(sec)
+  secDecElement.textContent = newsec[0]
+  secUniElement.textContent = newsec[1]
+  return sec
 }
 
 // ==> BONUS
