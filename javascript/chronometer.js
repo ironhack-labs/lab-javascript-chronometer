cb94b707eb75a8e5d9e229fd93a7dc869b1b9d7f
“Michael <michaelrodiles@gmail.com>
2021-11-09 08:43:01 -0500

    review and polish

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,34 +1,65 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.currentMillis = 0;
+    this.intervalId = null;
+    this.millisIntervalId = null;
   }
 
-  start(callback) {
-    // ... your code goes here
-  }
+  start(callback, interval) {
+    if (typeof callback === 'function') {
+      setInterval(callback, interval);
+    } else {
+      this.intervalId = setInterval(() => {
+        this.currentTime++;
+      }, 1000);
 
-  getMinutes() {
-    // ... your code goes here
-  }
-
-  getSeconds() {
-    // ... your code goes here
-  }
-
-  computeTwoDigitNumber(value) {
-    // ... your code goes here
+      this.millisIntervalId = setInterval(() => {
+        this.currentMillis++;
+      }, 1);
+    }
   }
 
   stop() {
-    // ... your code goes here
+    clearInterval(this.intervalId);
+    clearInterval(this.millisIntervalId);
   }
 
   reset() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.currentMillis = 0;
+    this.intervalId = null;
+    this.millisIntervalId = null;
+  }
+
+  getMilliSecond() {
+    const value = this.currentMillis;
+    const length = ('' + value).length;
+    if (length === 1) {
+      return '0' + value;
+    } else {
+      return ('' + value).slice(-2);
+    }
+  }
+
+  getMinutes() {
+    return Math.floor(this.currentTime / 60);
+  }
+
+  getSeconds() {
+    return this.currentTime % 60;
+  }
+
+  computeTwoDigitNumber(value) {
+    const length = ('' + value).length;
+    return length === 1 ? '0' + value : '' + value;
   }
 
   split() {
-    // ... your code goes here
+    const minStr = this.computeTwoDigitNumber(this.getMinutes());
+    const secStr = this.computeTwoDigitNumber(this.getSeconds());
+    const milStr = this.computeTwoDigitNumber(this.getMilliSecond());
+    return `${minStr}:${secStr}:${millis}`;
   }
 }
 
