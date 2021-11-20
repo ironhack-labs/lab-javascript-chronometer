e7b3522e2dc038c718ebf1145a69cfe2c277d234
joaonacho <joaonacho@gmail.com>
2021-11-20 15:54:10 +0000

    at getminutes

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,23 +1,29 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    (this.currentTime = 0), (this.intervalId = null);
   }
 
   start(callback) {
-    // ... your code goes here
+    this.intervalId = setInterval(() => {
+      this.currentTime += 1;
+    }, 1000);
   }
 
   getMinutes() {
-    // ... your code goes here
+    let minutes = 0;
+    if (this.currentTime === 0) {
+      return 0;
+    } else if ((this.currentTime = 60000)) {
+      return (minutes += 1);
+    }
+    return minutes;
   }
 
   getSeconds() {
-    // ... your code goes here
+    // return this.currentTime;
   }
 
-  computeTwoDigitNumber(value) {
-    // ... your code goes here
-  }
+  computeTwoDigitNumber(value) {}
 
   stop() {
     // ... your code goes here
