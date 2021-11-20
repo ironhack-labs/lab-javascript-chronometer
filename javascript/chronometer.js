6f538b4744adff06124bdd8e74dd33922fd06ff7
Sergio Villabrille <sergiovillabrille@hotmail.com>
2021-11-20 14:57:47 +0100

    Lab done

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,34 +1,69 @@
 class Chronometer {
   constructor() {
     // ... your code goes here
+    this.currentTime = 0
+    this.intervalId = null
   }
 
   start(callback) {
     // ... your code goes here
+    this.intervalId = setInterval(() => {
+      this.currentTime++;
+    }, 1000);
+    return this.currentTime;
   }
 
   getMinutes() {
     // ... your code goes here
+
+    return Math.floor(this.currentTime/60);
   }
 
   getSeconds() {
     // ... your code goes here
+
+    return this.currentTime%60
   }
 
   computeTwoDigitNumber(value) {
     // ... your code goes here
+/*
+    if (value.toString().length > 1){
+      return value.toString()
+    } else{
+      return ('0' + value)
+    }
+*/
+
+
+
+return value > 9 ? value.toString() : '0' + value
+
+/*
+   if (value > 9){
+      return value.toString()
+    } else{
+      return ('0' + value)
+    }*/
   }
 
   stop() {
-    // ... your code goes here
+    clearInterval(this.intervalId)
   }
 
   reset() {
     // ... your code goes here
+
+    this.currentTime = 0
+
+    clearInterval(this.currentTime)
   }
 
   split() {
     // ... your code goes here
+        //let min = this.computeTwoDigitNumber(this.getMinutes(this.currentTime))
+    //let seg = this.computeTwoDigitNumber(this.getSeconds(this.currentTime))
+    return `${this.computeTwoDigitNumber(this.getMinutes(this.currentTime))}:${this.computeTwoDigitNumber(this.getSeconds(this.currentTime))}`;
   }
 }
 
