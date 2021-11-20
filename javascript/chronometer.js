8f76bba51146682f77144ec9361d0ccbc6af3aeb
Calvin T <58273749+Calvin087@users.noreply.github.com>
2021-11-20 14:28:03 +0100

    Done

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,34 +1,56 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
   }
 
-  start(callback) {
-    // ... your code goes here
+  start() {
+    this.intervalId = setInterval(() => {
+      this.currentTime++;
+    }, 1000);
   }
 
   getMinutes() {
-    // ... your code goes here
+    let minutes = this.currentTime / 60;
+    let roundDown = Math.floor(minutes);
+    let result = Number(roundDown.toFixed());
+    return result;
   }
 
   getSeconds() {
-    // ... your code goes here
+    let secondsPerMin = 60;
+    let result = this.currentTime % secondsPerMin;
+    return result;
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    if (value.toString().length === 1) {
+      return '0' + value.toString();
+    } else {
+      return value.toString();
+    }
   }
 
   stop() {
-    // ... your code goes here
+    clearInterval(this.intervalId);
   }
 
   reset() {
-    // ... your code goes here
+    this.currentTime = 0;
   }
 
   split() {
-    // ... your code goes here
+    let result = [];
+    let minutes = this.getMinutes();
+    let seconds = this.getSeconds();
+    let minsString = this.computeTwoDigitNumber(minutes);
+    let secsString = this.computeTwoDigitNumber(seconds);
+    return `${minsString}:${secsString}`;
+    // get mins
+    // mins to two digit num
+    // get seconds
+    // secs two dig num
+    // combinar con "mm":"ss"
   }
 }
 
