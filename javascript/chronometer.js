a9d23c0141fd4f16653be54b39ac7926adfe37e5
Arturo Vivar <arthas1237@hotmail.com>
2021-11-20 14:57:46 +0100

    chronodone

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,33 +1,79 @@
 class Chronometer {
   constructor() {
+   
+    this.currentTime = 0;
+    this.intervalId = null;
+  
+
     // ... your code goes here
   }
 
   start(callback) {
+
+    this.intervalId = setInterval(()=> {
+    this.currentTime++;
+    },1000);
+
+    return this.currentTime;
     // ... your code goes here
   }
 
   getMinutes() {
+
+    return Math.floor((this.currentTime/60))
     // ... your code goes here
   }
 
   getSeconds() {
+
+    return Math.floor((this.currentTime%60))
     // ... your code goes here
   }
 
   computeTwoDigitNumber(value) {
+
+  if (value > 9)
+  {return value.toString()}
+else
+
+ {   return ('0' + value)}
+
+
+ /*
+
+ if (value.toString().length > 1){
+      return value.toString()
+    } else{
+      return (‘0’ + value)
+    }
+
+ */
+
+    
+
+
+    //input.Substring(input.Length - 2, 1);
+
+
     // ... your code goes here
   }
 
   stop() {
+
+    clearInterval(this.intervalId)
     // ... your code goes here
   }
 
   reset() {
     // ... your code goes here
+    this.currentTime = 0
+
+    clearInterval(this.curren)
   }
 
   split() {
+  
+return `${this.computeTwoDigitNumber(this.getMinutes(this.currentTime))}:${this.computeTwoDigitNumber(this.getSeconds(this.currentTime))}`;
     // ... your code goes here
   }
 }
