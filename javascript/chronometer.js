ce2ff105770e8b6d9344efa01438ebf4851f52b4
Vitor Afonso <vittokm@hotmail.com>
2021-11-20 16:40:42 +0000

    Solved lab

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,34 +1,64 @@
+/*jshint esversion:8 */
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
   }
+  
 
   start(callback) {
-    // ... your code goes here
+    this.intervalId = setInterval(() => {
+      this.currentTime += 1;
+    }, 1000);
   }
 
   getMinutes() {
-    // ... your code goes here
+
+    let min = parseInt(this.currentTime / 60);
+    return min;
   }
+
+  //We're now able to get the number of minutes that have passed. But what if we wanted to get the number of seconds that have passed after the start of the current minute?
+  //The getSeconds method should return the number of seconds that have passed after the start of the current minute.
+
 
   getSeconds() {
-    // ... your code goes here
+    
+    if (this.currentTime === 0) {
+      return 0;
+    }
+    if (parseInt(this.currentTime / 60) > 0) {
+      this.currentTime = this.currentTime - (parseInt(this.currentTime / 60) * 60);
+    } 
+    
+    return this.currentTime;
   }
+
+  //The computeTwoDigitNumber method should take a number, and return a string where the number received as an argument has been padded with 0s to ensure the value is at least 2 characters long.
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
-  }
+    let str = "0";
+    
+    if (value.toString().length < 2) {
+      return str + value;
+    } else {
+      return value.toString();
+    }
 
+  }
   stop() {
-    // ... your code goes here
+    clearInterval(this.intervalId);
   }
-
+  //The split method should expect no arguments, and return a string where the time since the start is formatted as "mm:ss". Internally, the split method can make usage of previously declared methods such as getMinutes, getSeconds, and computeTwoDigitNumber.
   reset() {
-    // ... your code goes here
+    this.currentTime = 0;
   }
 
   split() {
-    // ... your code goes here
+
+    let minutes = this.computeTwoDigitNumber(this.getMinutes());
+    let seconds = this.computeTwoDigitNumber(this.getSeconds());
+    return `${minutes}:${seconds}`;
   }
 }
 
