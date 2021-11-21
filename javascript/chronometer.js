1345909eb77180940faf6e1ff28be5b1579391c0
Miguel <info.miguelbanos@gmail.com>
2021-11-21 16:39:34 +0100

    lab done

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,34 +1,50 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
   }
 
   start(callback) {
-    // ... your code goes here
+  
+    this.intervalId = setInterval(() => {
+      this.currentTime++;
+    }, 1000);
   }
+  
+  //66segundos
 
   getMinutes() {
-    // ... your code goes here
-  }
+    const minutes = Math.floor(this.currentTime / 60);
+    return minutes;
+  } 
 
   getSeconds() {
-    // ... your code goes here
+    const seconds = this.currentTime % 60;
+    if(this.currentTime == 0){
+      return 0; 
+    }
+    return seconds;
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    if(value < 10){
+      return `0${value}`
+    } else {
+      return value.toString();
+    }
+  
   }
 
   stop() {
-    // ... your code goes here
+    clearInterval(this.intervalId);
   }
 
   reset() {
-    // ... your code goes here
+    this.currentTime = 0;
   }
 
   split() {
-    // ... your code goes here
+    return `${this.computeTwoDigitNumber(this.getMinutes())}:${this.computeTwoDigitNumber(this.getSeconds())}`
   }
 }
 
@@ -37,3 +53,5 @@ class Chronometer {
 if (typeof module !== 'undefined') {
   module.exports = Chronometer;
 }
+
+
