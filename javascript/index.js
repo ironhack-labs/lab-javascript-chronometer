3c4523e8c4eeafebd4efde777671db61567b7092
Eduardo Francos Margareto <efmargareto@hotmail.com>
2021-11-24 21:57:52 +0100

    Done Chronometer

--- javascript/index.js
+++ javascript/index.js
@@ -16,22 +16,6 @@ const milUniElement = document.getElementById('milUni');
 const splitsElement = document.getElementById('splits');
 
 function printTime() {
-<<<<<<< HEAD
-  printMinutes()
-  printSeconds() 
-}
-
-function printMinutes() {
-  const minutes = chronometer.computeTwoDigitNumber(chronometer.getMinutes())
-  minDecElement.innerHTML = minutes[0]
-  minUniElement.innerHTML = minutes[1]
-}
-
-function printSeconds() {
-  const second = chronometer.computeTwoDigitNumber(chronometer.getSeconds())
-  secDecElement.innerHTML = second[0]
-  secUniElement.innerHTML = second[1]
-=======
   printMinutes();
   printSeconds();
 }
@@ -44,7 +28,6 @@ function printMinutes() {
 function printSeconds() {
   secUniElement.innerText = chronometer.computeTwoDigitNumber(chronometer.getSeconds())[1];
   secDecElement.innerText = chronometer.computeTwoDigitNumber(chronometer.getSeconds())[0];
->>>>>>> e8562ed4e8dbb27e75648255ca9f6c635efc8bf9
 }
 
 
@@ -78,39 +61,6 @@ function setResetBtn() {
 }
 
 // Start/Stop Button
-<<<<<<< HEAD
-btnLeftElement.addEventListener('click', event => {
-  const elem = event.target
-
-  if ( elem.classList.contains('stop') ) {
-    elem.innerText = 'START'
-    btnRightElement.innerText = 'RESET'
-    chronometer.stop()
-  } else {
-    elem.innerText = 'STOP';
-    btnRightElement.innerText = 'SPLIT';
-    chronometer.start(printTime)
-  }
-
-  elem.classList.toggle('start')
-  elem.classList.toggle('stop')
-
-  btnRightElement.classList.toggle('reset')
-  btnRightElement.classList.toggle('split')
-});
-
-// Reset/Split Button
-btnRightElement.addEventListener('click', event => {
-  if ( event.target.classList.contains('split') ) {
-    let newSplitTime = document.createElement('li')
-    newSplitTime.innerHTML = `${chronometer.split()}`
-    splitsElement.appendChild(newSplitTime)
-  } else {
-    chronometer.reset();
-    printTime();
-    splitsElement.innerHTML = ''
-  }
-=======
 btnLeftElement.addEventListener('click', (event) => {
   btnLeftElement.classList.toggle('stop')
   btnLeftElement.classList.toggle('start')
@@ -147,7 +97,6 @@ btnRightElement.addEventListener('click', (event) => {
     splitsElement.innerHTML = ""
   }
 
->>>>>>> e8562ed4e8dbb27e75648255ca9f6c635efc8bf9
 });
 
 
