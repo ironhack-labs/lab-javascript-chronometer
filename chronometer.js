25d8747086763828904e224e33c26c0e48ab7fb0
MatheusBenamor <91330349+MatheusBenamor@users.noreply.github.com>
2021-11-27 16:27:58 +0000

    [WDPT SAO 11.2021] Matheus Benamor

---
+++ chronometer.js
@@ -0,0 +1,48 @@
+class Chronometer {
+  constructor() {
+    this.currentTime = 0;
+    this.intervalId = null;
+    }
+
+  start(callback) {
+    this.intervalId = setInterval(() => {
+      this.currentTime += 1;
+   }, 1000)
+  }
+
+  getMinutes() {
+    let minutes = Math.floor(this.currentTime / 60);
+    return minutes
+  }
+
+  getSeconds() {
+    let seconds = Math.floor(this.currentTime % 60);
+    return seconds
+  }
+
+  computeTwoDigitNumber(value) {
+    if (value < 10) {
+return ('0' + `${value}`) 
+    } else {
+      return (`${value}`)
+    }
+  }
+
+  stop() {
+    clearInterval(this.intervalId);
+  }
+
+  reset() {
+    return this.currentTime = 0;
+  }
+
+  split() {
+    return `${this.computeTwoDigitNumber(this.getMinutes())}:${this.computeTwoDigitNumber(this.getSeconds())}`;
+  }
+}
+
+// The following is required to make unit tests work.
+/* Environment setup. Do not modify the below code. */
+if (typeof module !== 'undefined') {
+  module.exports = Chronometer;
+}
