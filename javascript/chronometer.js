8e1d6bc8b73bff261e45fdb9854a28b0d9b8af0a
Clara Villa <Clara.Villa@lego.com>
2022-01-11 18:46:38 +0000

    done js task

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,34 +1,63 @@
 class Chronometer {
   constructor() {
     // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
   }
 
   start(callback) {
     // ... your code goes here
-  }
+      this.intervalId = setInterval(()=>{ 
+        if (!callback) {
+          callback;
+        }
+        this.currentTime = this.currentTime+1 }, 1000);
+    }
+
+
+
 
   getMinutes() {
     // ... your code goes here
+    if (this.currentTime===0) {
+      return 0;
+    }
+    return Math.floor(this.currentTime/60);
   }
 
   getSeconds() {
     // ... your code goes here
+    if (this.currentTime===0) {
+      return 0;
+    }
+    return this.currentTime%60; 
   }
 
   computeTwoDigitNumber(value) {
     // ... your code goes here
+    let stringValue = value.toString();
+    if (stringValue.length===1) {
+      return "0"+stringValue;
+    }
+
+    return stringValue;
   }
 
   stop() {
     // ... your code goes here
+    clearInterval(this.intervalId);
   }
 
   reset() {
     // ... your code goes here
+    this.currentTime=0;
   }
 
   split() {
     // ... your code goes here
+    let minutes = this.computeTwoDigitNumber(this.getMinutes());
+    let seconds = this.computeTwoDigitNumber(this.getSeconds());
+    return `${minutes}:${seconds}`;
   }
 }
 
