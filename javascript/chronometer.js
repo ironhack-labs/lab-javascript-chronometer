3a4fdb0e832bc73c0da35eda86e5f2174ba5afed
Joaquin Velazquez <joaquin.ve@outlook.com>
2022-01-17 15:32:38 -0600

    iteracion 1

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -31,15 +31,16 @@ class Chronometer {
 
 
   stop() {
-    // ... your code goes here
+    clearInterval(this.intervalId)
   }
 
   reset() {
-    // ... your code goes here
+    this.currentTime = 0
   }
 
   split() {
-    // ... your code goes here
+   
+    return `${this.computeTwoDigitNumber(this.getMinutes())}:${this.computeTwoDigitNumber(this.getSeconds())}`
   }
 }
 
