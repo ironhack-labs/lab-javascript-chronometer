8f0e8c2cb8929bc5b1e8073466ccf8305654148d
Joaquin Velazquez <joaquin.ve@outlook.com>
2022-01-17 15:03:41 -0600

    iteracion 1 hasta two digit number

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,23 +1,34 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    (this.currentTime = 0);
+    (this.intervalId = null);    
   }
 
   start(callback) {
-    // ... your code goes here
+    
+    this.intervalId = setInterval(() => {
+      this.currentTime += 1
+    }, 1000);
   }
 
+
   getMinutes() {
-    // ... your code goes here
+    return Math.floor(this.currentTime / 60)
   }
 
   getSeconds() {
-    // ... your code goes here
+    return (this.currentTime % 60)
   }
 
   computeTwoDigitNumber(value) {
     // ... your code goes here
+    if (value < 10){
+      return `0${value}`;
+    }else{
+       return String(value);
+    }
   }
+
 
   stop() {
     // ... your code goes here
