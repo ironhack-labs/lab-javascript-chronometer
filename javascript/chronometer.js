50ff82771c0974b2a7dba76ca8867020f7349f68
Marten Zand-Vakili <martenzv@hotmail.de>
2022-01-17 19:39:46 +0100

    Martens work

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,34 +1,44 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
   }
 
   start(callback) {
-    // ... your code goes here
+    this.setIntervalId = setInterval(() => {
+      this.currentTime++
+
+
+    }, 1000)
   }
 
   getMinutes() {
-    // ... your code goes here
+   return Math.floor(this.currentTime / 60)
+
   }
 
   getSeconds() {
-    // ... your code goes here
+    return this.currentTime % 60;
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    if (value < 10){                // return (value < 10) ? "0" + value : value + "";
+      return "0" + value;
+    } else {
+      return value + "";
+    }
   }
 
   stop() {
-    // ... your code goes here
+    clearInterval(this.setIntervalId);
   }
 
   reset() {
-    // ... your code goes here
+    this.currentTime = 0;
   }
 
   split() {
-    // ... your code goes here
+   return this.computeTwoDigitNumber(this.getMinutes()) + ":" + this.computeTwoDigitNumber(this.getSeconds());
   }
 }
 
