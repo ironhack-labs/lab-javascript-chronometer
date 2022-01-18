1ebad41f5e52c1b077ba9b80471068a13527d444
Shirley Gianina <shirley.cd853f@gmail.com>
2022-01-19 00:33:51 +0100

    feat: Iteration 1

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,34 +1,55 @@
 class Chronometer {
   constructor() {
     // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
   }
 
   start(callback) {
     // ... your code goes here
+    this.intervalId = setInterval(() => {
+      this.currentTime += 1;
+      callback();
+    }, 1000);
   }
 
   getMinutes() {
     // ... your code goes here
+    const getMinutes = parseInt(this.currentTime / 60);
+    return getMinutes;
   }
 
   getSeconds() {
     // ... your code goes here
+    const getSeconds = parseInt(this.currentTime % 60);
+    return getSeconds;
   }
 
   computeTwoDigitNumber(value) {
     // ... your code goes here
+    if (value < 10) {
+      return `0${value}`;
+    } else {
+      return `${value}`;
+    }
   }
 
   stop() {
     // ... your code goes here
+    clearInterval(this.intervalId);
+    this.intervalId = null;
   }
 
   reset() {
     // ... your code goes here
+    this.currentTime = 0;
   }
 
   split() {
     // ... your code goes here
+    const minutes = this.computeTwoDigitNumber(this.getMinutes());
+    const seconds = this.computeTwoDigitNumber(this.getSeconds());
+    return `${minutes}:${seconds}`;
   }
 }
 
