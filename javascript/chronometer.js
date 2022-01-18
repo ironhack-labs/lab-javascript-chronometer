c45cd3ae91381014da193df3f1628c43e0c09f55
Ernesto Espinoza M <emenocal07@gmail.com>
2022-01-18 19:16:15 +0100

    chronometer

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -2,36 +2,43 @@ class Chronometer {
   constructor() {
     this.currentTime = 0
     this.intervalId = null
-
-
   }
 
   start(callback) {
-    // ... your code goes here
+    this.intervalId = setInterval(() => {
+      this.currentTime++
+    }, 1000)
   }
 
+
   getMinutes() {
-    // ... your code goes here
+    return parseInt(this.currentTime / 60)
   }
 
   getSeconds() {
-    // ... your code goes here
+    return this.currentTime % 60
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    // let padNum
+    // padNum = String(value).padStart(2, '0')
+    // return padNum
+    if (value < 9) {
+      return `0${value}`
+    } else {
+      return `${value}`
+    }
   }
-
   stop() {
-    // ... your code goes here
+    clearInterval(this.intervalId)
   }
 
   reset() {
-    // ... your code goes here
+    return this.currentTime = 0
   }
 
   split() {
-    // ... your code goes here
+    return `${this.computeTwoDigitNumber(this.getMinutes())}:${this.computeTwoDigitNumber(this.getSeconds())}`
   }
 }
 
