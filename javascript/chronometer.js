9647e91b25391f97f0e6f8c56ba9185515276893
angelita15 <95506994+angelita15@users.noreply.github.com>
2022-01-18 22:35:20 +0100

    [MAD DE] Ángela rueda

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,33 +1,52 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0
+    this.intervalId = null // ... your code goes here
   }
 
   start(callback) {
+    this.intervalId= setInterval (() => { 
+      this.currentTime++  
+    }, 1000)
     // ... your code goes here
   }
 
   getMinutes() {
+  
+   let minutes = this.currentTime / 60
+   return Math.floor (minutes)
     // ... your code goes here
   }
 
   getSeconds() {
+  let seconds = this.currentTime % 60
+  return seconds
     // ... your code goes here
   }
 
   computeTwoDigitNumber(value) {
+    if (value < 10) { 
+      return "0" + value.toString()
+    } else { 
+    return value.toString() }
     // ... your code goes here
   }
 
   stop() {
-    // ... your code goes here
+
+  clearInterval(this.intervalId)// ... your code goes here
   }
 
   reset() {
-    // ... your code goes here
+   this.currentTime = 0 // ... your code goes here
   }
 
   split() {
+
+    let minutes = this.computeTwoDigitNumber(this.getMinutes())
+    let seconds = this.computeTwoDigitNumber(this.getSeconds())
+    
+    return `${minutes}:${seconds}` 
     // ... your code goes here
   }
 }
