1fd9ba30bb90aefc30ffd7b185450d01b9957ab6
Aleksandra Wos <aleksandra1.wos@gmail.com>
2022-01-18 23:07:32 +0000

    Aleksandra lab chronometer 2-3-4

--- javascript/index.js
+++ javascript/index.js
@@ -7,22 +7,36 @@ const btnRightElement = document.getElementById('btnRight');
 // get the DOM elements that will serve us to display the time:
 const minDecElement = document.getElementById('minDec');
 const minUniElement = document.getElementById('minUni');
+
 const secDecElement = document.getElementById('secDec');
 const secUniElement = document.getElementById('secUni');
-const milDecElement = document.getElementById('milDec');
-const milUniElement = document.getElementById('milUni');
+// const milDecElement = document.getElementById('milDec');
+// const milUniElement = document.getElementById('milUni');
 const splitsElement = document.getElementById('splits');
 
 function printTime() {
-  // ... your code goes here
+  printMinutes();
+  printSeconds();
 }
 
 function printMinutes() {
   // ... your code goes here
+  minDecElement.innerText = chronometer.computeTwoDigitNumber(
+    chronometer.getMinutes()
+  )[0];
+  minUniElement.innerText = chronometer.computeTwoDigitNumber(
+    chronometer.getMinutes()
+  )[1];
 }
 
 function printSeconds() {
   // ... your code goes here
+  secDecElement.innerText = chronometer.computeTwoDigitNumber(
+    chronometer.getSeconds()
+  )[0];
+  secUniElement.innerText = chronometer.computeTwoDigitNumber(
+    chronometer.getSeconds()
+  )[1];
 }
 
 // ==> BONUS
@@ -54,12 +68,34 @@ function setResetBtn() {
   // ... your code goes here
 }
 
+let everySecond = setInterval(function () {
+  printTime();
+}, 10);
+
 // Start/Stop Button
 btnLeftElement.addEventListener('click', () => {
-  // ... your code goes here
+  if (btnLeftElement.classList.contains('start')) {
+    btnLeftElement.innerText = 'STOP';
+    btnRightElement.innerText = 'SPLIT';
+    btnLeftElement.classList.toggle('start');
+    btnLeftElement.classList.toggle('stop');
+    chronometer.start();
+  } else {
+    chronometer.stop();
+    btnLeftElement.innerText = 'START';
+    btnRightElement.innerText = 'RESET';
+    btnLeftElement.classList.toggle('stop');
+    btnLeftElement.classList.toggle('start');
+    chronometer.stop();
+  }
 });
 
 // Reset/Split Button
-btnRightElement.addEventListener('click', () => {
+
+
+ 
   // ... your code goes here
-});
+
+
+
+  
