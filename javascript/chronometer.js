875a8dc7abf767e11565d8870c934f0d9568afb9
Mario Diaz <mariodzls@gmail.com>
2022-01-18 20:52:55 +0100

    [MAD DE] Mario Díaz

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,34 +1,46 @@
 class Chronometer {
-  constructor() {
-    // ... your code goes here
-  }
+  
+  constructor() {}
 
-  start(callback) {
-    // ... your code goes here
+  currentTime = 0
+  intervalId = null
+
+  start() {
+    this.intervalId = setInterval(() => {
+      this.currentTime += 1
+    }
+    ,1000)
   }
 
   getMinutes() {
-    // ... your code goes here
+    let minutes = Math.floor(this.currentTime/60)
+    return minutes
   }
 
   getSeconds() {
-    // ... your code goes here
+    let seconds = this.currentTime % 60 
+    return seconds
   }
-
+  
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    if (value <=9) {
+      return `0${value}`
+    }
+      else {return `${value}`}
   }
 
   stop() {
-    // ... your code goes here
+    clearInterval(this.intervalId)
   }
 
   reset() {
-    // ... your code goes here
+    return this.currentTime = 0
   }
 
   split() {
-    // ... your code goes here
+    const minutesSplit = this.computeTwoDigitNumber(this.getMinutes())
+    const secondsSplit = this.computeTwoDigitNumber(this.getSeconds())
+    return `${minutesSplit}:${secondsSplit}`
   }
 }
 
