4337a8fe0e54d1f2ef90f581eb8a90e8bb958a97
Guillermo Rodríguez Calero <guillermorodriguezcalero@MacBook-Air-de-Guillermo.local>
2022-01-18 23:02:45 +0100

    solved malb

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,36 +1,71 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+
+    this.currentTime = 0
+    this.intervalId = null
+
   }
 
   start(callback) {
-    // ... your code goes here
+    this.intervalID = setInterval(() => {
+      this.currentTime++
+    }, 1000)
   }
 
+
   getMinutes() {
-    // ... your code goes here
+
+    let passedMinutes = 0
+    const minutesWithDecimals = this.currentTime / 60
+
+    passedMinutes = Math.trunc(minutesWithDecimals)
+
+    return passedMinutes
+
+
+
   }
 
   getSeconds() {
-    // ... your code goes here
+    const secondsWithDecimals = this.currentTime % 60
+    return secondsWithDecimals
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+
+    if (value < 10) {
+      return `0${value}`
+    } else {
+      return `${value}`
+    }
+
   }
 
+
   stop() {
-    // ... your code goes here
+
+    clearInterval(this.intervalID)
   }
 
   reset() {
-    // ... your code goes here
+    clearInterval(this.currentTime = 0)
+
   }
 
   split() {
-    // ... your code goes here
+
+    let minutes = this.computeTwoDigitNumber(this.getMinutes())
+    let seconds = this.computeTwoDigitNumber(this.getSeconds())
+    return `${minutes}:${seconds}`
+
   }
+
+
+
+
+
 }
+
 
 // The following is required to make unit tests work.
 /* Environment setup. Do not modify the below code. */
