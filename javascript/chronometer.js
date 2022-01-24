59a15f1862c1a0367a05be6fd2c8ef94233db08c
Vitoria Borba <vitoriaborba@icloud.com>
2022-01-24 18:28:06 +0000

    Done

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,34 +1,41 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
   }
+  start(callback) { 
+    this.intervalId = setInterval (() => {
+      if (callback) return callback;
+      this.currentTime++;
+    }, 1000);
 
-  start(callback) {
-    // ... your code goes here
   }
 
   getMinutes() {
-    // ... your code goes here
+    return Math.floor(this.currentTime/60);
   }
 
   getSeconds() {
-    // ... your code goes here
+    return this.currentTime % 60;
   }
-
+  
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    if ( value < 10) {
+      return `0${value}`;
+    };
+    return `${value}`;
   }
 
   stop() {
-    // ... your code goes here
+    clearInterval(this.intervalId);
   }
 
   reset() {
-    // ... your code goes here
+   this.currentTime = 0;
   }
 
   split() {
-    // ... your code goes here
+    return `${this.computeTwoDigitNumber(this.getMinutes(this.currentTime))}:${this.computeTwoDigitNumber(this.getSeconds(this.currentTime))}`;
   }
 }
 
