b77154805e6274c084f6aacf25916773786d5f91
aBoswell <A.B0ZZW3LL@gmail.com>
2022-03-29 08:24:26 -0400

    a few methods added still more to complete

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,18 +1,22 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
   }
 
-  start(callback) {
-    // ... your code goes here
+  start(callback) { 
+    this.intervalId = setInterval(() => {
+      this.currentTime = (this.currentTime + 1);
+    }, 1000);
   }
+  
 
   getMinutes() {
-    // ... your code goes here
+      return(Math.floor(this.currentTime / 60));  
   }
 
   getSeconds() {
-    // ... your code goes here
+    return(this.currentTime % 60);
   }
 
   computeTwoDigitNumber(value) {
@@ -20,11 +24,11 @@ class Chronometer {
   }
 
   stop() {
-    // ... your code goes here
+    clearInterval(this.intervalId);
   }
 
   reset() {
-    // ... your code goes here
+    this.currentTime = 0;
   }
 
   split() {
