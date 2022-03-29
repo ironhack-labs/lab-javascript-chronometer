19bd354cf02214871ab36114b4b31e75a7b9646f
Iñigo Malluguiza <malluguizamae@gmail.com>
2022-03-29 19:47:11 +0200

    not yet

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,34 +1,69 @@
 class Chronometer {
+    
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0
+    this.intervalId = null
   }
 
+    
   start(callback) {
-    // ... your code goes here
+    
+    this.intervalId = setInterval(() => {
+      this.currentTime++
+      }, 1000)
   }
 
   getMinutes() {
-    // ... your code goes here
+    
+    return Math.floor(this.currentTime/60)
   }
 
   getSeconds() {
-    // ... your code goes here
+    
+
+
+    return this.currentTime % 60
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    
+
+    if(value <= 9){
+
+      return '0'+value
+    }else{
+
+      return value + ''
+    }
   }
 
   stop() {
-    // ... your code goes here
+    
+    clearInterval(this.intervalId)
+
   }
 
   reset() {
-    // ... your code goes here
+    
+    return this.currentTime = 0
+
   }
 
   split() {
-    // ... your code goes here
+    
+
+    
+    
+    const minuts = this.computeTwoDigitNumber(this.getMinutes())
+
+    const seconds = this.computeTwoDigitNumber(this.getSeconds())
+
+
+    return minuts + ':' + seconds
+
+    
+
+
   }
 }
 
