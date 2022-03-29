b10b907da8ef544cfc69c14452370557d18a129b
Julian Rivera <julianarevircoins@gmail.com>
2022-03-28 20:44:17 -0400

    In progress

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,33 +1,50 @@
 class Chronometer {
   constructor() {
+    this.currentTime = 0;
+    this.intervalId = null;
     // ... your code goes here
   }
 
   start(callback) {
-    // ... your code goes here
+    const increment = setInterval(() => {
+      this.currentTime = this.currentTime + 1;
+    }, 1000);
   }
 
   getMinutes() {
+    return Math.floor(this.currentTime / 60);
     // ... your code goes here
   }
 
   getSeconds() {
+    //this.currenttime holds the seconds then we find the remainder of that by modulos 60 cause 60 is a minute so whatever is left over it returns it.
+    return this.currentTime % 60;
+
     // ... your code goes here
   }
 
   computeTwoDigitNumber(value) {
     // ... your code goes here
+    if (value < 10) {
+      return `0${value}`;
+    } else {
+      return `${value}`;
+    }
   }
 
   stop() {
+    return clearInterval(this.intervalId);
     // ... your code goes here
   }
 
   reset() {
+    this.currentTime = 0;
     // ... your code goes here
   }
 
   split() {
+    const result = increment * 1000;
+    return `${result}`;
     // ... your code goes here
   }
 }
