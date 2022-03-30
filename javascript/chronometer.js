4bb938dc16d8e5269e40ce723c0fe1312647ad6a
Alejandro Mazas de Lizana <alejandro.mazasdelizana@gmail.com>
2022-03-30 09:08:14 +0200

    done

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,34 +1,47 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0
+    this.intervalId = null
   }
 
   start(callback) {
-    // ... your code goes here
+    this.intervalId = setInterval(() => {
+      this.currentTime++
+    }, 1000)
+    console.log(this.currentTime)
   }
 
   getMinutes() {
-    // ... your code goes here
+    const minutes = Math.floor(this.currentTime / 60)
+    return minutes
   }
 
   getSeconds() {
-    // ... your code goes here
+    const seconds = this.currentTime % 60
+    return seconds
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    if (value < 10) {
+      return `0${value}`
+    } else {
+      return `${value}`
+    }
   }
 
   stop() {
-    // ... your code goes here
+    clearInterval(this.intervalId)
   }
 
   reset() {
-    // ... your code goes here
+    this.currentTime = 0
+    return this.currentTime
   }
 
   split() {
-    // ... your code goes here
+    const value1 = this.computeTwoDigitNumber(this.getMinutes(this.currentTime))
+    const value2 = this.computeTwoDigitNumber(this.getSeconds(this.currentTime))
+    return `${value1}:${value2}`
   }
 }
 
--- javascript/index.js
+++ javascript/index.js
@@ -56,7 +56,7 @@ function setResetBtn() {
 
 // Start/Stop Button
 btnLeftElement.addEventListener('click', () => {
-  // ... your code goes here
+
 });
 
 // Reset/Split Button
