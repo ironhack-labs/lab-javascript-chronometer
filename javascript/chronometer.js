77f574176478d9194df890a30f71ea67ccbda819
Andrew Lamagna <andrew.lamagna@LEGO.com>
2022-03-29 23:18:12 -0400

    Completed w bonuses

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,36 +1,51 @@
 class Chronometer {
+
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
   }
 
+  //typeof callback === 'function' ? callback() : 0
   start(callback) {
-    // ... your code goes here
+    this.intervalId = setInterval(() => {
+      callback ? callback() : 0
+      this.currentTime++
+    }, 10);
   }
 
   getMinutes() {
-    // ... your code goes here
+    return Math.floor((this.currentTime/100)/60)
   }
 
   getSeconds() {
-    // ... your code goes here
+    return Math.floor((this.currentTime/100)%60)
+  }
+
+  getMilliseconds() {
+    // return(this.currentTime*10);
+    // console.log(this.currentTime);
+    // console.log(this.computeTwoDigitNumber(this.currentTime%100));
+    return (this.currentTime%100);
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    return ( value < 10 ? "0" + value : value.toString() );
   }
 
   stop() {
-    // ... your code goes here
+    clearInterval(this.intervalId);
   }
 
   reset() {
-    // ... your code goes here
+    this.currentTime = 0;
   }
 
   split() {
-    // ... your code goes here
+    // return (this.computeTwoDigitNumber(this.getMinutes())+":"+this.computeTwoDigitNumber(this.getSeconds()));
+    return `${this.computeTwoDigitNumber(this.getMinutes())}:${this.computeTwoDigitNumber(this.getSeconds())}:${this.computeTwoDigitNumber(this.getMilliseconds())}`;
   }
 }
+
 
 // The following is required to make unit tests work.
 /* Environment setup. Do not modify the below code. */
--- javascript/index.js
+++ javascript/index.js
@@ -1,3 +1,5 @@
+// const Chronometer = require("./chronometer");
+
 const chronometer = new Chronometer();
 
 // get the buttons:
@@ -14,28 +16,41 @@ const milUniElement = document.getElementById('milUni');
 const splitsElement = document.getElementById('splits');
 
 function printTime() {
-  // ... your code goes here
+  let seconds      = chronometer.computeTwoDigitNumber(chronometer.getSeconds());
+  let minutes      = chronometer.computeTwoDigitNumber(chronometer.getMinutes());
+  let milliseconds = chronometer.computeTwoDigitNumber(chronometer.getMilliseconds());
+  printMinutes(minutes);
+  printSeconds(seconds);
+  printMilliseconds(milliseconds);
 }
 
-function printMinutes() {
-  // ... your code goes here
+function printMinutes(minutes) {
+  minDecElement.innerHTML = minutes[0];
+  minUniElement.innerHTML = minutes[1];
 }
 
-function printSeconds() {
-  // ... your code goes here
+function printSeconds(seconds) {
+  secDecElement.innerHTML = seconds[0];
+  secUniElement.innerHTML = seconds[1];
 }
 
 // ==> BONUS
-function printMilliseconds() {
-  // ... your code goes here
+function printMilliseconds(milliseconds) {
+  milDecElement.innerHTML = milliseconds[0];
+  milUniElement.innerHTML = milliseconds[1];
 }
 
 function printSplit() {
-  // ... your code goes here
+  let splitOL  = document.querySelector('ol');
+  let splitVal = chronometer.split();
+  let newSplit = document.createElement('li');
+  newSplit.appendChild(document.createTextNode(splitVal));
+  splitOL.appendChild(newSplit);
 }
 
 function clearSplits() {
   // ... your code goes here
+  document.querySelector('ol').innerHTML = '';
 }
 
 function setStopBtn() {
@@ -56,10 +71,48 @@ function setResetBtn() {
 
 // Start/Stop Button
 btnLeftElement.addEventListener('click', () => {
-  // ... your code goes here
+  // btnLeftElement.className="btn stop";
+  let action = btnLeftElement.innerHTML;
+  switch (action) {
+      case 'START':
+        chronometer.start(printTime);
+        btnLeftElement.innerHTML = "STOP";
+        btnLeftElement.classList.toggle('stop')
+        //btnLeftElement.setAttribute("class","btn stop");
+        btnRightElement.innerHTML = "SPLIT";
+        btnRightElement.classList.toggle('split');
+        // btnRightElement.setAttribute("class","btn split");
+        break;
+    case 'STOP':
+        chronometer.stop();
+        btnLeftElement.innerHTML = "START";
+        btnLeftElement.setAttribute("class","btn start");
+        btnRightElement.innerHTML = "RESET";
+        btnRightElement.setAttribute("class","btn reset");
+        break;
+    default:
+        throw new Error('Unrecognized action');
+  }
 });
 
 // Reset/Split Button
-btnRightElement.addEventListener('click', () => {
-  // ... your code goes here
+btnRightElement.addEventListener('click', (e) => {
+  // console.log(e);
+  let action = btnRightElement.innerHTML;
+  switch (action) {
+    case 'SPLIT':
+        printSplit();
+        break;
+    case 'RESET':
+        clearSplits();
+        printSeconds('00');
+        printMinutes('00');
+        printMilliseconds('00');
+        chronometer.reset();
+        break;
+    default:
+        throw new Error('Unrecognized action');
+        break;
+  }
+
 });
