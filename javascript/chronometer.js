35ee2e0d423bef39997df86981a887e3fb4bc339
Josipa314 <101945658+josipa314@users.noreply.github.com>
2022-04-04 22:50:44 +0200

    lab done

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,34 +1,64 @@
 class Chronometer {
   constructor() {
     // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId= null;
   }
 
   start(callback) {
     // ... your code goes here
+   
+    this.intervalId = setInterval(() => {
+      this.currentTime+=1;
+    }, 1000);
   }
 
-  getMinutes() {
-    // ... your code goes here
+  getMinutes(){
+        // ... your code goes here
+    if (this.currentTime==0) {
+        return 0;
+    }
+    else {
+      return this.currentTime=Math.floor(this.currentTime/60);
+    }
   }
 
   getSeconds() {
     // ... your code goes here
+    if (this.currentTime==0) {
+      return 0;
+    }
+    else {
+      return this.currentTime = this.currentTime%60;
+    }
+
   }
 
   computeTwoDigitNumber(value) {
     // ... your code goes here
+    if (value<10) {   
+        return 0 + `${value}`;
+    }
+    else {
+      return `${value}`;
+    }
+    
   }
 
   stop() {
     // ... your code goes here
+      clearInterval(this.intervalId);
+      this.intervalId = null;
   }
 
   reset() {
     // ... your code goes here
+    this.currentTime = 0;
   }
 
   split() {
     // ... your code goes here
+    return this.currentTime=computeTwoDigitNumber(this.currentTime.getMinutes()) + ':' + computeTwoDigitNumber(this.currentTime.getSeconds());
   }
 }
 
