26fa8e02f6f89dd779c1c783d25331527a155c56
Mafalda Oliveira <mafalda.oliveira.21@icloud.com>
2022-04-04 17:44:30 +0100

    solve some iterations

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,34 +1,57 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
   }
 
   start(callback) {
-    // ... your code goes here
+    this.intervalId = setInterval(() => {
+      if(callback) {
+        callback();
+      }
+      this.currentTime++
+    }, 1000)
   }
 
   getMinutes() {
-    // ... your code goes here
+    let minutes = (this.currentTime)/60;
+    minutes = Math.floor(minutes);
+    return minutes;
   }
 
   getSeconds() {
-    // ... your code goes here
+    let seconds = (this.currentTime);
+    seconds = Math.floor(seconds);
+    if (this.currentTime===0) {
+      return 0
+    } else if (this.currentTime>0) {
+      return seconds;
+    } else if (this.minutes >0) {
+      return seconds - (this.minutes*60)
+    }
+  
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+      if(value<=9) {
+        return "0" + value
+      } else if (value>9) {
+      return `${value}`
+    }
   }
 
   stop() {
-    // ... your code goes here
+    clearInterval (this.intervalId)
   }
 
   reset() {
-    // ... your code goes here
+    this.currentTime= 0 
   }
 
   split() {
-    // ... your code goes here
+    let minutes= this.computeTwoDigitNumber(this.getMinutes())
+    let seconds= this.computeTwoDigitNumber(this.getSeconds())
+    return `${minutes}:${seconds}`;
   }
 }
 
