c761750ee56fc8429bc771e03b3ff3633b5d060c
Thibaut Lartigue <lartiguethibaut@gmail.com>
2022-04-04 18:54:45 +0200

    Iteration one is done

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,36 +1,59 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
   }
 
   start(callback) {
-    // ... your code goes here
+   this.intervalId = setInterval(() => {
+     if(callback){
+       callback();
+     }
+     this.currentTime++
+   }, 1000)
   }
 
   getMinutes() {
-    // ... your code goes here
+    let minutes = this.currentTime/60;
+    minutes = Math.floor(minutes)
+   return minutes
+    
   }
 
   getSeconds() {
-    // ... your code goes here
+    if(this.currentTime === 0){
+      return 0;
+    }
+    let seconds = this.currentTime % 60;
+      return seconds;
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    if(value <= 9){
+      return `0${value}`
+    } else {
+      return `${value}`;
+    }
   }
 
   stop() {
-    // ... your code goes here
+    clearInterval(this.intervalId);
   }
 
   reset() {
-    // ... your code goes here
+    this.currentTime = 0;
   }
 
   split() {
-    // ... your code goes here
+  return `${this.computeTwoDigitNumber(this.getMinutes())}:${this.computeTwoDigitNumber(this.getSeconds())}`
   }
 }
+
+
+
+
+
+
 
 // The following is required to make unit tests work.
 /* Environment setup. Do not modify the below code. */
