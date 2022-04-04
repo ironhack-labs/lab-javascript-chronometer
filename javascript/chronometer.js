8679940b6d1c29e531943c1c95fb234a54758b21
Marija Strahinjic <marija.strahinjic@hotmail.com>
2022-04-04 19:16:09 +0200

    first iteration

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,34 +1,50 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
   }
 
   start(callback) {
-    // ... your code goes here
+    this.intervalId = setInterval(() => {
+      if (callback !== undefined) {
+        callback();
+      }
+      this.currentTime++;
+    }, 1000);
   }
 
-  getMinutes() {
-    // ... your code goes here
+  getMinutes() { 
+    return Math.round(this.currentTime / 60);
   }
 
   getSeconds() {
-    // ... your code goes here
+    if (this.currentTime <=59) {
+      return this.currentTime;
+    } else if (this.currentTime % 60 === 0) {
+      return this.currentTime - (this.currentTime % 60 === 0);
+    }
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    if (value < 10 && value > 0) {
+      return '0'+ value;
+    } else if (value === 0) {
+      return '00';
+    } else if (value >= 10 && value < 60) {
+      return `${value}`;
+    }
   }
 
   stop() {
-    // ... your code goes here
+    clearInterval(this.intervalId);
   }
 
   reset() {
-    // ... your code goes here
+    return this.currentTime = 0;
   }
 
   split() {
-    // ... your code goes here
+    return value;
   }
 }
 
