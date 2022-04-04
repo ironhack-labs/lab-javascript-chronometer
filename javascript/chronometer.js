9bfe15c1c542fbdce7e7b0ad1c42b7a528bc7178
hmarchbank <93050431+hmarchbank@users.noreply.github.com>
2022-04-04 17:58:38 +0200

    passed all test

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,33 +1,52 @@
 class Chronometer {
   constructor() {
+    this.currentTime = 0
+    this.intervalId = null
     // ... your code goes here
   }
 
   start(callback) {
+    this.intervalId = setInterval( () => {
+      this.currentTime ++
+      if(callback){
+        callback()
+      }
+    }, 1000)
     // ... your code goes here
   }
 
   getMinutes() {
+    return Math.floor(this.currentTime/60)
     // ... your code goes here
   }
 
   getSeconds() {
+    return this.currentTime % 60
     // ... your code goes here
   }
 
   computeTwoDigitNumber(value) {
+    let num = value.toString()
+    if (num.length === 2){
+      return num
+    } else {
+      return `0${num}`
+    }
     // ... your code goes here
   }
 
   stop() {
+    clearInterval(this.intervalId)
     // ... your code goes here
   }
 
   reset() {
+    this.currentTime = 0
     // ... your code goes here
   }
 
   split() {
+    return `${this.computeTwoDigitNumber(this.getMinutes())}:${this.computeTwoDigitNumber(this.getSeconds())}`
     // ... your code goes here
   }
 }
