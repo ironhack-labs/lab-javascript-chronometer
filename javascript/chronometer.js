dd4bfdae39b94883720f8a6cc2b759ecd73d9df0
hmarchbank <93050431+hmarchbank@users.noreply.github.com>
2022-04-04 18:03:37 +0200

    made one change to start method.

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -2,27 +2,23 @@ class Chronometer {
   constructor() {
     this.currentTime = 0
     this.intervalId = null
-    // ... your code goes here
   }
 
   start(callback) {
     this.intervalId = setInterval( () => {
-      this.currentTime ++
       if(callback){
         callback()
       }
+      this.currentTime ++
     }, 1000)
-    // ... your code goes here
   }
 
   getMinutes() {
     return Math.floor(this.currentTime/60)
-    // ... your code goes here
   }
 
   getSeconds() {
     return this.currentTime % 60
-    // ... your code goes here
   }
 
   computeTwoDigitNumber(value) {
@@ -32,22 +28,18 @@ class Chronometer {
     } else {
       return `0${num}`
     }
-    // ... your code goes here
   }
 
   stop() {
     clearInterval(this.intervalId)
-    // ... your code goes here
   }
 
   reset() {
     this.currentTime = 0
-    // ... your code goes here
   }
 
   split() {
     return `${this.computeTwoDigitNumber(this.getMinutes())}:${this.computeTwoDigitNumber(this.getSeconds())}`
-    // ... your code goes here
   }
 }
 
