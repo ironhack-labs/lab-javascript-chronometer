d76df43b507531d19214f15def1c8fcf424d4fdb
“Sina <johndoe@example.com>
2022-04-05 02:16:32 +0200

    tough iteration as need more time with lesson

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,7 +1,33 @@
 class Chronometer {
   constructor() {
     // ... your code goes here
+    currentTime = 0;
+    intervalId  = '';
+    
+    start(stop){
+      
+        console.log(this.currentTime);
+        let i=1;
+        this.intervalId = setInterval(function () {
+        console.log(i);
+          this.currentTime= this.currentTime +i;
+        i++;
+  
+        }, 1000);
+    
+    }
+     stop(){
+       clearInterval(this.intervalId);
+       this.intervalId='';
+    }
   }
+}
+  
+  
+  const chn= new Chronometer();
+  //console.log(chn.currentTime);
+  chn.start();
+  chn.stop();
 
   start(callback) {
     // ... your code goes here
