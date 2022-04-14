76f59b6e1ecda4ae2cfc8bc5399a364064969a49
Emmanuel Alejandro Borrayo Cortes <alexborrayo21@hotmail.com>
2022-04-14 13:46:33 -0500

    done

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,34 +1,65 @@
 class Chronometer {
   constructor() {
     // ... your code goes here
+    this.currentTime = 0 
+    this.currentTimeMili =
+    this.intervalId = null
+    this.intervalmili = null
   }
 
-  start(callback) {
+  start() {
     // ... your code goes here
+    const Guardar =()=>{
+      this.currentTime +=1
+    }
+    this.intervalId = setInterval(Guardar,1000)
+    
+  }
+  Empezar(){
+    this.intervalId = setInterval(this.currentTime+=1,1000)
+    
   }
 
   getMinutes() {
     // ... your code goes here
+    return Math.floor(this.currentTime/60)
   }
 
   getSeconds() {
     // ... your code goes here
+    return this.currentTime%60
   }
+  
 
   computeTwoDigitNumber(value) {
     // ... your code goes here
+    const Numero = value.toString()
+
+    if(Numero.length === 1 ){  
+      return `0${Numero}`
+    } else {
+      return Numero
+    }
   }
 
   stop() {
     // ... your code goes here
+    clearInterval(this.intervalId)
+  }
+  parar(){
+    clearInterval(this.intervalId)
   }
 
   reset() {
     // ... your code goes here
+    this.currentTime = 0 
   }
 
   split() {
     // ... your code goes here
+    const minutos = this.computeTwoDigitNumber(this.getMinutes())
+    const seconds = this.computeTwoDigitNumber(this.getSeconds())
+    return `${minutos}:${seconds}`
   }
 }
 
--- javascript/index.js
+++ javascript/index.js
@@ -1,4 +1,12 @@
 const chronometer = new Chronometer();
+let Splits = []
+let MotorSegundos;
+let MotorMinutos;
+let setMilisegundos;
+let ImprimirMilisegundos;
+let mili=0
+
+
 
 // get the buttons:
 const btnLeftElement = document.getElementById('btnLeft');
@@ -15,51 +23,149 @@ const splitsElement = document.getElementById('splits');
 
 function printTime() {
   // ... your code goes here
+  while(splitsElement.firstChild){
+    splitsElement.removeChild(splitsElement.firstChild)
+  }
+  Splits.forEach(split=>{
+    const NuevoElemento  = document.createElement("li")
+    NuevoElemento.textContent = split 
+    splitsElement.appendChild(NuevoElemento)
+  })
 }
 
 function printMinutes() {
   // ... your code goes here
+  const Numero1 = chronometer.computeTwoDigitNumber(chronometer.getMinutes())
+console.log(Numero1)
+minDecElement.textContent = Numero1[0]
+minUniElement.textContent = Numero1[1]
 }
 
 function printSeconds() {
   // ... your code goes here
+  chronometer.Empezar()
+  const Numero1 = chronometer.computeTwoDigitNumber(chronometer.getSeconds())
+  secDecElement.textContent = Numero1[0]
+  secUniElement.textContent = Numero1[1]
 }
 
 // ==> BONUS
 function printMilliseconds() {
   // ... your code goes here
+  const SumarMili =()=>{
+    mili +=1
+    if(mili ===100){
+      mili = 0
+    }
+    if(mili.toString().length === 1){
+      ImprimirMilisegundos=`0${mili}`
+    }else{
+      ImprimirMilisegundos=mili.toString()
+    }
+
+    milDecElement.textContent = ImprimirMilisegundos[0]
+    milUniElement.textContent = ImprimirMilisegundos[1]
+  }
+
+
+
+  setMilisegundos = setInterval(SumarMili,10)
+  
+  
 }
 
-function printSplit() {
+function printSplit(split) {
   // ... your code goes here
+
+  Splits.push(split)
+  console.log(Splits)
+  printTime()
+
+  
 }
 
 function clearSplits() {
   // ... your code goes here
+  Splits = []
 }
 
 function setStopBtn() {
   // ... your code goes here
+  if(btnLeftElement.classList.contains("start")){
+    MotorSegundos = setInterval(printSeconds,1000)
+    MotorMinutos = setInterval(printMinutes,1000)
+    btnLeftElement.classList.remove("start")
+    btnLeftElement.classList.add("stop")
+    btnLeftElement.textContent = "STOP"
+    setSplitBtn()
+    printMilliseconds()
+
+  } else{
+    setStartBtn()
+  }
 }
 
 function setSplitBtn() {
   // ... your code goes here
+  if(btnLeftElement.classList.contains("stop")){
+    btnRightElement.classList.remove("reset")
+    btnRightElement.classList.add("split")
+    btnRightElement.textContent = "SPLIT"
+  } else if ( btnRightElement.classList.contains("split")) {
+    btnRightElement.classList.remove("split")
+    btnRightElement.classList.add("reset")
+    btnRightElement.textContent = "RESET"
+  }
 }
 
 function setStartBtn() {
   // ... your code goes here
+  if(btnLeftElement.classList.contains("stop")){
+    btnLeftElement.classList.remove("stop")
+    clearInterval(MotorSegundos)
+    clearInterval(MotorMinutos)
+    clearInterval(setMilisegundos)
+    chronometer.stop()
+    btnLeftElement.classList.add("start")
+     btnLeftElement.textContent = "START"
+     setSplitBtn()
+  }
+  
+
 }
 
 function setResetBtn() {
   // ... your code goes here
+  chronometer.reset()
+  secDecElement.textContent = "0"
+  secUniElement.textContent = "0"
+  minDecElement.textContent = "0"
+  minUniElement.textContent = "0"
+  milDecElement.textContent = "0"
+  milUniElement.textContent = "0"
+
 }
 
 // Start/Stop Button
 btnLeftElement.addEventListener('click', () => {
   // ... your code goes here
+  setStopBtn()
+
+
+ 
 });
 
 // Reset/Split Button
 btnRightElement.addEventListener('click', () => {
   // ... your code goes here
+  if(btnRightElement.classList.contains("split")){
+    
+    console.log(chronometer.split())
+    const Split = `${chronometer.split()}:${ImprimirMilisegundos}`
+    printSplit(Split)
+  }
+  if(btnRightElement.classList.contains("reset")){
+    setResetBtn()
+  }
+
 });
