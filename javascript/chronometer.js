a51f010296e5022430000dc409a6bc1e7a8362c6
Anna Egger <anna.3gg3r@gmail.comgit usernamegit config --global user.email anna.3gg3r@gmail.com>
2022-04-18 15:50:44 +0100

    solves iteration 1

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,34 +1,48 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
   }
+  
 
   start(callback) {
-    // ... your code goes here
+    this.intervalID = setInterval(() => this.currentTime++, 1000)
+    if (callback) {
+      callback();
+      }
   }
 
   getMinutes() {
-    // ... your code goes here
+   return Math.floor(this.currentTime /60)
   }
 
   getSeconds() {
-    // ... your code goes here
+    return this.currentTime % 60;
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+
+    if (value < 10) {
+      return `0${value}`
+    } else {
+      return value.toString()
+    }
   }
 
   stop() {
-    // ... your code goes here
+  clearInterval(this.intervalID)
   }
 
   reset() {
-    // ... your code goes here
+    this.currentTime = 0;
   }
 
   split() {
-    // ... your code goes here
+
+    let minutes = this.getMinutes(this.currentTime);
+    let seconds = this.getSeconds(this.currentTime);
+
+ return `${this.computeTwoDigitNumber(minutes)}:${this.computeTwoDigitNumber(seconds)}`
   }
 }
 
