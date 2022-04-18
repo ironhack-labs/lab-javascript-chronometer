dc411211eb5b09ffdfe7f3b4e6b3c632193b3f1a
keven andrade <keven111299@gmail.com>
2022-04-18 15:59:47 +0100

    done

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,39 +1,49 @@
 class Chronometer {
-  constructor() {
-    // ... your code goes here
-  }
+    constructor() {
+        this.currentTime = 0;
+        this.intervalId = null;
+    }
 
-  start(callback) {
-    // ... your code goes here
-  }
+    start(callback) {
+        this.intervalId = setInterval(() => {
+            this.currentTime++;
+        }, 1000);
+    }
 
-  getMinutes() {
-    // ... your code goes here
-  }
+    getMinutes() {
+        return Math.floor(this.currentTime / 60);
+    }
 
-  getSeconds() {
-    // ... your code goes here
-  }
+    getSeconds() {
+        return Math.floor(this.currentTime % 60);
+    }
 
-  computeTwoDigitNumber(value) {
-    // ... your code goes here
-  }
+    computeTwoDigitNumber(value) {
+        if (value < 10) {
+            return `0${value}`;
+        } else {
+            return `${value}`;
+        }
+    }
+    stop() {
+        clearInterval(this.intervalId);
+    }
 
-  stop() {
-    // ... your code goes here
-  }
+    reset() {
+        this.currentTime = 0;
+    }
 
-  reset() {
-    // ... your code goes here
-  }
-
-  split() {
-    // ... your code goes here
-  }
+    split() {
+        let min = this.getMinutes();
+        let sec = this.getSeconds();
+        return `${this.computeTwoDigitNumber(min)}:${this.computeTwoDigitNumber(
+      sec
+    )}`;
+    }
 }
 
 // The following is required to make unit tests work.
 /* Environment setup. Do not modify the below code. */
 if (typeof module !== 'undefined') {
-  module.exports = Chronometer;
+    module.exports = Chronometer;
 }
