e3967f50d44b5ffea98c89ae4b3cac51c0cc7624
Melissa Osorio Tavera <melissa.osorio.tavera@gmail.com>
2022-04-20 09:59:29 -0500

    done

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,34 +1,60 @@
 class Chronometer {
   constructor() {
     // ... your code goes here
+    this.currentTime=0;
+    this.intervalId=null;
   }
 
   start(callback) {
     // ... your code goes here
-  }
+    this.intervalId = setInterval(() => {
+      if (callback) {
+        callback()
+      }
+      this.currentTime++
+      }, 1000)
+      }
 
   getMinutes() {
     // ... your code goes here
+    return Math.floor(this.currentTime/60);
+
+
   }
 
   getSeconds() {
     // ... your code goes here
+    return this.currentTime%60;
   }
 
   computeTwoDigitNumber(value) {
     // ... your code goes here
+    if (value<=9){
+      return `0${value}`
+    }else if(value>=10){
+      return `${value}`
+    }
+
   }
 
   stop() {
     // ... your code goes here
+    clearInterval(this.intervalId)
+
   }
 
   reset() {
     // ... your code goes here
+    if(this.intervalId===null){
+      this.currentTime=0;
+    }
   }
 
   split() {
     // ... your code goes here
+    let minutes = this.computeTwoDigitNumber(this.getMinutes());
+    let seconds = this.computeTwoDigitNumber(this.getSeconds());
+    return `${minutes}:${seconds}`;
   }
 }
 
