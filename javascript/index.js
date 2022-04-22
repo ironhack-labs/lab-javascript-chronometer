27dae5d29689f0e0e90942b067eec980a2d4b5cd
Georgina Aceves <aceves.georgina@hotmail.com>
2022-04-23 00:47:23 +0200

    solved except all the bonus

--- javascript/index.js
+++ javascript/index.js
@@ -14,15 +14,27 @@ const milUniElement = document.getElementById('milUni');
 const splitsElement = document.getElementById('splits');
 
 function printTime() {
-  // ... your code goes here
+  printSeconds();
+  printMinutes();
+  printMilliseconds();
 }
 
 function printMinutes() {
-  // ... your code goes here
+  currentTime = chronometer.currentTime;
+  minutes = chronometer.getMinutes(currentTime);
+  secDigits = chronometer.computeTwoDigitNumber(minutes)
+
+  minDecElement.innerHTML = secDigits[0];
+  minUniElement.innerHTML = secDigits[1];
 }
 
 function printSeconds() {
-  // ... your code goes here
+  currentTime = chronometer.currentTime;
+  seconds = chronometer.getSeconds(currentTime);
+  secDigits = chronometer.computeTwoDigitNumber(seconds)
+
+  secDecElement.innerHTML = secDigits[0];
+  secUniElement.innerHTML = secDigits[1];
 }
 
 // ==> BONUS
@@ -56,7 +68,25 @@ function setResetBtn() {
 
 // Start/Stop Button
 btnLeftElement.addEventListener('click', () => {
-  // ... your code goes here
+  if (btnLeftElement.innerHTML === "START") {
+    btnLeftElement.innerHTML = "STOP";
+    btnLeftElement.classList.toggle("start");
+    btnLeftElement.classList.toggle("stop");
+    btnRightElement.innerHTML = "SPLIT"
+    btnRightElement.classList.toggle("reset");
+    btnRightElement.classList.toggle("split");
+    btnLeftElement.addEventListener('click', chronometer.start(printTime));
+
+
+  } else if (btnLeftElement.innerHTML === "STOP") {
+    btnLeftElement.innerHTML = "START";
+    btnLeftElement.classList.toggle("stop");
+    btnLeftElement.classList.toggle("start");
+    btnRightElement.innerHTML = "RESET"
+    btnRightElement.classList.toggle("split");
+    btnRightElement.classList.toggle("reset");
+    btnLeftElement.addEventListener("click", chronometer.stop());
+  };
 });
 
 // Reset/Split Button
