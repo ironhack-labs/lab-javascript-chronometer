f8487672136cf15583771bda4d855c23d7d5529d
CheloCr <xchell_cruz@hotmail.com>
2022-04-25 18:47:50 -0500

    solved

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,34 +1,43 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0,
+    this.intervalId = null
   }
 
   start(callback) {
-    // ... your code goes here
+    this.intervalId= setInterval(() => { // Se llama a la propiedad intervalId y se le da la indicacion de que iniciara un intervalo de tiempo a donde se le sumara 1 segudno a currentTime
+      this.currentTime++
+    }, 1000); //----> Cada segundo sumara 1 
   }
 
   getMinutes() {
-    // ... your code goes here
+    return Math.floor(this.currentTime / 60); // --> Devuelve los minutos convirtindolos a entero.
   }
 
   getSeconds() {
-    // ... your code goes here
+    return Math.floor(this.currentTime % 60);//---> devuelve los segundos.
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    if (value < 10) {
+      return `0${value}`
+    } else {
+      return `${value}`;
+    }
   }
 
   stop() {
-    // ... your code goes here
+    clearInterval(this.intervalId)
   }
 
   reset() {
-    // ... your code goes here
+    this.currentTime = 0
   }
 
   split() {
-    // ... your code goes here
+    let minutes = this.computeTwoDigitNumber(this.getMinutes());
+    let seconds = this.computeTwoDigitNumber(this.getSeconds());
+    return `${minutes}:${seconds}`;
   }
 }
 
