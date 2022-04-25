68821a23e69b7cd4c19645c037236ef57d72bd81
Azdhy Galvan <azdhy16@gmail.com>
2022-04-25 16:13:35 -0500

    WDPTMEX-[Azdhy]

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,34 +1,67 @@
+
+
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null; 
   }
 
   start(callback) {
-    // ... your code goes here
+
+    this.intervalId=setInterval(()=>{
+      (this.currentTime +=1)
+     },1000)
+
+   
   }
 
   getMinutes() {
-    // ... your code goes here
+    let minutos = 0;
+
+    minutos = this.currentTime / 60;
+    minutos = Math.floor(minutos);
+    minutos = minutos.toFixed(0);
+ 
+    return Number(minutos)
+   
   }
 
   getSeconds() {
     // ... your code goes here
+    let totalSeconds=this.getMinutes()*60;
+
+    let portSeconds= this.currentTime-totalSeconds
+
+    return portSeconds
+
   }
 
   computeTwoDigitNumber(value) {
     // ... your code goes here
+    var croSegunds= value;
+
+    if (croSegunds < 10){
+    return (croSegunds = '0' + croSegunds.toFixed(0))
+  }
+  else if (croSegunds >= 10){
+    return(croSegunds= croSegunds.toFixed(0))
+  }
   }
 
   stop() {
-    // ... your code goes here
+    let interval =this.intervalId;
+    return clearInterval(interval)
   }
 
   reset() {
-    // ... your code goes here
+    this.currentTime=0
   }
 
   split() {
-    // ... your code goes here
+    let tSeconds= this.computeTwoDigitNumber(this.getSeconds());
+    let tMinutes=this.computeTwoDigitNumber(this.getMinutes());
+  
+    return tMinutes + ':' + tSeconds;
   }
 }
 
