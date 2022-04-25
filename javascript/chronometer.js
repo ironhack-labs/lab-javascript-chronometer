e6d03884d7b8ed966114cc47ca9001d929a4c3a6
“Mapy <mapy.villalobos@gmail.com>
2022-04-25 16:48:29 -0500

    done

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,34 +1,41 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
   }
 
   start(callback) {
-    // ... your code goes here
+    this.intervalId = setInterval(() => {
+      this.currentTime ++ 
+      if (callback !== undefined) callback();
+    }, 1000);
   }
 
   getMinutes() {
-    // ... your code goes here
+    return Math.floor(this.currentTime / 60);
   }
 
   getSeconds() {
-    // ... your code goes here
+    return this.currentTime % 60;
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    const stringValue = `${value}`;
+    return stringValue.length < 2 ? `0${stringValue.slice(-2)}` : stringValue;
   }
 
   stop() {
-    // ... your code goes here
+    clearInterval(this.intervalId);
   }
 
   reset() {
-    // ... your code goes here
+    return (this.currentTime = 0);
   }
 
   split() {
-    // ... your code goes here
+    const fixedMin = this.computeTwoDigitNumber(this.getMinutes())
+    const fixedSec = this.computeTwoDigitNumber(this.getSeconds())
+    return `${fixedMin}:${fixedSec}`
   }
 }
 
