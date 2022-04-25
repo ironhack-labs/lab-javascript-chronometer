86a6809f5392ed016b8ad959e3c004718af04885
Arturo Barrantes <arturo.barrantesv@pucp.pe>
2022-04-25 14:24:32 -0500

    solve iteration 1

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,36 +1,107 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
   }
 
   start(callback) {
-    // ... your code goes here
+    this.intervalId = setInterval(() => {
+      this.currentTime += 1;
+      if (callback) {
+        return callback;
+      }
+    }, 1000);
   }
 
   getMinutes() {
-    // ... your code goes here
+    let minutes = Math.floor(this.currentTime / 60);
+    return minutes;
+
+    // let singleNumber = '';
+    // let cero = '0';
+    // let compound = '';
+
+    // if (minutes < 10) {
+    //   singleNumber = value.toString().slice(0);
+    //   compound = cero + singleNumber;
+    // } 
+
+    // else if (value >= 10 && value < 60) {
+
+    //   compound = value.toString();
+    // }
+
+    // return compound;
+
+
+
   }
 
   getSeconds() {
-    // ... your code goes here
+    let seconds = 0;
+    if (this.currentTime === 0) {
+      seconds = 0;
+    } else if (this.currentTime >= 60) {
+      seconds = this.currentTime % 60;
+    } else {
+      seconds = this.currentTime;
+    }
+    return seconds;
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    let singleNumber = '';
+    let cero = '0';
+    let compound = '';
+
+    if (value < 10) {
+      singleNumber = value.toString().slice(0);
+      compound = cero + singleNumber;
+    } 
+
+    else if (value >= 10 && value < 60) {
+
+      compound = value.toString();
+    }
+
+    return compound; // texto
   }
 
   stop() {
-    // ... your code goes here
+    return clearInterval(this.intervalId)
   }
 
   reset() {
-    // ... your code goes here
+    this.currentTime = 0
   }
 
   split() {
-    // ... your code goes here
+
+let ss = this.computeTwoDigitNumber(this.getSeconds());//texto
+let mm =  this.getMinutes();
+let singleNumber = '';
+let cero = '0';
+let compound = '';
+
+if (mm< 10) {
+  singleNumber = mm.toString().slice(0);
+  compound = cero + singleNumber;
+} 
+
+else if (mm>= 10 && mm < 60) {
+
+  compound = mm.toString();
+}
+
+return `${compound}:${ss}`
   }
 }
+
+
+
+
+
+
 
 // The following is required to make unit tests work.
 /* Environment setup. Do not modify the below code. */
