f13247774db8a1b1e17bcf1d30f8220b64481ac2
Ulises-lab <yaftemiranda@gmail.com>
2022-04-24 23:46:36 -0500

    Solo me ha faltado el metodo stop la parte de evaluar que el cronometro ya haya iniciado para poder dar un stop en caso de que lo requiera

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,34 +1,40 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0; 
+    this.intervalId = null;
   }
 
   start(callback) {
-    // ... your code goes here
+    const internalId = setInterval(() => {
+        this.currentTime++;     
+    }, 1000);
+    
   }
 
   getMinutes() {
-    // ... your code goes here
+    let minutes = Math.floor(this.currentTime/60);
+    return minutes;
   }
 
   getSeconds() {
-    // ... your code goes here
+    return (this.currentTime%60);
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    return ('0'+value).slice(-2);
   }
 
   stop() {
-    // ... your code goes here
+    clearInterval(this.intervalId);
   }
 
   reset() {
-    // ... your code goes here
+    this.currentTime = 0;
+    //const content = element.innerHTML; ??? DOM
   }
 
   split() {
-    // ... your code goes here
+    return `${this.computeTwoDigitNumber(this.getMinutes())}:${this.computeTwoDigitNumber(this.getSeconds())}`
   }
 }
 
