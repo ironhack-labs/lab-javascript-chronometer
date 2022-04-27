19ee7b1d3b1977ba30414b8e3bff0630a6af25c3
Eric Aguilar <ar_eric@hotmail.com>
2022-04-27 00:10:36 -0500

    done

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,34 +1,50 @@
 class Chronometer {
   constructor() {
     // ... your code goes here
+    this.currentTime = 0
+    this.intervalId = null
   }
 
-  start(callback) {
+  start() {
     // ... your code goes here
+    this.intervalId = setInterval(this.currentTime+=1,1000)
   }
 
   getMinutes() {
     // ... your code goes here
+    return Math.floor(this.currentTime/60)
   }
 
   getSeconds() {
     // ... your code goes here
+    return this.currentTime%60
   }
 
   computeTwoDigitNumber(value) {
     // ... your code goes here
+    const Number = value.toString()
+    if(Number.length === 1 ){
+      return `0${Number}`
+    } else {
+      return Number
+    }
   }
 
   stop() {
     // ... your code goes here
+    clearInterval(this.intervalId)
   }
 
   reset() {
     // ... your code goes here
+    this.currentTime = 0
   }
 
   split() {
     // ... your code goes here
+    const minutos = this.computeTwoDigitNumber(this.getMinutes())
+    const seconds = this.computeTwoDigitNumber(this.getSeconds())
+    return `${minutos}:${seconds}`
   }
 }
 
