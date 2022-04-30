c835d95a957539d57edf9b2cd46b6bbd0ecd1476
Ulises-lab <yaftemiranda@gmail.com>
2022-04-30 01:28:36 -0500

    el bonus de milisegundo me quedo a medias, no pude hacer que se detenga cuando le doy el stop al cronometro

--- javascript/index.js
+++ javascript/index.js
@@ -13,53 +13,117 @@ const milDecElement = document.getElementById('milDec');
 const milUniElement = document.getElementById('milUni');
 const splitsElement = document.getElementById('splits');
 
+console.log('Que es minDecElement', minDecElement.innerText)
+
 function printTime() {
-  // ... your code goes here
+
+  printMinutes();
+  printSeconds();
+  printMilliseconds();
 }
 
 function printMinutes() {
-  // ... your code goes here
+
+  let minutes = chronometer.getMinutes();
+  minDecElement.innerText = chronometer.computeTwoDigitNumber(minutes)[0];
+  minUniElement.innerText = chronometer.computeTwoDigitNumber(minutes)[1];
 }
 
 function printSeconds() {
-  // ... your code goes here
+
+  let seconds = chronometer.getSeconds();
+  secDecElement.innerText = chronometer.computeTwoDigitNumber(seconds)[0];
+  secUniElement.innerText = chronometer.computeTwoDigitNumber(seconds)[1];
 }
+
+// Parametros creados fuera de cualquier function
+let contador = 0;
+    const miliSeconds = setInterval(() => {
+      contador++;
+      if(contador > 99){
+        contador = 0;
+      }
+    },1);
+
+
+function getMiliSeconds() {
+  valor =(('0'+contador).slice(-2));
+  return valor;
+}
+
 
 // ==> BONUS
 function printMilliseconds() {
-  // ... your code goes here
+  let mil_sec = getMiliSeconds();
+  milDecElement.innerText = `${mil_sec}`[0];
+  milUniElement.innerText = `${mil_sec}`[1];
+
 }
 
+///////////////////////////////////////////////////
+
 function printSplit() {
-  // ... your code goes here
+
+  let mil_sec = chronometer.computeTwoDigitNumber(getMiliSeconds);
+
+  const list_ident = document.createElement('li');
+  list_ident.innerText = `${chronometer.split()}:${getMiliSeconds()}`;
+  const orden_list = document.getElementById('splits');
+  orden_list.appendChild(list_ident);
+  console.log('getMiliSeconds',getMiliSeconds())
 }
 
 function clearSplits() {
-  // ... your code goes here
+  const parent = document.querySelectorAll('li');
+  parent.forEach(element =>{element.remove()});
 }
 
 function setStopBtn() {
-  // ... your code goes here
+
+  btnLeftElement.className = 'btn start';
+  btnLeftElement.innerHTML = 'START';
+  btnRightElement.className = 'btn reset';
+  btnRightElement.innerHTML = 'RESET';
+  chronometer.stop();
+  contador = 0;
 }
 
 function setSplitBtn() {
-  // ... your code goes here
+
+  printSplit();
 }
 
 function setStartBtn() {
-  // ... your code goes here
+
+  btnLeftElement.className = 'btn stop';
+  btnLeftElement.innerHTML = 'STOP';
+  btnRightElement.className = 'btn split';
+  btnRightElement.innerHTML = 'SPLIT';
+  chronometer.start();
 }
 
 function setResetBtn() {
-  // ... your code goes here
+
+  chronometer.reset();
+  clearSplits();
 }
 
 // Start/Stop Button
 btnLeftElement.addEventListener('click', () => {
-  // ... your code goes here
+  if(btnLeftElement.className === 'btn start'){
+    setStartBtn();
+  }else {
+    setStopBtn();
+  }
+  //Va mostrando en el reloj el cambio de tiempo
+  setInterval(()=>(printTime()),10);
 });
 
 // Reset/Split Button
 btnRightElement.addEventListener('click', () => {
   // ... your code goes here
+  if(btnRightElement.innerHTML === 'SPLIT'){
+    setSplitBtn();
+  } else {
+    setResetBtn()}
 });
