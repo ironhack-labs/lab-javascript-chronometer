3e90e11bc7184ae4f021b884ba1181e59f0544f7
Giancarlo Patti <gpatti.11@gmail.com>
2022-05-18 17:22:41 +0200

    solved iteration 1

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,36 +1,70 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime=0
+    this.intervalId=null
   }
 
   start(callback) {
-    // ... your code goes here
+   if (!callback){
+    if (!this.intervalId){
+      this.intervalId = setInterval((callback)=>{
+        this.currentTime++
+      },1000)
+    }
+   }else {
+    if (!this.intervalId){
+      this.intervalId = setInterval(()=>{
+        this.currentTime++
+      },1000)
+    }
+   }
   }
 
   getMinutes() {
-    // ... your code goes here
+    if (this.currentTime>60)
+    return Math.floor(this.currentTime/60);
+    else if (this.currentTime===60)
+      return 1; 
+    else return 0;
   }
 
   getSeconds() {
-    // ... your code goes here
+    if (this.currentTime>=60)
+    return this.currentTime%60;
+    else if (this.currentTime===60)
+    return 0; 
+    return this.currentTime;
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    if (value>9)
+    return value.toFixed().slice()
+      else {
+    let string = ""
+    string = value.toFixed().slice();
+    return `0${string}`}
   }
 
   stop() {
-    // ... your code goes here
+    clearInterval(this.intervalId);
+    this.intervalId = undefined;
   }
 
   reset() {
-    // ... your code goes here
+    this.currentTime=0;
   }
 
   split() {
-    // ... your code goes here
+    return `${this.computeTwoDigitNumber(this.getMinutes())}:${this.computeTwoDigitNumber(this.getSeconds())}`
   }
 }
+
+const crono = new Chronometer()
+crono.start();
+
+setInterval(() =>{
+  console.log(crono.split())
+},1000)
 
 // The following is required to make unit tests work.
 /* Environment setup. Do not modify the below code. */
