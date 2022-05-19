3fbefac2f1a53e853cc034d077c61b1f11328708
Sébastien R <103518570+sebastienrxs@users.noreply.github.com>
2022-05-19 08:11:05 +0200

    Iteration 1

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -20,19 +20,19 @@ class Chronometer {
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    return value.toString().padStart(2, "0")
   }
 
   stop() {
-    // ... your code goes here
+    clearInterval(this.intervalId)
   }
 
   reset() {
-    // ... your code goes here
+    this.currentTime = 0
   }
 
   split() {
-    // ... your code goes here
+    return this.computeTwoDigitNumber(this.getMinutes()) + ":" + this.computeTwoDigitNumber(this.getSeconds())
   }
 }
 
