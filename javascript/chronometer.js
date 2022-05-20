18cf7bf229b21ed27aa3fab197ae083d828ce7d0
LuisEUM <luiseum95@gmail.com>
2022-05-20 19:06:43 +0200

    Iteration 1 - Done

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -34,8 +34,7 @@ class Chronometer {
   }
 
   split() {
-
-    // ... your code goes here
+  return `${this.computeTwoDigitNumber(this.getMinutes())}:${this.computeTwoDigitNumber(this.getSeconds())}`
   }
 }
 
