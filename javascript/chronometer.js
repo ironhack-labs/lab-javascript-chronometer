44a4ba8208200f8704685f275b028b31c3a8b7a0
unknown <rodrigozapanaflores@gmail.com>
2022-05-23 14:11:08 +0200

    solved lab

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,37 +1,53 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0
+    this.intervalId = null
   }
 
   start(callback) {
-    // ... your code goes here
+   
+    if (!this.intervalId) {
+    this.intervalId = setInterval(() => {
+      if (callback) {
+        callback
+      }
+      this.currentTime++
+    } , 1000)
+  }
   }
 
   getMinutes() {
-    // ... your code goes here
+    
+    return Math.floor(this.currentTime / 60)
   }
 
   getSeconds() {
-    // ... your code goes here
+    
+    return this.currentTime % 60
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    
+    return [0, value].join("").slice(-2)
+    /*otra forma -> return String("0" + value).slice(-2)*/
   }
 
   stop() {
-    // ... your code goes here
+   
+    clearInterval(this.intervalId)
+    this.intervalId = null
   }
 
   reset() {
-    // ... your code goes here
+    
+    this.currentTime = 0
   }
 
   split() {
-    // ... your code goes here
+    
+    return `${this.computeTwoDigitNumber(this.getMinutes())}:${this.computeTwoDigitNumber(this.getSeconds())}`
   }
 }
-
 // The following is required to make unit tests work.
 /* Environment setup. Do not modify the below code. */
 if (typeof module !== 'undefined') {
--- javascript/index.js
+++ javascript/index.js
@@ -14,16 +14,27 @@ const milUniElement = document.getElementById('milUni');
 const splitsElement = document.getElementById('splits');
 
 function printTime() {
-  // ... your code goes here
+  printMinutes();
+  printSeconds();
+  printMilliseconds();
 }
 
 function printMinutes() {
-  // ... your code goes here
+  let min = chronometer.computeTwoDigitNumber(chronometer.getMinutes());
+  minUniElement.textContent = min.toString().slice(1) ;
+  minDecElement.textContent = min.toString().slice(0,1);
 }
 
+
 function printSeconds() {
-  // ... your code goes here
-}
+  let sec = chronometer.computeTwoDigitNumber(chronometer.getSeconds());
+  secUniElement.textContent = sec.toString().slice(1);
+  secDecElement.textContent = sec.toString().slice(0,1);
+  console.log(secUniElement.textContent);
+  console.log(secDecElement.textContent);
+ // secUniElement.textContent = 1 ;
+ // secDecElement.textContent = '3';
+ }
 
 // ==> BONUS
 function printMilliseconds() {
@@ -56,10 +67,47 @@ function setResetBtn() {
 
 // Start/Stop Button
 btnLeftElement.addEventListener('click', () => {
-  // ... your code goes here
-});
+  if (chronometer.intervalId === null) {
+    btnLeftElement.innerText = 'STOP'
+    btnRightElement.innerText = 'SPLIT'
+    btnLeftElement.classList.add('stop')
+    btnLeftElement.classList.remove('start')
+    btnRightElement.classList.add('split')
+    btnRightElement.classList.remove('reset')
+    chronometer.start()
+  } else {
+    btnLeftElement.innerText = 'START'
+    btnRightElement.innerText = 'RESET'
+    btnLeftElement.classList.add('start')
+    btnLeftElement.classList.remove('stop')
+    btnRightElement.classList.add('reset')
+    btnRightElement.classList.remove('split')
+    chronometer.stop()
+  }
+})
+;
 
 // Reset/Split Button
 btnRightElement.addEventListener('click', () => {
-  // ... your code goes here
+  const splitContainer = document.querySelector('#splits'); 
+  let splitList = document.createElement('li');
+  splitContainer.appendChild(splitList);
+  splitList.textContent = chronometer.split()
+
+  if (btnRightElement.classList.contains('split')) {
+    splitList;
+  }
+  if (btnRightElement.classList.contains('reset')){
+    chronometer.reset()
+    printTime()
+    splitContainer.innerHTML =""
+  }
+  // document.querySelector("#btnRight").textContent ="SPLIT";
+  // document.classList.replace('btn reset','btn split');
+  // btnRightElement.textContent ='SPLIT';
+  // btnRightElement.replace('btn reset', 'btn split');
+  //  if (btnLeftElement.classList.contains('stop')) {
+  //    btnRightElement
+  //  }
 });
+
