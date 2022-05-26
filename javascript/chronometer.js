c9dabdc016963eb91676c2af9c01d9f5322f8ee1
marialalin <mariavencesantorum@gmail.com>
2022-05-26 12:05:07 +0200

    First iteration solved

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,34 +1,57 @@
+
 class Chronometer {
+
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0
+    this.intervalId = null
   }
 
   start(callback) {
-    // ... your code goes here
+    if(callback){
+      callback()
+    } else {
+    this.intervalId = setInterval(() => {
+      this.currentTime += 1
+    }, 1000)
+   }
   }
 
   getMinutes() {
-    // ... your code goes here
+    return Math.floor(this.currentTime / 60)
+    
   }
 
   getSeconds() {
-    // ... your code goes here
+      return this.currentTime % 60
+      
   }
 
-  computeTwoDigitNumber(value) {
-    // ... your code goes here
+/*   computeTwoDigitNumber(value){
+    return String("0" + value).slice(-2)
+  } */
+
+
+  computeTwoDigitNumber(value) {    
+    const newNumber = String(value)  
+    if (newNumber.length === 1){
+      return `0${newNumber}`
+    } else {
+      return newNumber
+    }
   }
+
 
   stop() {
-    // ... your code goes here
+    clearInterval(this.intervalId)
   }
 
   reset() {
-    // ... your code goes here
+    this.currentTime = 0 //falta We also need to reset 
+    //the values in our HTML file, by using .innerHTML.
   }
 
   split() {
-    // ... your code goes here
+    return `${this.computeTwoDigitNumber(this.getMinutes())}:${this.computeTwoDigitNumber(this.getSeconds())}`
   }
 }
 
