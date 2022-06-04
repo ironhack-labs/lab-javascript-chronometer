db65629b311337eafca9b836411ee3c9e027ea7e
Carmen Garcia <c.garcia.guzman2@gmail.com>
2022-06-04 21:47:42 +0200

    yeahhh

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,34 +1,56 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId= null;
   }
 
   start(callback) {
-    // ... your code goes here
+    this.intervalId= setInterval ( ()=> {
+      if (callback){
+        return null 
+      }
+      this.currentTime++
+
+    },1000);
+     
   }
 
   getMinutes() {
-    // ... your code goes here
+    
+    return Math.floor(this.currentTime /60)
+    
+    
   }
 
   getSeconds() {
-    // ... your code goes here
+    
+    return this.currentTime%60
   }
 
-  computeTwoDigitNumber(value) {
-    // ... your code goes here
-  }
+  computeTwoDigitNumber (value) {
+    let stringNumber = String(value);
+
+    if (stringNumber.length <= 1){
+      return "0" + stringNumber
+    }else if ( value === 0){
+      return "00"
+    }
+    return stringNumber.slice(0,2)
+   }
 
   stop() {
-    // ... your code goes here
+    clearInterval(this.intervalId)
   }
 
   reset() {
-    // ... your code goes here
+    this.currentTime=0;
   }
 
   split() {
-    // ... your code goes here
+    let minutes= this.computeTwoDigitNumber(this.getMinutes());
+    let seconds = this.computeTwoDigitNumber(this.getSeconds());
+
+    return `${minutes}:${seconds}`
   }
 }
 
