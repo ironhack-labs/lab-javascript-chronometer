d2830f45c91e4447cd62cb75ce3bfecf789e46b2
mblason <marina.blasongraviz@gmail.com>
2022-06-04 18:39:34 +0200

    first commit

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,34 +1,42 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
   }
 
-  start(callback) {
-    // ... your code goes here
+  start() {
+    this.intervalId = setInterval(() => this.currentTime++,1000);  
+    
+    if (this.currentTime === 3) {setTimeout(() => this.currentTime,3000)}
   }
 
   getMinutes() {
-    // ... your code goes here
+    return Math.floor(this.currentTime / 60);
   }
 
   getSeconds() {
-    // ... your code goes here
+    return Math.floor(this.currentTime % 60);
   }
 
-  computeTwoDigitNumber(value) {
-    // ... your code goes here
+  computeTwoDigitNumber(num) {
+    
+    if (num <= 10) {return "0" + num}
+      else {return String(num)}
+  
   }
 
   stop() {
-    // ... your code goes here
+    clearInterval(this.intervalId);
   }
 
   reset() {
-    // ... your code goes here
+    this.currentTime = 0;
   }
 
   split() {
-    // ... your code goes here
+    let num1 = this.getMinutes();
+    let num2 = this.getSeconds();
+    return `${this.computeTwoDigitNumber(num1)}:${this.computeTwoDigitNumber(num2)}`
   }
 }
 
