fe67bbc54a4173c84e2eba770fe9c0a489d1cafc
Marina Laporte Boni <marinaolboni@gmail.com>
2022-06-04 14:49:30 +0200

    ended project

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,34 +1,47 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
   }
 
   start(callback) {
-    // ... your code goes here
+    if(callback) {
+      callback();
+    }
+    this.intervalId = setInterval(() => {
+      return this.currentTime += 1;
+    }, 1000);
   }
 
   getMinutes() {
-    // ... your code goes here
+    return Math.floor(this.currentTime/60)
   }
 
   getSeconds() {
-    // ... your code goes here
+    return this.currentTime % 60
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    let str = value.toString()
+    if (str.length !== 2) {
+      return 0 + str
+    }
+    return str
   }
 
   stop() {
-    // ... your code goes here
+    clearInterval(this.intervalId)
   }
 
   reset() {
-    // ... your code goes here
+    this.currentTime = 0
   }
 
   split() {
-    // ... your code goes here
+    let minutes = this.computeTwoDigitNumber(this.getMinutes())
+    let seconds = this.computeTwoDigitNumber(this.getSeconds())
+
+    return `${minutes}:${seconds}`
   }
 }
 
