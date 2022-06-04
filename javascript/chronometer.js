3c422d843b85a26d152a4e02b67f7293211f7186
Nahum Jaime <nahum.jsz@gmail.com>
2022-06-04 15:36:49 +0200

    lab done

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,34 +1,44 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
   }
-
   start(callback) {
-    // ... your code goes here
+    if(callback){
+      this.intervalId = setInterval(callback, 1000)
+    }else{
+      this.intervalId = setInterval(()=> {
+        this.currentTime++
+      }, 1000)
+    }
   }
 
   getMinutes() {
-    // ... your code goes here
+    return Math.floor(this.currentTime / 60)
   }
 
-  getSeconds() {
-    // ... your code goes here
+  getSeconds() { 
+    return Math.floor(this.currentTime % 60)
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    if(value < 10){
+      return "0"+ value.toString();
+    }else{
+      return value.toString()
+    }
   }
 
   stop() {
-    // ... your code goes here
+    clearInterval(this.intervalId);
   }
 
   reset() {
-    // ... your code goes here
+    this.currentTime = 0;
   }
 
   split() {
-    // ... your code goes here
+    return this.computeTwoDigitNumber(this.getMinutes())+":"+this.computeTwoDigitNumber(this.getSeconds())
   }
 }
 
