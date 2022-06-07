871042a6e961b1a4a3a182281c62f8d668e631f8
afisman <afisman@gmail.com>
2022-06-07 21:58:27 +0200

    finished changing colors, unsure of function within start

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,37 +1,60 @@
 class Chronometer {
   constructor() {
+    this.currentTime = 0
+    this.intervalId = null
     // ... your code goes here
   }
 
   start(callback) {
+    const interval = setInterval(() => {
+      if (callback !== undefined) {
+        return callback
+      }
+      this.currentTime++
+
+    }, 1000)
+    this.intervalId += interval
     // ... your code goes here
   }
 
   getMinutes() {
+
+    return Math.floor(this.currentTime / 60);
     // ... your code goes here
   }
 
   getSeconds() {
+    return Math.floor(this.currentTime % 60)
     // ... your code goes here
   }
 
   computeTwoDigitNumber(value) {
+    if (value < 10) {
+      return '0' + value
+    } else {
+      return '' + value
+    }
     // ... your code goes here
   }
 
   stop() {
+    clearInterval(this.intervalId)
+
+
     // ... your code goes here
   }
 
   reset() {
+    this.currentTime = 0
     // ... your code goes here
   }
 
   split() {
+    return '' + this.computeTwoDigitNumber(this.getMinutes()) + ':' + this.computeTwoDigitNumber(this.getSeconds()) + ''
     // ... your code goes here
   }
-}
 
+}
 // The following is required to make unit tests work.
 /* Environment setup. Do not modify the below code. */
 if (typeof module !== 'undefined') {
--- javascript/index.js
+++ javascript/index.js
@@ -22,6 +22,8 @@ function printMinutes() {
 }
 
 function printSeconds() {
+
+
   // ... your code goes here
 }
 
@@ -39,27 +41,69 @@ function clearSplits() {
 }
 
 function setStopBtn() {
+  if (btnLeftElement.classList.contains('start')) {
+    btnLeftElement.style.backgroundColor = '#f14949';
+    btnLeftElement.innerHTML = 'STOP';
+    btnLeftElement.classList.toggle('stop')
+    btnLeftElement.classList.toggle('start')
+
+  }
+
   // ... your code goes here
 }
 
 function setSplitBtn() {
+  if (btnRightElement.classList.contains('reset')) {
+    btnRightElement.style.backgroundColor = '#0851ab';
+    btnRightElement.innerHTML = 'SPLIT';
+    btnRightElement.classList.toggle('split')
+    btnRightElement.classList.toggle('reset')
+
+  }
   // ... your code goes here
 }
 
 function setStartBtn() {
+  if (btnLeftElement.classList.contains('stop')) {
+    btnLeftElement.style.backgroundColor = '#5fca5f';
+    btnLeftElement.innerHTML = 'START';
+    btnLeftElement.classList.toggle('stop')
+    btnLeftElement.classList.toggle('start')
+  }
+
   // ... your code goes here
 }
 
 function setResetBtn() {
+
+  if (btnRightElement.classList.contains('split')) {
+    btnRightElement.style.backgroundColor = '#908e8e';
+    btnRightElement.innerHTML = 'RESET';
+    btnRightElement.classList.toggle('reset')
+    btnRightElement.classList.toggle('split')
+  }
   // ... your code goes here
 }
 
 // Start/Stop Button
 btnLeftElement.addEventListener('click', () => {
+
+  if (btnLeftElement.classList.contains('start')) {
+    chronometer.start('click')
+    setSplitBtn()
+    setStopBtn()
+  } else if (btnLeftElement.classList.contains('stop')) {
+    setStartBtn()
+    setResetBtn()
+  }
+
   // ... your code goes here
 });
 
 // Reset/Split Button
 btnRightElement.addEventListener('click', () => {
-  // ... your code goes here
+  // if (btnRightElement.classList.contains('split')) {
+
+  //}
+
 });
