6324c7c89efcf020b0266994eac40efda9df7dde
rafasanzvi <rafasv41@gmail.com>
2022-06-07 21:14:04 +0200

    done

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,34 +1,51 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null
+
   }
 
+
   start(callback) {
-    // ... your code goes here
+    this.intervalId = setInterval(() => {
+      this.currentTime++
+      if (callback) {
+        callback()
+      }
+    }, 1000)
   }
 
   getMinutes() {
-    // ... your code goes here
+    const minutes = this.currentTime / 60
+    return Math.floor(minutes)
   }
 
+
+
   getSeconds() {
-    // ... your code goes here
+    const seconds = this.currentTime % 60
+    return seconds
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    if (value > 9) {
+      return value.toString()
+    } else {
+      return "0" + value
+    }
   }
 
+
   stop() {
-    // ... your code goes here
+    clearInterval(this.intervalId)
   }
 
   reset() {
-    // ... your code goes here
+    this.currentTime = 0
   }
 
   split() {
-    // ... your code goes here
+    return `${this.computeTwoDigitNumber(this.getMinutes())}:${this.computeTwoDigitNumber(this.getSeconds())}`
   }
 }
 
