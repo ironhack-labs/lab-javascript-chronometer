a6764c10d3218c0f28168bb3a738cdece89a0e9b
idoiafforero <idoia.forero@gmail.com>
2022-06-08 00:32:00 +0200

    solve iteration x, y, z

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,22 +1,32 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0
+    this.intervalId = null
   }
 
   start(callback) {
-    // ... your code goes here
+    if (callback) {
+      this.intervalId = setInterval(callback,1000)
+    } else {
+      this.intervalId = setInterval(()=> {
+        this.currentTime += 1
+      }, 1000)  
+    }
   }
 
   getMinutes() {
-    // ... your code goes here
+
+   return Math.round(Math.floor(this.currentTime)/60)
   }
 
   getSeconds() {
-    // ... your code goes here
+    return Math.round(Math.floor(this.currentTime) % 60)
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+   // no sé cómo decirle que cualquier valor que reciba le añada un 0
+     parseFloat(this.getMinutes).toFixed(2);
+     parseFloat(this.getSeconds).toFixed(2);
   }
 
   stop() {
