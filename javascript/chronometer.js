4473907d91f94d5e61673478798a0ea468b1dc7f
Pabl0Parra <pabsblue16@gmail.com>
2022-09-16 15:25:41 +0200

    finished Iteration 1

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,34 +1,53 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.milliseconds = 0;
+    this.intervalId = 0;
+    this.milliIntervalId = 0;
   }
+  start(callback, milliCallback) {
+    this.intervalId = setInterval(() => {
+      this.currentTime += 1;
+      callback();
+    }, 1000);
 
-  start(callback) {
-    // ... your code goes here
+    this.milliIntervalId = setInterval(() => {
+      this.milliseconds += 1;
+      milliCallback();
+    }, 10);
   }
 
   getMinutes() {
-    // ... your code goes here
+    return Math.floor(this.currentTime / 60);
   }
 
   getSeconds() {
-    // ... your code goes here
+    return this.currentTime % 60;
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    let num = '0' + value;
+    return num.slice(-2);
   }
 
   stop() {
-    // ... your code goes here
+    clearInterval(this.intervalId);
+    clearInterval(this.milliIntervalId);
   }
 
   reset() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.milliseconds = 0;
   }
 
   split() {
-    // ... your code goes here
+    let min = this.getMinutes();
+    let sec = this.getSeconds();
+    let twoDigitMin = this.twoDigitsNumber(minmin);
+    let twoDigitSec = this.twoDigitsNumber(sec);
+    let twoDigitMilli = this.twoDigitsNumber(this.milliseconds);
+
+    return `${twoDigitMin}:${twoDigitSec}:${twoDigitMilli}`;
   }
 }
 
