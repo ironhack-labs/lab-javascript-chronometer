36291478b071be350984897a293332978b4b5a02
Barbara-rodcab <barbara.rodrigues@cabify.com>
2022-11-12 15:47:50 +0100

    complete it 1

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,34 +1,81 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0; 
+    this.intervalId = null
   }
 
   start(callback) {
-    // ... your code goes here
+
+   /*no sigas leyendo el codigo si no hay un callback de la funcion; en caso de que 
+   haya start callback llama el interval ID que luego me representará el currentTime*/
+
+     if (callback){
+      return intervalId
+     }
+   
+    this.intervalId= setInterval (() => {
+      return this.currentTime++ 
+    }, 1000)
   }
 
   getMinutes() {
-    // ... your code goes here
+   let minutes = Math.floor(this.currentTime / 60)
+  return minutes
   }
 
   getSeconds() {
-    // ... your code goes here
+    let seconds = this.currentTime % 60 
+    let secnd = Math.ceil (seconds)
+    return secnd
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+
+    const twoDigits = value < 10 ? "0" + value : value.toString()
+    return twoDigits 
+     
+   /* if (this.getMinutes()< 10 ) {
+      return "0" + this.getMinutes()
+    }
+    
+    if (this.getMinutes ()> 10 ){
+      return this.getMinutes.toString()
+    }
+
+    if (this.getSeconds () < 10 ){
+      return "0" + this.getSeconds()
+    }
+    if (this.getSeconds ()> 10 ){
+      return this.getSeconds.toString()
+    }
+    return computeTwoDigitNumber(value) */
+    
+    /*let strMinutes = num.ToString(getMinutes).slice(-2)
+    let strSecond = num.ToString (getSeconds) .slice (-2)*/
+
+    
+    /*let finalTime = `${twoDigitsMinutes} : ${twoDigitsSeconds}`
+    return finalTime */
+    
   }
 
   stop() {
-    // ... your code goes here
+    return clearInterval (this.intervalId)
+    
   }
 
   reset() {
-    // ... your code goes here
+    return this.currentTime = 0 
   }
 
   split() {
-    // ... your code goes here
+    const currentMinutes = this.getMinutes(this.currentTime);
+    const currentTwoDigitsMinutes = this.computeTwoDigitNumber(currentMinutes);
+
+    const currentSeconds = this.getSeconds(this.currentTime);
+    const currentTwoDigitsSeconds = this.computeTwoDigitNumber(currentSeconds);
+    
+    return `${currentTwoDigitsMinutes}:${currentTwoDigitsSeconds}`
   }
 }
 
