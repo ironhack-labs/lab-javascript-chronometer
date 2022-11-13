ac0d9f812f3cda06be70dba8a955dba35703faef
silvi-hdez <silviahdez.b@gmail.com>
2022-11-13 20:43:15 +0100

    Iteracion 1 completa

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,34 +1,40 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0
+    this.intervalId = null
   }
-
+ 
   start(callback) {
-    // ... your code goes here
+    this.intervalId = setInterval (() =>{
+      this.currentTime += 1
+    }, 1000) 
   }
 
   getMinutes() {
-    // ... your code goes here
+    return Math.floor(this.currentTime / 60)
   }
 
   getSeconds() {
-    // ... your code goes here
+    return this.currentTime % 60
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    let stringValue = value.toString ()
+    if (stringValue.length === 1) {
+      return 0+stringValue
+    } return stringValue
   }
 
   stop() {
-    // ... your code goes here
+    clearInterval(this.intervalId)
   }
 
   reset() {
-    // ... your code goes here
+    return this.currentTime = 0
   }
 
   split() {
-    // ... your code goes here
+    return `${this.computeTwoDigitNumber(this.getMinutes())}:${this.computeTwoDigitNumber(this.getSeconds())}`
   }
 }
 
