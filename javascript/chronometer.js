649cb86527180703c436db9b87f4596e0ff68ddf
Blanca <blancabcbt@gmail.com>
2022-11-18 12:51:29 +0100

    first commit

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,22 +1,30 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
-  }
+    this.currentTime = 0;
+    this.intervalId = null;
+   }
 
   start(callback) {
-    // ... your code goes here
+    this.intervalID = setInterval(() =>{
+      this.currentTime ++
+      if(callback){
+        callback()}
+
+    }, 1000);
+
   }
 
   getMinutes() {
-    // ... your code goes here
+  
+    return Math.floor(this.currentTime/60)
   }
 
   getSeconds() {
-    // ... your code goes here
+    return Math.floor(this.currentTime% 60)
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    
   }
 
   stop() {
