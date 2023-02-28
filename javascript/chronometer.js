bb6773eba7381259f84810a8f668c2d482dd3687
edankap <edankap20@gmail.com>
2023-02-28 19:20:40 +0100

    Iteration 1: Done until stop method

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,26 +1,35 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null
   }
 
   start(callback) {
-    // ... your code goes here
+    if(!this.intervalId){
+      this.intervalId = setInterval(()=>{
+        this.currentTime++
+        if(callback){
+          callback
+        }
+      }, 1000)
+    }
   }
 
   getMinutes() {
-    // ... your code goes here
+    return Math.floor(this.currentTime / 60)
   }
 
   getSeconds() {
-    // ... your code goes here
+    return this.currentTime % 60
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    return value.toString().padStart(2, '0')
   }
 
   stop() {
-    // ... your code goes here
+    clearInterval(this.intervalId)
+    this.intervalId = null
   }
 
   reset() {
