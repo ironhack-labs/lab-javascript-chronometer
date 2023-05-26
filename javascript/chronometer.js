81a3f1bf71831937da2766174d8a9cacbd596dad
Gabriela García <ggarciaborregon@gmail.com>
2023-05-26 15:49:02 +0200

    iteration 2

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -3,52 +3,59 @@ class Chronometer {
   constructor() {
     this.currentTime = 0;
     this.intervalId = null;
+    this.milliseconds = 0;
   }
 
 
   start(callback) {
     this.intervalId = setInterval(() => {
 
-      if (callback) {
+      if (callback) { // si el método start recibe la función callback tenemos que ejecutar la función callback
        callback();
       }
 
-      this.currentTime++
-    }, 1000);
+      this.milliseconds++; // cada 10 milisegundos sumamos un número a milliseconds
+
+      if(this.milliseconds === 99){ //si los miliseconds son igual a 99 (pq no queremos que los milliseconds lleguen a 100);
+      this.currentTime++; // mientras la condición suceda sumamos un número a currentTime
+      this.milliseconds = 0; // reseteamos los milliseconds para que se produzca todo el rato un bucle.
+      }
+    }, 10);
   }
 
 
   getMinutes() {
-    return Math.floor(this.currentTime / 60);
+    return Math.floor(this.currentTime / 60); // dividimos el currentTime entre 60 para que nos de los minutos y con Math.Floor redondeamos el resultado para que nos de un número exacto. 
   }
 
 
   getSeconds() {
-    return this.currentTime % 60;
+    return this.currentTime % 60; // utilizamos el módulo para que de currentTime al dividirlo entre sesenta nos de el resto, esto nos daría el número de segundos que hay en ese momento entre un minuto y otro. 
   }
 
 
   computeTwoDigitNumber(value) {
-    if (value < 10) {
+    if (value < 10) { // si el valor es menor de 10, es decir, sólo tiene un dígito, le estamos indicando que nos devuelva ese dígito con un 0 delante. 
     return `0${value}`;
     } 
-
-    return value.toString();
+    return value.toString(); // si es 10 o mayor, es decir, tiene más de un dígito, le estamos indicando que nos devuelva ese número convertido a string. 
   }
 
 
   stop() {
-    clearInterval(this.intervalId);
+    clearInterval(this.intervalId); // estamos parando el contador haciendo que el intervalo que sumaba segundos deje de producirse.
   }
 
   reset() {
-    this.currentTime = 0;
+    this.currentTime = 0; // hemos reseteado el currentTime.
+    this.milliseconds = 0;
   }
 
   split() {
-    const minutes = this.computeTwoDigitNumber(this.getMinutes());
-    const seconds = this.computeTwoDigitNumber(this.getSeconds());
-    return `${minutes}:${seconds}`;
+    const minutes = this.computeTwoDigitNumber(this.getMinutes()); // asignamos a una constante la función computeTwoDigitNumber y le pasamos como parámetro los minutos para que nos devuelva un string con 2 cifras
+    const seconds = this.computeTwoDigitNumber(this.getSeconds()); // igual que arriba
+    const milliseconds = this.computeTwoDigitNumber(this.milliseconds); // igual que arriba
+    return `${minutes}:${seconds}`; // interpolamos las tres constantes con 2 puntos entre medias. "00:00:00"
   }
 }
 
--- javascript/index.js
+++ javascript/index.js
@@ -1,11 +1,11 @@
-const chronometer = new Chronometer();
+const chronometer = new Chronometer();  // Han creado una nueva instancia de la clase Chronometer.
 
 // get the buttons:
-const btnLeftElement = document.getElementById('btnLeft');
-const btnRightElement = document.getElementById('btnRight');
+const btnLeftElement = document.getElementById('btnLeft');    // Acceden al elemento del botón izq. a través de su id en el HTML.
+const btnRightElement = document.getElementById('btnRight');  // Acceden al elemento del botón dcho. a través de su id en el HTML.
 
 // get the DOM elements that will serve us to display the time:
-const minDecElement = document.getElementById('minDec');
+const minDecElement = document.getElementById('minDec');      // Acceden a todos los elementos de tiempo a través de su id en el HTML. 
 const minUniElement = document.getElementById('minUni');
 const secDecElement = document.getElementById('secDec');
 const secUniElement = document.getElementById('secUni');
@@ -13,16 +13,21 @@ const milDecElement = document.getElementById('milDec');
 const milUniElement = document.getElementById('milUni');
 const splitsElement = document.getElementById('splits');
 
-function printTime() {
-  // ... your code goes here
+function printTime() {  // A esta función le indicamos que al llamarla ejecute las funciones de printMinutes y printSeconds.
+  printMinutes();       
+  printSeconds();
 }
 
 function printMinutes() {
-  // ... your code goes here
+  const minutes = chronometer.computeTwoDigitNumber(chronometer.getMinutes()); // guardo en una variable la función de computeTwoDigitNumber que recibe la función de getMinutes como parámetro. De esta manera ocurre el proceso de ir añadiendo minutos.
+  minDecElement.textContent = minutes[0];  // Indico que el contenido de texto del elemento minDecElement (que equivale a las decenas) sea minutes con index[0], para que aparezca en el primer número.
+  minUniElement.textContent = minutes[1];  // Indico que el contenido de texto del elemento minUniElement (que equivale a las unidades) sea minutes con index[1], para que aparezca en el segundo número.
 }
 
 function printSeconds() {
-  // ... your code goes here
+  const seconds = chronometer.computeTwoDigitNumber(chronometer.getSeconds()); // Aquí hago el mismo proceso que con los minutos pero esta vez para los segundos. 
+  secDecElement.innerText = seconds[0];
+  secUniElement.innerText = seconds[1];
 }
 
 // ==> BONUS
@@ -39,27 +44,45 @@ function clearSplits() {
 }
 
 function setStopBtn() {
-  // ... your code goes here
+    btnLeftElement.classList.toggle("stop");         // Utilizo toggle para quitar la clase .stop del botón (al tener la clase .stop, toggle quita esta clase)
+    btnLeftElement.classList.toggle("start");        // Utilizo toggle para añadir la clase .start al botón (al no tener la clase .start, toggle la añade).
+    btnLeftElement.innerText = "START";              // Indico que el texto del contenido del botón sea START.
+    chronometer.stop()                               // LLamo a la función stop() para que así se pare el contador.
 }
 
 function setSplitBtn() {
-  // ... your code goes here
+  btnRightElement.classList.toggle("split");         // Utilizo toggle para quitar la clase .split del botón.
+  btnRightElement.classList.toggle("reset");         // Utilizo toggle para añadir la clase .reset al botón.
+  btnRightElement.innerText = "RESET"                // Indico que el texto del contenido del botón sea RESET.
 }
 
 function setStartBtn() {
-  // ... your code goes here
+    btnLeftElement.classList.toggle("start");        // Utilizo toggle para quitar la clase .start.
+    btnLeftElement.classList.toggle("stop");         // Utilizo toggle para añadir la clase .stop.
+    btnLeftElement.innerText = "STOP";               // Indico que el texto del elemento del botón sea STOP.
+    chronometer.start(printTime);                    // LLamo a la función start() y le paso como parámetro la función printTime (que ejecuta las funciones de printMinutes y printSeconds) para que el tiempo comience a contar.
 }
 
 function setResetBtn() {
-  // ... your code goes here
+  btnRightElement.classList.toggle("reset");         // Utilizo toggle para quitar la clase .reset.
+    btnRightElement.classList.toggle("split");       // Utilizo toggle para añadir la clase .split.
+    btnRightElement.innerText = "SPLIT";             // Indico que el texto del elemento del botón sea SPLIT.
 }
 
 // Start/Stop Button
 btnLeftElement.addEventListener('click', () => {
-  // ... your code goes here
+  if (btnLeftElement.classList.contains("start")) {  // Indico que si el elemento del botón izq. contiene la clase .start:
+    setStartBtn();                                   // Llamo a la función setStartBtn().
+  } else {                                           // Si no (es decir, si el botón no tiene clase .start y tiene clase .stop pq no hay más opciones):
+    setStopBtn();                                    // Llamo a la función setStopBtn().
+  }
 });
 
 // Reset/Split Button
 btnRightElement.addEventListener('click', () => {
-  // ... your code goes here
+  if (btnRightElement.classList.contains("reset")) { // Indico que si el elemento del botón izq. contiene la clase .reset:
+    setResetBtn();                                   // Llamo a la función setResetBtn().
+  } else {                                           // Si no (es decir, si contiene la clase .split):
+    setSplitBtn();                                   // Llamo a la función setSplitBtn().
+  }
 });
