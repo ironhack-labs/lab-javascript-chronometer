397d977288176fe0124aedc872462ab65715808c
Joan Vila Valls <joan.o.jan@gmail.com>
2024-02-11 22:56:28 +0100

    solved iteration 1,2,3 and 4

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,34 +1,49 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
   }
 
-  start(callback) {
-    // ... your code goes here
+  start(printTimeCallback) {
+    if (printTimeCallback) {
+      this.intervalId = setInterval(() => {
+        this.currentTime++;
+        printTimeCallback(this.getMinutes(), this.getSeconds());
+      }, 1000);
+    } else {
+      this.intervalId = setInterval(() => {
+        this.currentTime++;
+      }, 1000);
+    }
   }
 
   getMinutes() {
-    // ... your code goes here
+    return Math.floor(this.currentTime / 60);
   }
 
   getSeconds() {
-    // ... your code goes here
+    return this.currentTime % 60;
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    return value < 10 ? '0' + value.toString() : value.toString();
   }
 
   stop() {
-    // ... your code goes here
+    clearInterval(this.intervalId);
   }
 
-  reset() {
-    // ... your code goes here
+  reset(printTimeCallback) {
+    this.currentTime = 0;
+    if (printTimeCallback) {
+      printTimeCallback(0, 0);
+    }
   }
 
   split() {
-    // ... your code goes here
+    const minutes = this.computeTwoDigitNumber(this.getMinutes());
+    const seconds = this.computeTwoDigitNumber(this.getSeconds());
+    return minutes + ':' + seconds;
   }
 }
 
--- javascript/index.js
+++ javascript/index.js
@@ -1,5 +1,4 @@
 const chronometer = new Chronometer();
-
 // get the buttons:
 const btnLeftElement = document.getElementById('btnLeft');
 const btnRightElement = document.getElementById('btnRight');
@@ -13,25 +12,32 @@ const milDecElement = document.getElementById('milDec');
 const milUniElement = document.getElementById('milUni');
 const splitsElement = document.getElementById('splits');
 
-function printTime() {
-  // ... your code goes here
+function printTime(minutes, seconds) {
+  printMinutes(minutes);
+  printSeconds(seconds);
 }
 
-function printMinutes() {
-  // ... your code goes here
+function printMinutes(minutes) {
+  minDecElement.innerHTML = chronometer.computeTwoDigitNumber(minutes)[0];
+  minUniElement.innerHTML = chronometer.computeTwoDigitNumber(minutes)[1];
 }
 
-function printSeconds() {
-  // ... your code goes here
+function printSeconds(seconds) {
+  secDecElement.innerHTML = chronometer.computeTwoDigitNumber(seconds)[0];
+  secUniElement.innerHTML = chronometer.computeTwoDigitNumber(seconds)[1];
 }
 
 // ==> BONUS
 function printMilliseconds() {
+
   // ... your code goes here
 }
 
 function printSplit() {
-  // ... your code goes here
+  const splitsOlElement = document.getElementById('splits');
+  const splitLi = document.createElement('li');
+  splitLi.textContent = chronometer.split();
+  splitsOlElement.appendChild(splitLi);
 }
 
 function clearSplits() {
@@ -39,27 +45,67 @@ function clearSplits() {
 }
 
 function setStopBtn() {
-  // ... your code goes here
+  btnLeftElement.className = 'btn stop';
+  btnLeftElement.innerHTML = 'STOP';
 }
 
 function setSplitBtn() {
-  // ... your code goes here
+  btnRightElement.className = 'btn split';
+  btnRightElement.innerHTML = 'SPLIT';
 }
 
 function setStartBtn() {
-  // ... your code goes here
+  btnLeftElement.className = 'btn start';
+  btnLeftElement.innerHTML = 'START';
 }
 
-function setResetBtn() {
-  // ... your code goes here
+function setResetBtn(printTime) {
+  btnRightElement.className = 'btn reset';
+  btnRightElement.innerHTML = 'RESET';
 }
 
 // Start/Stop Button
 btnLeftElement.addEventListener('click', () => {
-  // ... your code goes here
+  if (btnLeftElement.className === 'btn start') {
+    setStopBtn();
+    setSplitBtn();
+    chronometer.start(printTime);
+  } else {
+    setStartBtn();
+    setResetBtn();
+    chronometer.stop();
+  }
 });
 
 // Reset/Split Button
 btnRightElement.addEventListener('click', () => {
-  // ... your code goes here
+  if (btnRightElement.className === 'btn reset'){
+    chronometer.reset(printTime);
+  } else {
+    printSplit();
+  }
 });
+
+//TO-DO - Throttling: prevent unwanted clicks
+/*
+let isClickable = true;
+
+function handleClick() {
+    if (!isClickable) {
+        return;
+    }
+
+    // Your click handler logic goes here
+    console.log('Button clicked');
+
+    // Prevent further clicks for a specified time interval
+    isClickable = false;
+    setTimeout(() => {
+        isClickable = true;
+    }, 300); // Adjust the throttle interval as needed
+}
+
+// Example usage:
+const button = document.getElementById('myButton');
+button.addEventListener('click', handleClick);
+*/
