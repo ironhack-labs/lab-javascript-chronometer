5a5859cd27a571860d8505aa400fb2b90094cc8c
Adrià Chalaux <adriachalaux@gmail.com>
2024-02-11 12:41:40 +0100

    Lab done

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -2,43 +2,77 @@ class Chronometer {
   constructor() {
     this.currentTime = 0
     this.intervalId = null
+
+    this.miliIntervalID = null
+    this.miliCurrentTime = 0
+    this.miliseconds = 0
   }
 
   start(callback) {
     this.intervalId = setInterval(() => {
       this.currentTime++;
+      this.setTime()
+
+      printTime(this.minutes, this.seconds)
+
       if (callback) {
         callback();
       }
     }, 1000);
+    // Miliseconds interval
+    this.miliIntervalID = setInterval(() => {
+      this.miliCurrentTime++
+      this.miliseconds = this.computeTwoDigitNumber(this.setTimeMiliseconds())
+      if (this.milliseconds === 99) this.milliseconds = 0; 
+      printMilliseconds(this.miliseconds)
+    }, 10)
   }
 
+  // Transformar el tiempo en minutos
   getMinutes() {
     return Math.floor(this.currentTime / 60)
   }
 
+  // Transformar el tiempo en segundos
   getSeconds() {
     return this.currentTime % 60;
   }
 
+  // Añadir un cero al delante del tiempo y quedarse con los dos últimos elementos del string
   computeTwoDigitNumber(value) {
     const finalNumber = '0' + value.toString();
     return finalNumber.slice(-2)
   }
 
-  stop() {
-    if (this.intervalId) {
-      clearInterval(this.intervalId);
-      this.intervalId = null;
-    }
+  // Crear los valores finales en string para cada uno de los elementos minutos o segundos
+  setTime() {
+    this.minutes = this.computeTwoDigitNumber(this.getMinutes())
+    this.seconds = this.computeTwoDigitNumber(this.getSeconds())
   }
 
+  // Crear valores finales para los milisegundos
+  setTimeMiliseconds() {
+    return this.miliCurrentTime.toString().slice(-2);
+  }
+
+  // Pausar el paso del tiempo
+  stop() {
+    clearInterval(this.intervalId);
+    clearInterval(this.miliIntervalID);
+  }
+
+  // Resetear el tiempo
   reset() {
     this.currentTime = 0
+    this.miliCurrentTime = 0
+    printTime("00", "00")
+    printMilliseconds("00")
   }
 
+  // Transformar el tiempo en un string compuesto para mostrar el tiempo en un solo array
   split() {
-    return `${this.computeTwoDigitNumber(this.getMinutes)} : ${computeTwoDigitNumber(this.getSeconds)}`
+    let time = `${this.minutes} : ${this.seconds} : ${this.miliseconds}`
+    printSplit(time)
   }
 }
 
--- javascript/index.js
+++ javascript/index.js
@@ -13,27 +13,39 @@ const milDecElement = document.getElementById('milDec');
 const milUniElement = document.getElementById('milUni');
 const splitsElement = document.getElementById('splits');
 
-function printTime() {
-  // ... your code goes here
+// Mostrar el tiempo en el DOM
+function printTime(min, sec) {
+  printMinutes(min)
+  printSeconds(sec)
 }
 
-function printMinutes() {
-  // ... your code goes here
+// Mostrar cada uno de los elementos del string en su elemento html
+function printMinutes(minutes) {
+  minDecElement.innerHTML = minutes[0]
+  minUniElement.innerHTML = minutes[1]
 }
 
-function printSeconds() {
-  // ... your code goes here
+// Mostrar cada uno de los elementos del string en su elemento html
+
+function printSeconds(seconds) {
+  secDecElement.innerHTML = seconds[0]
+  secUniElement.innerHTML = seconds[1]
 }
 
 // ==> BONUS
-function printMilliseconds() {
-  // ... your code goes here
+function printMilliseconds(miliseconds) {
+  milDecElement.innerHTML = miliseconds[0]
+  milUniElement.innerHTML = miliseconds[1]
 }
 
-function printSplit() {
-  // ... your code goes here
+// Crear un nuevo elemento li para la lista de splits. Esta función se ejecuta en el otro JS y time hace referencia al tiempo actual
+function printSplit(time) {
+  let newSplit = document.createElement("li")
+  newSplit.innerHTML = time
+  splitsElement.appendChild(newSplit)
 }
 
+// Limpiar la lista de splits
 function clearSplits() {
   splitsElement.innerHTML = '';
 }
@@ -56,6 +68,10 @@ function setStartBtn() {
 function setResetBtn() {
   btnRightElement.innerHTML = 'RESET';
   btnRightElement.setAttribute('class', 'btn reset');
+}
+
+function updateNumbers() {
+
 }
 
 // Start/Stop Button
